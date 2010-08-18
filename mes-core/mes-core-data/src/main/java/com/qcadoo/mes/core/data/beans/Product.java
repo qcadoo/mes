bbdbc97cd48db3a65f5b388b5e814dc465ae4dda
Maciej Szczytowski <maciej.szczytowski@qcadoo.com>
2010-08-18 15:46:53 +0200

    SC#QCADOO-22 add Product entity

---
+++ mes-core/mes-core-data/src/main/java/com/qcadoo/mes/core/data/beans/Product.java
@@ -0,0 +1,80 @@
+package com.qcadoo.mes.core.data.beans;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    private Long id;
+
+    private String number;
+
+    private String name;
+
+    private String typeOfMaterial;
+
+    private String ean;
+
+    private String category;
+
+    private String unit;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTypeOfMaterial() {
+        return typeOfMaterial;
+    }
+
+    public void setTypeOfMaterial(String typeOfMaterial) {
+        this.typeOfMaterial = typeOfMaterial;
+    }
+
+    public String getEan() {
+        return ean;
+    }
+
+    public void setEan(String ean) {
+        this.ean = ean;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+}
