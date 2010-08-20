3981f98356f29cbd523fd97f5d75bf8d1e74924a
Krzysztof Nadolski <krzysztof.nadolski@qcadoo.com>
2010-08-20 10:41:38 +0200

    SC#QCADOO-20 add test

---
+++ mes-core/mes-core-data/src/test/java/com/qcadoo/mes/core/data/internal/DataAccessServiceDeleteTest.java
@@ -0,0 +1,109 @@
+package com.qcadoo.mes.core.data.internal;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
+import static org.mockito.Mockito.mock;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import com.qcadoo.mes.core.data.api.DataAccessService;
+import com.qcadoo.mes.core.data.api.DataDefinitionService;
+import com.qcadoo.mes.core.data.definition.DataDefinition;
+import com.qcadoo.mes.core.data.definition.FieldDefinition;
+import com.qcadoo.mes.core.data.definition.FieldTypes;
+
+public class DataAccessServiceDeleteTest {
+
+    private DataDefinitionService dataDefinitionService = mock(DataDefinitionService.class);
+
+    private SessionFactory sessionFactory = mock(SessionFactory.class, RETURNS_DEEP_STUBS);
+
+    private DataAccessService dataAccessService = null;
+
+    @Before
+    public void init() {
+        dataAccessService = new DataAccessServiceImpl();
+        ReflectionTestUtils.setField(dataAccessService, "dataDefinitionService", dataDefinitionService);
+        ReflectionTestUtils.setField(dataAccessService, "sessionFactory", sessionFactory);
+    }
+
+    @Test
+    public void shouldProperlyDelete() throws Exception {
+        // given
+        DataDefinition dataDefinition = new DataDefinition("test.Entity");
+        dataDefinition.setFullyQualifiedClassName(SimpleDatabaseObject.class.getCanonicalName());
+
+        List<FieldDefinition> fieldDefinitions = new ArrayList<FieldDefinition>();
+
+        FieldDefinition fieldDefinitionName = new FieldDefinition("name");
+        fieldDefinitionName.setType(FieldTypes.stringType());
+        fieldDefinitions.add(fieldDefinitionName);
+
+        FieldDefinition fieldDefinitionAge = new FieldDefinition("age");
+        fieldDefinitionAge.setType(FieldTypes.integerType());
+        fieldDefinitions.add(fieldDefinitionAge);
+
+        FieldDefinition fieldDefinitionDeleted = new FieldDefinition("deleted");
+        fieldDefinitionDeleted.setType(FieldTypes.booleanType());
+        fieldDefinitions.add(fieldDefinitionDeleted);
+
+        dataDefinition.setFields(fieldDefinitions);
+
+        given(dataDefinitionService.get("test.Entity")).willReturn(dataDefinition);
+
+        SimpleDatabaseObject simpleDatabaseObject = new SimpleDatabaseObject();
+        simpleDatabaseObject.setId(1L);
+        simpleDatabaseObject.setName("Mr T");
+        simpleDatabaseObject.setAge(66);
+        simpleDatabaseObject.setDeleted(false);
+
+        given(sessionFactory.getCurrentSession().get(SimpleDatabaseObject.class, 1L)).willReturn(simpleDatabaseObject);
+
+        // when
+        dataAccessService.delete("test.Entity", 1L);
+
+        // then
+        SimpleDatabaseObject simpleDatabaseObjectDeleted = new SimpleDatabaseObject();
+        simpleDatabaseObjectDeleted.setId(1L);
+        simpleDatabaseObjectDeleted.setName("Mr T");
+        simpleDatabaseObjectDeleted.setAge(66);
+        simpleDatabaseObjectDeleted.setDeleted(true);
+
+        Mockito.verify(sessionFactory.getCurrentSession()).update(simpleDatabaseObjectDeleted);
+    }
+
+    @Test
+    public void shouldFailIfEntityNotFound() throws Exception {
+        // given
+        DataDefinition dataDefinition = new DataDefinition("test.Entity");
+        dataDefinition.setFullyQualifiedClassName(SimpleDatabaseObject.class.getCanonicalName());
+
+        List<FieldDefinition> fieldDefinitions = new ArrayList<FieldDefinition>();
+
+        FieldDefinition fieldDefinitionName = new FieldDefinition("name");
+        fieldDefinitionName.setType(FieldTypes.stringType());
+        fieldDefinitions.add(fieldDefinitionName);
+
+        FieldDefinition fieldDefinitionAge = new FieldDefinition("age");
+        fieldDefinitionAge.setType(FieldTypes.integerType());
+        fieldDefinitions.add(fieldDefinitionAge);
+
+        dataDefinition.setFields(fieldDefinitions);
+
+        given(dataDefinitionService.get("test.Entity")).willReturn(dataDefinition);
+
+        given(sessionFactory.getCurrentSession().get(SimpleDatabaseObject.class, 1L)).willReturn(null);
+
+        // when
+        dataAccessService.delete("test.Entity", 1L);
+
+        // then
+    }
+}
