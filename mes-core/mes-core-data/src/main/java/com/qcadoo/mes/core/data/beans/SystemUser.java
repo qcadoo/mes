827bdddb31d9bf43c4ec9b24f6412537706f0770
Krzysztof Nadolski <krzysztof.nadolski@qcadoo.com>
2010-09-01 16:18:12 +0200

    SC#QCADOO-55 add viewDefinition for user

---
+++ mes-core/mes-core-data/src/main/java/com/qcadoo/mes/core/data/beans/SystemUser.java
@@ -0,0 +1,102 @@
+package com.qcadoo.mes.core.data.beans;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public final class SystemUser {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String login;
+
+    private String userGroup;
+
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String description;
+
+    private String password;
+
+    private boolean deleted;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(final boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(final String description) {
+        this.description = description;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(final String login) {
+        this.login = login;
+    }
+
+    public String getUserGroup() {
+        return userGroup;
+    }
+
+    public void setUserGroup(final String userGroup) {
+        this.userGroup = userGroup;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(final String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(final String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(final String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(final String password) {
+        this.password = password;
+    }
+
+}
