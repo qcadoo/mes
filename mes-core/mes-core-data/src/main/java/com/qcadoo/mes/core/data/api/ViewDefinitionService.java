8d80a80050e179194d2592eb31cb9b78050d0c08
Krzysztof Nadolski <krzysztof.nadolski@qcadoo.com>
2010-09-10 11:59:14 +0200

    SC#QCADOO-134 add authorization in core

--- mes-core/mes-core-data/src/main/java/com/qcadoo/mes/core/data/api/ViewDefinitionService.java
+++ mes-core/mes-core-data/src/main/java/com/qcadoo/mes/core/data/api/ViewDefinitionService.java
@@ -2,10 +2,14 @@ package com.qcadoo.mes.core.data.api;
 
 import java.util.List;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+
 import com.qcadoo.mes.core.data.definition.view.ViewDefinition;
 
 public interface ViewDefinitionService {
 
+    @PreAuthorize("hasRole('ROLE_ADMIN') or ((#viewName != 'users.userGridView') and (#viewName != 'users.userDetailsView')"
+            + " and (#viewName != 'users.groupGridView') and (#viewName != 'users.groupDetailsView'))")
     ViewDefinition getViewDefinition(String viewName);
 
     List<ViewDefinition> getAllViews();
