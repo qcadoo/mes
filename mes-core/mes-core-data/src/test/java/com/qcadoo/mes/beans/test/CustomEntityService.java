e1ca0023927605db0d978881a1a0d86b1aa75e91
Maciej Szczytowski <maciej.szczytowski@qcadoo.com>
2010-09-23 11:57:50 +0200

    SC#QCADOO-132 xml parser for model and view definitions

---
+++ mes-core/mes-core-data/src/test/java/com/qcadoo/mes/beans/test/CustomEntityService.java
@@ -0,0 +1,32 @@
+package com.qcadoo.mes.beans.test;
+
+import com.qcadoo.mes.core.data.beans.Entity;
+import com.qcadoo.mes.core.data.model.DataDefinition;
+
+public class CustomEntityService {
+
+    public void onUpdate(final DataDefinition dataDefinition, final Entity entity) {
+        entity.setField("name", "update");
+    }
+
+    public void onSave(final DataDefinition dataDefinition, final Entity entity) {
+        entity.setField("age", 11);
+    }
+
+    public void onCreate(final DataDefinition dataDefinition, final Entity entity) {
+        entity.setField("name", "create");
+    }
+
+    public void onDelete(final DataDefinition dataDefinition, final Entity entity) {
+        entity.setField("name", "delete");
+    }
+
+    public boolean isEqualToQwerty(final DataDefinition dataDefinition, final Object object) {
+        return String.valueOf(object).equals("qwerty");
+    }
+
+    public boolean hasAge18AndNameMrT(final DataDefinition dataDefinition, final Entity entity) {
+        return (entity.getField("age").equals(18) && entity.getField("name").equals("Mr T"));
+    }
+
+}
---
+++ mes-core/mes-core-data/src/test/java/com/qcadoo/mes/beans/test/ParentDatabaseObject.java
@@ -0,0 +1,61 @@
+package com.qcadoo.mes.beans.test;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+public final class ParentDatabaseObject {
+
+    private Long id;
+
+    private String name;
+
+    private boolean deleted;
+
+    public ParentDatabaseObject() {
+    }
+
+    public ParentDatabaseObject(final Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(final boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+
+}
