3dda707feafd24b7df54bc2818f007cb249937e1
Maciej Szczytowski <maciej.szczytowski@qcadoo.com>
2010-09-30 13:19:57 +0200

    RF start xml parsers from Application class

--- mes-application/src/main/java/com/qcadoo/mes/Application.java
+++ mes-application/src/main/java/com/qcadoo/mes/Application.java
@@ -14,6 +14,7 @@ public class Application {
     @Autowired
     private DataDefinitionParser dataDefinitionParser;
 
+    @Autowired
     private ViewDefinitionParser viewDefinitionParser;
 
     @PostConstruct
