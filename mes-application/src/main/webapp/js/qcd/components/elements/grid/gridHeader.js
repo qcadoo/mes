cd626af6ed410834d9fe73338889736d1f39d33e
Krzysztof Nadolski <krzysztof.nadolski@qcadoo.com>
2010-10-05 12:23:44 +0200

    SC#QCADOO-172 add header and footer

--- mes-application/src/main/webapp/js/qcd/components/elements/grid/gridHeader.js
+++ mes-application/src/main/webapp/js/qcd/components/elements/grid/gridHeader.js
@@ -18,53 +18,10 @@ QCD.components.elements.grid.GridHeader = function(_gridController) {
 	
 	function constructor() {
 		pagingVars.first = 0;
-		pagingVars.max = 10;
-		pagingVars.totalNumberOfEntities = 0;
-		header = new QCD.components.elements.grid.GridHeaderElement(pagingVars)
-		footer = new QCD.components.elements.grid.GridHeaderElement(pagingVars)
-	}
-	
-
-	
-
-	this.getPagingParameters = function() {
-		
-		return {
-			first : 0,
-			max : 10
-		}
-	}
-	
-	this.updatePagingParameters = function(first, max, totalNumberOfEntities) {
-		pagingVars.first = first;
-		pagingVars.max = max;
-		pagingVars.totalNumberOfEntities = totalNumberOfEntities;
-	}
-	
-	this.getHeaderElement = function() {
-		return $("<div>").addClass('grid_header').append(header.getHeaderElement(pagingVars));
-	}
-	
-	this.getFooterElement = function() {
-		return $("<div>").addClass('grid_footer').append(footer.getHeaderElement(pagingVars));
-	}
-
-	constructor();
-}
-
-
-QCD.components.elements.grid.GridHeaderElement = function() {
-	
-	var pagingElements = new Object();
-	pagingElements.prevButton = null;
-	pagingElements.nextButton = null;
-	pagingElements.firstButton = null;
-	pagingElements.lastButton = null;
-	pagingElements.recordsNoSelect = null;
-	pagingElements.pageNoSpan = null;
-	pagingElements.allPagesNoSpan = null;
-	
-	function constructor() {
+		pagingVars.max = 30;
+		pagingVars.totalNumberOfEntities = 1000;
+		header = new QCD.components.elements.grid.GridHeaderElement(this)
+		footer = new QCD.components.elements.grid.GridHeaderElement(this)
 	}
 	
 	function paging_prev() {
@@ -81,6 +38,7 @@ QCD.components.elements.grid.GridHeaderElement = function() {
 	}
 
 	function paging_onRecordsNoSelectChange() {
+		var pagingElements = header.getPagingElements();
 		pagingVars.max = parseInt(pagingElements.recordsNoSelect.val());
 		// gridController.onPagingParametersChange();
 	}
@@ -95,8 +53,49 @@ QCD.components.elements.grid.GridHeaderElement = function() {
 		// gridController.onPagingParametersChange();
 	}
 	
+	this.getPagingParameters = function() {
+		return pagingVars;
+	}
+	
+	this.updatePagingParameters = function(_pagingVars) {
+		pagingVars.first = _pagingVars.first;
+		pagingVars.max = _pagingVars.max;
+		pagingVars.totalNumberOfEntities = _pagingVars.totalNumberOfEntities;
+	}
+	
+	this.getHeaderElement = function() {
+		return $("<div>").addClass('grid_header').append(header.getHeaderElement(pagingVars));
+	}
+	
+	this.getFooterElement = function() {
+		return $("<div>").addClass('grid_footer').append(footer.getHeaderElement(pagingVars));
+	}
+
+	constructor();
+}
+
+
+QCD.components.elements.grid.GridHeaderElement = function(_gridHeader) {
+	
+	var gridHeader = _gridHeader;
+	
+	var pagingElements = new Object();
+	pagingElements.prevButton = null;
+	pagingElements.nextButton = null;
+	pagingElements.firstButton = null;
+	pagingElements.lastButton = null;
+	pagingElements.recordsNoSelect = null;
+	pagingElements.pageNo = null;
+	pagingElements.allPagesNoSpan = null;
+	
+	function constructor() {
+	}
+	
+	this.getPagingElements = function() {
+		return pagingElements;
+	}
+	
 	this.getHeaderElement = function(pagingVars) {
-		
 		var pagingDiv = $("<div>").addClass('grid_paging');
 		pagingDiv.append('<span>Na stronie: </span>');
 		pagingElements.recordsNoSelect = $("<select>");
@@ -105,6 +104,7 @@ QCD.components.elements.grid.GridHeaderElement = function() {
 			pagingElements.recordsNoSelect.append("<option value=30>30</option>");
 			pagingElements.recordsNoSelect.append("<option value=50>50</option>");
 			pagingElements.recordsNoSelect.append("<option value=100>100</option>");
+			pagingElements.recordsNoSelect.val(pagingVars.max);
 		pagingDiv.append(pagingElements.recordsNoSelect);
 		
 		pagingElements.firstButton =  $("<button>").html("first");
@@ -120,11 +120,10 @@ QCD.components.elements.grid.GridHeaderElement = function() {
 			pagesNo = 1;
 		}
 		var currPage = Math.ceil(pagingVars.first / pagingVars.max) + 1;
-		QCD.info(currPage);
+		pagingElements.pageNo = currPage;
 		
 		var pageInfoSpan = $("<span>").addClass('grid_paging_pageInfo');
-			pagingElements.pageNoSpan = $("<span>");
-			pagingValueInput.val(currPage);
+			pagingValueInput.val(pagingElements.pageNo);
 			pageInfoSpan.append(pagingValueInput);
 			pageInfoSpan.append('<span>z</span>');
 			pagingElements.allPagesNoSpan = $("<span>");
@@ -136,22 +135,27 @@ QCD.components.elements.grid.GridHeaderElement = function() {
 		pagingElements.lastButton =  $("<button>").html("last");
 		pagingDiv.append(pagingElements.lastButton);
 		
-		pagingElements.firstButton.click(function() {paging_first();});
-		pagingElements.prevButton.click(function() {paging_prev();});
-		pagingElements.recordsNoSelect.change(function() {paging_onRecordsNoSelectChange();});
-		pagingElements.nextButton.click(function() {paging_next();});
-		pagingElements.lastButton.click(function() {paging_last();});
+		pagingElements.firstButton.click(gridHeader.paging_first);
+		pagingElements.prevButton.click(gridHeader.paging_prev);
+		pagingElements.recordsNoSelect.change(gridHeader.paging_onRecordsNoSelectChange);
+		pagingElements.nextButton.click(gridHeader.paging_next);
+		pagingElements.lastButton.click(gridHeader.paging_last);
 		if (pagingVars.first > 0) {
 			pagingElements.prevButton.attr("disabled", false);
 			pagingElements.firstButton.attr("disabled", false);
+		} else {
+			pagingElements.prevButton.attr("disabled", true);
+			pagingElements.firstButton.attr("disabled", true);
 		}
 		if (pagingVars.first + pagingVars.max < pagingVars.totalNumberOfEntities) {
 			pagingElements.nextButton.attr("disabled", false);
 			pagingElements.lastButton.attr("disabled", false);
+		} else {
+			pagingElements.nextButton.attr("disabled", true);
+			pagingElements.lastButton.attr("disabled", true);
 		}
 		pagingElements.recordsNoSelect.attr("disabled", false);
 
-		
 		return pagingDiv;
 	}
 	
