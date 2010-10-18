fe60edbeabf70ba1d643f13d36c2f863decefffe
Krzysztof Nadolski <krzysztof.nadolski@qcadoo.com>
2010-10-18 12:33:05 +0200

    SC#QCADOO226 error with deleteing children

---
+++ mes-application/src/main/java/com/qcadoo/mes/TranslatedMessageExceptionResolver.java
@@ -0,0 +1,48 @@
+package com.qcadoo.mes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
+
+import com.qcadoo.mes.api.TranslationService;
+
+public final class TranslatedMessageExceptionResolver extends SimpleMappingExceptionResolver {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TranslatedMessageExceptionResolver.class);
+
+    public static final String DEFAULT_EXCEPTION_MESSAGE_ATTRIBUTE = "exceptionMessage";
+
+    private static final String OBJECT_IN_USE = "Trying delete entity in use";
+
+    private String exceptionMessageAttribute = DEFAULT_EXCEPTION_MESSAGE_ATTRIBUTE;
+
+    @Autowired
+    private TranslationService translationService;
+
+    @Override
+    protected ModelAndView doResolveException(final HttpServletRequest request, final HttpServletResponse response,
+            final Object handler, final Exception ex) {
+        ModelAndView mv = super.doResolveException(request, response, handler, ex);
+        if (mv != null) {
+            if (OBJECT_IN_USE.equals(ex.getMessage())) {
+                String message = translationService.translate("commons.exception.illegalStateException.objectInUse",
+                        request.getLocale());
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("Adding exception message to view: " + message);
+                }
+                mv.addObject(this.exceptionMessageAttribute, message);
+            } else {
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("Adding exception message to view: " + ex.getMessage());
+                }
+                mv.addObject(this.exceptionMessageAttribute, ex.getMessage());
+            }
+        }
+        return mv;
+    }
+}
