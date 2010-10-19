e25888f77d41af648bcd1fb32c722a100e3117b9
Krzysztof Nadolski <krzysztof.nadolski@qcadoo.com>
2010-10-19 16:31:49 +0200

    SC#QCADOO58 add pdf print

--- mes-plugins/mes-plugins-products/src/main/java/com/qcadoo/mes/products/PdfOrderView.java
+++ mes-plugins/mes-plugins-products/src/main/java/com/qcadoo/mes/products/PdfOrderView.java
@@ -1,5 +1,6 @@
 package com.qcadoo.mes.products;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -13,7 +14,9 @@ import org.springframework.web.servlet.view.document.AbstractPdfView;
 
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
+import com.lowagie.text.Font;
 import com.lowagie.text.Paragraph;
+import com.lowagie.text.pdf.BaseFont;
 import com.lowagie.text.pdf.PdfWriter;
 import com.qcadoo.mes.api.Entity;
 import com.qcadoo.mes.api.SecurityService;
@@ -49,23 +52,28 @@ public class PdfOrderView extends AbstractPdfView {
         document.addCreator("QCADOO");
     }
 
-    private void addContent(final Document document, final DefaultEntity order, final Locale locale) throws DocumentException {
+    private void addContent(final Document document, final DefaultEntity order, final Locale locale) throws DocumentException,
+            IOException {
+        BaseFont times = BaseFont.createFont(BaseFont.TIMES_ROMAN, BaseFont.CP1250, BaseFont.EMBEDDED);
+        Font t12 = new Font(times, 12);
+        Font tb12 = new Font(times, 12, Font.BOLD);
         SimpleDateFormat df = new SimpleDateFormat(DateType.DATE_FORMAT);
-        document.add(new Paragraph(df.format(new Date())));
+        document.add(new Paragraph(df.format(new Date()), t12));
         UsersUser user = securityService.getCurrentUser();
-        document.add(new Paragraph(translationService.translate("products.order.report.login", locale) + " " + user.getUserName()));
-        document.add(new Paragraph(translationService.translate("products.order.report.order", locale)));
+        document.add(new Paragraph(
+                translationService.translate("products.order.report.login", locale) + " " + user.getUserName(), t12));
+        document.add(new Paragraph(translationService.translate("products.order.report.order", locale), tb12));
         document.add(new Paragraph(translationService.translate("products.order.report.number", locale) + " "
-                + order.getField("number")));
+                + order.getField("number"), t12));
         document.add(new Paragraph(translationService.translate("products.order.report.name", locale) + " "
-                + order.getField("name")));
+                + order.getField("name"), t12));
         Entity product = (Entity) order.getField("product");
         if (product == null) {
-            document.add(new Paragraph(""));
+            document.add(new Paragraph("", t12));
         } else {
-            document.add(new Paragraph((String) product.getField("name")));
+            document.add(new Paragraph((String) product.getField("name"), t12));
         }
         document.add(new Paragraph(translationService.translate("products.order.report.state", locale) + " "
-                + order.getField("state")));
+                + order.getField("state"), t12));
     }
 }
