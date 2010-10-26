536a9bff0eba9f4008b7545c21c7122aed374cc4
Krzysztof Nadolski <krzysztof.nadolski@qcadoo.com>
2010-10-26 14:09:02 +0200

    SC#QCADOO59 add views for pdf

--- mes-plugins/mes-plugins-products/src/main/java/com/qcadoo/mes/products/print/view/pdf/MaterialRequirementPdfView.java
+++ mes-plugins/mes-plugins-products/src/main/java/com/qcadoo/mes/products/print/view/pdf/MaterialRequirementPdfView.java
@@ -5,20 +5,29 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.servlet.view.document.AbstractPdfView;
+
 import com.lowagie.text.Document;
 import com.lowagie.text.pdf.PdfReader;
 import com.lowagie.text.pdf.PdfWriter;
 import com.qcadoo.mes.internal.DefaultEntity;
 
-public final class MaterialRequirementPdfView {
+public final class MaterialRequirementPdfView extends AbstractPdfView {
 
     private static final String PDF_EXTENSION = ".pdf";
 
+    @Override
     protected void buildPdfDocument(final Map<String, Object> model, final Document document, final PdfWriter writer,
             final HttpServletRequest request, final HttpServletResponse response) throws Exception {
         DefaultEntity entity = (DefaultEntity) model.get("entity");
         PdfReader reader = new PdfReader((String) entity.getField("fileName") + PDF_EXTENSION);
-        writer.freeReader(reader);
-    }
+        int n = reader.getNumberOfPages();
+        int i = 0;
+        while (i < n) {
+            i++;
 
+            writer.getImportedPage(reader, i);
+        }
+        // writer.freeReader(reader);
+    }
 }
--- mes-plugins/mes-plugins-products/src/main/java/com/qcadoo/mes/products/print/view/xls/MaterialRequirementXlsView.java
+++ mes-plugins/mes-plugins-products/src/main/java/com/qcadoo/mes/products/print/view/xls/MaterialRequirementXlsView.java
@@ -1,31 +1,30 @@
 package com.qcadoo.mes.products.print.view.xls;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.springframework.web.servlet.view.document.AbstractExcelView;
 
-import com.qcadoo.mes.api.TranslationService;
 import com.qcadoo.mes.internal.DefaultEntity;
 
 public final class MaterialRequirementXlsView extends AbstractExcelView {
 
-    @Autowired
-    private TranslationService translationService;
+    private static final String XLS_EXTENSION = ".xls";
 
     @Override
-    protected void buildExcelDocument(final Map<String, Object> model, final HSSFWorkbook workbook,
-            final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+    protected void buildExcelDocument(final Map<String, Object> model, HSSFWorkbook workbook, final HttpServletRequest request,
+            final HttpServletResponse response) throws Exception {
         DefaultEntity entity = (DefaultEntity) model.get("entity");
 
-        HSSFSheet sheet = workbook.createSheet(translationService.translate("products.materialRequirement.report.title",
-                request.getLocale()));
+        POIFSFileSystem fs = new POIFSFileSystem(new FileInputStream(new File((String) entity.getField("fileName")
+                + XLS_EXTENSION)));
+        workbook = new HSSFWorkbook(fs);
 
     }
-
 }
