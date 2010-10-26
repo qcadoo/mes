38d295f3e023e67332d72a0baa7e2c2c2e4971d6
Maciej Szczytowski <maciej.szczytowski@qcadoo.com>
2010-10-26 15:26:16 +0200

    SC#QCADOO-287 fix ignoreMode in abstract component

--- mes-plugins/mes-plugins-crud/src/main/java/com/qcadoo/mes/crud/CrudController.java
+++ mes-plugins/mes-plugins-crud/src/main/java/com/qcadoo/mes/crud/CrudController.java
@@ -94,16 +94,7 @@ public final class CrudController {
         return modelAndView;
     }
 
-    @RequestMapping(value = CONTROLLER_PATH + "/dataUpdate", method = RequestMethod.POST)
-    @ResponseBody
-    public Object getDataUpdate(@PathVariable(PLUGIN_IDENTIFIER_VARIABLE) final String pluginIdentifier,
-            @PathVariable(VIEW_NAME_VARIABLE) final String viewName, @ModelAttribute(JSON_BODY) final StringBuilder body,
-            final Locale locale) {
-        // TODO masz remove me
-        return getData(pluginIdentifier, viewName, body, locale);
-    }
-
-    @RequestMapping(value = CONTROLLER_PATH + "/data", method = RequestMethod.POST)
+    @RequestMapping(value = { CONTROLLER_PATH + "/data", CONTROLLER_PATH + "/dataUpdate" }, method = RequestMethod.POST)
     @ResponseBody
     public Object getData(@PathVariable(PLUGIN_IDENTIFIER_VARIABLE) final String pluginIdentifier,
             @PathVariable(VIEW_NAME_VARIABLE) final String viewName, @ModelAttribute(JSON_BODY) final StringBuilder body,
