ae4bf6a2839c8a4f3a22b8c27736222f6cd9c713
Michal Nawalany <michal.nawalany@qcadoo.com>
2010-10-28 15:33:28 +0200

    SC#QCADOO-306 add 'textRepresentationOnDisabled' option to input

--- mes-application/src/main/webapp/js/qcd/components/elements/textInput.js
+++ mes-application/src/main/webapp/js/qcd/components/elements/textInput.js
@@ -4,4 +4,33 @@ QCD.components.elements = QCD.components.elements || {};
 
 QCD.components.elements.TextInput = function(_element, _mainController) {
 	$.extend(this, new QCD.components.elements.FormComponent(_element, _mainController));
+	
+	var textRepresentation = $("#" + _element.attr('id') + "_text");
+	
+	var input = this.input;
+	
+	this.getComponentData = function() {
+		return {
+			value : input.val()
+		}
+	}
+	
+	this.setComponentData = function(data) {
+		if (data.value) {
+			input.val(data.value);
+			textRepresentation.html(data.value);
+		}
+	}
+	
+	this.setFormComponentEnabled = function(isEnabled) {
+		if(this.options.textRepresentationOnDisabled) {
+			if(isEnabled) {
+				input.show();
+				textRepresentation.hide();
+			} else {
+				input.hide();
+				textRepresentation.show();
+			}
+		}
+	}
 }
