ccb8d32b6787bc0887680e5429880297509c3d4b
MADY <mateusz.dymczyk@qcadoo.com>
2010-11-19 09:40:36 +0100

    SC#429 first try to resolve the IE8 cursor problem

--- mes-plugins/mes-plugins-crud/src/main/resources/META-INF/js/crud/qcd/components/elements/lookup.js
+++ mes-plugins/mes-plugins-crud/src/main/resources/META-INF/js/crud/qcd/components/elements/lookup.js
@@ -130,6 +130,25 @@ QCD.components.elements.Lookup = function(_element, _mainController) {
 		}		
 	}
 	
+	function setSelectionRange(input, selectionStart, selectionEnd) {
+		  if (input.setSelectionRange) {
+		    input.focus();
+		    input.setSelectionRange(selectionStart, selectionEnd);
+		  }
+		  else if (input.createTextRange) {
+		    var range = input.createTextRange();
+		    range.collapse(true);
+		    range.moveEnd('character', selectionEnd);
+		    range.moveStart('character', selectionStart);
+		    range.select();
+		  }
+		}
+
+	function setCaretToPos(input, pos) {
+		setSelectionRange(input, pos, pos);
+	}
+
+	
 	function onInputFocus() {
 		isFocused = true;
 		valueDivElement.hide();
