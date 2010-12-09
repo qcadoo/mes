9da4bc913bab31b22a6b38566fa114080755ce51
Maciej Szczytowski <maciej.szczytowski@qcadoo.com>
2010-12-09 16:50:51 +0100

    SC#QCADOO-676 change parser

---
+++ mes-core/src/main/java/com/qcadoo/mes/view/xml/ViewDefinitionParser.java
@@ -0,0 +1,20 @@
+package com.qcadoo.mes.view.xml;
+
+import org.w3c.dom.Node;
+
+import com.qcadoo.mes.view.ComponentOption;
+import com.qcadoo.mes.view.ComponentPattern;
+import com.qcadoo.mes.view.ContainerPattern;
+import com.qcadoo.mes.view.internal.ComponentCustomEvent;
+
+public interface ViewDefinitionParser {
+
+    ComponentOption parseOption(Node option);
+
+    ComponentPattern parseComponent(Node componentNode, ContainerPattern parent);
+
+    ComponentCustomEvent parseCustomEvent(Node listenerNode);
+
+    String getStringAttribute(Node groupNode, String string);
+
+}
