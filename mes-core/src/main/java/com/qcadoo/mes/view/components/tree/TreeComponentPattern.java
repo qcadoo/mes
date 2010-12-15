a2948bb6033417303ee9cb0df99b035f10ffdc92
Michal Nawalany <michal.nawalany@qcadoo.com>
2010-12-15 13:22:13 +0100

    SC#QCADOO-544 change tree component state

--- mes-core/src/main/java/com/qcadoo/mes/view/components/tree/TreeComponentPattern.java
+++ mes-core/src/main/java/com/qcadoo/mes/view/components/tree/TreeComponentPattern.java
@@ -1,10 +1,14 @@
 package com.qcadoo.mes.view.components.tree;
 
+import java.util.Locale;
+
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import com.qcadoo.mes.model.FieldDefinition;
 import com.qcadoo.mes.model.types.HasManyType;
 import com.qcadoo.mes.view.ComponentDefinition;
+import com.qcadoo.mes.view.ComponentOption;
 import com.qcadoo.mes.view.ComponentState;
 import com.qcadoo.mes.view.ViewComponent;
 import com.qcadoo.mes.view.patterns.AbstractComponentPattern;
@@ -18,18 +22,44 @@ public final class TreeComponentPattern extends AbstractComponentPattern {
 
     private FieldDefinition belongsToFieldDefinition;
 
+    private String correspondingView;
+
+    private String correspondingComponent;
+
+    private String nodeLabelExpression;
+
     public TreeComponentPattern(final ComponentDefinition componentDefinition) {
         super(componentDefinition);
     }
 
     @Override
     public ComponentState getComponentStateInstance() {
-        return new TreeComponentState(belongsToFieldDefinition);
+        return new TreeComponentState(belongsToFieldDefinition, nodeLabelExpression);
     }
 
     @Override
     protected void initializeComponent() throws JSONException {
         getBelongsToFieldDefinition();
+
+        for (ComponentOption option : getOptions()) {
+            if ("correspondingView".equals(option.getType())) {
+                correspondingView = option.getValue();
+            } else if ("correspondingComponent".equals(option.getType())) {
+                correspondingComponent = option.getValue();
+            } else if ("nodeLabelExpression".equals(option.getType())) {
+                nodeLabelExpression = option.getValue();
+            } else {
+                throw new IllegalStateException("Unknown option for tree: " + option.getType());
+            }
+        }
+    }
+
+    @Override
+    protected JSONObject getJsOptions(final Locale locale) throws JSONException {
+        JSONObject json = new JSONObject();
+        json.put("correspondingView", correspondingView);
+        json.put("correspondingComponent", correspondingComponent);
+        return json;
     }
 
     private void getBelongsToFieldDefinition() {
