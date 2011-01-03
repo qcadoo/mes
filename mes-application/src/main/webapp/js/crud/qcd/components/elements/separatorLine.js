f2fa31e23da9c55d522586c6769a3f9eff5c31dc
Michal Nawalany <michal.nawalany@qcadoo.com>
2011-01-03 14:57:43 +0100

    RF minor fixes

--- mes-application/src/main/webapp/js/crud/qcd/components/elements/separatorLine.js
+++
@@ -1,8 +0,0 @@
-var QCD = QCD || {};
-QCD.components = QCD.components || {};
-QCD.components.elements = QCD.components.elements || {};
-
-QCD.components.containers.layout.SeperatorLine = function(_element, _mainController) {
-	$.extend(this, new QCD.components.containers.layout.Layout(_element, _mainController));
-
-}
