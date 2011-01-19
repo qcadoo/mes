c28e17887b63cb77e6b3afc46e1fb648536f1e8d
Michal Nawalany <michal.nawalany@qcadoo.com>
2011-01-19 16:03:48 +0100

    SC#QCADOO-746 improve ADL

--- mes-plugins/mes-plugins-crud/src/main/resources/META-INF/js/crud/qcd/components/elements/awesomeDynamicList.js
+++ mes-plugins/mes-plugins-crud/src/main/resources/META-INF/js/crud/qcd/components/elements/awesomeDynamicList.js
@@ -23,11 +23,16 @@ QCD.components.elements.AwesomeDynamicList = function(_element, _mainController)
 	
 	var BUTTONS_WIDTH = 50;
 	
+	var hasButtons = this.options.hasButtons;
+	
 	function constructor(_this) {
 		innerFormContainer = $("#"+_this.elementSearchName+" > .awesomeDynamicListInnerForm").children();
 		awesomeDynamicListContent = $("#"+_this.elementSearchName+" > .awesomeDynamicListContent");
 		formObjects = new Array();
 		formObjectsMap = new Object();
+		if (!hasButtons) {
+			BUTTONS_WIDTH = 0;
+		}
 		updateButtons();
 	}
 	
@@ -83,6 +88,8 @@ QCD.components.elements.AwesomeDynamicList = function(_element, _mainController)
 	this.updateSize = function(_width, _height) {
 		currentWidth = _width;
 		currentHeight = _height;
+		//this.element.width(currentWidth);
+		QCD.info("updateSize - "+_width);
 		for (var i in formObjects) {
 			formObjects[i].updateSize(_width-BUTTONS_WIDTH, _height);
 		}
@@ -95,7 +102,8 @@ QCD.components.elements.AwesomeDynamicList = function(_element, _mainController)
 		var formContainer = $("<div>").addClass("awesomeListFormContainer");
 		formContainer.append(copy);
 		line.append(formContainer);
-		var buttons = $("<div>").addClass("awesomeListButtons");
+		if (hasButtons) {
+			var buttons = $("<div>").addClass("awesomeListButtons");
 		
 			var removeLineButton = $("<a>").addClass("awesomeListButton").addClass("awesomeListMinusButton").addClass("enabled").attr("id", elementPath+"_line_"+formId+"_removeButton");
 			removeLineButton.css("display", "none");
@@ -118,7 +126,8 @@ QCD.components.elements.AwesomeDynamicList = function(_element, _mainController)
 			addLineButton.css("display", "none");
 			buttons.append(addLineButton);
 			
-		line.append(buttons);
+			line.append(buttons);
+		}
 		awesomeDynamicListContent.append(line);
 		var formObject = QCDPageConstructor.getChildrenComponents(copy, mainController)["innerForm_"+formId];
 		formObject.updateSize(currentWidth-BUTTONS_WIDTH, currentHeight);
@@ -141,6 +150,9 @@ QCD.components.elements.AwesomeDynamicList = function(_element, _mainController)
 	}
 	
 	function updateButtons() {
+		if (!hasButtons) {
+			return;
+		}
 		var objectCounter = 0;
 		var lastObject = 0;
 		for (var i in formObjects) {
