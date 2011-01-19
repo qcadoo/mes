beb68c50ba098feedc215848059b2e4e1522347e
Mateusz Dymczyk <mateusz.dymczyk@qcadoo.com>
2011-01-19 10:53:24 +0100

    SC#763 added a new plugin - genealogy

---
+++ mes-plugins/mes-plugins-genealogies/src/main/java/com/qcadoo/mes/genealogies/GenealogyController.java
@@ -0,0 +1,34 @@
+package com.qcadoo.mes.genealogies;
+
+import java.util.Locale;
+import java.util.Map;
+
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.google.common.collect.ImmutableMap;
+import com.qcadoo.mes.crud.CrudController;
+
+@Controller
+public class GenealogyController {
+
+    @Autowired
+    private CrudController crudController;
+
+    @Autowired
+    private GenealogyAttributeService genealogyService;
+
+    @RequestMapping(value = "genealogyAttribute", method = RequestMethod.GET)
+    public ModelAndView getGenealogyAttributesPageView(final Locale locale) {
+
+        JSONObject json = new JSONObject(ImmutableMap.of("window.currentAttribute.id", genealogyService.getGenealogyAttributeId()
+                .toString()));
+        Map<String, String> arguments = ImmutableMap.of("context", json.toString());
+        return crudController.prepareView("genealogies", "currentAttribute", arguments, locale);
+    }
+
+}
