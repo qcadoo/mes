d05cf778ca4b08addeb65dcaf26e46d1dc6d75f7
Maciej Szczytowski <maciej.szczytowski@qcadoo.com>
2011-01-20 12:56:07 +0100

    SC#QCADOO-731

--- mes-plugins/mes-plugins-genealogies/src/main/java/com/qcadoo/mes/genealogies/GenealogyService.java
+++ mes-plugins/mes-plugins-genealogies/src/main/java/com/qcadoo/mes/genealogies/GenealogyService.java
@@ -1,12 +1,20 @@
 package com.qcadoo.mes.genealogies;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.qcadoo.mes.api.DataDefinitionService;
+import com.qcadoo.mes.api.Entity;
 import com.qcadoo.mes.api.TranslationService;
+import com.qcadoo.mes.internal.DefaultEntity;
 import com.qcadoo.mes.view.ComponentState;
 import com.qcadoo.mes.view.ViewDefinitionState;
+import com.qcadoo.mes.view.components.awesomeDynamicList.AwesomeDynamicListState;
 import com.qcadoo.mes.view.components.form.FormComponentState;
 import com.qcadoo.mes.view.components.grid.GridComponentState;
 
@@ -32,6 +40,51 @@ public final class GenealogyService {
                     + orderId + "\"}";
             viewDefinitionState.redirectTo(url, false);
         }
+    }
+
+    @SuppressWarnings({ "unchecked" })
+    public void fillProductInComponents(final ViewDefinitionState state, final Locale locale) {
+        FormComponentState orderForm = (FormComponentState) state.getComponentByReference("order");
+        FormComponentState genealogyForm = (FormComponentState) state.getComponentByReference("form");
+        AwesomeDynamicListState list = (AwesomeDynamicListState) state.getComponentByReference("productInComponentsList");
+
+        Entity genealogy = null;
+        List<Entity> existingProductInComponents = Collections.EMPTY_LIST;
+
+        if (genealogyForm.getEntityId() != null) {
+            genealogy = dataDefinitionService.get("genealogies", "genealogy").get(orderForm.getEntityId());
+            existingProductInComponents = genealogy.getHasManyField("productInComponents");
+        }
+
+        Entity order = dataDefinitionService.get("products", "order").get(orderForm.getEntityId());
+        Entity technology = order.getBelongsToField("technology");
+
+        List<Entity> targetProductInComponents = Collections.emptyList();
+
+        for (Entity operationComponent : technology.getHasManyField("operationComponents")) {
+            for (Entity operationProductInComponent : operationComponent.getHasManyField("operationProductInComponents")) {
+                if ((Boolean) operationProductInComponent.getField("batchRequired")) {
+                    targetProductInComponents.add(createGenealogyProductInComponent(genealogy, operationProductInComponent,
+                            existingProductInComponents));
+                }
+            }
+        }
+
+        list.setFieldValue(targetProductInComponents);
+    }
+
+    private Entity createGenealogyProductInComponent(final Entity genealogy, final Entity operationProductInComponent,
+            final List<Entity> existingProductInComponents) {
+        for (Entity e : existingProductInComponents) {
+            if (e.getBelongsToField("productInComponent").getId().equals(operationProductInComponent.getId())) {
+                return operationProductInComponent;
+            }
+        }
+        Entity genealogyProductInComponent = new DefaultEntity("genealogies", "genealogyProductInComponent");
+        genealogyProductInComponent.setField("genealogy", genealogy);
+        genealogyProductInComponent.setField("productInComponent", operationProductInComponent);
+        genealogyProductInComponent.setField("batch", new ArrayList<Entity>());
+        return genealogyProductInComponent;
     }
 
     // public void addProductInToForm(final ViewDefinition viewDefinition, final JSONObject jsonObject, final Locale locale) {
