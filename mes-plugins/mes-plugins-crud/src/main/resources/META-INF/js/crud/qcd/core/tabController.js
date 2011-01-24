82c60949abd4c798ba9bda6154daf7349841bbad
Michal Nawalany <michal.nawalany@qcadoo.com>
2011-01-24 16:44:02 +0100

    SC#QCADOO-746 tab index

--- mes-plugins/mes-plugins-crud/src/main/resources/META-INF/js/crud/qcd/core/tabController.js
+++ mes-plugins/mes-plugins-crud/src/main/resources/META-INF/js/crud/qcd/core/tabController.js
@@ -3,37 +3,35 @@ var QCD = QCD || {};
 QCD.TabController = function() {
 	
 	this.updateTabObjects = function() {
-//		tabObjects = new Array();
-//		var focusables = $(":focusable");
-//		$.each(focusables, function(key, component) {
-//			component = $(component);
-//			var tabindex = component.attr("tabindex");
-//			if (tabindex) {
-//				tabObjects[tabindex] = component;
-//				component.addClass("tabbable");
-//			}
-//		});
-//		QCD.info(tabObjects);
 		
-//		$('input').live('keydown',function(event) {
-//            var keycode = event.keyCode;
-//            if (keycode == 9) {
-////                var $div = $(this).parent();
-////                var nextID = parseInt($div.attr('id')) + 1;
-////                $div.html($(this).val());
-////                $('#' + nextID).click();
-//            	QCD.info("TAB");
-//                event.preventDefault();
-//            }   
-//        });
+		$('input, select, a').live('keydown',function(event) {
+            if (event.keyCode == 9) { // TAB
+            	var element = $(this);
+            	if (element.hasClass("customTabIndex")) {
+            		
+//            		var nextElement = null;
+//            		while (nextElement == null) {
+//            			var children = element.find("*[tabindex]");
+//            			if (children.length > 0) {
+//            				nextElement = 0;
+//            			}
+//            				
+//            		}
+//            		
+//            		var children = element.find("*[tabindex]");
+//            		if (children.length == 0)
+//            		
+//            		QCD.info(children);
+//            		QCD.info(this.nextTabElement);
+//            		
+//            		if (this.nextTabElement) {
+//            			this.nextTabElement.focus();
+//            			event.preventDefault();	
+//            		}
+            	}
+            }   
+        });
 
-	}
-	
-	this.getTabIndexInfo = function(component) {
-//		QCD.info("getTabIndexInfo");
-//		QCD.info(component)
-//		var focusables = component.find(".tabbable");
-//		QCD.info(focusables);
 	}
 	
 };
