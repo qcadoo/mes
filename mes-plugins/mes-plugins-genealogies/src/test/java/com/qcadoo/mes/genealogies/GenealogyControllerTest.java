c48b2fa48426d3522158c0d5127f79bf0b255028
Maciej Szczytowski <maciej.szczytowski@qcadoo.com>
2011-02-11 16:40:54 +0100

    RF# tests

--- mes-plugins/mes-plugins-genealogies/src/test/java/com/qcadoo/mes/genealogies/GenealogyControllerTest.java
+++ mes-plugins/mes-plugins-genealogies/src/test/java/com/qcadoo/mes/genealogies/GenealogyControllerTest.java
@@ -1,24 +1,66 @@
 package com.qcadoo.mes.genealogies;
 
-import junit.framework.Assert;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.springframework.test.util.ReflectionTestUtils.setField;
+
+import java.util.Locale;
+import java.util.Map;
 
 import org.junit.Test;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.google.common.collect.ImmutableMap;
+import com.qcadoo.mes.crud.CrudController;
 
 public class GenealogyControllerTest {
 
     @Test
     public void shouldPrepareViewForGenealogyAttributes() throws Exception {
         // // given
-        // CrudController crudController = mock(CrudController.class);
-        // GenealogyAttributeService genealogyAttributeService = mock(GenealogyAttributeService.class);
-        // GenealogyController genealogyController = new GenealogyController();
-        // ReflectionTestUtils.setField(genealogyController, "crudController", crudController);
-        // ReflectionTestUtils.setField(genealogyController, "genealogyAttributeService", genealogyAttributeService);
-        //
+        Map<String, String> arguments = ImmutableMap.of("context", "{\"window.currentAttribute.id\":\"13\"}");
+        ModelAndView expectedMav = mock(ModelAndView.class);
+        CrudController crudController = mock(CrudController.class);
+        given(crudController.prepareView("genealogies", "currentAttribute", arguments, Locale.ENGLISH)).willReturn(expectedMav);
+
+        GenealogyAttributeService genealogyAttributeService = mock(GenealogyAttributeService.class);
+        given(genealogyAttributeService.getGenealogyAttributeId()).willReturn(13L);
+
+        GenealogyController genealogyController = new GenealogyController();
+        setField(genealogyController, "crudController", crudController);
+        setField(genealogyController, "genealogyService", genealogyAttributeService);
+
         // // when
-        // genealogyController.getGenealogyAttributesPageView(Locale.ENGLISH);
-        //
+        ModelAndView mav = genealogyController.getGenealogyAttributesPageView(Locale.ENGLISH);
+
         // // then
-        Assert.assertTrue(true);
+        assertEquals(expectedMav, mav);
+    }
+
+    @Test
+    public void shouldPrepareViewForComponentPdf() throws Exception {
+        // given
+        GenealogyController genealogyController = new GenealogyController();
+
+        // when
+        ModelAndView mav = genealogyController.genealogyForComponentPdf("v13");
+
+        // then
+        assertEquals("genealogyForComponentView", mav.getViewName());
+        assertEquals("v13", mav.getModel().get("value"));
+    }
+
+    @Test
+    public void shouldPrepareViewForProductPdf() throws Exception {
+        // given
+        GenealogyController genealogyController = new GenealogyController();
+
+        // when
+        ModelAndView mav = genealogyController.genealogyForProductPdf("v13");
+
+        // then
+        assertEquals("genealogyForProductView", mav.getViewName());
+        assertEquals("v13", mav.getModel().get("value"));
     }
 }
