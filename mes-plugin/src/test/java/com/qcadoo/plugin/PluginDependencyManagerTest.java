ccc6f7d1285f0f88e016379da319bd1f9598959a
Mateusz Dymczyk <mateusz.dymczyk@qcadoo.com>
2011-03-08 16:55:41 +0100

    new tests and functionalities for DefaultPluginDependencyManager, added cyclic graph handling

--- mes-plugin/src/test/java/com/qcadoo/plugin/PluginDependencyManagerTest.java
+++ mes-plugin/src/test/java/com/qcadoo/plugin/PluginDependencyManagerTest.java
@@ -3,6 +3,7 @@ package com.qcadoo.plugin;
 import static java.util.Collections.singletonList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
@@ -53,6 +54,11 @@ public class PluginDependencyManagerTest {
     private PluginInformation dependencyPluginInformation4_2 = new PluginInformation("defaultPlugin4", "",
             "defaultPlugin4_vendor", "", "defaultPlugin4_version", "");
 
+    private Plugin dependencyPlugin5 = mock(Plugin.class);
+
+    private PluginInformation dependencyPluginInformation5 = new PluginInformation("defaultPlugin5", "", "defaultPlugin5_vendor",
+            "", "defaultPlugin5_version", "");
+
     private PluginAccessor pluginAccessor = mock(PluginAccessor.class);
 
     @Before
@@ -66,11 +72,14 @@ public class PluginDependencyManagerTest {
         given(pluginAccessor.getPlugin("defaultPlugin3:defaultPlugin3_vendor:defaultPlugin3_version")).willReturn(
                 dependencyPlugin3);
         given(pluginAccessor.getPlugin("defaultPlugin4:defaultPlugin4_vendor:defaultPlugin4_version")).willReturn(null);
+        given(pluginAccessor.getPlugin("defaultPlugin5:defaultPlugin5_vendor:defaultPlugin5_version")).willReturn(
+                dependencyPlugin5);
 
         createDependencyPlugin(dependencyPlugin1, dependencyPluginInformation1, "defaultPlugin");
         createDependencyPlugin(dependencyPlugin2, dependencyPluginInformation2, "defaultPlugin2");
         createDependencyPlugin(dependencyPlugin3, dependencyPluginInformation3, "defaultPlugin3");
         createDependencyPlugin(null, dependencyPluginInformation4, "defaultPlugin4");
+        createDependencyPlugin(dependencyPlugin5, dependencyPluginInformation5, "defaultPlugin5");
         createDependencyPlugin(null, dependencyPluginInformation1_2, "defaultPlugin");
         createDependencyPlugin(null, dependencyPluginInformation2_2, "defaultPlugin2");
         createDependencyPlugin(null, dependencyPluginInformation3_2, "defaultPlugin3");
@@ -352,7 +361,7 @@ public class PluginDependencyManagerTest {
 
         // then
         assertEquals(0, result.getDisabledDependencies().size());
-        assertEquals(0, result.getUnsatisfiedDependencies().size());
+        assertEquals(1, result.getUnsatisfiedDependencies().size());
         assertEquals(0, result.getEnabledDependencies().size());
     }
 
@@ -512,7 +521,7 @@ public class PluginDependencyManagerTest {
     }
 
     @Test
-    public void shouldReturnValidResultForCyclicDependenciesForDisabling() throws Exception {
+    public void shouldCheckDependenciesDependenciesAndReturnMultipleDependencies() throws Exception {
         // given
         Plugin p = mock(Plugin.class);
         given(p.getPluginInformation()).willReturn(dependencyPluginInformation1);
@@ -523,33 +532,213 @@ public class PluginDependencyManagerTest {
         Plugin p2 = mock(Plugin.class);
         given(p2.getPluginInformation()).willReturn(dependencyPluginInformation2);
         Set<PluginInformation> disabledRequiredPlugins2 = new HashSet<PluginInformation>();
-        disabledRequiredPlugins2.add(dependencyPluginInformation1_2);
-        disabledRequiredPlugins2.add(dependencyPluginInformation3_2);
-        given(p2.getRequiredPlugins()).willReturn(disabledRequiredPlugins2);
+        disabledRequiredPlugins2.add(dependencyPluginInformation3);
+        given(dependencyPlugin2.getRequiredPlugins()).willReturn(disabledRequiredPlugins2);
 
-        Plugin p3 = mock(Plugin.class);
-        given(p3.getPluginInformation()).willReturn(dependencyPluginInformation3);
-        Set<PluginInformation> disabledRequiredPlugins3 = new HashSet<PluginInformation>();
-        disabledRequiredPlugins3.add(dependencyPluginInformation2_2);
-        given(p3.getRequiredPlugins()).willReturn(disabledRequiredPlugins3);
+        given(dependencyPlugin1.getPluginState()).willReturn(PluginState.TEMPORARY);
+        given(dependencyPlugin2.getPluginState()).willReturn(PluginState.DISABLED);
+        given(dependencyPlugin3.getPluginState()).willReturn(PluginState.TEMPORARY);
 
         PluginDependencyManager m = new DefaultPluginDependencyManager(pluginAccessor);
+
+        // when
+        PluginDependencyResult result = m.getDependenciesToEnable(singletonList(p));
+
+        // then
+        assertEquals(2, result.getDisabledDependencies().size());
+
+        for (PluginInformation information : result.getDisabledDependencies()) {
+            assertNotNull(dependencyPluginInformations.get(information.getName()));
+
+            assertEquals(information.getName(), dependencyPluginInformations.get(information.getName()).getName());
+            assertEquals(information.getName() + "_vendor", dependencyPluginInformations.get(information.getName()).getVendor());
+            assertEquals(information.getName() + "_version", dependencyPluginInformations.get(information.getName()).getVersion());
+
+            dependencyPluginInformations.remove(information.getName());
+        }
+
+        assertEquals(0, result.getUnsatisfiedDependencies().size());
+        assertEquals(0, result.getEnabledDependencies().size());
+    }
+
+    @Test
+    public void shouldCheckDependenciesDependenciesAndReturnMultipleDependenciesForDisable() throws Exception {
+        // given
+        Plugin p = mock(Plugin.class);
+        given(p.getPluginInformation()).willReturn(dependencyPluginInformation1);
+        Set<PluginInformation> disabledRequiredPlugins = new HashSet<PluginInformation>();
+        disabledRequiredPlugins.add(dependencyPluginInformation2);
+        given(p.getRequiredPlugins()).willReturn(disabledRequiredPlugins);
+
+        Plugin p2 = mock(Plugin.class);
+        given(p2.getPluginInformation()).willReturn(dependencyPluginInformation2);
+        Set<PluginInformation> disabledRequiredPlugins2 = new HashSet<PluginInformation>();
+        disabledRequiredPlugins2.add(dependencyPluginInformation3);
+        given(dependencyPlugin2.getRequiredPlugins()).willReturn(disabledRequiredPlugins2);
 
         given(dependencyPlugin1.getPluginState()).willReturn(PluginState.ENABLED);
         given(dependencyPlugin2.getPluginState()).willReturn(PluginState.ENABLED);
         given(dependencyPlugin3.getPluginState()).willReturn(PluginState.ENABLED);
 
+        PluginDependencyManager m = new DefaultPluginDependencyManager(pluginAccessor);
+
+        // when
+        PluginDependencyResult result = m.getDependenciesToDisable(singletonList(p));
+
+        // then
+        assertEquals(2, result.getEnabledDependencies().size());
+
+        for (PluginInformation information : result.getDisabledDependencies()) {
+            assertNotNull(dependencyPluginInformations.get(information.getName()));
+
+            assertEquals(information.getName(), dependencyPluginInformations.get(information.getName()).getName());
+            assertEquals(information.getName() + "_vendor", dependencyPluginInformations.get(information.getName()).getVendor());
+            assertEquals(information.getName() + "_version", dependencyPluginInformations.get(information.getName()).getVersion());
+
+            dependencyPluginInformations.remove(information.getName());
+        }
+
+        assertEquals(0, result.getUnsatisfiedDependencies().size());
+        assertEquals(0, result.getDisabledDependencies().size());
+    }
+
+    @Test
+    public void shouldSetCyclicFlagOnCyclicDependencies() throws Exception {
+        // given
+        Plugin p = mock(Plugin.class);
+        given(p.getPluginInformation()).willReturn(dependencyPluginInformation1);
+        Set<PluginInformation> disabledRequiredPlugins = new HashSet<PluginInformation>();
+        disabledRequiredPlugins.add(dependencyPluginInformation2);
+        given(p.getRequiredPlugins()).willReturn(disabledRequiredPlugins);
+
+        Plugin p2 = mock(Plugin.class);
+        given(p2.getPluginInformation()).willReturn(dependencyPluginInformation2);
+        Set<PluginInformation> disabledRequiredPlugins2 = new HashSet<PluginInformation>();
+        disabledRequiredPlugins2.add(dependencyPluginInformation1);
+        given(p2.getRequiredPlugins()).willReturn(disabledRequiredPlugins2);
+
+        PluginDependencyManager m = new DefaultPluginDependencyManager(pluginAccessor);
+
+        given(dependencyPlugin1.getPluginState()).willReturn(PluginState.TEMPORARY);
+        given(dependencyPlugin2.getPluginState()).willReturn(PluginState.TEMPORARY);
+
         List<Plugin> plugins = new ArrayList<Plugin>();
         plugins.add(p);
         plugins.add(p2);
-        plugins.add(p3);
 
         // when
-        PluginDependencyResult result = m.getDependenciesToDisable(plugins);
+        PluginDependencyResult result = m.getDependenciesToEnable(plugins);
 
         // then
-        assertEquals(0, result.getDisabledDependencies().size());
-        assertEquals(0, result.getUnsatisfiedDependencies().size());
-        assertEquals(0, result.getEnabledDependencies().size());
+        assertTrue(result.isCyclic());
+    }
+
+    @Test
+    public void shouldSetCyclicFlagWhenCyclicDependenciesDependencies() throws Exception {
+        // given
+        Plugin p = mock(Plugin.class);
+        given(p.getPluginInformation()).willReturn(dependencyPluginInformation1);
+        Set<PluginInformation> disabledRequiredPlugins = new HashSet<PluginInformation>();
+        disabledRequiredPlugins.add(dependencyPluginInformation2);
+        given(p.getRequiredPlugins()).willReturn(disabledRequiredPlugins);
+
+        Plugin p2 = mock(Plugin.class);
+        given(p2.getPluginInformation()).willReturn(dependencyPluginInformation2);
+        Set<PluginInformation> disabledRequiredPlugins2 = new HashSet<PluginInformation>();
+        disabledRequiredPlugins2.add(dependencyPluginInformation3);
+        given(dependencyPlugin2.getRequiredPlugins()).willReturn(disabledRequiredPlugins2);
+
+        Plugin p3 = mock(Plugin.class);
+        given(p3.getPluginInformation()).willReturn(dependencyPluginInformation3);
+        Set<PluginInformation> disabledRequiredPlugins3 = new HashSet<PluginInformation>();
+        disabledRequiredPlugins3.add(dependencyPluginInformation1);
+        given(dependencyPlugin3.getRequiredPlugins()).willReturn(disabledRequiredPlugins3);
+
+        given(dependencyPlugin1.getPluginState()).willReturn(PluginState.TEMPORARY);
+        given(dependencyPlugin2.getPluginState()).willReturn(PluginState.TEMPORARY);
+        given(dependencyPlugin3.getPluginState()).willReturn(PluginState.TEMPORARY);
+
+        PluginDependencyManager m = new DefaultPluginDependencyManager(pluginAccessor);
+
+        // when
+        PluginDependencyResult result = m.getDependenciesToEnable(singletonList(p));
+
+        // then
+        assertTrue(result.isCyclic());
+    }
+
+    @Test
+    public void shouldSetCyclicFlagWhenCyclicDependenciesDependenciesButNotDependencyToArgument() throws Exception {
+        // given
+        Plugin p = mock(Plugin.class);
+        given(p.getPluginInformation()).willReturn(dependencyPluginInformation1);
+        Set<PluginInformation> disabledRequiredPlugins = new HashSet<PluginInformation>();
+        disabledRequiredPlugins.add(dependencyPluginInformation2);
+        given(p.getRequiredPlugins()).willReturn(disabledRequiredPlugins);
+
+        Plugin p2 = mock(Plugin.class);
+        given(p2.getPluginInformation()).willReturn(dependencyPluginInformation2);
+        Set<PluginInformation> disabledRequiredPlugins2 = new HashSet<PluginInformation>();
+        disabledRequiredPlugins2.add(dependencyPluginInformation3_2);
+        given(dependencyPlugin2.getRequiredPlugins()).willReturn(disabledRequiredPlugins2);
+
+        Plugin p3 = mock(Plugin.class);
+        given(p3.getPluginInformation()).willReturn(dependencyPluginInformation3);
+        Set<PluginInformation> disabledRequiredPlugins3 = new HashSet<PluginInformation>();
+        disabledRequiredPlugins3.add(dependencyPluginInformation2_2);
+        given(dependencyPlugin3.getRequiredPlugins()).willReturn(disabledRequiredPlugins3);
+
+        given(dependencyPlugin1.getPluginState()).willReturn(PluginState.TEMPORARY);
+        given(dependencyPlugin2.getPluginState()).willReturn(PluginState.TEMPORARY);
+        given(dependencyPlugin3.getPluginState()).willReturn(PluginState.TEMPORARY);
+
+        PluginDependencyManager m = new DefaultPluginDependencyManager(pluginAccessor);
+
+        // when
+        PluginDependencyResult result = m.getDependenciesToEnable(singletonList(p));
+
+        // then
+        assertTrue(result.isCyclic());
+    }
+
+    @Test
+    public void shouldSetCyclicFlagWhenCyclicDependenciesDependenciesToArgumentsDependency() throws Exception {
+        // given
+        Plugin p = mock(Plugin.class);
+        given(p.getPluginInformation()).willReturn(dependencyPluginInformation1);
+        Set<PluginInformation> disabledRequiredPlugins = new HashSet<PluginInformation>();
+        disabledRequiredPlugins.add(dependencyPluginInformation2);
+        given(p.getRequiredPlugins()).willReturn(disabledRequiredPlugins);
+
+        Plugin p2 = mock(Plugin.class);
+        given(p2.getPluginInformation()).willReturn(dependencyPluginInformation2);
+        Set<PluginInformation> disabledRequiredPlugins2 = new HashSet<PluginInformation>();
+        disabledRequiredPlugins2.add(dependencyPluginInformation3);
+        given(dependencyPlugin2.getRequiredPlugins()).willReturn(disabledRequiredPlugins2);
+
+        Plugin p3 = mock(Plugin.class);
+        given(p3.getPluginInformation()).willReturn(dependencyPluginInformation3);
+        Set<PluginInformation> disabledRequiredPlugins3 = new HashSet<PluginInformation>();
+        disabledRequiredPlugins3.add(dependencyPluginInformation5);
+        given(dependencyPlugin3.getRequiredPlugins()).willReturn(disabledRequiredPlugins3);
+
+        Plugin p5 = mock(Plugin.class);
+        given(p5.getPluginInformation()).willReturn(dependencyPluginInformation5);
+        Set<PluginInformation> disabledRequiredPlugins5 = new HashSet<PluginInformation>();
+        disabledRequiredPlugins5.add(dependencyPluginInformation2);
+        given(dependencyPlugin5.getRequiredPlugins()).willReturn(disabledRequiredPlugins5);
+
+        given(dependencyPlugin1.getPluginState()).willReturn(PluginState.TEMPORARY);
+        given(dependencyPlugin2.getPluginState()).willReturn(PluginState.TEMPORARY);
+        given(dependencyPlugin3.getPluginState()).willReturn(PluginState.TEMPORARY);
+        given(dependencyPlugin5.getPluginState()).willReturn(PluginState.TEMPORARY);
+
+        PluginDependencyManager m = new DefaultPluginDependencyManager(pluginAccessor);
+
+        // when
+        PluginDependencyResult result = m.getDependenciesToEnable(singletonList(p));
+
+        // then
+        assertTrue(result.isCyclic());
     }
 }
