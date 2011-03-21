ddc663b1c8baee324362600fc5fe521ac5c664ae
Krzysztof Nadolski <krzysztof.nadolski@qcadoo.com>
2011-03-21 11:41:59 +0100

    SC#QCADOO919 add view module

---
+++ mes-core/src/main/java/com/qcadoo/mes/view/internal/module/HookModuleFactory.java
@@ -0,0 +1,27 @@
+package com.qcadoo.mes.view.internal.module;
+
+import org.jdom.Element;
+
+import com.qcadoo.plugin.internal.api.ModuleFactory;
+
+public class HookModuleFactory implements ModuleFactory<ViewModule> {
+
+    @Override
+    public void init() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public ViewModule parse(final String pluginIdentifier, final Element element) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getIdentifier() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
