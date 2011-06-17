594072a15539318bc24931735a2ff95dcb07a26d
Maciej Szczytowski <maciej.szczytowski@qcadoo.com>
2011-06-17 12:31:58 +0200

    SC#QCADOOCLS-24

--- mes-plugins/mes-plugins-production-scheduling/src/main/java/com/qcadoo/mes/productionScheduling/ShiftsGanttChartItemResolver.java
+++ mes-plugins/mes-plugins-production-scheduling/src/main/java/com/qcadoo/mes/productionScheduling/ShiftsGanttChartItemResolver.java
@@ -110,7 +110,7 @@ public class ShiftsGanttChartItemResolver implements GanttChartItemResolver {
                     hoursToAdd.add(new ShiftHour(hour.getDateFrom(), from));
                     continue;
                 }
-                if (hour.getDateTo().before(to) && hour.getDateFrom().before(from)) {
+                if (hour.getDateTo().after(to) && hour.getDateFrom().before(from)) {
                     hoursToRemove.add(hour);
                     hoursToAdd.add(new ShiftHour(hour.getDateFrom(), from));
                     hoursToAdd.add(new ShiftHour(to, hour.getDateTo()));
@@ -272,8 +272,41 @@ public class ShiftsGanttChartItemResolver implements GanttChartItemResolver {
         }
 
         @Override
-        public String toString() {
-            return "[" + dateFrom + " - " + dateTo + "]";
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + ((dateFrom == null) ? 0 : dateFrom.hashCode());
+            result = prime * result + ((dateTo == null) ? 0 : dateTo.hashCode());
+            return result;
+        }
+
+        @Override
+        public boolean equals(final Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null) {
+                return false;
+            }
+            if (!(obj instanceof ShiftHour)) {
+                return false;
+            }
+            ShiftHour other = (ShiftHour) obj;
+            if (dateFrom == null) {
+                if (other.dateFrom != null) {
+                    return false;
+                }
+            } else if (!dateFrom.equals(other.dateFrom)) {
+                return false;
+            }
+            if (dateTo == null) {
+                if (other.dateTo != null) {
+                    return false;
+                }
+            } else if (!dateTo.equals(other.dateTo)) {
+                return false;
+            }
+            return true;
         }
 
     }
