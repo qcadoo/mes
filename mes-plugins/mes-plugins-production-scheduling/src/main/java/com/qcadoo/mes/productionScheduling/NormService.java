42cb5b6ff8c585d2b4b7d5d98b6a442c1db7694c
Alina Brauer <alina.brauer@qcadoo.com>
2011-06-20 16:26:26 +0200

    sc#QCADOOMESCLS-17

--- mes-plugins/mes-plugins-production-scheduling/src/main/java/com/qcadoo/mes/productionScheduling/NormService.java
+++ mes-plugins/mes-plugins-production-scheduling/src/main/java/com/qcadoo/mes/productionScheduling/NormService.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Service;
 
 import com.qcadoo.model.api.DataDefinition;
 import com.qcadoo.model.api.Entity;
+import com.qcadoo.model.api.search.SearchRestrictions;
+import com.qcadoo.model.api.search.SearchResult;
 import com.qcadoo.view.api.ComponentState;
 import com.qcadoo.view.api.ViewDefinitionState;
 import com.qcadoo.view.api.components.FieldComponent;
@@ -11,33 +13,33 @@ import com.qcadoo.view.api.components.FieldComponent;
 @Service
 public class NormService {
 
-	public void changeEnableNorm(final ViewDefinitionState viewDefinitionState,
+	public void changeDefaultValue(final ViewDefinitionState viewDefinitionState,
 			final ComponentState state, final String[] args) {
 
-		FieldComponent enableNorm = (FieldComponent) viewDefinitionState
-				.getComponentByReference("enableNorm");
+		FieldComponent useDefaultValue = (FieldComponent) viewDefinitionState
+				.getComponentByReference("useDefaultValue");
 		FieldComponent tpzNorm = (FieldComponent) viewDefinitionState
-				.getComponentByReference("tpzNorm");
+				.getComponentByReference("tpz");
 		FieldComponent tjNorm = (FieldComponent) viewDefinitionState
-				.getComponentByReference("tjNorm");
+				.getComponentByReference("tj");
 		FieldComponent countRealizedNorm = (FieldComponent) viewDefinitionState
 				.getComponentByReference("countRealizedNorm");
-		FieldComponent changeNorm = (FieldComponent) viewDefinitionState
-				.getComponentByReference("changeNorm");
+		FieldComponent useMachineNorm = (FieldComponent) viewDefinitionState
+				.getComponentByReference("useMachineNorm");
 		FieldComponent timeNextOperationNorm = (FieldComponent) viewDefinitionState
 				.getComponentByReference("timeNextOperationNorm");
 
-		if (enableNorm.getFieldValue().equals("1")) {
+		if (useDefaultValue.getFieldValue().equals("1")) {
 			tpzNorm.setEnabled(false);
 			tjNorm.setEnabled(false);
 			countRealizedNorm.setEnabled(false);
-			changeNorm.setEnabled(false);
+			useMachineNorm.setEnabled(false);
 			timeNextOperationNorm.setEnabled(false);
 		} else {
 			tpzNorm.setEnabled(true);
 			tjNorm.setEnabled(true);
 			countRealizedNorm.setEnabled(true);
-			changeNorm.setEnabled(true);
+			useMachineNorm.setEnabled(true);
 			timeNextOperationNorm.setEnabled(true);
 		}
 	}
@@ -59,11 +61,11 @@ public class NormService {
 	public void copyDefaultDataToOperationComponent(final DataDefinition dataDefinition, final Entity entity) {
 
 		Entity operation = entity.getBelongsToField("operation");
-		if (entity.getField("tpzOperation") == null
-				|| "".equals(entity.getField("tpzOperation"))) {
+		if (entity.getField("tpz") == null
+				|| "".equals(entity.getField("tpz"))) {
 
-			entity.setField("tpzNorm", operation.getField("tpzOperation"));
-			entity.setField("tjNorm", operation.getField("tjOperation"));
+			entity.setField("tpz", operation.getField("tpz"));
+			entity.setField("tj", operation.getField("tj"));
 			entity.setField("countRealizedNorm",
 					operation.getStringField("countRealizedOperation"));
 			entity.setField("timeNextOperationNorm",
@@ -74,4 +76,24 @@ public class NormService {
 			return;
 		}
 	}
+	
+/*	public boolean checkOperationComponentUniqueness(final DataDefinition dataDefinition, final Entity entity) {
+        Entity machine = entity.getBelongsToField("machine");
+        Entity operationComponent = entity.getBelongsToField("operationComponent");
+
+        if (operationComponent == null || machine == null) {
+            return false;
+        }
+
+        SearchResult searchResult = dataDefinition.find().add(SearchRestrictions.belongsTo("machine", machine))
+                .add(SearchRestrictions.belongsTo("operationComponent", operationComponent)).list();
+
+        if (searchResult.getTotalNumberOfEntities() > 0 && !searchResult.getEntities().get(0).getId().equals(entity.getId())) {
+            entity.addError(dataDefinition.getField("machine"),
+                    "productionScheduling.validate.global.error.machineInOperationDuplicated");
+            return false;
+        } else {
+            return true;
+        }
+    }*/
 }
