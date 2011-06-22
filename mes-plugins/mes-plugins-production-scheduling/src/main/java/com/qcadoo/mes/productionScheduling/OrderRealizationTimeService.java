a91489b08727ec93ca8de73a7bd540decb60294a
Krzysztof Nadolski <krzysztof.nadolski@qcadoo.com>
2011-06-22 16:39:24 +0200

    SC#QCADOOMESCLS-72 add realization time

--- mes-plugins/mes-plugins-production-scheduling/src/main/java/com/qcadoo/mes/productionScheduling/OrderRealizationTimeService.java
+++ mes-plugins/mes-plugins-production-scheduling/src/main/java/com/qcadoo/mes/productionScheduling/OrderRealizationTimeService.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-import com.qcadoo.mes.technologies.constants.TechnologiesConstants;
+import com.qcadoo.mes.orders.constants.OrdersConstants;
 import com.qcadoo.model.api.DataDefinitionService;
 import com.qcadoo.model.api.Entity;
+import com.qcadoo.model.api.EntityTree;
+import com.qcadoo.model.api.EntityTreeNode;
 import com.qcadoo.view.api.ComponentState;
 import com.qcadoo.view.api.ViewDefinitionState;
 import com.qcadoo.view.api.components.FieldComponent;
@@ -50,12 +52,14 @@ public class OrderRealizationTimeService {
         if (!(state instanceof FieldComponent)) {
             return;
         }
+
         FieldComponent plannedQuantity = (FieldComponent) viewDefinitionState.getComponentByReference("plannedQuantity");
         FieldComponent technology = (FieldComponent) viewDefinitionState.getComponentByReference("technology");
         FieldComponent realizationTime = (FieldComponent) viewDefinitionState.getComponentByReference("realizationTime");
 
         if (technology.getFieldValue() != null && StringUtils.hasText((String) plannedQuantity.getFieldValue())) {
-            realizationTime.setFieldValue(BigDecimal.valueOf(estimateRealizationTime((Long) technology.getFieldValue())));
+            // realizationTime.setFieldValue(BigDecimal.valueOf(estimateRealizationTime((Long) viewDefinitionState
+            // .getComponentByReference("form").getFieldValue())));
             // TODO KRNA value > max
         } else {
             realizationTime.setFieldValue(BigDecimal.ZERO);
@@ -63,10 +67,39 @@ public class OrderRealizationTimeService {
         // TODO KRNA what with product lookup ?
     }
 
-    private int estimateRealizationTime(final Long technologyId) {
-        Entity entity = dataDefinitionService
-                .get(TechnologiesConstants.PLUGIN_IDENTIFIER, TechnologiesConstants.MODEL_TECHNOLOGY).get(technologyId);
+    private int estimateRealizationTime(final Long id) {
+        Entity order = dataDefinitionService.get(OrdersConstants.PLUGIN_IDENTIFIER, OrdersConstants.MODEL_ORDER).get(id);
+        EntityTree tree = order.getTreeField("orderOperationComponents");
+        EntityTreeNode node = tree.getRoot();
+
+        int pathTime = 0;
+
+        if ((Boolean) node.getField("useMachineNorm")) {
+            node.setField("tj", "1");
+            node.setField("tpz", "1");
+            // TODO KRNA machines
+        }
+        if (node.getChildren().size() == 0) {
+            // TODO KRNA empty parent
+            boolean operationHasParent = true;
+            Entity parent = node.getBelongsToField("parent");
+            if ("01all".equals(parent.getField("countRealized"))) {
+                pathTime += (((BigDecimal) order.getField("plannedQuantity")).multiply(BigDecimal.valueOf((Integer) node
+                        .getField("tj")))).intValue()
+                        + (Integer) node.getField("tpz")
+                        + (Integer) node.getField("timeNextOperation");
+            } else {
+                pathTime += (((BigDecimal) order.getField("countMachine")).multiply(BigDecimal.valueOf((Integer) node
+                        .getField("tj")))).intValue()
+                        + (Integer) node.getField("tpz")
+                        + (Integer) node.getField("timeNextOperation");
+            }
+
+            while (operationHasParent) {
+                operationHasParent = false;
+            }
+        }
+
         return 1;
     }
-
 }
