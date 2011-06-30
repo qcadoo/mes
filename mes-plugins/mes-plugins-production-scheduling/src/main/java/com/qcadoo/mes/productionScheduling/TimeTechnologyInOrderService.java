c09c05ab8e17edb6bc3caf547d7f808284f2ca02
Alina Brauer <alina.brauer@qcadoo.com>
2011-06-30 15:59:53 +0200

    QCADOOMESCLS-77

--- mes-plugins/mes-plugins-production-scheduling/src/main/java/com/qcadoo/mes/productionScheduling/TimeTechnologyInOrderService.java
+++ mes-plugins/mes-plugins-production-scheduling/src/main/java/com/qcadoo/mes/productionScheduling/TimeTechnologyInOrderService.java
@@ -4,16 +4,20 @@ import org.springframework.stereotype.Service;
 
 import com.qcadoo.view.api.ComponentState;
 import com.qcadoo.view.api.ViewDefinitionState;
-import com.qcadoo.view.api.components.FieldComponent;
+import com.qcadoo.view.api.components.FormComponent;
 
 @Service
 public class TimeTechnologyInOrderService {
 
     public void setVisibleAlert(final ViewDefinitionState viewDefinitionState) {
-        FieldComponent realizationTime = (FieldComponent) viewDefinitionState.getComponentByReference("realizationTime");
+        // ieldComponent realizationTime = (FieldComponent) viewDefinitionState.getComponentByReference("realizationTime");
         ComponentState alert = (ComponentState) viewDefinitionState.getComponentByReference("alert");
 
-        if (realizationTime == null || realizationTime.getFieldValue() == null) {
+        FormComponent form = (FormComponent) viewDefinitionState.getComponentByReference("form");
+
+        if (form.getEntityId() == null) {
+            // if (realizationTime == null || realizationTime.getFieldValue() == null /* ||
+            // "0".equals(realizationTime.getFieldValue()) */) {
             alert.setVisible(true);
         } else {
             alert.setVisible(false);
