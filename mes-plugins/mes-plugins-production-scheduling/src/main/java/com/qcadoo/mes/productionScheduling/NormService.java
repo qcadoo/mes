635174f2b1c527bc9792989a421f53b0799409bc
Maciej Szczytowski <maciej.szczytowski@qcadoo.com>
2011-07-04 15:49:31 +0200

    SC#QCADOOMESCLS-112

--- mes-plugins/mes-plugins-production-scheduling/src/main/java/com/qcadoo/mes/productionScheduling/NormService.java
+++ mes-plugins/mes-plugins-production-scheduling/src/main/java/com/qcadoo/mes/productionScheduling/NormService.java
@@ -1,8 +1,9 @@
 package com.qcadoo.mes.productionScheduling;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.qcadoo.model.api.DataDefinition;
+import com.qcadoo.model.api.DataDefinitionService;
 import com.qcadoo.model.api.Entity;
 import com.qcadoo.view.api.ComponentState;
 import com.qcadoo.view.api.ViewDefinitionState;
@@ -11,40 +12,28 @@ import com.qcadoo.view.api.components.FieldComponent;
 @Service
 public class NormService {
 
+    @Autowired
+    private DataDefinitionService dataDefinitionService;
+
     public void updateFieldsStateOnWindowLoad(final ViewDefinitionState viewDefinitionState) {
-        FieldComponent useDefaultValue = (FieldComponent) viewDefinitionState.getComponentByReference("useDefaultValue");
         FieldComponent tpzNorm = (FieldComponent) viewDefinitionState.getComponentByReference("tpz");
         FieldComponent tjNorm = (FieldComponent) viewDefinitionState.getComponentByReference("tj");
         FieldComponent countRealizedNorm = (FieldComponent) viewDefinitionState.getComponentByReference("countRealizedNorm");
-        FieldComponent countMachineNorm = (FieldComponent) viewDefinitionState.getComponentByReference("countMachineNorm");
         FieldComponent timeNextOperationNorm = (FieldComponent) viewDefinitionState
                 .getComponentByReference("timeNextOperationNorm");
         Object value = countRealizedNorm.getFieldValue();
 
-        if (useDefaultValue.getFieldValue().equals("true") || useDefaultValue.getFieldValue().equals("1")) {
-            tpzNorm.setEnabled(false);
-            tjNorm.setEnabled(false);
-            countRealizedNorm.setEnabled(false);
-            if (!"02specified".equals(value)) {
-                countRealizedNorm.setFieldValue("01all");
-            }
-            countMachineNorm.setEnabled(false);
-            timeNextOperationNorm.setEnabled(false);
-        } else {
-            tpzNorm.setEnabled(true);
-            tjNorm.setEnabled(true);
-            countRealizedNorm.setEnabled(true);
-            if (!"02specified".equals(value)) {
-                countRealizedNorm.setFieldValue("01all");
-            }
-            timeNextOperationNorm.setEnabled(true);
+        tpzNorm.setEnabled(true);
+        tjNorm.setEnabled(true);
+        countRealizedNorm.setEnabled(true);
+        if (!"02specified".equals(value)) {
+            countRealizedNorm.setFieldValue("01all");
         }
-
+        timeNextOperationNorm.setEnabled(true);
     }
 
     public void updateFieldsStateWhenDefaultValueCheckboxChanged(final ViewDefinitionState viewDefinitionState,
             final ComponentState componentState, final String[] args) {
-
         /* uruchamia sie hook before render */
     }
 
@@ -60,35 +49,34 @@ public class NormService {
         }
     }
 
-    public void copyDefaultDataToOperationComponent(final DataDefinition dataDefinition, final Entity entity) {
-        Entity operation = entity.getBelongsToField("operation");
-        if (entity.getField("tpz") == null || "".equals(entity.getField("tpz"))) {
-            entity.setField("tpz", operation.getField("tpz"));
-            entity.setField("tj", operation.getField("tj"));
-            entity.setField("countRealizedNorm", operation.getStringField("countRealizedOperation"));
-            entity.setField("timeNextOperationNorm", operation.getField("timeNextOperation"));
-            entity.setField("countMachineNorm", operation.getField("countMachineOperation"));
+    public void copyNormFromOperation(final ViewDefinitionState viewDefinitionState, final ComponentState componentState,
+            final String[] args) {
+        FieldComponent tpzNorm = (FieldComponent) viewDefinitionState.getComponentByReference("tpz");
+        FieldComponent tjNorm = (FieldComponent) viewDefinitionState.getComponentByReference("tj");
+        FieldComponent countRealizedNorm = (FieldComponent) viewDefinitionState.getComponentByReference("countRealizedNorm");
+        FieldComponent countMachineNorm = (FieldComponent) viewDefinitionState.getComponentByReference("countMachineNorm");
+        FieldComponent timeNextOperationNorm = (FieldComponent) viewDefinitionState
+                .getComponentByReference("timeNextOperationNorm");
+
+        Long operationId = (Long) componentState.getFieldValue();
+
+        Entity operation = operationId != null ? dataDefinitionService.get("technologies", "operation").get(operationId) : null;
+
+        if (operation != null) {
+            tpzNorm.setFieldValue(operation.getField("tpz"));
+            tjNorm.setFieldValue(operation.getField("tj"));
+            countRealizedNorm.setFieldValue(operation.getField("countRealizedOperation"));
+            countMachineNorm.setFieldValue(operation.getField("countMachineOperation"));
+            timeNextOperationNorm.setFieldValue(operation.getField("timeNextOperation"));
         } else {
-            return;
+            tpzNorm.setFieldValue(null);
+            tjNorm.setFieldValue(null);
+            countRealizedNorm.setFieldValue("01all");
+            countMachineNorm.setFieldValue(null);
+            timeNextOperationNorm.setFieldValue(null);
         }
     }
 
-    public void copyDefaultDataFromOperationToTechnology(final DataDefinition dataDefinition, final Entity entity) {
-
-        if (entity.getId() != null) {
-            return;
-        }
-
-        Entity operation = entity.getBelongsToField("operation");
-
-        entity.setField("tpz", operation.getField("tpz"));
-        entity.setField("tj", operation.getField("tj"));
-        entity.setField("countRealizedNorm", operation.getStringField("countRealizedOperation"));
-        entity.setField("timeNextOperationNorm", operation.getField("timeNextOperation"));
-        entity.setField("countMachineNorm", operation.getField("countMachineOperation"));
-    }
-
-    /* hook */
     public void updateCountMachineOperationFieldStateonWindowLoad(final ViewDefinitionState viewDefinitionState) {
         FieldComponent countRealizedOperation = (FieldComponent) viewDefinitionState.getComponentByReference("countRealizedNorm");
         FieldComponent countMachineOperation = (FieldComponent) viewDefinitionState.getComponentByReference("countMachineNorm");
