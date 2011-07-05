c1f9759a17d7cd7e354785f59575fdf0003190e1
Alina Brauer <alina.brauer@qcadoo.com>
2011-07-05 15:39:31 +0200

    QCADOOMESCLS-115

---
+++ mes-plugins/mes-plugins-minimal-affordable-quantity/src/main/java/com/qcadoo/mes/minimalAffordableQuantity/QuantityService.java
@@ -0,0 +1,19 @@
+package com.qcadoo.mes.minimalAffordableQuantity;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class QuantityService {
+
+    /*
+     * @Autowired private DataDefinitionService dataDefinitionService; public void checkMinimalAffordableQuantity(final
+     * ViewDefinitionState viewDefinitionState) { System.out.println("xxx dziala"); FieldComponent plannedQuantity =
+     * (FieldComponent) viewDefinitionState.getComponentByReference("plannedQuantity"); FieldComponent technologyLookup =
+     * (FieldComponent) viewDefinitionState.getComponentByReference("technology"); Entity technology =
+     * dataDefinitionService.get("technologies", "technology").get((Long) technologyLookup.getFieldValue()); if (technology ==
+     * null || technology.getId() == null) { return; } Entity technologyEntity = dataDefinitionService.get("technologies",
+     * "technology").get(technology.getId()); System.out.println("***" + technologyEntity.getField("quantity"));
+     * System.out.println("***111" + plannedQuantity); }
+     */
+
+}
