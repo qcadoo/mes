6253f79fd62a0d7a3e3e04abfc115d02155150d2
Marcin Kubala <marcin.kubala@qcadoo.com>
2011-08-19 14:57:54 +0200

    SC#QCADOOMES-188..190

--- mes-plugins/mes-plugins-cost-norms-for-operation/src/main/java/com/qcadoo/mes/costNormsForOperation/CostNormsForOperationService.java
+++ mes-plugins/mes-plugins-cost-norms-for-operation/src/main/java/com/qcadoo/mes/costNormsForOperation/CostNormsForOperationService.java
@@ -5,48 +5,52 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.qcadoo.mes.productionScheduling.constants.ProductionSchedulingConstants;
+import com.qcadoo.mes.technologies.constants.TechnologiesConstants;
 import com.qcadoo.model.api.DataDefinitionService;
 import com.qcadoo.model.api.Entity;
-import com.qcadoo.view.api.ComponentState;
 import com.qcadoo.view.api.ViewDefinitionState;
+import com.qcadoo.view.api.components.FieldComponent;
 import com.qcadoo.view.api.components.FormComponent;
 
 @Service
 public class CostNormsForOperationService {
 
-	@Autowired
-	private DataDefinitionService dataDefinitionService;
-	
-	/* ****** VIEW HOOKS ******* */
-	
-	public void inheirtOperationCostValuesFromOperation(final ViewDefinitionState viewDefinitionState) {	
-		FormComponent form = (FormComponent) viewDefinitionState.getComponentByReference("form");	
-		// form.getEntity() may will also return that entity, but in detached state!
-		Entity entity = dataDefinitionService
-							.get("technologies", "technologyOperationComponent")
-								.get(form.getEntityId());
-		
-		applyCostValuesFromoperation(viewDefinitionState, entity.getBelongsToField("operation"));
-	}
-	
-	public void inheirtOperationCostValuesFromTechnology(final ViewDefinitionState viewDefinitionState) {
-		FormComponent form = (FormComponent) viewDefinitionState.getComponentByReference("form");
-		// form.getEntity() may will also return that entity, but in detached state!
-		Entity entity = dataDefinitionService
-							.get("productionScheduling", "orderOperationComponent")
-								.get(form.getEntityId());
-		
-		applyCostValuesFromoperation(viewDefinitionState, entity.getBelongsToField("technologyOperationComponent"));
-	}
-	
-	private void applyCostValuesFromoperation(final ViewDefinitionState viewDefinitionState, final Entity source) {
-		if (source != null) {
-			for(String componentReference : Arrays.asList("pieceworkCost", "numberOfOperations", "laborHourlyCost", "machineHourlyCost")) {
-				ComponentState component = (ComponentState) viewDefinitionState.getComponentByReference(componentReference);
-				if (component.getFieldValue().toString().isEmpty()) {
-					component.setFieldValue(source.getField(componentReference));
-				}
-			}
-		}
-	}
+    @Autowired
+    private DataDefinitionService dataDefinitionService;
+
+    /* ****** VIEW HOOKS ******* */
+
+    public void inheirtOperationCostValuesFromOperation(final ViewDefinitionState viewDefinitionState) {
+        FormComponent form = (FormComponent) viewDefinitionState.getComponentByReference("form");
+        // form.getEntity() may will also return that entity, but in detached state!
+        Entity entity = dataDefinitionService.get(TechnologiesConstants.PLUGIN_IDENTIFIER,
+                TechnologiesConstants.MODEL_TECHNOLOGY_OPERATION_COMPONENT).get(form.getEntityId());
+
+        copyCostValuesFromGivenOperation(viewDefinitionState, entity.getBelongsToField("operation"));
+    }
+
+    public void inheirtOperationCostValuesFromTechnology(final ViewDefinitionState viewDefinitionState) {
+        FormComponent form = (FormComponent) viewDefinitionState.getComponentByReference("form");
+        // form.getEntity() may will also return that entity, but in detached state!
+        Entity entity = dataDefinitionService.get(ProductionSchedulingConstants.PLUGIN_IDENTIFIER,
+                ProductionSchedulingConstants.MODEL_ORDER_OPERATION_COMPONENT).get(form.getEntityId());
+
+        copyCostValuesFromGivenOperation(viewDefinitionState, entity.getBelongsToField("technologyOperationComponent"));
+    }
+
+    private void copyCostValuesFromGivenOperation(final ViewDefinitionState viewDefinitionState, final Entity source) {
+        if (source == null) {
+            return;
+        }
+
+        for (String componentReference : Arrays.asList("pieceworkCost", "numberOfOperations", "laborHourlyCost",
+                "machineHourlyCost")) {
+            FieldComponent component = (FieldComponent) viewDefinitionState.getComponentByReference(componentReference);
+            if (component.getFieldValue() != null && component.getFieldValue().toString().isEmpty()) {
+                component.setFieldValue(source.getField(componentReference).toString());
+            }
+
+        }
+    }
 }
