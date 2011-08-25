4929f07987804c45179cca4bd75a6f2d2059e751
Marcin Kubala <marcin.kubala@qcadoo.com>
2011-08-25 16:13:56 +0200

    SC order inhritance & more cleaner pom dependencies

--- mes-plugins/mes-plugins-cost-norms-for-operation/src/main/java/com/qcadoo/mes/costNormsForOperation/constants/CostNormsForOperationConstants.java
+++ mes-plugins/mes-plugins-cost-norms-for-operation/src/main/java/com/qcadoo/mes/costNormsForOperation/constants/CostNormsForOperationConstants.java
@@ -1,5 +1,9 @@
 package com.qcadoo.mes.costNormsForOperation.constants;
 
+import java.util.Arrays;
+
 public interface CostNormsForOperationConstants {
 	public static final String PLUGIN_IDENTIFIER = "costNorms";
+	public static final Iterable<String> FIELDS = Arrays.asList("pieceworkCost", "numberOfOperations", "laborHourlyCost",
+    "machineHourlyCost");
 }
