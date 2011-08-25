9c9e1f3ce888bdaa0ec499cdbd577c5ddcbad4dd
Marcin Kubala <marcin.kubala@qcadoo.com>
2011-08-25 11:31:18 +0200

    SC#QCADOOMES-188-190 - fix costs inheritance; add missing translation

--- mes-plugins/mes-plugins-cost-norms-for-operation/src/main/java/com/qcadoo/mes/costNormsForOperation/CostNormsForOperationService.java
+++ mes-plugins/mes-plugins-cost-norms-for-operation/src/main/java/com/qcadoo/mes/costNormsForOperation/CostNormsForOperationService.java
@@ -2,12 +2,15 @@ package com.qcadoo.mes.costNormsForOperation;
 
 import java.util.Arrays;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.qcadoo.mes.productionScheduling.constants.ProductionSchedulingConstants;
 import com.qcadoo.mes.technologies.constants.TechnologiesConstants;
-import com.qcadoo.model.api.DataDefinition;
 import com.qcadoo.model.api.DataDefinitionService;
 import com.qcadoo.model.api.Entity;
 import com.qcadoo.view.api.ViewDefinitionState;
@@ -20,41 +23,66 @@ public class CostNormsForOperationService {
     @Autowired
     private DataDefinitionService dataDefinitionService;
 
+    private final static Iterable<String> FIELDS = Arrays.asList("pieceworkCost", "numberOfOperations", "laborHourlyCost",
+            "machineHourlyCost");
+
+    private final static Logger LOG = LoggerFactory.getLogger(CostNormsForOperationService.class);
+
     /* ****** VIEW HOOKS ******* */
 
-    public void inheirtOperationCostValuesFromOperation(final ViewDefinitionState viewDefinitionState) {
+    public void inheritOperationCostValues(final ViewDefinitionState viewDefinitionState) {
         FormComponent form = (FormComponent) viewDefinitionState.getComponentByReference("form");
-        // form.getEntity() may will also return that entity, but in detached state!
-        Entity entity = dataDefinitionService.get(TechnologiesConstants.PLUGIN_IDENTIFIER,
-                TechnologiesConstants.MODEL_TECHNOLOGY_OPERATION_COMPONENT).get(form.getEntityId());
+        String formName = form.getName();
+        Entity target, source;
 
-        copyCostValuesFromGivenOperation(viewDefinitionState, entity.getBelongsToField("operation"));
-    }
-
-    public void inheirtOperationCostValuesFromTechnology(final ViewDefinitionState viewDefinitionState) {
-        FormComponent form = (FormComponent) viewDefinitionState.getComponentByReference("form");
-        // form.getEntity() may will also return that entity, but in detached state!
-        Entity entity = dataDefinitionService.get(ProductionSchedulingConstants.PLUGIN_IDENTIFIER,
-                ProductionSchedulingConstants.MODEL_ORDER_OPERATION_COMPONENT).get(form.getEntityId());
-
-        copyCostValuesFromGivenOperation(viewDefinitionState, entity.getBelongsToField("technologyOperationComponent"));
-    }
-
-    
-    /* ******* AWESOME HELPERS ;) ******* */
-    
-    private void copyCostValuesFromGivenOperation(final ViewDefinitionState viewDefinitionState, final Entity source) {
-        if (source == null) {
+        if ("form".equals(formName)) { // technology
+            target = dataDefinitionService.get(TechnologiesConstants.PLUGIN_IDENTIFIER,
+                    TechnologiesConstants.MODEL_TECHNOLOGY_OPERATION_COMPONENT).get(form.getEntityId());
+            source = target.getBelongsToField("operation");
+            copyCostValuesFromGivenOperation(target, source);
+        } else if ("orderOperationComponent".equals(formName)) { // technology instance (technology inside order)
+            target = dataDefinitionService.get(ProductionSchedulingConstants.PLUGIN_IDENTIFIER,
+                    ProductionSchedulingConstants.MODEL_ORDER_OPERATION_COMPONENT).get(form.getEntityId());
+            source = target.getBelongsToField("technologyOperationComponent");
+            if (copyCostValuesFromGivenOperation(target, source)) {
+                return;
+            }
+            copyCostValuesFromGivenOperation(target, source.getBelongsToField("operation"));
+            copyCostValuesFromGivenOperation(source, source.getBelongsToField("operation")); // Fill missing technology costs
+        } else {
             return;
         }
 
-        for (String componentReference : Arrays.asList("pieceworkCost", "numberOfOperations", "laborHourlyCost",
-                "machineHourlyCost")) {
+        fillCostFormFields(viewDefinitionState, source); // propagate model changes into the view
+    }
+
+    /* ******* AWESOME HELPERS ;) ******* */
+
+    private boolean copyCostValuesFromGivenOperation(final Entity target, final Entity source) {
+        checkArgument(source != null, "given source is null");
+        boolean result = false;
+        for (String fieldName : FIELDS) {
+            if (target.getField(fieldName) == null && source.getField(fieldName) != null) {
+                target.setField(fieldName, source.getField(fieldName));
+                result = true;
+            }
+        }
+        target.getDataDefinition().save(target);
+        return result;
+    }
+
+    private void fillCostFormFields(final ViewDefinitionState viewDefinitionState, final Entity source) {
+        for (String componentReference : FIELDS) {
             FieldComponent component = (FieldComponent) viewDefinitionState.getComponentByReference(componentReference);
             if (component.getFieldValue() != null && component.getFieldValue().toString().isEmpty()) {
                 component.setFieldValue(source.getField(componentReference).toString());
             }
+        }
+    }
 
+    private void debug(final String message) {
+        if (LOG.isDebugEnabled()) {
+            LOG.debug(message);
         }
     }
 }
