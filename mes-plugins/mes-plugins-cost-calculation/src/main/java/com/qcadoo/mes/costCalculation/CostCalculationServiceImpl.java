363f67ad154c3951b81980a079f5cca3e1cdcfbd
Marcin Kubala <marcin.kubala@qcadoo.com>
2011-09-07 12:33:23 +0200

    SC#QCADOOMES-155 fight with EntityTree

--- mes-plugins/mes-plugins-cost-calculation/src/main/java/com/qcadoo/mes/costCalculation/CostCalculationServiceImpl.java
+++ mes-plugins/mes-plugins-cost-calculation/src/main/java/com/qcadoo/mes/costCalculation/CostCalculationServiceImpl.java
@@ -1,15 +1,10 @@
 package com.qcadoo.mes.costCalculation;
 
 import static com.google.common.base.Preconditions.checkArgument;
-import static com.qcadoo.mes.costCalculation.constants.CostCalculateConstants.MODEL_COST_CALCULATION;
-import static com.qcadoo.mes.costCalculation.constants.CostCalculateConstants.PLUGIN_IDENTIFIER;
 import static com.qcadoo.mes.costNormsForOperation.constants.OperationsCostCalculationConstants.HOURLY;
-import static com.qcadoo.mes.orders.constants.OrdersConstants.MODEL_ORDER;
-import static com.qcadoo.mes.technologies.constants.TechnologiesConstants.MODEL_TECHNOLOGY;
 
 import java.math.BigDecimal;
 import java.util.Date;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,16 +12,10 @@ import org.springframework.stereotype.Service;
 import com.qcadoo.mes.costNormsForOperation.OperationsCostCalculationService;
 import com.qcadoo.mes.costNormsForOperation.constants.OperationsCostCalculationConstants;
 import com.qcadoo.mes.costNormsForProduct.ProductsCostCalculationService;
-import com.qcadoo.mes.orders.constants.OrdersConstants;
-import com.qcadoo.model.api.DataDefinition;
-import com.qcadoo.model.api.DataDefinitionService;
 import com.qcadoo.model.api.Entity;
 
 @Service
 public class CostCalculationServiceImpl implements CostCalculationService {
-
-    // @Autowired
-    // private DataDefinitionService dataDefinitionService;
 
     @Autowired
     private OperationsCostCalculationService operationsCostCalculationService;
@@ -34,39 +23,18 @@ public class CostCalculationServiceImpl implements CostCalculationService {
     @Autowired
     private ProductsCostCalculationService productsCostCalculationService;
 
-    /*
-     * public Entity calculateTotalCost(final Entity givenSource, final Map<String, Object> parameters) {
-     * checkArgument(givenSource != null && givenSource.getDataDefinition() != null, "incompatible source entity");
-     * checkArgument(parameters.size() != 0, "parameter is empty"); Entity costCalculation =
-     * dataDefinitionService.get(PLUGIN_IDENTIFIER, MODEL_COST_CALCULATION).create(); for (String key : parameters.keySet()) {
-     * costCalculation.setField(key, parameters.get(key)); }
-     */
-    public Entity calculateTotalCost(final Entity costCalculation) {
+    public Entity calculateTotalCost(Entity costCalculation) {
 
         BigDecimal productionCosts;
         BigDecimal materialCostMargin = getBigDecimal(costCalculation.getField("materialCostMargin"));
         BigDecimal productionCostMargin = getBigDecimal(costCalculation.getField("productionCostMargin"));
         BigDecimal additionalOverhead = getBigDecimal(costCalculation.getField("additionalOverhead"));
         BigDecimal quantity = getBigDecimal(costCalculation.getField("quantity"));
-        // Entity technology;
         OperationsCostCalculationConstants operationMode = getOperationModeFromField(costCalculation
                 .getField("calculateOperationCostsMode"));
 
-        // Be sure that source Entity isn't in detached state
-        // Entity source = dd.get(givenSource.getId());
-        // Entity order = costCalculation.getBelongsToField("order");
-
-        // if (MODEL_TECHNOLOGY.equals(dd.getName())) {
-        // technology = source;
-        // } else if (MODEL_ORDER.equals(dd.getName())) {
-        // technology = source.getBelongsToField("technology");
-        // } else {
-        // throw new IllegalArgumentException("incompatible source entity!");
-        // }
-
-        // costCalculation.setField("technology", technology);
-
         costCalculation.setField("dateOfCalculation", new Date());
+        costCalculation = costCalculation.getDataDefinition().save(costCalculation);
         operationsCostCalculationService.calculateOperationsCost(costCalculation);
         productsCostCalculationService.calculateProductsCost(costCalculation);
 
