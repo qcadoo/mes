00ea58085b55672cc3b81e58da7769e447683de5
Andrzej Kieltyka <andrzej.kieltyka@qcadoo.com>
2011-09-15 09:47:21 +0200

    SC#QCADOOMES-290 report model

--- mes-plugins/mes-plugins-production-counting/src/main/java/com/qcadoo/mes/productionCounting/internal/constants/ProductionCountingConstants.java
+++ mes-plugins/mes-plugins-production-counting/src/main/java/com/qcadoo/mes/productionCounting/internal/constants/ProductionCountingConstants.java
@@ -5,11 +5,13 @@ public class ProductionCountingConstants {
     public static final String PLUGIN_IDENTIFIER = "productionCounting";
 
     // MODEL
-    public static final String MODEL_PRODUCTION_COUNTING_PARAMETERS = "productionCountingParameters";
-
     public static final String MODEL_PRODUCTION_RECORD = "productionRecord";
 
     public static final String MODEL_PRODUCTION_BALANCE = "productionBalance";
+
+    public static final String MODEL_RECORD_OPERATION_PRODUCT_IN_COMPONENT = "recordOperationProductInComponent";
+
+    public static final String MODEL_RECORD_OPERATION_PRODUCT_OUT_COMPONENT = "recordOperationProductOutComponent";
 
     // VIEW
     public static final String VIEW_PRODUCTION_RECORDS_LIST = "productionRecordsList";
