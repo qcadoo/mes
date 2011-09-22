e97d0f6ecd4b9c49f53074198a4f8c6e5e6d7d2a
Marcin Kubala <marcin.kubala@qcadoo.com>
2011-09-22 10:57:28 +0200

    SC#QCADOOMES-284 add untracked class

---
+++ mes-plugins/mes-plugins-production-counting/src/main/java/com/qcadoo/mes/productionCounting/internal/ProductionRecordViewService.java
@@ -0,0 +1,115 @@
+package com.qcadoo.mes.productionCounting.internal;
+
+import static com.qcadoo.mes.basic.constants.BasicConstants.MODEL_PARAMETER;
+import static com.qcadoo.mes.orders.constants.OrdersConstants.MODEL_ORDER;
+
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.qcadoo.mes.basic.constants.BasicConstants;
+import com.qcadoo.mes.orders.constants.OrdersConstants;
+import com.qcadoo.model.api.DataDefinitionService;
+import com.qcadoo.model.api.Entity;
+import com.qcadoo.view.api.ComponentState;
+import com.qcadoo.view.api.ViewDefinitionState;
+import com.qcadoo.view.api.components.FieldComponent;
+import com.qcadoo.view.api.components.FormComponent;
+
+@Service
+public class ProductionRecordViewService {
+
+    @Autowired
+    private DataDefinitionService dataDefinitionService;
+
+    private final static String CUMULATE = "02cumulated";
+
+    private final static String FOR_EACH_OPERATION = "03forEach";
+    
+    private final static Logger LOG = LoggerFactory.getLogger(ProductionRecordViewService.class);
+
+    public void setParametersDefaultValue(final ViewDefinitionState viewDefinitionState) {
+        FormComponent form = (FormComponent) viewDefinitionState.getComponentByReference("form");
+        Entity parameter = dataDefinitionService.get(BasicConstants.PLUGIN_IDENTIFIER, MODEL_PARAMETER).get(form.getEntityId());
+
+        for (String componentReference : Arrays.asList("registerQuantityInProduct", "registerQuantityOutProduct",
+                "registerProductionTime")) {
+            FieldComponent component = (FieldComponent) viewDefinitionState.getComponentByReference(componentReference);
+            if (parameter == null || parameter.getField(componentReference) == null) {
+                component.setFieldValue(true);
+                component.requestComponentUpdateState();
+            }
+        }
+    }
+
+    public void setOrderDefaultValue(final ViewDefinitionState viewDefinitionState) {
+        FieldComponent typeOfProductionRecording = (FieldComponent) viewDefinitionState
+                .getComponentByReference("typeOfProductionRecording");
+
+        FormComponent form = (FormComponent) viewDefinitionState.getComponentByReference("form");
+        if (form.getEntityId() != null) {
+            Entity order = dataDefinitionService.get(OrdersConstants.PLUGIN_IDENTIFIER, MODEL_ORDER).get(
+                    (Long) form.getEntityId());
+            if (order == null || "".equals(order.getField("typeOfProductionRecording"))) {
+                typeOfProductionRecording.setFieldValue("01none");
+            }
+            for (String componentReference : Arrays.asList("registerQuantityInProduct", "registerQuantityOutProduct",
+                    "registerProductionTime")) {
+                FieldComponent component = (FieldComponent) viewDefinitionState.getComponentByReference(componentReference);
+                if (order == null || order.getField(componentReference) == null) {
+                    component.setFieldValue(true);
+                    component.requestComponentUpdateState();
+                }
+            }
+        } else {
+            typeOfProductionRecording.setFieldValue("01none");
+            for (String componentReference : Arrays.asList("registerQuantityInProduct", "registerQuantityOutProduct",
+                    "registerProductionTime")) {
+                FieldComponent component = (FieldComponent) viewDefinitionState.getComponentByReference(componentReference);
+                if (component.getFieldValue() == null) {
+                    component.setFieldValue(true);
+                    component.requestComponentUpdateState();
+                }
+            }
+        }
+    }
+
+    public void checkOrderState(final ViewDefinitionState viewDefinitionState) {
+        FieldComponent orderState = (FieldComponent) viewDefinitionState.getComponentByReference("state");
+        if ("03inProgress".equals(orderState.getFieldValue()) || "04done".equals(orderState.getFieldValue())) {
+            for (String componentName : Arrays.asList("typeOfProductionRecording", "registerQuantityInProduct",
+                    "registerQuantityOutProduct", "registerProductionTime", "allowedPartial", "blockClosing", "autoCloseOrder")) {
+                FieldComponent component = (FieldComponent) viewDefinitionState.getComponentByReference(componentName);
+                component.setEnabled(false);
+                component.requestComponentUpdateState();
+            }
+        }
+    }
+
+    public void enabledOrDisabledOperationField(final ViewDefinitionState viewDefinitionState,
+            final ComponentState componentState, final String[] args) {
+        Long orderId = (Long) viewDefinitionState.getComponentByReference("order").getFieldValue();
+        Entity order = dataDefinitionService.get(OrdersConstants.PLUGIN_IDENTIFIER, MODEL_ORDER).get(orderId);
+        if (order == null) {
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("order is null");
+            }
+            return;
+        }
+        setComponentVisible((String) order.getField("typeOfProductionRecording"), viewDefinitionState);
+    }
+
+    private void setComponentVisible(final String recordingType, final ViewDefinitionState view) {
+        view.getComponentByReference("orderOperationComponent").setVisible(
+                FOR_EACH_OPERATION.equals(recordingType) || CUMULATE.equals(recordingType));
+        view.getComponentByReference("borderLayoutConsumedTimeForEach").setVisible(FOR_EACH_OPERATION.equals(recordingType));
+        view.getComponentByReference("borderLayoutConsumedTimeCumulated").setVisible(CUMULATE.equals(recordingType));
+        view.getComponentByReference("operationNoneLabel").setVisible(
+                !CUMULATE.equals(recordingType) && !FOR_EACH_OPERATION.equals(recordingType));
+
+        ((FieldComponent) view.getComponentByReference("orderOperationComponent")).requestComponentUpdateState();
+    }
+}
