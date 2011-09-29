ee42cf7a11a65f7adcdc90d08280f28c6633d9ed
Alina Brauer <alina.brauer@qcadoo.com>
2011-09-29 14:50:08 +0200

    QCADOOMES-328 changed copy and generatedNumber method

--- mes-plugins/mes-plugins-quality-controls/src/main/java/com/qcadoo/mes/qualityControls/print/QualityControlsReportService.java
+++ mes-plugins/mes-plugins-quality-controls/src/main/java/com/qcadoo/mes/qualityControls/print/QualityControlsReportService.java
@@ -313,8 +313,13 @@ public class QualityControlsReportService {
         }
 
         String name = "";
+
         if (product != null) {
-            name = product.getField("name").toString();
+            if (type.equals("operation")) {
+                name = product.getBelongsToField("operation").getStringField("name");
+            } else {
+                name = product.getField("name").toString();
+            }
         }
         title.add(new Phrase(" " + name, PdfUtil.getArialBold11Dark()));
 
