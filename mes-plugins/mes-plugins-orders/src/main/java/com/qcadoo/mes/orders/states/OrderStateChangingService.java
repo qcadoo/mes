65a120d709cb2b3158aaaf1da72c794859fcaa70
Alina Brauer <alina.brauer@qcadoo.com>
2011-10-07 13:45:22 +0200

    SC#QCADOOMES-357 add validation

--- mes-plugins/mes-plugins-orders/src/main/java/com/qcadoo/mes/orders/states/OrderStateChangingService.java
+++ mes-plugins/mes-plugins-orders/src/main/java/com/qcadoo/mes/orders/states/OrderStateChangingService.java
@@ -1,6 +1,8 @@
 package com.qcadoo.mes.orders.states;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -36,4 +38,35 @@ public class OrderStateChangingService {
         logging.setField("worker", securityService.getCurrentUserName());
         logging.setField("dateAndTime", dateTime);
     }
+
+    public boolean validationPending(final Entity entity) {
+        List<String> references = Arrays.asList("number", "name", "product", "plannedQuantity");
+        return checkValidation(references, entity);
+    }
+
+    public boolean validationAccepted(final Entity entity) {
+        List<String> references = Arrays.asList("number", "name", "product", "plannedQuantity", "dateTo", "dateFrom",
+                "defaultTechnology", "technology");
+        return checkValidation(references, entity);
+    }
+
+    public boolean validationInProgress(final Entity entity) {
+        return validationAccepted(entity);
+    }
+
+    public boolean validationCompleted(final Entity entity) {
+        List<String> references = Arrays.asList("number", "name", "product", "plannedQuantity", "dateTo", "dateFrom",
+                "defaultTechnology", "technology", "doneQuantity");
+        return checkValidation(references, entity);
+    }
+
+    public boolean checkValidation(final List<String> references, final Entity entity) {
+        for (String reference : references)
+            if (entity.getField(reference) == null) {
+                entity.addError(entity.getDataDefinition().getField("order"), "orders.order.orderStates.fieldRequired");
+                return false;
+            }
+        return true;
+    }
+
 }
