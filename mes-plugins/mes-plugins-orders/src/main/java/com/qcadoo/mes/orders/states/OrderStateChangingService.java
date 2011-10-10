23f628662f425eafec6840a66aff7ceb7f182316
Alina Brauer <alina.brauer@qcadoo.com>
2011-10-10 17:02:44 +0200

    SC#QCADOOMES-333 add tests

--- mes-plugins/mes-plugins-orders/src/main/java/com/qcadoo/mes/orders/states/OrderStateChangingService.java
+++ mes-plugins/mes-plugins-orders/src/main/java/com/qcadoo/mes/orders/states/OrderStateChangingService.java
@@ -1,5 +1,7 @@
 package com.qcadoo.mes.orders.states;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -44,35 +46,40 @@ public class OrderStateChangingService {
     }
 
     public ChangeOrderStateError validationPending(final Entity entity) {
-        List<String> references = Arrays.asList("number", "name", "product", "plannedQuantity");
+        checkArgument(entity != null, "entity is null");
+        List<String> references = Arrays.asList("product", "plannedQuantity");
         return checkValidation(references, entity);
     }
 
     public ChangeOrderStateError validationAccepted(final Entity entity) {
-        List<String> references = Arrays.asList("number", "name", "product", "plannedQuantity", "dateTo", "dateFrom",
-                "defaultTechnology", "technology");
+        checkArgument(entity != null, "entity is null");
+        List<String> references = Arrays.asList("product", "plannedQuantity", "dateTo", "dateFrom", "defaultTechnology",
+                "technology");
         return checkValidation(references, entity);
     }
 
     public ChangeOrderStateError validationInProgress(final Entity entity) {
+        checkArgument(entity != null, "entity is null");
         return validationAccepted(entity);
     }
 
     public ChangeOrderStateError validationCompleted(final Entity entity) {
-        List<String> references = Arrays.asList("number", "name", "product", "plannedQuantity", "dateTo", "dateFrom",
-                "defaultTechnology", "technology", "doneQuantity");
+        checkArgument(entity != null, "entity is null");
+        List<String> references = Arrays.asList("product", "plannedQuantity", "dateTo", "dateFrom", "defaultTechnology",
+                "technology", "doneQuantity");
         return checkValidation(references, entity);
     }
 
-    public ChangeOrderStateError checkValidation(final List<String> references, final Entity entity) {
+    private ChangeOrderStateError checkValidation(final List<String> references, final Entity entity) {
+        checkArgument(entity != null, "entity is null");
         ChangeOrderStateError error = null;
         for (String reference : references)
             if (entity.getField(reference) == null) {
+                error = new ChangeOrderStateError();
                 error.setMessage("orders.order.orderStates.fieldRequired");
                 error.setReferenceToField(reference);
                 return error;
             }
         return null;
     }
-
 }
