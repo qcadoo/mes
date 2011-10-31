b438ac3ab5b6f5bc2280b87953d0cbfa550f618e
Bartek Kubala <bartosz.kubala@gmail.com>
2011-10-31 09:37:56 +0100

    SC#QCADOOMES-404 add new plugin

---
+++ mes-plugins/mes-plugins-basic-production-counting/src/main/java/com/qcadoo/mes/basicProductionCounting/BasicProductionCountingService.java
@@ -0,0 +1,147 @@
+/**
+ * ***************************************************************************
+ * Copyright (c) 2010 Qcadoo Limited
+ * Project: Qcadoo MES
+ * Version: 0.4.8
+ *
+ * This file is part of Qcadoo.
+ *
+ * Qcadoo is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation; either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * ***************************************************************************
+ */
+package com.qcadoo.mes.basicProductionCounting;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.qcadoo.mes.materialRequirements.api.MaterialRequirementReportDataService;
+import com.qcadoo.model.api.DataDefinitionService;
+import com.qcadoo.model.api.Entity;
+import com.qcadoo.model.api.search.SearchRestrictions;
+import com.qcadoo.view.api.ComponentState;
+import com.qcadoo.view.api.ViewDefinitionState;
+import com.qcadoo.view.api.components.FieldComponent;
+import com.qcadoo.view.api.components.FormComponent;
+
+@Service
+public class BasicProductionCountingService {
+
+    @Autowired
+    private DataDefinitionService dataDefinitionService;
+
+    @Autowired
+    private MaterialRequirementReportDataService materialRequirementReportDataService;
+
+    public void generateProducedProducts(final ViewDefinitionState state) {
+        String orderNumber = (String) state.getComponentByReference("number").getFieldValue();
+
+        if (orderNumber != null) {
+            Entity order = dataDefinitionService.get("orders", "order").find().add(SearchRestrictions.eq("number", orderNumber))
+                    .uniqueResult();
+            if (order != null)
+                updateProductsForOrder(order, "basicProductionCounting", "basicProductionCounting", "plannedQuantity");
+        }
+    }
+
+    private void updateProductsForOrder(Entity order, String pluginName, String modelName, String fieldName) {
+        List<Entity> orderList = Arrays.asList(order);
+
+        Map<Entity, BigDecimal> products = materialRequirementReportDataService.getQuantitiesForOrdersTechnologyProducts(
+                orderList, true);
+
+        List<Entity> producedProducts = dataDefinitionService.get(pluginName, modelName).find()
+                .add(SearchRestrictions.belongsTo("order", order)).list().getEntities();
+
+        for (Entry<Entity, BigDecimal> product : products.entrySet()) {
+            Entity foundProduct = null;
+
+            for (Entity producedProduct : producedProducts) {
+                if (producedProduct.getBelongsToField("product").getId().equals(product.getKey().getId())) {
+                    foundProduct = producedProduct;
+                    break;
+                }
+            }
+
+            if (foundProduct == null) {
+                Entity newProduct = dataDefinitionService.get(pluginName, modelName).create();
+                newProduct.setField("order", order);
+                newProduct.setField("product", product.getKey());
+                newProduct.setField(fieldName, product.getValue());
+                newProduct.getDataDefinition().save(newProduct);
+                if (!newProduct.isValid())
+                    throw new IllegalStateException("new product entity is invalid  " + product.getValue() + "\n\n\n");
+            } else {
+                BigDecimal plannedQuantity = (BigDecimal) foundProduct.getField(fieldName);
+                // if (plannedQuantity.compareTo(product.getValue()) != 0) {
+                if (plannedQuantity != product.getValue()) {
+                    foundProduct.setField(fieldName, product.getValue());
+                    foundProduct.getDataDefinition().save(foundProduct);
+                }
+            }
+        }
+        producedProducts = dataDefinitionService.get(pluginName, modelName).find()
+                .add(SearchRestrictions.belongsTo("order", order)).list().getEntities();
+
+        for (Entity producedProduct : producedProducts) {
+            boolean found = false;
+            for (Entry<Entity, BigDecimal> product : products.entrySet()) {
+                if (producedProduct.getBelongsToField("product").equals(product.getKey())) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+                dataDefinitionService.get(pluginName, modelName).delete(producedProduct.getId());
+        }
+    }
+
+    public void showProductionCounting(final ViewDefinitionState viewDefinitionState, final ComponentState triggerState,
+            final String[] args) {
+        Long orderId = (Long) triggerState.getFieldValue();
+
+        if (orderId == null) {
+            return;
+        }
+
+        JSONObject json = new JSONObject();
+        try {
+            json.put("order.id", orderId);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        String url = "../page/basicProductionCounting/basicProductionCountingList.html?context=" + json.toString();
+        viewDefinitionState.redirectTo(url, false, true);
+    }
+
+    public void getProductNameFromCounting(final ViewDefinitionState view) {
+        FieldComponent productField = (FieldComponent) view.getComponentByReference("product");
+        FormComponent formComponent = (FormComponent) view.getComponentByReference("form");
+        Entity basicProductionCountingEntity = formComponent.getEntity();
+        basicProductionCountingEntity = basicProductionCountingEntity.getDataDefinition().get(
+                basicProductionCountingEntity.getId());
+        Entity product = basicProductionCountingEntity.getBelongsToField("product");
+        productField.setFieldValue(product.getField("name"));
+        productField.requestComponentUpdateState();
+    }
+}
