61ee33955c6f1574ae814bd2dc3932df85e8f6a9
Krzysztof Nadolski <krzysztof.nadolski@qcadoo.com>
2011-11-16 16:57:47 +0100

    RF# add some sonar fixes

--- mes-plugins/mes-plugins-orders/src/main/java/com/qcadoo/mes/orders/states/OrderStatesChangingService.java
+++ mes-plugins/mes-plugins-orders/src/main/java/com/qcadoo/mes/orders/states/OrderStatesChangingService.java
@@ -29,11 +29,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.qcadoo.localization.api.TranslationService;
 import com.qcadoo.mes.orders.constants.OrderStates;
 import com.qcadoo.model.api.Entity;
 
 @Service
 public class OrderStatesChangingService {
+
+    @Autowired
+    TranslationService translationService;
 
     @Autowired
     OrderStateValidationService orderStateValidationService;
