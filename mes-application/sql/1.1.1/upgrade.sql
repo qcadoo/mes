d699c6d583e0e00b13f9e564c9e1ad70cd5b11b8
Lukasz Poniewierski <lukasz.poniewierski@gmail.com>
2011-12-09 16:27:02 +0100

    RF Added update_view procedure.

--- mes-application/sql/1.1.1/upgrade.sql
+++ mes-application/sql/1.1.1/upgrade.sql
@@ -23,3 +23,43 @@ COMMIT;
 -- end
 
 ALTER TABLE basic_currency DROP COLUMN isactive;
+
+UPDATE basic_company SET owner = true;
+
+UPDATE qcadooview_view SET name='company' WHERE name='companyDetails';
+
+UPDATE qcadooview_item SET name='company' WHERE name='companyDetails';
+
+CREATE OR REPLACE FUNCTION update_view() RETURNS INTEGER AS 
+'
+	DECLARE
+		tenant RECORD;  
+	BEGIN  
+		FOR tenant IN SELECT * FROM qcadootenant_tenant LOOP  
+			INSERT INTO qcadooview_view (id, pluginidentifier, name, view, tenantid) 
+			VALUES (
+				nextval(''hibernate_sequence''), 
+				''basic'', 
+				''companiesList'', 
+				''companiesList'', 
+				tenant."id"
+			);
+
+			INSERT INTO qcadooview_item (id, pluginidentifier, name, active, category_id, view_id, succession, tenantid) 
+			VALUES (
+				nextval(''hibernate_sequence''), 
+				''basic'', 
+				''companies'', 
+				true,  
+				(SELECT id FROM qcadooview_category WHERE tenantid = tenant."id" AND name=''basic''), 
+				(SELECT id FROM qcadooview_view WHERE tenantid = tenant."id" AND name=''companiesList''), 
+				(SELECT COUNT(*) FROM qcadooview_item WHERE tenantid = tenant."id" AND category_id = (SELECT id FROM qcadooview_category WHERE tenantid = tenant."id" AND name=''basic'')) + 1,
+				tenant."id"
+			); 
+		END LOOP;
+
+		RETURN 1; 
+	END;  
+' 
+LANGUAGE 'plpgsql';
+SELECT * FROM update_view();
