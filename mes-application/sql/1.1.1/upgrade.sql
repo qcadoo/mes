afc72e17153c0fb48c58b3ef367a16cd135e00d7
Lukasz Poniewierski <lukasz.poniewierski@gmail.com>
2011-12-15 14:44:01 +0100

    RF Updated sql - added columns for advanced genealogy parameters.

--- mes-application/sql/1.1.1/upgrade.sql
+++ mes-application/sql/1.1.1/upgrade.sql
@@ -72,16 +72,36 @@ UPDATE basic_company SET number = '1';
 
 -- end
 
--- Table: basic_product
--- changed 14.12.2011
 
-ALTER TABLE basic_product DROP COLUMN productbatchrequired;
+-- Table: basic_parameter
+-- changed 15.12.2011
+ALTER TABLE basic_parameter
+        ADD COLUMN trackingrecordforordertreatment character varying(255) DEFAULT '01duringProduction'::character varying,
+        ADD COLUMN batchnumberrequiredproducts boolean,
+        ADD COLUMN batchnumberuniqueness character varying(255) DEFAULT '01globally'::character varying;
 
 -- end
 
--- Table: technologies_operationproductincomponent
--- changed 14.12.2011
+-- Table: technologies_technology
+-- changed 15.12.2011
+ALTER TABLE technologies_technology
+        ADD COLUMN technologybatchrequired boolean;
 
-ALTER TABLE technologies_operationproductincomponent ADD COLUMN productbatchrequired boolean;
+-- end
+
+        
+-- Table: technologies_operationproductincomponent
+-- changed 15.12.2011
+ALTER TABLE technologies_operationproductincomponent 
+		ADD COLUMN productbatchrequired boolean;
+
+-- end
+
+
+-- Table: orders_order
+-- changed 15.12.2011
+        
+ALTER TABLE orders_order
+        ADD COLUMN trackingrecordtreatment character varying(255) DEFAULT '01duringProduction'::character varying;
 
 -- end
