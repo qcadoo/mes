484ee8c7109c43f5fc496ae156e435b88f1de8ae
Marcin Kubala <marcin.kubala@qcadoo.com>
2012-01-02 16:17:19 +0100

    SC#QCADOOMES-535 New GUI for work plan generation

--- mes-plugins/mes-plugins-work-plans/src/main/java/com/qcadoo/mes/workPlans/WorkPlanService.java
+++ mes-plugins/mes-plugins-work-plans/src/main/java/com/qcadoo/mes/workPlans/WorkPlanService.java
@@ -1,69 +1,20 @@
-/**
- * ***************************************************************************
- * Copyright (c) 2010 Qcadoo Limited
- * Project: Qcadoo MES
- * Version: 1.1.1
- *
- * This file is part of Qcadoo.
- *
- * Qcadoo is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published
- * by the Free Software Foundation; either version 3 of the License,
- * or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty
- * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- * ***************************************************************************
- */
 package com.qcadoo.mes.workPlans;
 
 import java.io.IOException;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.Set;
 
 import com.lowagie.text.DocumentException;
-import com.qcadoo.mes.basic.constants.BasicConstants;
-import com.qcadoo.mes.workPlans.constants.WorkPlanType;
-import com.qcadoo.mes.workPlans.constants.WorkPlansConstants;
-import com.qcadoo.mes.workPlans.print.WorkPlanPdfService2;
-import com.qcadoo.model.api.DataDefinition;
-import com.qcadoo.model.api.DataDefinitionService;
 import com.qcadoo.model.api.Entity;
-import com.qcadoo.model.api.search.SearchRestrictions;
 import com.qcadoo.view.api.ComponentState;
 
-@Service
-public final class WorkPlanService {
+public interface WorkPlanService {
 
-    @Autowired
-    private DataDefinitionService dataDefinitionService;
+    void generateWorkPlanDocuments(final ComponentState state, final Entity workPlan) throws IOException, DocumentException;
 
-    @Autowired
-    private WorkPlanPdfService2 workPlanPdfService;
+    Entity generateWorkPlanEntity(final List<Entity> orders);
 
-    @Transactional
-    void generateWorkPlanDocuments(final ComponentState state, final Entity workPlan) throws IOException, DocumentException {
-        Entity company = dataDefinitionService.get(BasicConstants.PLUGIN_IDENTIFIER, BasicConstants.MODEL_COMPANY).find()
-                .add(SearchRestrictions.eq("owner", true)).setMaxResults(1).uniqueResult();
-        // FIXME mici thats just temporary
-        workPlan.setField("type", WorkPlanType.ALL_OPERATIONS.getStringValue());
-        Entity workPlanWithFilename = workPlanPdfService.updateFileName(workPlan, "Work_plan");
-        workPlanPdfService.generateDocument(workPlanWithFilename, company, state.getLocale());
-    }
+    List<Entity> getSelectedOrders(final Set<Long> selectedOrderIds);
 
-    public Entity getWorkPlan(final Long workPlanId) {
-        return getWorkPlanDataDefinition().get(workPlanId);
-    }
-
-    private DataDefinition getWorkPlanDataDefinition() {
-        return dataDefinitionService.get(WorkPlansConstants.PLUGIN_IDENTIFIER, WorkPlansConstants.MODEL_WORK_PLAN);
-    }
+    Entity getWorkPlan(final Long workPlanId);
 }
