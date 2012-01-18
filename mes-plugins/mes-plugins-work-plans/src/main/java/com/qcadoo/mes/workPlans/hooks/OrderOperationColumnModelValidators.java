c343d6929283b604156723df9fd6e6dbbc91e948
Lukasz Poniewierski <lukasz.poniewierski@qcadoo.com>
2012-01-18 14:58:09 +0100

    RF Refactored models and hooks, fixed problem with fieldCode.

---
+++ mes-plugins/mes-plugins-work-plans/src/main/java/com/qcadoo/mes/workPlans/hooks/OrderOperationColumnModelValidators.java
@@ -0,0 +1,48 @@
+/**
+ * ***************************************************************************
+ * Copyright (c) 2010 Qcadoo Limited
+ * Project: Qcadoo Framework
+ * Version: 1.1.1
+ *
+ * This file is part of Qcadoo.
+ *
+ * Qcadoo is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation; either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * ***************************************************************************
+ */
+package com.qcadoo.mes.workPlans.hooks;
+
+import org.springframework.stereotype.Service;
+
+import com.qcadoo.model.api.DataDefinition;
+import com.qcadoo.model.api.Entity;
+
+@Service
+public class OrderOperationColumnModelValidators {
+
+    public final boolean checkIfColumnForInputProductsIsNotAlreadyUsed(final DataDefinition inputColumnDD,
+            final Entity inputColumn) {
+
+        return new ValidatorServiceImpl().checkIfColumnForProductsIsNotUsed(inputColumnDD, inputColumn, "orderOperationColumn",
+                "columnForInputProducts", "orderOperatonInputColumns");
+    }
+
+    public final boolean checkIfColumnForOutputProductsIsNotAlreadyUsed(final DataDefinition outputColumnDD,
+            final Entity outputColumn) {
+
+        return new ValidatorServiceImpl().checkIfColumnForProductsIsNotUsed(outputColumnDD, outputColumn, "orderOperationColumn",
+                "columnForOutputProducts", "orderOperationOutputColumns");
+    }
+
+}
