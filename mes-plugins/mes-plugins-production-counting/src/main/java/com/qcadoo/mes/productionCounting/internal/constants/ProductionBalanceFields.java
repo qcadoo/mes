6bdc8526fc1877afce5afc98eab57d56a6bad684
Alina Brauer <alina.brauer@qcadoo.com>
2012-03-07 14:14:30 +0100

    SC#QCADOOMES-741 changed report layout

---
+++ mes-plugins/mes-plugins-production-counting/src/main/java/com/qcadoo/mes/productionCounting/internal/constants/ProductionBalanceFields.java
@@ -0,0 +1,30 @@
+package com.qcadoo.mes.productionCounting.internal.constants;
+
+public class ProductionBalanceFields {
+
+    public static final String GENERATED = "generated";
+
+    public static final String ORDER = "order";
+
+    public static final String PRODUCT = "product";
+
+    public static final String NAME = "name";
+
+    public static final String DATE = "date";
+
+    public static final String WORKER = "worker";
+
+    public static final String RECORDS_NUMBER = "recordsNumber";
+
+    public static final String DESCRIPTION = "description";
+
+    public static final String FILE_NAME = "fileName";
+
+    public static final String PRINT_OPERATION_NORMS = "printOperationNorms";
+
+    public static final String CALCULATE_OPERATION_COST_MODE = "calculateOperationCostsMode";
+
+    public static final String INCLUDE_TPZ = "includeTPZ";
+
+    public static final String INCLUDE_ADDITIONAL_TIME = "includeAdditionalTime";
+}
