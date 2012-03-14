702f697fe21dfe28f42655d788eed42ad34564a8
Lukasz Poniewierski <lukasz.poniewierski@qcadoo.com>
2012-03-14 20:15:05 +0100

    SC#QCADOOMES-740 Added new constants.

---
+++ mes-plugins/mes-plugins-production-counting-with-costs/src/main/java/com/qcadoo/mes/productionCountingWithCosts/constants/ProductionCountingWithCostsConstants.java
@@ -0,0 +1,10 @@
+package com.qcadoo.mes.productionCountingWithCosts.constants;
+
+public interface ProductionCountingWithCostsConstants {
+
+    String PLUGIN_IDENTIFIER = "productionCountingWithCosts";
+
+    String MODEL_ORDER_OPERATION_PRODUCT_IN_COMPONENT = "orderOperationProductInComponent";
+
+    String MODEL_OPERATION_COST_COMPONENT = "operationCostComponent";
+}
