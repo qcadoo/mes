9215a64cf300abdda579ca36d71e4c9912d85ac5
Lukasz Poniewierski <lukasz.poniewierski@qcadoo.com>
2012-03-14 13:40:43 +0100

    RF Fixed some checkstyle problems.

--- mes-plugins/mes-plugins-technologies/src/main/java/com/qcadoo/mes/technologies/constants/TechnologyFields.java
+++ mes-plugins/mes-plugins-technologies/src/main/java/com/qcadoo/mes/technologies/constants/TechnologyFields.java
@@ -2,20 +2,20 @@ package com.qcadoo.mes.technologies.constants;
 
 public class TechnologyFields {
 
-    public final static String NUMBER = "number";
+    public static final String NUMBER = "number";
 
-    public final static String NAME = "name";
+    public static final String NAME = "name";
 
-    public final static String PRODUCT = "product";
+    public static final String PRODUCT = "product";
 
-    public final static String MASTER = "master";
+    public static final String MASTER = "master";
 
-    public final static String DESCRIPTION = "description";
+    public static final String DESCRIPTION = "description";
 
-    public final static String OPERATION_COMPONENTS = "operationComponents";
+    public static final String OPERATION_COMPONENTS = "operationComponents";
 
-    public final static String STATE = "state";
+    public static final String STATE = "state";
 
-    public final static String LOGGINGS = "loggings";
+    public static final String LOGGINGS = "loggings";
 
 }
