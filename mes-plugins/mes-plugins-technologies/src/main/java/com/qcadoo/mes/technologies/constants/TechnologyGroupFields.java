4ca0844b34e4dcb9afbaeb12e6bdcd18c8e6b7ac
Lukasz Poniewierski <lukasz.poniewierski@qcadoo.com>
2012-04-04 12:32:28 +0200

    SC#QCADOOMES-806 Added groups to technologies.

--- mes-plugins/mes-plugins-technologies/src/main/java/com/qcadoo/mes/technologies/constants/TechnologyGroupFields.java
+++ mes-plugins/mes-plugins-technologies/src/main/java/com/qcadoo/mes/technologies/constants/TechnologyGroupFields.java
@@ -23,24 +23,12 @@
  */
 package com.qcadoo.mes.technologies.constants;
 
-public class TechnologyFields {
+public class TechnologyGroupFields {
 
     public static final String NUMBER = "number";
 
     public static final String NAME = "name";
 
-    public static final String PRODUCT = "product";
-
-    public static final String MASTER = "master";
-
-    public static final String DESCRIPTION = "description";
-
-    public static final String OPERATION_COMPONENTS = "operationComponents";
-
-    public static final String STATE = "state";
-
-    public static final String LOGGINGS = "loggings";
-
-    public static final String TECHNOLOGY_GROUP = "technologyGroup";
+    public static final String TECHNOLOGIES = "technologies";
 
 }
