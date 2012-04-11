d826bd7a1ed64349e8b672306300102f1ace7fd2
Lukasz Poniewierski <lukasz.poniewierski@qcadoo.com>
2012-04-11 10:40:37 +0200

    RF Fixed some checkstyle problems.

--- mes-plugins/mes-plugins-technologies/src/main/java/com/qcadoo/mes/technologies/constants/TechnologyOperationComponentFields.java
+++ mes-plugins/mes-plugins-technologies/src/main/java/com/qcadoo/mes/technologies/constants/TechnologyOperationComponentFields.java
@@ -2,31 +2,31 @@ package com.qcadoo.mes.technologies.constants;
 
 public class TechnologyOperationComponentFields {
 
-    public final static String NODE_NUMBER = "nodeNumber";
+    public static final String NODE_NUMBER = "nodeNumber";
 
-    public final static String TECHNOLOGY = "technology";
+    public static final String TECHNOLOGY = "technology";
 
-    public final static String CHILDREN = "children";
+    public static final String CHILDREN = "children";
 
-    public final static String PARENT = "parent";
+    public static final String PARENT = "parent";
 
-    public final static String ENTITY_TYPE = "entityType";
+    public static final String ENTITY_TYPE = "entityType";
 
-    public final static String OPERATION = "operation";
+    public static final String OPERATION = "operation";
 
-    public final static String OPERATION_PRODUCT_IN_COMPONENTS = "operationProductInComponents";
+    public static final String OPERATION_PRODUCT_IN_COMPONENTS = "operationProductInComponents";
 
-    public final static String OPERATION_PRODUCT_OUT_COMPONENTS = "operationProductOutComponents";
+    public static final String OPERATION_PRODUCT_OUT_COMPONENTS = "operationProductOutComponents";
 
-    public final static String REFERENCETECHNOLOGY = "referenceTechnology";
+    public static final String REFERENCETECHNOLOGY = "referenceTechnology";
 
-    public final static String REFERENCEMODE = "referenceMode";
+    public static final String REFERENCEMODE = "referenceMode";
 
-    public final static String PRIORITY = "priority";
+    public static final String PRIORITY = "priority";
 
-    public final static String ATTACHMENT = "attachment";
+    public static final String ATTACHMENT = "attachment";
 
-    public final static String ARE_PRODUCT_QUANTITIES_DIVISIBLE = "areProductQuantitiesDivisible";
+    public static final String ARE_PRODUCT_QUANTITIES_DIVISIBLE = "areProductQuantitiesDivisible";
 
-    public final static String IS_TJ_DIVISIBLE = "isTjDivisible";
+    public static final String IS_TJ_DIVISIBLE = "isTjDivisible";
 }
