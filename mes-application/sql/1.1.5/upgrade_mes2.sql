9ba6c831830ee90f33b6adefdc3f59ddfabb1cf1
Alina Brauer <alina.brauer@qcadoo.com>
2012-04-16 13:35:11 +0200

    RF#changed script

--- mes-application/sql/1.1.5/upgrade_mes2.sql
+++ mes-application/sql/1.1.5/upgrade_mes2.sql
@@ -187,13 +187,6 @@ ALTER TABLE technologies_technologyinstanceoperationcomponent ADD COLUMN quantit
 
 -- end
 
-
--- Table: costnormsformaterials_calculationoperationcomponent
--- changed: 16.04.2012
-ALTER TABLE costnormsformaterials_calculationoperationcomponent  RENAME TO costnormsforoperation_calculationoperationcomponent;
-
---end
-
 --Table : costnormsforoperation_calculationoperationcomponent
 -- changed: 16.04.2012
 ALTER TABLE costnormsforoperation_calculationoperationcomponent ADD COLUMN areProductQuantitiesDivisible boolean DEFAULT true;
