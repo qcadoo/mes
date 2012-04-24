bd925be5b713c9b951223d2fbe588623b8430278
Marcin Kubala <marcin.kubala@qcadoo.com>
2012-04-24 21:40:24 +0200

    DF fix default values in database

--- mes-application/sql/1.1.5/upgrade_mes3.sql
+++ mes-application/sql/1.1.5/upgrade_mes3.sql
@@ -1,24 +1,29 @@
+ALTER TABLE costnormsforoperation_calculationoperationcomponent ALTER COLUMN areproductquantitiesdivisible SET DEFAULT false;
+ALTER TABLE costnormsforoperation_calculationoperationcomponent ALTER COLUMN istjdivisible SET DEFAULT false;
 UPDATE 
 	costnormsforoperation_calculationoperationcomponent
 SET 
 	isTjDivisible=false, 
 	areProductQuantitiesDivisible=false;
 
-
+ALTER TABLE technologies_technologyinstanceoperationcomponent ALTER COLUMN areproductquantitiesdivisible SET DEFAULT false;
+ALTER TABLE technologies_technologyinstanceoperationcomponent ALTER COLUMN istjdivisible SET DEFAULT false;
 UPDATE 
 	technologies_technologyinstanceoperationcomponent 
 SET 
 	isTjDivisible=false, 
 	areProductQuantitiesDivisible=false;
 
-	
+ALTER TABLE technologies_technologyoperationcomponent ALTER COLUMN areproductquantitiesdivisible SET DEFAULT false;
+ALTER TABLE technologies_technologyoperationcomponent ALTER COLUMN istjdivisible SET DEFAULT false;
 UPDATE 
 	technologies_technologyoperationcomponent
 SET 
 	isTjDivisible=false, 
 	areProductQuantitiesDivisible=false;
 
-	
+ALTER TABLE technologies_operation ALTER COLUMN areproductquantitiesdivisible SET DEFAULT false;
+ALTER TABLE technologies_operation ALTER COLUMN istjdivisible SET DEFAULT false;
 UPDATE 
 	technologies_operation 
 SET 
