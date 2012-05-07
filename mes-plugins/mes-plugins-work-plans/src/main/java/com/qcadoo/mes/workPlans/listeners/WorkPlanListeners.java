a8344d4bf136578c1e18a159f5bfdb5848716eda
Maciej Szmatloch <maciej.szmatloch@qcadoo.com>
2012-05-07 08:58:42 +0200

    SC#QCADOOMES-897 MGM-2 - Split parameters view

---
+++ mes-plugins/mes-plugins-work-plans/src/main/java/com/qcadoo/mes/workPlans/listeners/WorkPlanListeners.java
@@ -0,0 +1,44 @@
+/**
+ * ***************************************************************************
+ * Copyright (c) 2010 Qcadoo Limited
+ * Project: Qcadoo MES
+ * Version: 1.1.5
+ *
+ * This file is part of Qcadoo.
+ *
+ * Qcadoo is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation; either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * ***************************************************************************
+ */
+
+package com.qcadoo.mes.workPlans.listeners;
+
+import org.springframework.stereotype.Service;
+
+import com.qcadoo.view.api.ComponentState;
+import com.qcadoo.view.api.ViewDefinitionState;
+
+@Service
+public class WorkPlanListeners {
+
+    public void redirectToWorkPlanParameters(final ViewDefinitionState viewDefinitionState, final ComponentState triggerState,
+            final String[] args) {
+        Long orderId = (Long) triggerState.getFieldValue();
+
+        if (orderId != null) {
+            String url = "../page/workPlans/workPlanParameters.html?context={\"form.id\":\"" + orderId + "\"}";
+            viewDefinitionState.redirectTo(url, false, true);
+        }
+    }
+}
