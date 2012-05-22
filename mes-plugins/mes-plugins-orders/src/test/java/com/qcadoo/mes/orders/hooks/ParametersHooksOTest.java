d03e3e9a6940c5d5d85426a2eb45bfb9dce3cc51
Lukasz Poniewierski <lukasz.poniewierski@qcadoo.com>
2012-05-22 12:22:07 +0200

    SC#QCADOOMES-911 Added tests for hooks and listeners.

---
+++ mes-plugins/mes-plugins-orders/src/test/java/com/qcadoo/mes/orders/hooks/ParametersHooksOTest.java
@@ -0,0 +1,60 @@
+package com.qcadoo.mes.orders.hooks;
+
+import static com.qcadoo.mes.orders.constants.ParameterFieldsO.DELAYED_EFFECTIVE_DATE_FROM_TIME;
+import static com.qcadoo.mes.orders.constants.ParameterFieldsO.DELAYED_EFFECTIVE_DATE_TO_TIME;
+import static com.qcadoo.mes.orders.constants.ParameterFieldsO.EARLIER_EFFECTIVE_DATE_FROM_TIME;
+import static com.qcadoo.mes.orders.constants.ParameterFieldsO.EARLIER_EFFECTIVE_DATE_TO_TIME;
+import static com.qcadoo.mes.orders.constants.ParameterFieldsO.REASON_NEEDED_WHEN_DELAYED_EFFECTIVE_DATE_FROM;
+import static com.qcadoo.mes.orders.constants.ParameterFieldsO.REASON_NEEDED_WHEN_DELAYED_EFFECTIVE_DATE_TO;
+import static com.qcadoo.mes.orders.constants.ParameterFieldsO.REASON_NEEDED_WHEN_EARLIER_EFFECTIVE_DATE_FROM;
+import static com.qcadoo.mes.orders.constants.ParameterFieldsO.REASON_NEEDED_WHEN_EARLIER_EFFECTIVE_DATE_TO;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.util.ReflectionTestUtils.setField;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.qcadoo.mes.orders.OrderService;
+import com.qcadoo.view.api.ViewDefinitionState;
+
+public class ParametersHooksOTest {
+
+    @Mock
+    private ViewDefinitionState viewDefinitionState;
+
+    @Mock
+    private OrderService orderService;
+
+    private ParametersHooksO parametersHooksO;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+
+        parametersHooksO = new ParametersHooksO();
+
+        setField(parametersHooksO, "orderService", orderService);
+
+    }
+
+    @Test
+    public void shouldShowTimeFields() {
+        // given
+
+        // when
+        parametersHooksO.showTimeFields(viewDefinitionState);
+
+        // then
+        verify(orderService).changeFieldState(viewDefinitionState, REASON_NEEDED_WHEN_DELAYED_EFFECTIVE_DATE_FROM,
+                DELAYED_EFFECTIVE_DATE_FROM_TIME);
+        verify(orderService).changeFieldState(viewDefinitionState, REASON_NEEDED_WHEN_EARLIER_EFFECTIVE_DATE_FROM,
+                EARLIER_EFFECTIVE_DATE_FROM_TIME);
+        verify(orderService).changeFieldState(viewDefinitionState, REASON_NEEDED_WHEN_DELAYED_EFFECTIVE_DATE_TO,
+                DELAYED_EFFECTIVE_DATE_TO_TIME);
+        verify(orderService).changeFieldState(viewDefinitionState, REASON_NEEDED_WHEN_EARLIER_EFFECTIVE_DATE_TO,
+                EARLIER_EFFECTIVE_DATE_TO_TIME);
+    }
+
+}
