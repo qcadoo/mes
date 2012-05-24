38f5385cfd4b21d8eee66cf0263cf38977fd8430
Michal Ciebiada <michal.ciebiada@qcadoo.com>
2012-05-24 09:57:12 +0200

    SC#QCADOOMES-912

--- mes-plugins/mes-plugins-production-per-shift/src/main/java/com/qcadoo/mes/productionPerShift/listeners/ProductionPerShiftListeners.java
+++ mes-plugins/mes-plugins-production-per-shift/src/main/java/com/qcadoo/mes/productionPerShift/listeners/ProductionPerShiftListeners.java
@@ -2,14 +2,24 @@ package com.qcadoo.mes.productionPerShift.listeners;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.google.common.collect.Maps;
+import com.qcadoo.mes.technologies.TechnologyService;
+import com.qcadoo.model.api.DataDefinitionService;
+import com.qcadoo.model.api.Entity;
 import com.qcadoo.view.api.ComponentState;
 import com.qcadoo.view.api.ViewDefinitionState;
 
 @Service
 public class ProductionPerShiftListeners {
+
+    @Autowired
+    private DataDefinitionService dataDefinitionService;
+
+    @Autowired
+    private TechnologyService technologyService;
 
     public void redirectToProductionPerShift(final ViewDefinitionState viewState, final ComponentState componentState,
             final String[] args) {
@@ -20,9 +30,25 @@ public class ProductionPerShiftListeners {
         }
 
         Map<String, Object> parameters = Maps.newHashMap();
-        parameters.put("order.id", orderId);
+        parameters.put("form.id", orderId);
 
         String url = "../page/productionPerShift/productionPerShiftView.html";
         viewState.redirectTo(url, false, true, parameters);
+    }
+
+    public void fillProducedField(final ViewDefinitionState viewState, final ComponentState componentState, final String[] args) {
+        ComponentState operationLookup = viewState.getComponentByReference("productionPerShiftOperation");
+        Long id = (Long) operationLookup.getFieldValue();
+
+        Entity tioc = dataDefinitionService.get("technologies", "technologyInstanceOperationComponent").get(id);
+
+        Entity toc = tioc.getBelongsToField("technologyOperationComponent");
+
+        Entity prodComp = technologyService.getMainOutputProductComponent(toc);
+        Entity prod = prodComp.getBelongsToField("product");
+
+        ComponentState producesInput = viewState.getComponentByReference("produces");
+        producesInput.setFieldValue(prod.getStringField("name"));
+
     }
 }
