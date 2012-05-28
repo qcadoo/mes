c48567deabdec6fd767ada54592776ff1efb9fac
Lukasz Poniewierski <lukasz.poniewierski@qcadoo.com>
2012-05-28 15:52:02 +0200

    SC#QCADOOMES-918 Added views for line changeovers norms for orders.

---
+++ mes-plugins/mes-plugins-line-changeover-norms-for-orders/src/main/java/com/qcadoo/mes/lineChangeoverNormsForOrders/listeners/OrderDetailsListenersLCNFO.java
@@ -0,0 +1,50 @@
+/**
+ * ***************************************************************************
+ * Copyright (c) 2010 Qcadoo Limited
+ * Project: Qcadoo MES
+ * Version: 1.1.6
+ *
+ * This file is part of Qcadoo.
+ *
+ * Qcadoo is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation; either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * ***************************************************************************
+ */
+package com.qcadoo.mes.lineChangeoverNormsForOrders.listeners;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.google.common.collect.Maps;
+import com.qcadoo.view.api.ComponentState;
+import com.qcadoo.view.api.ViewDefinitionState;
+
+@Service
+public class OrderDetailsListenersLCNFO {
+
+    public final void showChangeover(final ViewDefinitionState viewState, final ComponentState componentState, final String[] args) {
+        Long orderId = (Long) componentState.getFieldValue();
+
+        if (orderId == null) {
+            return;
+        }
+
+        Map<String, Object> parameters = Maps.newHashMap();
+        parameters.put("form.id", orderId);
+
+        String url = "../page/lineChangeoverNormsForOrders/lineChangeoverNormsForOrderDetails.html";
+        viewState.redirectTo(url, false, true, parameters);
+    }
+}
