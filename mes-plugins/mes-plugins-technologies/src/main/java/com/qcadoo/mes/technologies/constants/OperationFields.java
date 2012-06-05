63588420b88774a81b178d9307e81c59c78972b1
Lukasz Poniewierski <lukasz.poniewierski@qcadoo.com>
2012-06-05 12:08:04 +0200

    SC#QCADOOMES-930 Added constants for operation fields.

---
+++ mes-plugins/mes-plugins-technologies/src/main/java/com/qcadoo/mes/technologies/constants/OperationFields.java
@@ -0,0 +1,52 @@
+/**
+ * ***************************************************************************
+ * Copyright (c) 2010 Qcadoo Limited
+ * Project: Qcadoo MES
+ * Version: 1.1.6
+ *
+ * This file is part of Qcadoo.
+ *
+ * Qcadoo is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation; either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * ***************************************************************************
+ */
+package com.qcadoo.mes.technologies.constants;
+
+public final class OperationFields {
+
+    private OperationFields() {
+
+    }
+
+    public static final String NUMBER = "number";
+
+    public static final String NAME = "name";
+
+    public static final String COMMENT = "comment";
+
+    public static final String WORKSTATION_TYPE = "workstationType";
+
+    public static final String OPERATION_COMPONENTS = "operationComponents";
+
+    public static final String ATTACHMENT = "attachment";
+
+    public static final String ARE_PRODUCT_QUANTITIES_DIVISIBLE = "areProductQuantitiesDivisible";
+
+    public static final String IS_TJ_DIVISIBLE = "isTjDivisible";
+
+    public static final String PRODUCT_IN_COMPONENTS = "productInComponents";
+
+    public static final String PRODUCT_OUT_COMPONENTS = "productOutComponents";
+
+}
