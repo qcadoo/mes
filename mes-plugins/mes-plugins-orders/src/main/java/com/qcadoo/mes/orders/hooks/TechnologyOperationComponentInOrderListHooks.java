61b87c86dba16db2120c6335afc4986d7bd63269
Alina Brauer <alina.brauer@qcadoo.com>
2012-07-13 09:54:51 +0200

    SC#QCADOOMES-944 changed view for technology parameters in order

---
+++ mes-plugins/mes-plugins-orders/src/main/java/com/qcadoo/mes/orders/hooks/TechnologyOperationComponentInOrderListHooks.java
@@ -0,0 +1,31 @@
+package com.qcadoo.mes.orders.hooks;
+
+import static com.qcadoo.mes.orders.constants.OrdersConstants.PLUGIN_IDENTIFIER;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.qcadoo.mes.orders.constants.OrdersConstants;
+import com.qcadoo.model.api.DataDefinitionService;
+import com.qcadoo.model.api.Entity;
+import com.qcadoo.view.api.ViewDefinitionState;
+import com.qcadoo.view.api.components.FieldComponent;
+import com.qcadoo.view.api.components.FormComponent;
+
+@Service
+public class TechnologyOperationComponentInOrderListHooks {
+
+    private static final String TECHNOLOGY = "technology";
+
+    @Autowired
+    private DataDefinitionService dataDefinitionService;
+
+    public void setTechnologyNumber(final ViewDefinitionState view) {
+        Entity order = dataDefinitionService.get(PLUGIN_IDENTIFIER, OrdersConstants.MODEL_ORDER).get(
+                ((FormComponent) view.getComponentByReference("form")).getEntityId());
+        FieldComponent technology = (FieldComponent) view.getComponentByReference(TECHNOLOGY);
+
+        technology.setFieldValue(order.getBelongsToField(TECHNOLOGY).getStringField("number"));
+        technology.requestComponentUpdateState();
+    }
+}
