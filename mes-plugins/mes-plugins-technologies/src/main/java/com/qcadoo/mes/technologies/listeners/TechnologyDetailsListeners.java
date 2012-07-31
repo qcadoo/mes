7950bb348ae26b0f10898038c10c29cd3df77c9c
Marcin Kubala <marcin.kubala@qcadoo.com>
2012-07-31 12:15:50 +0200

    RF TechnologyDetailsListeners.setGridEditable(..)

--- mes-plugins/mes-plugins-technologies/src/main/java/com/qcadoo/mes/technologies/listeners/TechnologyDetailsListeners.java
+++ mes-plugins/mes-plugins-technologies/src/main/java/com/qcadoo/mes/technologies/listeners/TechnologyDetailsListeners.java
@@ -2,24 +2,26 @@ package com.qcadoo.mes.technologies.listeners;
 
 import org.springframework.stereotype.Service;
 
+import com.google.common.collect.Sets;
 import com.qcadoo.view.api.ComponentState;
 import com.qcadoo.view.api.ViewDefinitionState;
-import com.qcadoo.view.api.components.GridComponent;
 import com.qcadoo.view.api.components.TreeComponent;
 
 @Service
 public class TechnologyDetailsListeners {
 
+    private static final String OUT_PRODUCTS_REFERENCE = "outProducts";
+
+    private static final String IN_PRODUCTS_REFERENCE = "inProducts";
+
+    private static final String TECHNOLOGY_TREE_REFERENCE = "technologyTree";
+
     public void setGridEditable(final ViewDefinitionState view, final ComponentState state, final String[] args) {
-        TreeComponent technologyTree = (TreeComponent) view.getComponentByReference("technologyTree");
-        GridComponent outProducts = (GridComponent) view.getComponentByReference("outProducts");
-        GridComponent inProducts = (GridComponent) view.getComponentByReference("inProducts");
-        if (technologyTree.getSelectedEntityId() != null) {
-            outProducts.setEditable(true);
-            inProducts.setEditable(true);
-        } else {
-            outProducts.setEditable(false);
-            inProducts.setEditable(false);
+        final TreeComponent technologyTree = (TreeComponent) view.getComponentByReference(TECHNOLOGY_TREE_REFERENCE);
+        final boolean gridsShouldBeEnabled = technologyTree.getSelectedEntityId() != null;
+        for (String componentReference : Sets.newHashSet(OUT_PRODUCTS_REFERENCE, IN_PRODUCTS_REFERENCE)) {
+            view.getComponentByReference(componentReference).setEnabled(gridsShouldBeEnabled);
         }
     }
+
 }
