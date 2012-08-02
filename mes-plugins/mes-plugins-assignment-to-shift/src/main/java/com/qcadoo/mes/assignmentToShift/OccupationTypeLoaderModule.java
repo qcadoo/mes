f13ead1d59e86ab2bd2b2e79c108aebad810ffc3
Lukasz Poniewierski <lukasz.poniewierski@qcadoo.com>
2012-08-02 13:11:05 +0200

    DF#QCADOOMES-998 Fixed problem with test data.

--- mes-plugins/mes-plugins-assignment-to-shift/src/main/java/com/qcadoo/mes/assignmentToShift/OccupationTypeLoaderModule.java
+++ mes-plugins/mes-plugins-assignment-to-shift/src/main/java/com/qcadoo/mes/assignmentToShift/OccupationTypeLoaderModule.java
@@ -15,6 +15,7 @@ import org.jdom.input.SAXBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,13 +28,22 @@ import com.qcadoo.plugin.api.Module;
 @Component
 public class OccupationTypeLoaderModule extends Module {
 
+    private static final String L_QCADOO_MODEL = "qcadooModel";
+
     protected static final Logger LOG = LoggerFactory.getLogger(OccupationTypeLoaderModule.class);
+
+    private static final String L_OCCUPATION_TYPE = "occupationType";
+
+    private static final String L_DICTIONARY_ITEM = "dictionaryItem";
 
     private static final String L_DICTIONARY = "dictionary";
 
     private static final String L_TECHNICAL_CODE = "technicalCode";
 
     private static final String L_NAME = "name";
+
+    @Value("${samplesDatasetLocale}")
+    private String locale;
 
     @Autowired
     private DataDefinitionService dataDefinitionService;
@@ -43,7 +53,7 @@ public class OccupationTypeLoaderModule extends Module {
     public final void multiTenantEnable() {
         if (databaseHasToBePrepared()) {
             if (LOG.isDebugEnabled()) {
-                LOG.debug("Currency table will be populated...");
+                LOG.debug("Occupation type table will be populated...");
             }
             readDataFromXML();
         }
@@ -54,7 +64,7 @@ public class OccupationTypeLoaderModule extends Module {
 
         try {
             SAXBuilder builder = new SAXBuilder();
-            Document document = builder.build(getCurrencyXmlFile());
+            Document document = builder.build(getOccupationTypeXmlFile());
             Element rootNode = document.getRootElement();
 
             @SuppressWarnings("unchecked")
@@ -74,7 +84,7 @@ public class OccupationTypeLoaderModule extends Module {
         List<Attribute> attributes = node.getAttributes();
         Map<String, String> values = new HashMap<String, String>();
         for (Attribute attribute : attributes) {
-            values.put(attribute.getName().toLowerCase(Locale.getDefault()), attribute.getValue());
+            values.put(attribute.getName().toLowerCase(Locale.ENGLISH), attribute.getValue());
         }
         addDictionaryItem(values);
     }
@@ -83,13 +93,13 @@ public class OccupationTypeLoaderModule extends Module {
         DataDefinition dictionaryItemDataDefinition = getDictionaryItemDataDefinition();
         Entity dictionaryItem = dictionaryItemDataDefinition.create();
 
-        dictionaryItem.setField(L_TECHNICAL_CODE, values.get(L_TECHNICAL_CODE.toLowerCase(Locale.getDefault())));
+        dictionaryItem.setField(L_TECHNICAL_CODE, values.get(L_TECHNICAL_CODE.toLowerCase(Locale.ENGLISH)));
         dictionaryItem.setField(L_NAME, values.get(L_NAME.toLowerCase(Locale.ENGLISH)));
         dictionaryItem.setField(L_DICTIONARY, getlDictionary());
 
         dictionaryItem = dictionaryItemDataDefinition.save(dictionaryItem);
         if (dictionaryItem.isValid() && LOG.isDebugEnabled()) {
-            LOG.debug("Currency saved {currency=" + dictionaryItem.toString() + "}");
+            LOG.debug("Occupation type saved {currency=" + dictionaryItem.toString() + "}");
         } else {
             throw new IllegalStateException("Saved dictionaries entity have validation errors - " + values.get(L_NAME));
         }
@@ -98,22 +108,33 @@ public class OccupationTypeLoaderModule extends Module {
     private boolean databaseHasToBePrepared() {
         return getDictionaryItemDataDefinition()
                 .find()
-                .add(SearchRestrictions.or(SearchRestrictions.eq("technicalCode", "01workForLine"),
-                        SearchRestrictions.eq("technicalCode", "02otherCase"))).list().getTotalNumberOfEntities() == 0;
+                .add(SearchRestrictions.or(SearchRestrictions.eq(L_TECHNICAL_CODE, "01workForLine"),
+                        SearchRestrictions.eq(L_TECHNICAL_CODE, "02otherCase"))).list().getTotalNumberOfEntities() == 0;
     }
 
     private DataDefinition getDictionaryItemDataDefinition() {
-        return dataDefinitionService.get("qcadooModel", "dictionaryItem");
+        return dataDefinitionService.get(L_QCADOO_MODEL, L_DICTIONARY_ITEM);
     }
 
     private Entity getlDictionary() {
-        return dataDefinitionService.get("qcadooModel", L_DICTIONARY).find().add(SearchRestrictions.eq(L_NAME, "occupationType"))
-                .uniqueResult();
+        return dataDefinitionService.get(L_QCADOO_MODEL, L_DICTIONARY).find()
+                .add(SearchRestrictions.eq(L_NAME, L_OCCUPATION_TYPE)).uniqueResult();
     }
 
-    private InputStream getCurrencyXmlFile() throws IOException {
+    private String getSamplesDatasetLocale() {
+        if ("pl".equalsIgnoreCase(locale)) {
+            return "pl";
+        }
+        if ("en".equalsIgnoreCase(locale)) {
+            return "en";
+        }
+
+        return Locale.getDefault().getLanguage();
+    }
+
+    private InputStream getOccupationTypeXmlFile() throws IOException {
         return OccupationTypeLoaderModule.class.getResourceAsStream("/assignmentToShift/model/data/occupationType" + "_"
-                + Locale.getDefault().getLanguage() + ".xml");
+                + getSamplesDatasetLocale() + ".xml");
     }
 
 }
