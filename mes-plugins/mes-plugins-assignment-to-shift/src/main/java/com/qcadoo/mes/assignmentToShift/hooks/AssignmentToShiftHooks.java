13c9159071f28f8b94147162d79a2464e388a9a3
Lukasz Poniewierski <lukasz.poniewierski@qcadoo.com>
2012-08-03 15:23:41 +0200

    DF#QCADOOMES-963 Fixed problem with validator and added some tests.

--- mes-plugins/mes-plugins-assignment-to-shift/src/main/java/com/qcadoo/mes/assignmentToShift/hooks/AssignmentToShiftHooks.java
+++ mes-plugins/mes-plugins-assignment-to-shift/src/main/java/com/qcadoo/mes/assignmentToShift/hooks/AssignmentToShiftHooks.java
@@ -2,16 +2,17 @@ package com.qcadoo.mes.assignmentToShift.hooks;
 
 import static com.qcadoo.mes.assignmentToShift.constants.AssignmentToShiftFields.SHIFT;
 import static com.qcadoo.mes.assignmentToShift.constants.AssignmentToShiftFields.START_DATE;
+import static com.qcadoo.mes.assignmentToShift.constants.AssignmentToShiftFields.STATE;
+import static com.qcadoo.mes.assignmentToShift.states.constants.AssignmentToShiftState.DRAFT;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.qcadoo.mes.assignmentToShift.constants.AssignmentToShiftFields;
-import com.qcadoo.mes.assignmentToShift.states.constants.AssignmentToShiftState;
 import com.qcadoo.mes.assignmentToShift.states.constants.AssignmentToShiftStateChangeDescriber;
 import com.qcadoo.mes.states.service.StateChangeEntityBuilder;
 import com.qcadoo.model.api.DataDefinition;
 import com.qcadoo.model.api.Entity;
+import com.qcadoo.model.api.search.SearchCriteriaBuilder;
 import com.qcadoo.model.api.search.SearchRestrictions;
 
 @Service
@@ -23,26 +24,34 @@ public class AssignmentToShiftHooks {
     @Autowired
     private AssignmentToShiftStateChangeDescriber describer;
 
-    public void setInitialState(final DataDefinition dataDefinition, final Entity assignmentToShift) {
-        stateChangeEntityBuilder.buildInitial(describer, assignmentToShift, AssignmentToShiftState.DRAFT);
+    public void setInitialState(final DataDefinition assignmentToShiftDD, final Entity assignmentToShift) {
+        stateChangeEntityBuilder.buildInitial(describer, assignmentToShift, DRAFT);
     }
 
-    public void clearState(final DataDefinition dataDefinition, final Entity entity) {
-        entity.setField(AssignmentToShiftFields.STATE, AssignmentToShiftState.DRAFT.getStringValue());
+    public void clearState(final DataDefinition assignmentToShiftDD, final Entity assignmentToShift) {
+        assignmentToShift.setField(STATE, DRAFT.getStringValue());
     }
 
-    public boolean checkUniqueEntity(final DataDefinition dataDefinition, final Entity entity) {
-        if (entity.getId() != null) {
-            return true;
+    public boolean checkUniqueEntity(final DataDefinition assignmentToShiftDD, final Entity assignmentToShift) {
+        SearchCriteriaBuilder searchCriteriaBuilder = assignmentToShiftDD.find()
+                .add(SearchRestrictions.belongsTo(SHIFT, assignmentToShift.getBelongsToField(SHIFT)))
+                .add(SearchRestrictions.eq(START_DATE, assignmentToShift.getField(START_DATE)));
+
+        if (assignmentToShift.getId() != null) {
+            searchCriteriaBuilder.add(SearchRestrictions.ne("id", assignmentToShift.getId()));
         }
 
-        Entity existsEntity = dataDefinition.find().add(SearchRestrictions.belongsTo(SHIFT, entity.getBelongsToField(SHIFT)))
-                .add(SearchRestrictions.eq(START_DATE, entity.getField(START_DATE))).uniqueResult();
-        if (existsEntity != null) {
-            entity.addError(dataDefinition.getField(SHIFT), "assignmentToShift.assignmentToShift.entityAlreadyExists");
-            entity.addError(dataDefinition.getField(START_DATE), "assignmentToShift.assignmentToShift.entityAlreadyExists");
+        Entity existingAssignmentToShift = searchCriteriaBuilder.setMaxResults(1).uniqueResult();
+
+        if (existingAssignmentToShift != null) {
+            assignmentToShift.addError(assignmentToShiftDD.getField(SHIFT),
+                    "assignmentToShift.assignmentToShift.entityAlreadyExists");
+            assignmentToShift.addError(assignmentToShiftDD.getField(START_DATE),
+                    "assignmentToShift.assignmentToShift.entityAlreadyExists");
+
             return false;
         }
+
         return true;
     }
 }
--- mes-plugins/mes-plugins-assignment-to-shift/src/test/java/com/qcadoo/mes/assignmentToShift/hooks/AssignmentToShiftHooksTest.java
+++ mes-plugins/mes-plugins-assignment-to-shift/src/test/java/com/qcadoo/mes/assignmentToShift/hooks/AssignmentToShiftHooksTest.java
@@ -1,42 +1,180 @@
 package com.qcadoo.mes.assignmentToShift.hooks;
 
-import junit.framework.Assert;
+import static com.qcadoo.mes.assignmentToShift.constants.AssignmentToShiftFields.SHIFT;
+import static com.qcadoo.mes.assignmentToShift.constants.AssignmentToShiftFields.START_DATE;
+import static com.qcadoo.mes.assignmentToShift.constants.AssignmentToShiftFields.STATE;
+import static com.qcadoo.mes.assignmentToShift.states.constants.AssignmentToShiftState.DRAFT;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
+import java.util.Date;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.util.ReflectionTestUtils;
 
+import com.qcadoo.mes.assignmentToShift.states.constants.AssignmentToShiftStateChangeDescriber;
+import com.qcadoo.mes.states.service.StateChangeEntityBuilder;
 import com.qcadoo.model.api.DataDefinition;
 import com.qcadoo.model.api.Entity;
+import com.qcadoo.model.api.FieldDefinition;
+import com.qcadoo.model.api.search.SearchCriteriaBuilder;
+import com.qcadoo.model.api.search.SearchCriterion;
+import com.qcadoo.model.api.search.SearchRestrictions;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(SearchRestrictions.class)
 public class AssignmentToShiftHooksTest {
 
     private AssignmentToShiftHooks hooks;
 
-    @Mock
-    private DataDefinition dataDefinition;
+    @Autowired
+    private AssignmentToShiftStateChangeDescriber describer;
 
     @Mock
-    private Entity entity;
+    private StateChangeEntityBuilder stateChangeEntityBuilder;
+
+    @Mock
+    private DataDefinition assignmentToShiftDD;
+
+    @Mock
+    private Entity assignmentToShift, shift;
+
+    @Mock
+    private SearchCriteriaBuilder searchCriteriaBuilder;
+
+    @Mock
+    private Date startDate;
 
     @Before
     public void init() {
         hooks = new AssignmentToShiftHooks();
+
         MockitoAnnotations.initMocks(this);
+
+        ReflectionTestUtils.setField(hooks, "stateChangeEntityBuilder", stateChangeEntityBuilder);
+        PowerMockito.mockStatic(SearchRestrictions.class);
     }
 
     @Test
-    public void shouldClearFieldStateOnCopy() throws Exception {
+    public void shouldSetInitialState() {
         // given
-        String state = "01draft";
-        Mockito.when(entity.getStringField("state")).thenReturn(state);
-        // when
 
-        hooks.clearState(dataDefinition, entity);
+        // when
+        hooks.setInitialState(assignmentToShiftDD, assignmentToShift);
+
         // then
-        Assert.assertEquals("01draft", state);
+        verify(stateChangeEntityBuilder).buildInitial(describer, assignmentToShift, DRAFT);
+    }
+
+    @Test
+    public void shouldClearFieldStateOnCopy() {
+        // given
+
+        // when
+        hooks.clearState(assignmentToShiftDD, assignmentToShift);
+
+        // then
+        verify(assignmentToShift).setField(STATE, DRAFT.getStringValue());
+    }
+
+    @Test
+    public void shouldReturnTrueWhenCheckUniqueEntityIfEntityIsntSaved() {
+        // given
+        given(assignmentToShift.getId()).willReturn(null);
+
+        given(assignmentToShift.getBelongsToField(SHIFT)).willReturn(null);
+        given(assignmentToShift.getField(START_DATE)).willReturn(null);
+
+        given(assignmentToShiftDD.find()).willReturn(searchCriteriaBuilder);
+        given(searchCriteriaBuilder.add(Mockito.any(SearchCriterion.class))).willReturn(searchCriteriaBuilder);
+        given(searchCriteriaBuilder.setMaxResults(1)).willReturn(searchCriteriaBuilder);
+        given(searchCriteriaBuilder.uniqueResult()).willReturn(null);
+
+        // when
+        boolean result = hooks.checkUniqueEntity(assignmentToShiftDD, assignmentToShift);
+
+        // then
+        Assert.assertTrue(result);
+
+        verify(assignmentToShift, never()).addError(Mockito.any(FieldDefinition.class), Mockito.anyString());
+    }
+
+    @Test
+    public void shouldReturnTrueWhenCheckUniqueEntityIfEntityIsSaved() {
+        // given
+        given(assignmentToShift.getId()).willReturn(1L);
+
+        given(assignmentToShift.getBelongsToField(SHIFT)).willReturn(null);
+        given(assignmentToShift.getField(START_DATE)).willReturn(null);
+
+        given(assignmentToShiftDD.find()).willReturn(searchCriteriaBuilder);
+        given(searchCriteriaBuilder.add(Mockito.any(SearchCriterion.class))).willReturn(searchCriteriaBuilder);
+        given(searchCriteriaBuilder.setMaxResults(1)).willReturn(searchCriteriaBuilder);
+        given(searchCriteriaBuilder.uniqueResult()).willReturn(null);
+
+        // when
+        boolean result = hooks.checkUniqueEntity(assignmentToShiftDD, assignmentToShift);
+
+        // then
+        Assert.assertTrue(result);
+
+        verify(assignmentToShift, never()).addError(Mockito.any(FieldDefinition.class), Mockito.anyString());
+    }
+
+    @Test
+    public void shouldReturnFalseWhenCheckUniqueEntityIfEntityIsntSaved() {
+        // given
+        given(assignmentToShift.getId()).willReturn(null);
+
+        given(assignmentToShift.getBelongsToField(SHIFT)).willReturn(shift);
+        given(assignmentToShift.getField(START_DATE)).willReturn(startDate);
+
+        given(assignmentToShiftDD.find()).willReturn(searchCriteriaBuilder);
+        given(searchCriteriaBuilder.add(Mockito.any(SearchCriterion.class))).willReturn(searchCriteriaBuilder);
+        given(searchCriteriaBuilder.setMaxResults(1)).willReturn(searchCriteriaBuilder);
+        given(searchCriteriaBuilder.uniqueResult()).willReturn(assignmentToShift);
+
+        // when
+        boolean result = hooks.checkUniqueEntity(assignmentToShiftDD, assignmentToShift);
+
+        // then
+        Assert.assertFalse(result);
+
+        verify(assignmentToShift, times(2)).addError(Mockito.any(FieldDefinition.class), Mockito.anyString());
+    }
+
+    @Test
+    public void shouldReturnFalseWhenCheckUniqueEntityIfEntityIsSaved() {
+        // given
+        given(assignmentToShift.getId()).willReturn(1L);
+
+        given(assignmentToShift.getBelongsToField(SHIFT)).willReturn(shift);
+        given(assignmentToShift.getField(START_DATE)).willReturn(startDate);
+
+        given(assignmentToShiftDD.find()).willReturn(searchCriteriaBuilder);
+        given(searchCriteriaBuilder.add(Mockito.any(SearchCriterion.class))).willReturn(searchCriteriaBuilder);
+        given(searchCriteriaBuilder.setMaxResults(1)).willReturn(searchCriteriaBuilder);
+        given(searchCriteriaBuilder.uniqueResult()).willReturn(assignmentToShift);
+
+        // when
+        boolean result = hooks.checkUniqueEntity(assignmentToShiftDD, assignmentToShift);
+
+        // then
+        Assert.assertFalse(result);
+
+        verify(assignmentToShift, times(2)).addError(Mockito.any(FieldDefinition.class), Mockito.anyString());
     }
 
 }
