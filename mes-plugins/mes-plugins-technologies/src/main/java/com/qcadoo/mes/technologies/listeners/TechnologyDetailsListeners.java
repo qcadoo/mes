8e3fbef0c4184164d73ea4ce79fb31f2a45eda60
Marcin Kubala <marcin.kubala@qcadoo.com>
2012-08-06 16:30:11 +0200

    DF#QCADOOMES-1002 technologyDetails view: disable in/out product's grid if no operations have been selected

--- mes-plugins/mes-plugins-technologies/src/main/java/com/qcadoo/mes/technologies/listeners/TechnologyDetailsListeners.java
+++ mes-plugins/mes-plugins-technologies/src/main/java/com/qcadoo/mes/technologies/listeners/TechnologyDetailsListeners.java
@@ -17,6 +17,10 @@ public class TechnologyDetailsListeners {
     private static final String TECHNOLOGY_TREE_REFERENCE = "technologyTree";
 
     public void setGridEditable(final ViewDefinitionState view, final ComponentState state, final String[] args) {
+        setGridEditable(view);
+    }
+
+    public void setGridEditable(final ViewDefinitionState view) {
         final TreeComponent technologyTree = (TreeComponent) view.getComponentByReference(TECHNOLOGY_TREE_REFERENCE);
         final boolean gridsShouldBeEnabled = technologyTree.getSelectedEntityId() != null;
         for (String componentReference : Sets.newHashSet(OUT_PRODUCTS_REFERENCE, IN_PRODUCTS_REFERENCE)) {
