247abbd7716a4a9a4948b25844d87d3eebe703d9
Alina Brauer <alina.brauer@qcadoo.com>
2012-08-17 10:08:47 +0200

    SC#QCADOOMES-1011 add new plugin for product catalog numbers

---
+++ mes-plugins/mes-plugins-product-catalog-numbers/src/main/java/com/qcadoo/mes/productCatalogNumbers/constants/ProductCatalogNumberFields.java
@@ -0,0 +1,13 @@
+package com.qcadoo.mes.productCatalogNumbers.constants;
+
+public class ProductCatalogNumberFields {
+
+    private ProductCatalogNumberFields() {
+    }
+
+    public static final String PRODUCT = "product";
+
+    public static final String COMPANY = "company";
+
+    public static final String CATALOG_NUMBER = "catalogNumber";
+}
---
+++ mes-plugins/mes-plugins-product-catalog-numbers/src/main/java/com/qcadoo/mes/productCatalogNumbers/constants/ProductCatalogNumbersConstants.java
@@ -0,0 +1,9 @@
+package com.qcadoo.mes.productCatalogNumbers.constants;
+
+public final class ProductCatalogNumbersConstants {
+
+    public static final String PLUGIN_IDENTIFIER = "productCatalogNumbers";
+
+    public static final String MODEL_PRODUCT_CATALOG_NUMBERS = "productCatalogNumbers";
+
+}
