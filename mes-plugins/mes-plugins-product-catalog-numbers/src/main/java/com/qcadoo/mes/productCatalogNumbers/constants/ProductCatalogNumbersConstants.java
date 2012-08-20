14b113b69e647217904933519b7f664fa2e3729a
Lukasz Poniewierski <lukasz.poniewierski@qcadoo.com>
2012-08-20 13:25:51 +0200

    DF Fixed some checkstyle problems.

--- mes-plugins/mes-plugins-product-catalog-numbers/src/main/java/com/qcadoo/mes/productCatalogNumbers/constants/ProductCatalogNumbersConstants.java
+++ mes-plugins/mes-plugins-product-catalog-numbers/src/main/java/com/qcadoo/mes/productCatalogNumbers/constants/ProductCatalogNumbersConstants.java
@@ -2,6 +2,9 @@ package com.qcadoo.mes.productCatalogNumbers.constants;
 
 public final class ProductCatalogNumbersConstants {
 
+    private ProductCatalogNumbersConstants() {
+    }
+
     public static final String PLUGIN_IDENTIFIER = "productCatalogNumbers";
 
     public static final String MODEL_PRODUCT_CATALOG_NUMBERS = "productCatalogNumbers";
