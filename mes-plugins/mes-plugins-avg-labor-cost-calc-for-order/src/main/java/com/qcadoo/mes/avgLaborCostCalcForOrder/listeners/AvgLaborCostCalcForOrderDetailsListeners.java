2c01be50615d89bfacf14ef83ab54a2b4c02791f
Lukasz Poniewierski <lukasz.poniewierski@qcadoo.com>
2012-08-20 12:34:23 +0200

    DF Fixed merge problem.

--- mes-plugins/mes-plugins-avg-labor-cost-calc-for-order/src/main/java/com/qcadoo/mes/avgLaborCostCalcForOrder/listeners/AvgLaborCostCalcForOrderDetailsListeners.java
+++ mes-plugins/mes-plugins-avg-labor-cost-calc-for-order/src/main/java/com/qcadoo/mes/avgLaborCostCalcForOrder/listeners/AvgLaborCostCalcForOrderDetailsListeners.java
@@ -151,14 +151,10 @@ public class AvgLaborCostCalcForOrderDetailsListeners {
             Entity worker = assignmentWorkerToShift.getBelongsToField(AssignmentWorkerToShiftFields.WORKER);
             averageCost = averageCost.add(worker.getDecimalField(StaffFieldsWG.LABOR_HOURLY_COST));
         }
-
-        averageCost = averageCost.divide(BigDecimal.valueOf(assignmentWorkerToShifts.size()));
-        if (BigDecimal.ZERO.compareTo(averageCost) == 0) {
-            return averageCost;
-        } else {
-            return BigDecimal.ZERO;
+        if (assignmentWorkerToShifts.size() != 0) {
+            averageCost = averageCost.divide(BigDecimal.valueOf(assignmentWorkerToShifts.size()));
         }
-
+        return averageCost;
     }
 
     private List<Entity> getAssignmentForGivenShift(final Entity shiftWorkAtStartDate, final Entity shiftWorkAtFinishDate) {
