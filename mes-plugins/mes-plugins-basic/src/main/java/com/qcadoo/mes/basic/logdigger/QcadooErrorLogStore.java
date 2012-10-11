87e72d27b896d1a4997c9911b8eca989ef71f4c6
Krzysztof Nadolski <krzysztof.nadolski@mobeelizer.com>
2012-10-11 09:46:07 +0200

    RF# add logdigger

---
+++ mes-plugins/mes-plugins-basic/src/main/java/com/qcadoo/mes/basic/logdigger/QcadooErrorLogStore.java
@@ -0,0 +1,99 @@
+package com.qcadoo.mes.basic.logdigger;
+
+import java.util.Date;
+
+import com.logdigger.connector.AttributeProvider;
+import com.logdigger.connector.ErrorLog;
+import com.logdigger.connector.ErrorLogStore;
+import com.logdigger.connector.ResultsPage;
+import com.logdigger.jdbc.JdbcErrorLogStore;
+
+public class QcadooErrorLogStore implements ErrorLogStore {
+
+    private JdbcErrorLogStore jdbcErrorLogStore;
+
+    private String[][] ignoredErrors;
+
+    private String logdiggerAddress;
+
+    @Override
+    public void destroy() {
+        jdbcErrorLogStore.destroy();
+    }
+
+    @Override
+    public ResultsPage<ErrorLog> findInDateRange(Date fromDate, Date toDate, int offset, int limit) {
+        return jdbcErrorLogStore.findInDateRange(fromDate, toDate, offset, limit);
+    }
+
+    @Override
+    public ResultsPage<ErrorLog> findSimilar(ErrorLog sample, int offset, int limit) {
+        return jdbcErrorLogStore.findSimilar(sample, offset, limit);
+    }
+
+    @Override
+    public ErrorLog getErrorLogById(String id) {
+        return jdbcErrorLogStore.getErrorLogById(id);
+    }
+
+    @Override
+    public ResultsPage<ErrorLog> getErrorLogs(int offset, int limit) {
+        return jdbcErrorLogStore.getErrorLogs(offset, limit);
+    }
+
+    @Override
+    public LastStoreError getLastStoreError() {
+        return jdbcErrorLogStore.getLastStoreError();
+    }
+
+    @Override
+    public int getMaxCategories() {
+        return jdbcErrorLogStore.getMaxCategories();
+    }
+
+    @Override
+    public String getStoreDescription() {
+        return jdbcErrorLogStore.getStoreDescription() + " + Qcadoo extention";
+    }
+
+    @Override
+    public void init(AttributeProvider attributeProvider) {
+        jdbcErrorLogStore = new JdbcErrorLogStore();
+        jdbcErrorLogStore.init(attributeProvider);
+
+        ignoredErrors = new String[][] {};
+
+        logdiggerAddress = String.valueOf(attributeProvider.getAttribute("logdiggerAddress"));
+    }
+
+    @Override
+    public boolean isAvailable(Feature feature) {
+        return jdbcErrorLogStore.isAvailable(feature);
+    }
+
+    @Override
+    public String save(ErrorLog errorLog) {
+        for (String[] ignoredError : ignoredErrors) {
+            for (int i = 0; i < ignoredError.length; i++) {
+                if (ignoredError[i] == null) {
+                    return null;
+                }
+                if (!ignoredError[i].equals(errorLog.getCategorySignature(i))) {
+                    break;
+                }
+                if (i >= ignoredError.length - 1) {
+                    return null;
+                }
+            }
+        }
+
+        String id = jdbcErrorLogStore.save(errorLog);
+
+        if (jdbcErrorLogStore.findSimilar(errorLog, 0, 1).getTotalCount() == 1) {
+            // TODO KRNA add jira integration
+        }
+
+        return id;
+    }
+
+}
