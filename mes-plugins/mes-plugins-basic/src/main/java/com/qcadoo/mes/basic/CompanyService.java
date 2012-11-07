b872c00d667cc4e7f725234b745dc6fa9c589296
dorotaradomanska <dorotaradomanska@qcadoo.com>
2012-11-07 19:46:44 +0100

    DF# - QCADOOMESCLS-453 - Add hooks for disabling grids if company is owner

--- mes-plugins/mes-plugins-basic/src/main/java/com/qcadoo/mes/basic/CompanyService.java
+++ mes-plugins/mes-plugins-basic/src/main/java/com/qcadoo/mes/basic/CompanyService.java
@@ -32,62 +32,104 @@ import com.qcadoo.model.api.DataDefinition;
 import com.qcadoo.model.api.DataDefinitionService;
 import com.qcadoo.model.api.Entity;
 import com.qcadoo.model.api.search.SearchRestrictions;
+import com.qcadoo.view.api.ComponentState;
 import com.qcadoo.view.api.ViewDefinitionState;
 import com.qcadoo.view.api.components.FormComponent;
+import com.qcadoo.view.api.components.GridComponent;
 import com.qcadoo.view.api.utils.NumberGeneratorService;
 
 @Service
 public class CompanyService {
 
-    private static final String L_FORM = "form";
+	private static final String L_FORM = "form";
 
-    @Autowired
-    private DataDefinitionService dataDefinitionService;
+	@Autowired
+	private DataDefinitionService dataDefinitionService;
 
-    @Autowired
-    private NumberGeneratorService numberGeneratorService;
+	@Autowired
+	private NumberGeneratorService numberGeneratorService;
 
-    @Transactional
-    public Long getParameterId() {
+	@Transactional
+	public Long getParameterId() {
 
-        DataDefinition dataDefinition = dataDefinitionService.get(BasicConstants.PLUGIN_IDENTIFIER, BasicConstants.MODEL_COMPANY);
-        Entity company = dataDefinition.find().add(SearchRestrictions.eq("owner", true)).setMaxResults(1).uniqueResult();
+		DataDefinition dataDefinition = dataDefinitionService.get(
+				BasicConstants.PLUGIN_IDENTIFIER, BasicConstants.MODEL_COMPANY);
+		Entity company = dataDefinition.find()
+				.add(SearchRestrictions.eq("owner", true)).setMaxResults(1)
+				.uniqueResult();
 
-        if (company == null) {
-            Entity newCompany = dataDefinition.create();
-            newCompany.setField("owner", true);
-            Entity savedCompany = dataDefinition.save(newCompany);
-            return savedCompany.getId();
+		if (company == null) {
+			Entity newCompany = dataDefinition.create();
+			newCompany.setField("owner", true);
+			Entity savedCompany = dataDefinition.save(newCompany);
+			return savedCompany.getId();
 
-        } else {
-            return company.getId();
-        }
+		} else {
+			return company.getId();
+		}
 
-    }
+	}
 
-    public void disableCompanyFormForOwner(final ViewDefinitionState state) {
-        FormComponent form = (FormComponent) state.getComponentByReference("form");
+	/**
+	 * Whether company is an owner or not
+	 * 
+	 * @param form
+	 *            Form to check
+	 * @return Boolean value
+	 */
+	public final Boolean getOwning(final FormComponent form) {
 
-        if (form.getEntityId() == null) {
-            return;
-        }
+		if (form.getEntityId() == null) {
+			return Boolean.FALSE;
+		}
 
-        Entity company = dataDefinitionService.get(BasicConstants.PLUGIN_IDENTIFIER, BasicConstants.MODEL_COMPANY).get(
-                form.getEntityId());
+		Entity company = dataDefinitionService.get(
+				BasicConstants.PLUGIN_IDENTIFIER, BasicConstants.MODEL_COMPANY)
+				.get(form.getEntityId());
 
-        if (company == null) {
-            return;
-        }
+		if (company == null) {
+			return Boolean.FALSE;
+		}
 
-        Object owner = company.getField("owner");
+		Object owner = company.getField("owner");
+		return owner instanceof Boolean ? (Boolean) owner : Boolean.FALSE;
+	}
 
-        if (owner != null && owner.equals(true)) {
-            form.setFormEnabled(false);
-        }
-    }
+	public void disableCompanyFormForOwner(final ViewDefinitionState state) {
+		FormComponent form = (FormComponent) state
+				.getComponentByReference(L_FORM);
+		Boolean owner = getOwning(form);
 
-    public void generateCompanyNumber(final ViewDefinitionState state) {
-        numberGeneratorService.generateAndInsertNumber(state, BasicConstants.PLUGIN_IDENTIFIER, BasicConstants.MODEL_COMPANY,
-                L_FORM, "number");
-    }
+		if (owner.booleanValue()) {
+			form.setFormEnabled(false);
+		}
+	}
+
+	public void disabledGridWhenCompanyIsAnOwner(
+			final ViewDefinitionState state, String... references) {
+
+		FormComponent form = (FormComponent) state
+				.getComponentByReference(L_FORM);
+		Boolean owner = getOwning(form);
+
+		if (owner.booleanValue()) {
+			disableGridComponents(state, references);
+		}
+	}
+
+	private void disableGridComponents(final ViewDefinitionState state,
+			String... references) {
+		for (String reference : references) {
+			ComponentState component = state.getComponentByReference(reference);
+			if (component instanceof GridComponent) {
+				((GridComponent) component).setEditable(false);
+			}
+		}
+	}
+
+	public void generateCompanyNumber(final ViewDefinitionState state) {
+		numberGeneratorService.generateAndInsertNumber(state,
+				BasicConstants.PLUGIN_IDENTIFIER, BasicConstants.MODEL_COMPANY,
+				L_FORM, "number");
+	}
 }
---
+++ mes-plugins/mes-plugins-tech-subcontracting/src/main/java/com/qcadoo/mes/techSubcontracting/hooks/CompanyDetailsHooksTS.java
@@ -0,0 +1,20 @@
+package com.qcadoo.mes.techSubcontracting.hooks;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.qcadoo.mes.basic.CompanyService;
+import com.qcadoo.view.api.ViewDefinitionState;
+
+@Service
+public class CompanyDetailsHooksTS {
+
+	@Autowired
+	private CompanyService companyService;
+
+	public void disabledGridWhenCompanyIsAnOwner(final ViewDefinitionState state) {
+
+		companyService.disabledGridWhenCompanyIsAnOwner(state,
+				"operationsGroup", "operations");
+	}
+}
