3f889357debe34c2ead6b3faf5b28131e4cdb98d
Alina Brauer <alina.brauer@qcadoo.com>
2012-11-26 15:01:16 +0100

    RF#QCADOOMES-1258 add default value for subcontracting plugins

---
+++ mes-plugins/mes-plugins-tech-subcontracting/src/main/java/com/qcadoo/mes/techSubcontracting/hooks/MaterialRequirementDetailsHooksTS.java
@@ -0,0 +1,21 @@
+package com.qcadoo.mes.techSubcontracting.hooks;
+
+import org.springframework.stereotype.Service;
+
+import com.qcadoo.view.api.ViewDefinitionState;
+import com.qcadoo.view.api.components.FieldComponent;
+import com.qcadoo.view.api.components.FormComponent;
+
+@Service
+public class MaterialRequirementDetailsHooksTS {
+
+    public void setMrpAlgoritmValue(final ViewDefinitionState view) {
+        FormComponent form = (FormComponent) view.getComponentByReference("form");
+        if (form.getEntityId() != null) {
+            return;
+        }
+        FieldComponent mrpAlgoritm = (FieldComponent) view.getComponentByReference("mrpAlgorithm");
+        mrpAlgoritm.setFieldValue("03componentsAndSubcontractorsProducts");
+        mrpAlgoritm.requestComponentUpdateState();
+    }
+}
