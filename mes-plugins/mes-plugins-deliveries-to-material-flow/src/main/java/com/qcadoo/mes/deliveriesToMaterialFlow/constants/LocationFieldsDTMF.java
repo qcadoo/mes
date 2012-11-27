a24b689dbb8ebf84dabe05bb6d5d087979322b1e
Michal Kostrzewa <michal.kostrzewa@qcadoo.com>
2012-11-27 13:52:06 +0100

    SC#QCADOOMES-1264 edit

---
+++ mes-plugins/mes-plugins-deliveries-to-material-flow/src/main/java/com/qcadoo/mes/deliveriesToMaterialFlow/constants/LocationFieldsDTMF.java
@@ -0,0 +1,9 @@
+package com.qcadoo.mes.deliveriesToMaterialFlow.constants;
+
+public class LocationFieldsDTMF {
+
+    public static final String LOCATION = "location";
+
+    public static final String TYPE = "type";
+
+}
