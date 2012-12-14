8b8fcb73550a40b63a70fa0816336835a2033397
Alina Brauer <alina.brauer@qcadoo.com>
2012-12-14 12:52:26 +0100

    SC#QCADOOMESCLS-511 add footer to reports

--- mes-plugins/mes-plugins-deliveries/src/main/java/com/qcadoo/mes/deliveries/controllers/DeliveriesController.java
+++ mes-plugins/mes-plugins-deliveries/src/main/java/com/qcadoo/mes/deliveries/controllers/DeliveriesController.java
@@ -23,17 +23,22 @@
  */
 package com.qcadoo.mes.deliveries.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.qcadoo.mes.basic.CompanyService;
 import com.qcadoo.mes.deliveries.constants.DeliveriesConstants;
 
 @Controller
 @RequestMapping(value = DeliveriesConstants.PLUGIN_IDENTIFIER, method = RequestMethod.GET)
 public class DeliveriesController {
+
+    @Autowired
+    private CompanyService companyService;
 
     @RequestMapping(value = "deliveryReport.pdf")
     public final ModelAndView deliveryReportPdf(@RequestParam("id") final String id) {
@@ -41,7 +46,7 @@ public class DeliveriesController {
 
         mav.setViewName("deliveryReportPdf");
         mav.addObject("id", id);
-
+        mav.addObject("company", companyService.getCompany());
         return mav;
     }
 
@@ -51,7 +56,7 @@ public class DeliveriesController {
 
         mav.setViewName("orderReportPdf");
         mav.addObject("id", id);
-
+        mav.addObject("company", companyService.getCompany());
         return mav;
     }
 
