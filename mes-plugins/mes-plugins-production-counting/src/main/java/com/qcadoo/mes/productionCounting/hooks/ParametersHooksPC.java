cac7c6e2cfd7b22338d23c3d3a795a22b0c325b3
Alina Brauer <alina.brauer@qcadoo.com>
2012-12-19 12:32:04 +0100

    SC#QCADOOMES-1357 fixed problem with copy parameters

--- mes-plugins/mes-plugins-production-counting/src/main/java/com/qcadoo/mes/productionCounting/hooks/ParametersHooksPC.java
+++ mes-plugins/mes-plugins-production-counting/src/main/java/com/qcadoo/mes/productionCounting/hooks/ParametersHooksPC.java
@@ -1,26 +1,16 @@
 package com.qcadoo.mes.productionCounting.hooks;
 
 import static com.qcadoo.mes.productionCounting.internal.constants.OrderFieldsPC.REGISTER_PIECEWORK;
-import static com.qcadoo.mes.productionCounting.internal.constants.OrderFieldsPC.REGISTER_QUANTITY_IN_PRODUCT;
 import static com.qcadoo.mes.productionCounting.internal.constants.OrderFieldsPC.TYPE_OF_PRODUCTION_RECORDING;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.qcadoo.mes.basic.ParameterService;
-import com.qcadoo.mes.productionCounting.internal.constants.OrderFieldsPC;
 import com.qcadoo.mes.productionCounting.internal.constants.TypeOfProductionRecording;
-import com.qcadoo.model.api.Entity;
 import com.qcadoo.view.api.ViewDefinitionState;
 import com.qcadoo.view.api.components.FieldComponent;
 
 @Service
 public class ParametersHooksPC {
-
-    @Autowired
-    private ParameterService parameterService;
 
     public void checkIfTypeIsCumulatedAndRegisterPieceworkIsFalse(final ViewDefinitionState viewDefinitionState) {
         String typeOfProductionRecording = ((FieldComponent) viewDefinitionState
@@ -36,28 +26,4 @@ public class ParametersHooksPC {
         registerPiecework.requestComponentUpdateState();
     }
 
-    public void setParametersDefaultValue(final ViewDefinitionState viewDefinitionState) {
-        Entity parameter = parameterService.getParameter();
-        if (parameter == null || parameter.getField(TYPE_OF_PRODUCTION_RECORDING) == null) {
-            FieldComponent typeOfProductionRecording = (FieldComponent) viewDefinitionState
-                    .getComponentByReference(TYPE_OF_PRODUCTION_RECORDING);
-            typeOfProductionRecording.setFieldValue(TypeOfProductionRecording.CUMULATED.getStringValue());
-            typeOfProductionRecording.requestComponentUpdateState();
-        }
-        if (parameter == null || parameter.getField(REGISTER_PIECEWORK) == null) {
-            FieldComponent registerPiecework = (FieldComponent) viewDefinitionState.getComponentByReference(REGISTER_PIECEWORK);
-            registerPiecework.setFieldValue(false);
-            registerPiecework.requestComponentUpdateState();
-        }
-        for (String componentReference : Arrays.asList(REGISTER_QUANTITY_IN_PRODUCT, OrderFieldsPC.REGISTER_QUANTITY_OUT_PRODUCT,
-                OrderFieldsPC.REGISTER_PRODUCTION_TIME, OrderFieldsPC.JUST_ONE, OrderFieldsPC.ALLOW_TO_CLOSE,
-                OrderFieldsPC.AUTO_CLOSE_ORDER)) {
-            FieldComponent component = (FieldComponent) viewDefinitionState.getComponentByReference(componentReference);
-            if (parameter == null || parameter.getField(componentReference) == null) {
-                component.setFieldValue(true);
-                component.requestComponentUpdateState();
-            }
-        }
-
-    }
 }
