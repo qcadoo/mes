e3be46272b514e2b005fe629cb5655e5c66ff4b7
Alina Brauer <alina.brauer@qcadoo.com>
2013-01-02 11:17:46 +0100

    DF#QCADOOMES-1362 add scripts

--- mes-application/sql/1.2.0/upgrade_mes3.sql
+++ mes-application/sql/1.2.0/upgrade_mes3.sql
@@ -13,3 +13,29 @@ ALTER TABLE productioncounting_productionrecord ADD COLUMN updateuser character 
 Alter table orders_order  DROP COLUMN  ordergroupname; 
 
 -- end
+
+
+
+CREATE OR REPLACE FUNCTION update_createdate() RETURNS INTEGER AS 
+'
+DECLARE
+		productionrecord RECORD;
+		tenant RECORD;
+
+	BEGIN 
+	FOR tenant IN SELECT * FROM qcadootenant_tenant LOOP  	
+		FOR productionrecord IN SELECT * FROM productioncounting_productionrecord WHERE tenantid=tenant."id" LOOP  	
+			UPDATE  productioncounting_productionrecord  SET createdate =
+				(SELECT dateandtime from productioncounting_productionrecordstatechange  WHERE productionrecord_id=productionrecord."id" 
+					and tenantid=tenant."id" ORDER BY dateandtime ASC LIMIT 1) 
+					WHERE id=productionrecord."id" 	and tenantid=tenant."id" ;
+
+		END  LOOP;
+	END  LOOP;
+RETURN 1; 
+	END;  
+' 
+LANGUAGE 'plpgsql';
+SELECT * FROM update_createdate();
+
+DROP FUNCTION update_createdate();
