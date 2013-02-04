d1d5995d463c219fe304fc7f2a615e9cbb86ca34
miko <michal.kostrzewa@qcadoo.com>
2013-02-04 13:36:14 +0100

    SC#QCADOOMES-1434 Add filtr on product family lookup

---
+++ mes-plugins/mes-plugins-basic/src/main/java/com/qcadoo/mes/basic/criteriaModifiers/ProductCriteriaModifiers.java
@@ -0,0 +1,18 @@
+package com.qcadoo.mes.basic.criteriaModifiers;
+
+import static com.qcadoo.mes.basic.constants.ProductFields.ENTITY_TYPE;
+
+import org.springframework.stereotype.Service;
+
+import com.qcadoo.mes.basic.constants.ProductFamilyElementType;
+import com.qcadoo.model.api.search.SearchCriteriaBuilder;
+import com.qcadoo.model.api.search.SearchRestrictions;
+
+@Service
+public class ProductCriteriaModifiers {
+
+    public void showProductFamilyOnly(final SearchCriteriaBuilder scb) {
+        scb.add(SearchRestrictions.eq(ENTITY_TYPE, ProductFamilyElementType.PRODUCTS_FAMILY.getStringValue()));
+    }
+
+}
