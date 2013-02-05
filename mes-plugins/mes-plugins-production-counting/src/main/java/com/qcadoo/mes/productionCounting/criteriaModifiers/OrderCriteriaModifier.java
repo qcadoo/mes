b21c5ce6af87f6194b7d86ee51486a6a0c208b23
miko <michal.kostrzewa@qcadoo.com>
2013-02-05 13:22:29 +0100

    SC#QCADOOMES-1439 Add filtr on order lookup in production balance

--- mes-plugins/mes-plugins-production-counting/src/main/java/com/qcadoo/mes/productionCounting/criteriaModifiers/OrderCriteriaModifier.java
+++ mes-plugins/mes-plugins-production-counting/src/main/java/com/qcadoo/mes/productionCounting/criteriaModifiers/OrderCriteriaModifier.java
@@ -1,10 +1,13 @@
 package com.qcadoo.mes.productionCounting.criteriaModifiers;
 
 import static com.qcadoo.mes.orders.constants.OrderFields.STATE;
+import static com.qcadoo.mes.orders.states.constants.OrderState.ABANDONED;
 import static com.qcadoo.mes.orders.states.constants.OrderState.COMPLETED;
+import static com.qcadoo.mes.orders.states.constants.OrderState.DECLINED;
 import static com.qcadoo.mes.orders.states.constants.OrderState.INTERRUPTED;
 import static com.qcadoo.mes.orders.states.constants.OrderState.IN_PROGRESS;
 import static com.qcadoo.mes.productionCounting.internal.constants.OrderFieldsPC.TYPE_OF_PRODUCTION_RECORDING;
+import static com.qcadoo.mes.productionCounting.states.constants.ProductionRecordState.ACCEPTED;
 
 import java.util.List;
 
@@ -19,12 +22,19 @@ import com.qcadoo.model.api.search.SearchRestrictions;
 public class OrderCriteriaModifier {
 
     public void orderFilter(final SearchCriteriaBuilder scb) {
-
         final List<String> allowStatesList = Lists.newArrayList(IN_PROGRESS.getStringValue(), INTERRUPTED.getStringValue(),
                 COMPLETED.getStringValue());
-
         scb.add(SearchRestrictions.and(
                 SearchRestrictions.ne(TYPE_OF_PRODUCTION_RECORDING, TypeOfProductionRecording.BASIC.getStringValue()),
                 SearchRestrictions.in(STATE, allowStatesList)));
+    }
+
+    public void orderFilterForProductionBalance(final SearchCriteriaBuilder scb) {
+        final List<String> allowStatesList = Lists.newArrayList(IN_PROGRESS.getStringValue(), COMPLETED.getStringValue(),
+                DECLINED.getStringValue(), INTERRUPTED.getStringValue(), ABANDONED.getStringValue());
+        scb.add(SearchRestrictions.and(SearchRestrictions.in(STATE, allowStatesList),
+                SearchRestrictions.ne(TYPE_OF_PRODUCTION_RECORDING, TypeOfProductionRecording.BASIC.getStringValue())))
+                .createCriteria("productionRecords", "productionRecords")
+                .add(SearchRestrictions.eq(STATE, ACCEPTED.getStringValue()));
     }
 }
