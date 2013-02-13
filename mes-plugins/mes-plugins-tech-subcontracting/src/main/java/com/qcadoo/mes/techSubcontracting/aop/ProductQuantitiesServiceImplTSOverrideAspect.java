465a6d9c02a9605a79284ff3c8ee80c7dadd6eaf
Lukasz Poniewierski <lukasz.poniewierski@qcadoo.com>
2013-02-13 15:51:45 +0100

    DF Fixed problem with aspect.

--- mes-plugins/mes-plugins-tech-subcontracting/src/main/java/com/qcadoo/mes/techSubcontracting/aop/ProductQuantitiesServiceImplTSOverrideAspect.java
+++ mes-plugins/mes-plugins-tech-subcontracting/src/main/java/com/qcadoo/mes/techSubcontracting/aop/ProductQuantitiesServiceImplTSOverrideAspect.java
@@ -32,11 +32,13 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
 
 import com.qcadoo.model.api.Entity;
 
 @Aspect
-public abstract class ProductQuantitiesServiceImplTSOverrideAspect {
+@Configurable
+public class ProductQuantitiesServiceImplTSOverrideAspect {
 
     @Autowired
     private ProductQuantitiesServiceImplTSOverrideUtil productQuantitiesServiceImplTSOverrideUtil;
