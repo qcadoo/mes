4564837008dfca56df50190852f62a3e2f157d95
Kamil Siwonia <kamil.siwonia@qcadoo.com>
2013-06-30 12:22:53 +0200

    DF#QCADOOMES-1613 PKT-4 add script

--- mes-application/sql/1.2.1/upgrade_mes_pkt.sql
+++ mes-application/sql/1.2.1/upgrade_mes_pkt.sql
@@ -61,3 +61,80 @@ ALTER TABLE basicproductioncounting_productioncountingquantity ADD COLUMN role c
 ALTER TABLE basicproductioncounting_productioncountingquantity ALTER COLUMN role SET DEFAULT '01used'::character varying;
      
 -- end
+
+
+-- Table: technologies_technologyoperationcomponent
+-- changed: 22.06.2013
+
+ALTER TABLE technologies_technologyoperationcomponent ADD COLUMN createdate timestamp without time zone;
+ALTER TABLE technologies_technologyoperationcomponent ADD COLUMN updatedate timestamp without time zone;
+ALTER TABLE technologies_technologyoperationcomponent ADD COLUMN createuser character varying(255);
+ALTER TABLE technologies_technologyoperationcomponent ADD COLUMN updateuser character varying(255);
+
+-- end
+
+
+-- Table: timenormsforoperations_techopercomptimecalculations
+-- changed: 22.06.2013
+
+CREATE TABLE timenormsforoperations_techopercomptimecalculations
+(
+  id bigint NOT NULL,
+  operationoffset integer,
+  effectiveoperationrealizationtime integer,
+  effectivedatefrom timestamp without time zone,
+  effectivedateto timestamp without time zone,
+  duration integer DEFAULT 0,
+  machineworktime integer DEFAULT 0,
+  laborworktime integer DEFAULT 0,
+  CONSTRAINT timenormsforoperations_techopercomptimecalculations_pkey PRIMARY KEY (id)
+);
+
+-- end
+
+
+-- Table: productionlines_techopercompworkstation
+-- changed: 22.06.2013
+
+CREATE TABLE productionlines_techopercompworkstation
+(
+  id bigint NOT NULL,
+  quantityofworkstationtypes integer DEFAULT 1,
+  CONSTRAINT productionlines_techopercompworkstation_pkey PRIMARY KEY (id)
+);
+
+-- end
+
+
+-- Table: operationaltasksfororders_techopercompoperationaltasks
+-- changed: 22.06.2013
+
+CREATE TABLE operationaltasksfororders_techopercompoperationaltasks
+(
+  id bigint NOT NULL,
+  technologyoperationcomponent_id bigint,
+  CONSTRAINT operationaltasksfororders_techopercompoperationaltasks_pkey PRIMARY KEY (id),
+  CONSTRAINT techopercompoperationaltasks_technologyoperationcomponent_fkey FOREIGN KEY (technologyoperationcomponent_id)
+      REFERENCES technologies_technologyoperationcomponent (id) DEFERRABLE
+);
+
+-- end
+
+
+-- Table: technologies_technologyoperationcomponent
+-- changed: 22.06.2013
+
+ALTER TABLE technologies_technologyoperationcomponent ADD COLUMN techopercomptimecalculations_id bigint;      
+
+ALTER TABLE technologies_technologyoperationcomponent
+  ADD CONSTRAINT technologyoperationcomponent_techopercomptimecalculations_fkey FOREIGN KEY (techopercomptimecalculations_id)
+      REFERENCES timenormsforoperations_techopercomptimecalculations (id) DEFERRABLE;
+
+
+ALTER TABLE technologies_technologyoperationcomponent ADD COLUMN techopercompworkstation_id bigint;      
+
+ALTER TABLE technologies_technologyoperationcomponent
+  ADD CONSTRAINT technologyoperationcomponent_techopercompworkstation_fkey FOREIGN KEY (techopercompworkstation_id)
+      REFERENCES productionlines_techopercompworkstation (id) DEFERRABLE;
+
+-- end
