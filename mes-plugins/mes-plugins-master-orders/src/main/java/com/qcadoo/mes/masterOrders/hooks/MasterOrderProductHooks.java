507a362aa09d5121e499b1554cc35e0091c75cd7
Alina Brauer <alina.brauer@qcadoo.com>
2013-07-10 09:50:41 +0200

    DF#QCADOOMES-1584 && SC#QCADOOMES-1497 add onDelete for masterOrder

--- mes-plugins/mes-plugins-master-orders/src/main/java/com/qcadoo/mes/masterOrders/hooks/MasterOrderProductHooks.java
+++ mes-plugins/mes-plugins-master-orders/src/main/java/com/qcadoo/mes/masterOrders/hooks/MasterOrderProductHooks.java
@@ -1,20 +1,29 @@
 package com.qcadoo.mes.masterOrders.hooks;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.qcadoo.mes.masterOrders.constants.MasterOrderFields;
 import com.qcadoo.mes.masterOrders.constants.MasterOrderProductFields;
+import com.qcadoo.mes.masterOrders.constants.OrderFieldsMO;
+import com.qcadoo.mes.orders.constants.OrderFields;
+import com.qcadoo.mes.orders.constants.OrdersConstants;
 import com.qcadoo.model.api.DataDefinition;
+import com.qcadoo.model.api.DataDefinitionService;
 import com.qcadoo.model.api.Entity;
+import com.qcadoo.model.api.search.SearchRestrictions;
 
 @Service
 public class MasterOrderProductHooks {
 
     @Autowired
     private MasterOrderHooks masterOrderHooks;
+
+    @Autowired
+    private DataDefinitionService dataDefinitionService;
 
     public void countCumulatedOrderQuantity(final DataDefinition masterOrderProductDD, final Entity masterOrderProduct) {
         if (masterOrderProduct.getId() == null) {
@@ -26,4 +35,20 @@ public class MasterOrderProductHooks {
         masterOrderProduct.setField(MasterOrderFields.CUMULATED_ORDER_QUANTITY, cumulatedQuantity);
     }
 
+    public boolean checkAssignedOrder(final DataDefinition masterOrderProductDD, final Entity masterOrderProduct) {
+        if (masterOrderProduct.getId() == null) {
+            return true;
+        }
+        Entity masterOrder = masterOrderProduct.getBelongsToField(MasterOrderProductFields.MASTER_ORDER);
+        Entity product = masterOrderProduct.getBelongsToField(MasterOrderProductFields.PRODUCT);
+        List<Entity> assignedOrders = dataDefinitionService.get(OrdersConstants.PLUGIN_IDENTIFIER, OrdersConstants.MODEL_ORDER)
+                .find().add(SearchRestrictions.belongsTo(OrderFields.PRODUCT, product))
+                .add(SearchRestrictions.belongsTo(OrderFieldsMO.MASTER_ORDER, masterOrder)).list().getEntities();
+
+        if (!assignedOrders.isEmpty()) {
+            masterOrderProduct.addGlobalError("masterOrders.masterOrderProduct.delete.existsAssisgnedOrder");
+            return false;
+        }
+        return true;
+    }
 }
