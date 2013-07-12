868afeebe700aa4e8e9d174eb58ef9bdc91376b8
Lukasz Poniewierski <lukasz.poniewierski@qcadoo.com>
2013-07-12 12:23:52 +0200

    SC#QCADOOMES-1721 Added validations for production counting quantities.

--- mes-plugins/mes-plugins-basic-production-counting/src/main/java/com/qcadoo/mes/basicProductionCounting/validators/ProductionCountingQuantityValidators.java
+++ mes-plugins/mes-plugins-basic-production-counting/src/main/java/com/qcadoo/mes/basicProductionCounting/validators/ProductionCountingQuantityValidators.java
@@ -27,6 +27,7 @@ import org.springframework.stereotype.Service;
 
 import com.qcadoo.mes.basicProductionCounting.constants.OrderFieldsBPC;
 import com.qcadoo.mes.basicProductionCounting.constants.ProductionCountingQuantityFields;
+import com.qcadoo.mes.basicProductionCounting.constants.ProductionCountingQuantityRole;
 import com.qcadoo.mes.basicProductionCounting.constants.ProductionCountingQuantityTypeOfMaterial;
 import com.qcadoo.model.api.DataDefinition;
 import com.qcadoo.model.api.Entity;
@@ -36,37 +37,77 @@ import com.qcadoo.model.api.search.SearchRestrictions;
 public class ProductionCountingQuantityValidators {
 
     public boolean validatesWith(final DataDefinition productionCountingQuantityDD, final Entity productionCountingQuantity) {
-        return checkTypeOfMaterial(productionCountingQuantityDD, productionCountingQuantity);
+        return checkRoleAndTypeOfMaterial(productionCountingQuantityDD, productionCountingQuantity);
     }
 
-    private boolean checkTypeOfMaterial(final DataDefinition productionCountingQuantityDD, final Entity productionCountingQuantity) {
+    private boolean checkRoleAndTypeOfMaterial(final DataDefinition productionCountingQuantityDD,
+            final Entity productionCountingQuantity) {
+        String role = productionCountingQuantity.getStringField(ProductionCountingQuantityFields.ROLE);
         String typeOfMaterial = productionCountingQuantity.getStringField(ProductionCountingQuantityFields.TYPE_OF_MATERIAL);
 
-        if (ProductionCountingQuantityTypeOfMaterial.FINAL_PRODUCT.getStringValue().equals(typeOfMaterial)) {
-            Entity order = productionCountingQuantity.getBelongsToField(ProductionCountingQuantityFields.ORDER);
-
-            if (checkIfAnotherFinalProductExists(order)) {
-                productionCountingQuantity.addError(
-                        productionCountingQuantityDD.getField(ProductionCountingQuantityFields.TYPE_OF_MATERIAL),
-                        "basicProductionCounting.productionCountingQuantity.typeOfMaterial.error.anotherFinalProductExists");
+        if (isRoleUsed(role)) {
+            if (isTypeOfMaterialFinalProduct(typeOfMaterial) || isTypeOfMaterialWaste(typeOfMaterial)) {
+                productionCountingQuantity.addError(productionCountingQuantityDD.getField(ProductionCountingQuantityFields.ROLE),
+                        "basicProductionCounting.productionCountingQuantity.role.error.finalProductOrWasteHasToBeProduced");
 
                 return false;
             }
-        } else if (ProductionCountingQuantityTypeOfMaterial.INTERMEDIATE.getStringValue().equals(typeOfMaterial)) {
-            Entity technologyOperationComponent = productionCountingQuantity
-                    .getBelongsToField(ProductionCountingQuantityFields.TECHNOLOGY_OPERATION_COMPONENT);
-
-            if (technologyOperationComponent == null) {
-                productionCountingQuantity
-                        .addError(productionCountingQuantityDD
-                                .getField(ProductionCountingQuantityFields.TECHNOLOGY_OPERATION_COMPONENT),
-                                "basicProductionCounting.productionCountingQuantity.typeOfMaterial.error.technologyOperationComponentRequired");
+        } else if (isRoleProduced(role)) {
+            if (isTypeOfMaterialComponent(typeOfMaterial)) {
+                productionCountingQuantity.addError(productionCountingQuantityDD.getField(ProductionCountingQuantityFields.ROLE),
+                        "basicProductionCounting.productionCountingQuantity.role.error.componentHasToBeUsed");
 
                 return false;
+            } else if (isTypeOfMaterialIntermediate(typeOfMaterial)) {
+                Entity technologyOperationComponent = productionCountingQuantity
+                        .getBelongsToField(ProductionCountingQuantityFields.TECHNOLOGY_OPERATION_COMPONENT);
+
+                if (technologyOperationComponent == null) {
+                    productionCountingQuantity
+                            .addError(productionCountingQuantityDD
+                                    .getField(ProductionCountingQuantityFields.TECHNOLOGY_OPERATION_COMPONENT),
+                                    "basicProductionCounting.productionCountingQuantity.technologyOperationComponent.error.technologyOperationComponentRequired");
+
+                    return false;
+                }
+            } else if (isTypeOfMaterialFinalProduct(typeOfMaterial)) {
+                Entity order = productionCountingQuantity.getBelongsToField(ProductionCountingQuantityFields.ORDER);
+
+                if (checkIfAnotherFinalProductExists(order)) {
+                    productionCountingQuantity.addError(
+                            productionCountingQuantityDD.getField(ProductionCountingQuantityFields.TYPE_OF_MATERIAL),
+                            "basicProductionCounting.productionCountingQuantity.typeOfMaterial.error.anotherFinalProductExists");
+
+                    return false;
+                }
             }
         }
 
         return true;
+    }
+
+    private boolean isRoleUsed(final String role) {
+        return ProductionCountingQuantityRole.USED.getStringValue().equals(role);
+    }
+
+    private boolean isRoleProduced(final String role) {
+        return ProductionCountingQuantityRole.PRODUCED.getStringValue().equals(role);
+    }
+
+    private boolean isTypeOfMaterialComponent(final String typeOfMaterial) {
+        return ProductionCountingQuantityTypeOfMaterial.COMPONENT.getStringValue().equals(typeOfMaterial);
+    }
+
+    private boolean isTypeOfMaterialIntermediate(final String typeOfMaterial) {
+        return ProductionCountingQuantityTypeOfMaterial.INTERMEDIATE.getStringValue().equals(typeOfMaterial);
+    }
+
+    private boolean isTypeOfMaterialFinalProduct(final String typeOfMaterial) {
+        return ProductionCountingQuantityTypeOfMaterial.FINAL_PRODUCT.getStringValue().equals(typeOfMaterial);
+    }
+
+    private boolean isTypeOfMaterialWaste(final String typeOfMaterial) {
+        return ProductionCountingQuantityTypeOfMaterial.WASTE.getStringValue().equals(typeOfMaterial);
     }
 
     private boolean checkIfAnotherFinalProductExists(final Entity order) {
