420e7308e04cd82ad396f34812a0c5cb99fcf86c
Lukasz Poniewierski <lukasz.poniewierski@qcadoo.com>
2013-08-14 07:35:25 +0200

    DF Added missing constants.

---
+++ mes-plugins/mes-plugins-basic/src/main/java/com/qcadoo/mes/basic/constants/StaffFields.java
@@ -0,0 +1,50 @@
+/**
+ * ***************************************************************************
+ * Copyright (c) 2010 Qcadoo Limited
+ * Project: Qcadoo MES
+ * Version: 1.2.0
+ *
+ * This file is part of Qcadoo.
+ *
+ * Qcadoo is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation; either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * ***************************************************************************
+ */
+package com.qcadoo.mes.basic.constants;
+
+public final class StaffFields {
+
+    private StaffFields() {
+
+    }
+
+    public static final String NAME = "name";
+
+    public static final String NUMBER = "number";
+
+    public static final String SURNAME = "surname";
+
+    public static final String PHONE = "phone";
+
+    public static final String EMAIL = "email";
+
+    public static final String POST = "post";
+
+    public static final String WORK_FOR = "workFor";
+
+    public static final String SHIFT = "shift";
+
+    public static final String DIVISION = "division";
+
+}
