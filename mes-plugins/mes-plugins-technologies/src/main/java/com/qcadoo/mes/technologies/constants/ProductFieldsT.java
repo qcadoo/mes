9588f7dc1b0146dd9ea2825a8e155420261da187
Marcin Kubala <marcin.kubala@qcadoo.com>
2013-09-26 16:14:35 +0200

    DF#QCADOOMES-1747 & #QCADOOMESCLS-967 - fix view with list of technologies using given product

---
+++ mes-plugins/mes-plugins-technologies/src/main/java/com/qcadoo/mes/technologies/constants/ProductFieldsT.java
@@ -0,0 +1,16 @@
+package com.qcadoo.mes.technologies.constants;
+
+public final class ProductFieldsT {
+
+    private ProductFieldsT() {
+    }
+
+    public static final String OPERATION_PRODUCT_IN_COMPONENTS = "operationProductInComponents";
+
+    public static final String OPERATION_PRODUCT_OUT_COMPONENTS = "operationProductOutComponents";
+
+    public static final String TECHNOLOGIES = "technologies";
+
+    public static final String TECHNOLOGY_GROUP = "technologyGroup";
+
+}
--- mes-plugins/mes-plugins-technologies/src/main/java/com/qcadoo/mes/technologies/hooks/TechnologiesWithUsingProductListHooks.java
+++ mes-plugins/mes-plugins-technologies/src/main/java/com/qcadoo/mes/technologies/hooks/TechnologiesWithUsingProductListHooks.java
@@ -1,85 +1,29 @@
 package com.qcadoo.mes.technologies.hooks;
 
-import static com.google.common.base.Preconditions.checkArgument;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.qcadoo.mes.basic.constants.BasicConstants;
-import com.qcadoo.mes.basic.constants.ProductFields;
-import com.qcadoo.mes.technologies.constants.OperationProductInComponentFields;
-import com.qcadoo.mes.technologies.constants.TechnologiesConstants;
-import com.qcadoo.model.api.DataDefinitionService;
-import com.qcadoo.model.api.Entity;
-import com.qcadoo.model.api.search.SearchQueryBuilder;
+import com.qcadoo.view.api.ComponentState;
 import com.qcadoo.view.api.ComponentState.MessageType;
 import com.qcadoo.view.api.ViewDefinitionState;
-import com.qcadoo.view.api.components.FieldComponent;
-import com.qcadoo.view.api.components.FormComponent;
 import com.qcadoo.view.api.components.GridComponent;
 
 @Service
 public class TechnologiesWithUsingProductListHooks {
 
-    private static final String L_PRODUCT = "product";
-
-    @Autowired
-    private DataDefinitionService dataDefinitionService;
-
     private static final String L_GRID = "grid";
 
     private static final String L_FORM = "form";
 
-    private static final String L_VIEW_DEFINITION_STATE_IS_NULL = "viewDefinitionState is null";
+    public void beforeRender(final ViewDefinitionState viewDefinitionState) {
+        showInfoIfNotUsed(viewDefinitionState);
+    }
 
-    public void fillTechnologiesWithUsingProduct(final ViewDefinitionState viewDefinitionState) {
-        checkArgument(viewDefinitionState != null, L_VIEW_DEFINITION_STATE_IS_NULL);
-
+    private void showInfoIfNotUsed(final ViewDefinitionState viewDefinitionState) {
         GridComponent grid = (GridComponent) viewDefinitionState.getComponentByReference(L_GRID);
-        FormComponent form = (FormComponent) viewDefinitionState.getComponentByReference(L_FORM);
-
-        if (form.getEntityId() == null) {
-            return;
-        }
-        Entity product = dataDefinitionService.get(BasicConstants.PLUGIN_IDENTIFIER, BasicConstants.MODEL_PRODUCT).get(
-                form.getEntityId());
-
-        List<Entity> inputProducts = getTechnologyWithProductInComponent(product);
-
-        grid.setEntities(inputProducts);
-
-        if (inputProducts.isEmpty()) {
+        if (grid.getEntities().isEmpty()) {
+            ComponentState form = viewDefinitionState.getComponentByReference(L_FORM);
             form.addMessage("technologies.product.info.notUsed", MessageType.INFO, true);
         }
     }
 
-    private List<Entity> getTechnologyWithProductInComponent(final Entity product) {
-        String query = String.format(
-                "SELECT operationProductInComponent FROM #technologies_operationProductInComponent AS operationProductInComponent "
-                        + "WHERE operationProductInComponent.%s.id = :product", OperationProductInComponentFields.PRODUCT);
-
-        SearchQueryBuilder searchQueryBuilder = dataDefinitionService.get(TechnologiesConstants.PLUGIN_IDENTIFIER,
-                TechnologiesConstants.MODEL_OPERATION_PRODUCT_IN_COMPONENT).find(query);
-        searchQueryBuilder.setEntity("product", product);
-
-        return searchQueryBuilder.list().getEntities();
-    }
-
-    public void fillProductName(final ViewDefinitionState viewDefinitionState) {
-        FieldComponent productField = (FieldComponent) viewDefinitionState.getComponentByReference(L_PRODUCT);
-        FormComponent form = (FormComponent) viewDefinitionState.getComponentByReference(L_FORM);
-        if (form.getEntityId() == null) {
-            return;
-        }
-        Entity product = dataDefinitionService.get(BasicConstants.PLUGIN_IDENTIFIER, BasicConstants.MODEL_PRODUCT).get(
-                form.getEntityId());
-        if (product == null) {
-            return;
-        }
-        productField.setFieldValue(String.format("%s - %s", product.getStringField(ProductFields.NUMBER),
-                product.getStringField(ProductFields.NAME)));
-        productField.requestComponentUpdateState();
-    }
 }
