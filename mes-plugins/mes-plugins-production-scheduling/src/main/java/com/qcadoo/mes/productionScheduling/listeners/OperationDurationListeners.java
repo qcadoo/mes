fb2146d1634994acf8ec9b2598577473bb6c22fa
ksiwonia <kamil.siwonia@gmail.com>
2013-11-19 07:43:58 +0100

    DF#QCADOOCLS-1519 small refactor

---
+++ mes-plugins/mes-plugins-production-scheduling/src/main/java/com/qcadoo/mes/productionScheduling/listeners/OperationDurationListeners.java
@@ -0,0 +1,49 @@
+package com.qcadoo.mes.productionScheduling.listeners;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.google.common.collect.Maps;
+import com.qcadoo.mes.orders.constants.OrderFields;
+import com.qcadoo.mes.orders.constants.OrderType;
+import com.qcadoo.mes.orders.constants.OrdersConstants;
+import com.qcadoo.model.api.DataDefinitionService;
+import com.qcadoo.model.api.Entity;
+import com.qcadoo.view.api.ComponentState;
+import com.qcadoo.view.api.ComponentState.MessageType;
+import com.qcadoo.view.api.ViewDefinitionState;
+
+@Service
+public class OperationDurationListeners {
+
+    @Autowired
+    private DataDefinitionService dataDefinitionService;
+
+    public void showCopyOfTechnology(final ViewDefinitionState view, final ComponentState componentState, final String[] args) {
+        Long orderId = (Long) componentState.getFieldValue();
+
+        if (orderId != null) {
+
+            Entity order = dataDefinitionService.get(OrdersConstants.PLUGIN_IDENTIFIER, OrdersConstants.MODEL_ORDER).get(orderId);
+
+            if (order.getField(OrderFields.ORDER_TYPE).equals(OrderType.WITH_PATTERN_TECHNOLOGY.getStringValue())) {
+
+                if (order.getBelongsToField(OrderFields.TECHNOLOGY_PROTOTYPE) == null) {
+
+                    componentState.addMessage("order.technology.patternTechnology.not.set", MessageType.INFO);
+                    return;
+                }
+
+            }
+            Long technologyId = order.getBelongsToField(OrderFields.TECHNOLOGY).getId();
+            Map<String, Object> parameters = Maps.newHashMap();
+            parameters.put("form.id", technologyId);
+
+            String url = "../page/orders/copyOfTechnologyDetails.html";
+            view.redirectTo(url, false, true, parameters);
+
+        }
+    }
+}
