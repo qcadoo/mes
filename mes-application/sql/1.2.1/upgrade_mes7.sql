57666bae07886a02a601a4dfc1301a41718bb4d7
ksiwonia <kamil.siwonia@gmail.com>
2013-11-22 11:43:19 +0100

    SC#QCADOOMES-1816 add scripts

--- mes-application/sql/1.2.1/upgrade_mes7.sql
+++ mes-application/sql/1.2.1/upgrade_mes7.sql
@@ -43,3 +43,38 @@ ALTER TABLE basic_division
       REFERENCES materialflow_location (id);
 
 -- end
+
+-- Table: basic_workstation
+-- changed: 22.11.2013
+      
+CREATE TABLE basic_workstation
+(
+  id bigint NOT NULL,
+  "number" character varying(255),
+  name character varying(1024),
+  description character varying(2048),
+  workstationtype_id bigint,
+  division_id bigint,
+  active boolean DEFAULT true,
+  CONSTRAINT basic_workstation_pkey PRIMARY KEY (id),
+  CONSTRAINT basic_workstationtype_fkey FOREIGN KEY (workstationtype_id)
+      REFERENCES basic_workstationtype (id) DEFERRABLE,
+  CONSTRAINT basic_divisiontion_fkey FOREIGN KEY (division_id)
+      REFERENCES basic_division (id) DEFERRABLE
+);
+
+-- end
+
+-- Table: basic_workstation
+-- changed: 22.11.2013
+CREATE TABLE jointable_company_negotiation
+(
+  negotiation_id bigint NOT NULL,
+  company_id bigint NOT NULL,
+  CONSTRAINT jointable_company_negotiation_pkey PRIMARY KEY (company_id, negotiation_id),
+  CONSTRAINT basic_company_fkey FOREIGN KEY (company_id)
+      REFERENCES basic_company (id) DEFERRABLE,
+  CONSTRAINT supplynegotiations_negotiation_fkey FOREIGN KEY (negotiation_id)
+      REFERENCES supplynegotiations_negotiation (id) DEFERRABLE
+);
+--end
