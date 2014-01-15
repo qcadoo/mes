0578cbaeeb12ae3cc85b65379f71550149f8220b
Lukasz Poniewierski <lukasz.poniewierski@qcadoo.com>
2014-01-15 15:34:13 +0100

    DF Some styling changes.

--- mes-plugins/mes-plugins-deliveries/src/main/java/com/qcadoo/mes/deliveries/states/constants/DeliveryStateStringValues.java
+++ mes-plugins/mes-plugins-deliveries/src/main/java/com/qcadoo/mes/deliveries/states/constants/DeliveryStateStringValues.java
@@ -28,6 +28,7 @@ import com.qcadoo.mes.states.aop.RunForStateTransitionAspect;
 public final class DeliveryStateStringValues {
 
     private DeliveryStateStringValues() {
+
     }
 
     public static final String WILDCARD_STATE = RunForStateTransitionAspect.WILDCARD_STATE;
