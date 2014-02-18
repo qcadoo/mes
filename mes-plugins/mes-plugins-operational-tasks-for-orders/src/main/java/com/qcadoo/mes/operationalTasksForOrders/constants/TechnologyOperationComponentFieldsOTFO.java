ffa80d55e89ce94c5b92dcc6071b898e56edf526
Lukasz Poniewierski <lukasz.poniewierski@qcadoo.com>
2014-02-18 10:16:47 +0100

    DF#QCADOOMES-2011 Fixed CVE when deleting technology operation component.

---
+++ mes-plugins/mes-plugins-operational-tasks-for-orders/src/main/java/com/qcadoo/mes/operationalTasksForOrders/constants/TechnologyOperationComponentFieldsOTFO.java
@@ -0,0 +1,11 @@
+package com.qcadoo.mes.operationalTasksForOrders.constants;
+
+public final class TechnologyOperationComponentFieldsOTFO {
+
+    private TechnologyOperationComponentFieldsOTFO() {
+
+    }
+
+    public static final String TECH_OPER_COMP_OPERATIONAL_TASKS = "techOperCompOperationalTasks";
+
+}
