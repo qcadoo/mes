594abb1be52e9b013d6ad4f22667b98c2da2f85b
Marcin Kubala <marcin.kubala@qcadoo.com>
2014-02-18 09:45:17 +0100

    CM increase upgrade SQL's count + add missing statement

--- mes-application/sql/1.2.1/upgrade_mes9.sql
+++
@@ -1,9 +0,0 @@
--- Table: workplans_workplan
--- changed: 14.02.2014
-
-ALTER TABLE workplans_workplan ADD COLUMN ordersorting character varying(255);
-ALTER TABLE workplans_workplan ADD COLUMN inputproductcolumntosortby_id bigint;
-ALTER TABLE workplans_workplan
- 	ADD CONSTRAINT workplans_workplan_fkey FOREIGN KEY (inputproductcolumntosortby_id)
- 	REFERENCES workplans_columnforinputproducts (id) DEFERRABLE;
--- end 
