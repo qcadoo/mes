f151b858d26891837a8e42f7c28900b9df21b41a
Marcin Kubala <marcin.kubala@qcadoo.com>
2014-03-04 16:40:46 +0100

    RF add TechnologyFieldsO constants

---
+++ mes-plugins/mes-plugins-orders/src/main/java/com/qcadoo/mes/orders/constants/TechnologyFieldsO.java
@@ -0,0 +1,10 @@
+package com.qcadoo.mes.orders.constants;
+
+public final class TechnologyFieldsO {
+
+    private TechnologyFieldsO() {
+    }
+
+    public static final String ORDERS = "orders";
+
+}
