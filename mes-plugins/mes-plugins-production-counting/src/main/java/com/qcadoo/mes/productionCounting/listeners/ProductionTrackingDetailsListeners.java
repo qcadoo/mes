56a77505451a0e2ed6a4d5e41b05f94c3f0f7f3c
ksiwonia <kamil.siwonia@gmail.com>
2014-04-14 10:46:12 +0200

    DF#QCADOOMES-2146

--- mes-plugins/mes-plugins-production-counting/src/main/java/com/qcadoo/mes/productionCounting/listeners/ProductionTrackingDetailsListeners.java
+++ mes-plugins/mes-plugins-production-counting/src/main/java/com/qcadoo/mes/productionCounting/listeners/ProductionTrackingDetailsListeners.java
@@ -148,9 +148,11 @@ public class ProductionTrackingDetailsListeners {
         }
         FieldComponent lastRecord = (FieldComponent) view.getComponentByReference(ProductionTrackingFields.LAST_TRACKING);
         boolean justOneRecord = order.getBooleanField(OrderFieldsPC.JUST_ONE);
-        lastRecord.setFieldValue(justOneRecord);
-        lastRecord.setEnabled(!justOneRecord);
-        lastRecord.requestComponentUpdateState();
+        if (justOneRecord) {
+            lastRecord.setFieldValue(justOneRecord);
+            lastRecord.setEnabled(!justOneRecord);
+            lastRecord.requestComponentUpdateState();
+        }
     }
 
     public void clearFields(final ViewDefinitionState view, final ComponentState componentState, final String[] args) {
