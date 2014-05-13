49547577d137291ef2bb86d58b36edc1a97aa113
ksiwonia <kamil.siwonia@gmail.com>
2014-05-13 13:03:06 +0200

    fix script

--- mes-application/sql/1.2.1/upgrade_mes12.sql
+++ mes-application/sql/1.2.1/upgrade_mes12.sql
@@ -4,3 +4,11 @@
 ALTER TABLE basic_parameter ADD COLUMN validateproductionrecordtimes boolean;
 
 -- end
+
+-- Table: productioncounting_productionrecord
+-- changed: 12.05.2014
+
+ALTER TABLE productioncounting_productionrecord ALTER COLUMN timerangefrom TYPE timestamp without time zone;
+
+ALTER TABLE productioncounting_productionrecord ALTER COLUMN timerangeto TYPE timestamp without time zone;
+-- end
