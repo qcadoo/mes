ee52fd55b313a86d9bd1b719bd7622cb9004a376
Marcin Kubala <marcin.kubala@qcadoo.com>
2014-06-13 16:42:28 +0200

    CM fix SQL scripts

--- mes-application/sql/1.3.0/upgrade_mes1-dev.sql
+++ mes-application/sql/1.3.0/upgrade_mes1-dev.sql
@@ -7,6 +7,8 @@ CREATE TABLE technologies_productstructuretreenode
   id bigint NOT NULL,
   priority integer,
   technology_id bigint,
+  parent_id BIGINT,
+  nodenumber VARCHAR(255),
   "number" character varying(255),
   product_id bigint,
   createDate timestamp,
@@ -14,8 +16,8 @@ CREATE TABLE technologies_productstructuretreenode
   createUser character varying(255),
   updateUser character varying(255),
   CONSTRAINT technologies_productstructuretreenode_pkey PRIMARY KEY (id),
-  CONSTRAINT productstructuretreenode_product_fkey FOREIGN KEY (product_id)
-      REFERENCES basic_product (id) DEFERRABLE,
+  CONSTRAINT productstructuretreenode_parent_fkey FOREIGN KEY (parent_id)
+      REFERENCES technologies_productstructuretreenode (id) DEFERRABLE,
   CONSTRAINT productstructuretreenode_technology_fkey FOREIGN KEY (technology_id)
       REFERENCES technologies_technology (id) DEFERRABLE
 );
