238439e2c4eb21166df2dd0a839ec7042c002c56
Marcin Kubala <marcin.kubala@qcadoo.com>
2014-07-03 15:08:09 +0200

    DF#QCADOOCLS-2084 add decimal value validation to material flow

--- mes-plugins/mes-plugins-material-flow/src/main/java/com/qcadoo/mes/materialFlow/hooks/TransformationsModelValidators.java
+++ mes-plugins/mes-plugins-material-flow/src/main/java/com/qcadoo/mes/materialFlow/hooks/TransformationsModelValidators.java
@@ -34,14 +34,29 @@ import static com.qcadoo.mes.materialFlow.constants.TransformationsFields.TRANSF
 import java.math.BigDecimal;
 import java.util.List;
 
+import org.apache.commons.lang.ObjectUtils;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
 
+import com.google.common.base.Function;
+import com.google.common.base.Functions;
+import com.google.common.base.Optional;
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+import com.qcadoo.commons.functional.Either;
 import com.qcadoo.mes.materialFlow.MaterialFlowService;
 import com.qcadoo.mes.materialFlow.constants.MaterialFlowConstants;
+import com.qcadoo.mes.materialFlow.constants.TransferFields;
+import com.qcadoo.model.api.BigDecimalUtils;
 import com.qcadoo.model.api.DataDefinition;
 import com.qcadoo.model.api.Entity;
 import com.qcadoo.model.api.FieldDefinition;
+import com.qcadoo.model.api.utils.EntityUtils;
 
 @Service
 public class TransformationsModelValidators {
@@ -53,47 +68,70 @@ public class TransformationsModelValidators {
         List<Entity> transfersConsumption = transformations.getHasManyField(TRANSFERS_CONSUMPTION);
         List<Entity> transfersProduction = transformations.getHasManyField(TRANSFERS_PRODUCTION);
 
-        return (checkIfTransfersAreValid(transfersConsumption) && checkIfTransfersAreValid(transfersProduction))
-                && (checkIfTransfersNumbersAreDistinct(transfersConsumption, transfersProduction) && checkIfTransfersNumbersAreDistinct(
-                        transfersProduction, transfersConsumption));
+        Iterable<Boolean> validationResults = Lists.newArrayList(areTransfersValid(transfersConsumption),
+                areTransfersValid(transfersProduction),
+                checkIfTransfersNumbersAreDistinct(transfersConsumption, transfersProduction),
+                checkIfTransfersNumbersAreDistinct(transfersProduction, transfersConsumption));
+        return Iterables.all(validationResults, Predicates.equalTo(true));
     }
 
-    private boolean checkIfTransfersAreValid(final List<Entity> transfers) {
+    private static final Function<Entity, Long> EXTRACT_TRANSFER_PRODUCT_ID = Functions.compose(EntityUtils.getIdExtractor(),
+            EntityUtils.getBelongsToFieldExtractor(TransferFields.PRODUCT));
+
+    private boolean areTransfersValid(final List<Entity> transfers) {
+        final Iterable<Long> usedProductIds = Iterables.transform(transfers, EXTRACT_TRANSFER_PRODUCT_ID);
+        return FluentIterable.from(transfers).filter(Predicates.notNull()).allMatch(new Predicate<Entity>() {
+
+            @Override
+            public boolean apply(final Entity transfer) {
+                return transferIsValid(usedProductIds, transfer);
+            }
+        });
+    }
+
+    private boolean transferIsValid(final Iterable<Long> usedProductIds, final Entity transfer) {
         boolean isValid = true;
+        isValid = transferQuantityIsNonEmptyDecimalValue(transfer) && isValid;
+        isValid = transferNumberIsNotEmpty(transfer) && isValid;
+        isValid = transferProductIsNotNullAndUnique(usedProductIds, transfer) && isValid;
+        return isValid;
+    }
 
-        if (transfers != null) {
-            for (Entity transfer : transfers) {
-                String number = transfer.getStringField(NUMBER);
-                Entity product = transfer.getBelongsToField(PRODUCT);
-                BigDecimal quantity = transfer.getDecimalField(QUANTITY);
+    private boolean transferNumberIsNotEmpty(final Entity transfer) {
+        String number = transfer.getStringField(NUMBER);
+        if (StringUtils.isEmpty(number)) {
+            appendErrorToModelField(transfer, NUMBER, "materialFlow.validate.global.error.fillRequired");
+            return false;
+        }
+        return true;
+    }
 
-                if ((number == null) || number.isEmpty()) {
-                    appendErrorToModelField(transfer, NUMBER, "materialFlow.validate.global.error.fillRequired");
-
-                    isValid = false;
-                }
-
-                if (product == null) {
-                    appendErrorToModelField(transfer, PRODUCT, "materialFlow.validate.global.error.fillRequired");
-
-                    isValid = false;
-                } else {
-                    if (isProductAlreadyAdded(transfers, product)) {
-                        appendErrorToModelField(transfer, PRODUCT, "materialFlow.validate.global.error.productAlreadyAdded");
-
-                        isValid = false;
-                    }
-                }
-
-                if (quantity == null) {
-                    appendErrorToModelField(transfer, QUANTITY, "materialFlow.validate.global.error.fillRequired");
-
-                    isValid = false;
-                }
+    private boolean transferProductIsNotNullAndUnique(final Iterable<Long> usedProductIds, final Entity transfer) {
+        Entity product = transfer.getBelongsToField(PRODUCT);
+        if (product == null) {
+            appendErrorToModelField(transfer, PRODUCT, "materialFlow.validate.global.error.fillRequired");
+            return false;
+        } else {
+            if (FluentIterable.from(usedProductIds).filter(Predicates.equalTo(product.getId())).limit(2).size() > 1) {
+                appendErrorToModelField(transfer, PRODUCT, "materialFlow.validate.global.error.productAlreadyAdded");
+                return false;
             }
         }
+        return true;
+    }
 
-        return isValid;
+    private boolean transferQuantityIsNonEmptyDecimalValue(final Entity transfer) {
+        Either<Exception, Optional<BigDecimal>> quantity = BigDecimalUtils.tryParse(
+                ObjectUtils.toString(transfer.getField(TransferFields.QUANTITY)), LocaleContextHolder.getLocale());
+        if (quantity.isLeft()) {
+            appendErrorToModelField(transfer, QUANTITY, "qcadooView.validate.field.error.invalidNumericFormat");
+            return false;
+        }
+        if (!quantity.getRight().isPresent()) {
+            appendErrorToModelField(transfer, QUANTITY, "materialFlow.validate.global.error.fillRequired");
+            return false;
+        }
+        return true;
     }
 
     private boolean checkIfTransfersNumbersAreDistinct(final List<Entity> transfersConsumption,
@@ -118,26 +156,6 @@ public class TransformationsModelValidators {
         }
 
         return isValid;
-    }
-
-    private boolean isProductAlreadyAdded(final List<Entity> transfers, final Entity product) {
-        if ((transfers != null) && (product != null)) {
-            int count = 0;
-
-            for (Entity transfer : transfers) {
-                Entity productAlreadyAdded = transfer.getBelongsToField(PRODUCT);
-
-                if (product.equals(productAlreadyAdded)) {
-                    count++;
-
-                    if (count > 1) {
-                        return true;
-                    }
-                }
-            }
-        }
-
-        return false;
     }
 
     private int isNumberAlreadyUsed(final List<Entity> transfers, final String number) {
