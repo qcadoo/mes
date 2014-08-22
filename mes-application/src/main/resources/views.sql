ce0547873c4f5b04a5397026a1e0f193144ee667
Marcin Kubala <marcin.kubala@qcadoo.com>
2014-08-22 09:41:03 +0200

    DF mes-application: restore drop table statement in views.sql

--- mes-application/src/main/resources/views.sql
+++ mes-application/src/main/resources/views.sql
@@ -1,4 +1,8 @@
-DROP VIEW IF EXISTS materialflowresources_warehousestock;
+-- This script is invoked when application starts with hbm2ddlAuto=create.
+
+--  Qcadoo-model & Hibernate will automatically generate regular db table due to existence of warehouseStock.xml model,
+-- we need to first drop this table, before create table view.
+DROP TABLE IF EXISTS materialflowresources_warehousestock;
 
 CREATE OR REPLACE FUNCTION createWarehouseStockView () RETURNS boolean AS 'BEGIN IF EXISTS (SELECT * FROM information_schema.columns WHERE table_name=''basic_parameter'' AND column_name=''tenantid'') THEN CREATE OR REPLACE VIEW materialflowresources_warehousestock AS SELECT row_number() OVER () AS id, location_id, product_id, SUM(quantity) AS quantity, tenantid FROM materialflowresources_resource GROUP BY tenantid, location_id, product_id ; ELSE CREATE OR REPLACE VIEW materialflowresources_warehousestock AS SELECT row_number() OVER () AS id, location_id, product_id, SUM(quantity) AS quantity FROM materialflowresources_resource GROUP BY location_id, product_id ; END IF; RETURN TRUE; END;' LANGUAGE 'plpgsql';
 
