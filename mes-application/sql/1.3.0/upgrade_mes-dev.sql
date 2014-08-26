14742c72e7d055b41511c373c57970160f9a1d5c
Tomasz Landowski <landowskitomasz@gmail.com>
2014-08-26 14:46:14 +0200

    SC#QCADOOCLS-2358 zip code length vaidation removed from company model

--- mes-application/sql/1.3.0/upgrade_mes-dev.sql
+++ mes-application/sql/1.3.0/upgrade_mes-dev.sql
@@ -2,3 +2,8 @@
 -- last touched at 26.08.2014 by tola
 
 ALTER TABLE materialflowresources_document ADD COLUMN description character varying(2048);
+
+--  Update 'zipcode' field type in company model.
+-- last touched at 26.08.2014 by tola
+
+ALTER TABLE basic_company ALTER COLUMN zipcode TYPE character varying(255);
