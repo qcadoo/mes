e18f9fa2a9e00a306d679ef200013a44c5ceba0d
Adrian Sobiczewski <adrian.sobiczewski@qcadoo.com>
2014-09-03 16:32:44 +0200

    DF#QCADOOCLS-2411 Grouping by orders separately

--- mes-plugins/mes-plugins-work-plans/src/main/java/com/qcadoo/mes/workPlans/pdf/document/operation/grouping/container/OperationProductInGroupingContainerDecorator.java
+++ mes-plugins/mes-plugins-work-plans/src/main/java/com/qcadoo/mes/workPlans/pdf/document/operation/grouping/container/OperationProductInGroupingContainerDecorator.java
@@ -1,26 +1,5 @@
-/**
- * ***************************************************************************
- * Copyright (c) 2010 Qcadoo Limited
- * Project: Qcadoo MES
- * Version: 1.3
- *
- * This file is part of Qcadoo.
- *
- * Qcadoo is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published
- * by the Free Software Foundation; either version 3 of the License,
- * or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty
- * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- * ***************************************************************************
- */
+
+
 package com.qcadoo.mes.workPlans.pdf.document.operation.grouping.container;
 
 import java.math.BigDecimal;
@@ -43,6 +22,7 @@ import com.qcadoo.mes.technologies.constants.OperationProductInComponentFields;
 import com.qcadoo.mes.technologies.constants.TechnologyOperationComponentFields;
 import com.qcadoo.mes.technologies.dto.OperationProductComponentWithQuantityContainer;
 import com.qcadoo.mes.technologies.grouping.OperationMergeService;
+import com.qcadoo.mes.workPlans.pdf.document.operation.grouping.container.util.OrderIdOperationNumberOperationComponentIdMap;
 import com.qcadoo.mes.workPlans.pdf.document.operation.grouping.holder.OrderOperationComponent;
 import com.qcadoo.mes.workPlans.pdf.document.operation.product.column.OperationProductColumn;
 import com.qcadoo.mes.workPlans.pdf.document.order.column.OrderColumn;
@@ -60,7 +40,7 @@ public class OperationProductInGroupingContainerDecorator implements GroupingCon
 
     private Map<Long, Entity> operationComponentIdToOperationComponent;
 
-    private Multimap<String, Long> operationNumberToOperationComponentId;
+    private OrderIdOperationNumberOperationComponentIdMap orderIdOperationNumberOperationComponentIdMap;
 
     public OperationProductInGroupingContainerDecorator(OperationMergeService operationMergeService,
             GroupingContainer groupingContainer, ProductQuantitiesService productQuantitiesService) {
@@ -73,7 +53,7 @@ public class OperationProductInGroupingContainerDecorator implements GroupingCon
     private void initMaps() {
         this.operationComponentIdToOrder = new HashMap<Long, Entity>();
         this.operationComponentIdToOperationComponent = new HashMap<Long, Entity>();
-        this.operationNumberToOperationComponentId = HashMultimap.create();
+        this.orderIdOperationNumberOperationComponentIdMap = OrderIdOperationNumberOperationComponentIdMap.create();
     }
 
     @Override
@@ -185,7 +165,7 @@ public class OperationProductInGroupingContainerDecorator implements GroupingCon
             return;
         }
 
-        operationNumberToOperationComponentId.put(operationNumber, operationComponent.getId());
+        orderIdOperationNumberOperationComponentIdMap.put(order.getId(), operationNumber, operationComponent.getId());
 
         for (Entity operationProductInComponent : operationProductInComponents(operationComponent)) {
             quantity(operationProductInComponent, productQuantities.get(operationProductInComponent));
@@ -267,8 +247,8 @@ public class OperationProductInGroupingContainerDecorator implements GroupingCon
         return operationProductInComponent.getDecimalField(OperationProductInComponentFields.QUANTITY);
     }
 
-    private boolean operationAlreadyExists(String operationNumber) {
-        return operationNumberToOperationComponentId.containsKey(operationNumber);
+    private boolean operationAlreadyExists(Entity order, String operationNumber) {
+        return orderIdOperationNumberOperationComponentIdMap.containsKey(order.getId(), operationNumber);
     }
 
     private String productNumber(Entity operationProductInComponent) {
