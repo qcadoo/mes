2bc8107b4faddb57a896e1c0a6aa9989754e89eb
Katarzyna Małyszek <katarzyna.malyszek@qcadoo.com>
2014-09-23 14:18:19 +0200

    DF#QCADOOCLS-2539

--- mes-plugins/mes-plugins-deliveries-to-material-flow/src/main/java/com/qcadoo/mes/deliveriesToMaterialFlow/hooks/DeliveredProductHooksDTMF.java
+++ mes-plugins/mes-plugins-deliveries-to-material-flow/src/main/java/com/qcadoo/mes/deliveriesToMaterialFlow/hooks/DeliveredProductHooksDTMF.java
@@ -14,7 +14,7 @@ public class DeliveredProductHooksDTMF {
     public boolean validateDates(final DataDefinition deliveredProductDD, final Entity deliveredProduct) {
         Date productionDate = deliveredProduct.getDateField(DeliveredProductFieldsDTMF.PRODUCTION_DATE);
         Date expirationDate = deliveredProduct.getDateField(DeliveredProductFieldsDTMF.EXPIRATION_DATE);
-        if (productionDate.compareTo(expirationDate) > 1) {
+        if (productionDate.compareTo(expirationDate) > 0) {
             deliveredProduct.addError(deliveredProductDD.getField(DeliveredProductFieldsDTMF.EXPIRATION_DATE),
                     "materialFlow.error.position.expirationDate.lessThenProductionDate");
             return false;
