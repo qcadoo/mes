43acc3fad16308aea0e0731cd336adc737ec8022
ksiwonia <kamil.siwonia@gmail.com>
2015-01-21 11:54:05 +0100

    DF#QCADOOCLS-3292 remove production line linked to division

--- mes-plugins/mes-plugins-production-lines/src/main/java/com/qcadoo/mes/productionLines/hooks/ProductionLineHooks.java
+++ mes-plugins/mes-plugins-production-lines/src/main/java/com/qcadoo/mes/productionLines/hooks/ProductionLineHooks.java
@@ -2,18 +2,7 @@ package com.qcadoo.mes.productionLines.hooks;
 
 import org.springframework.stereotype.Service;
 
-import com.qcadoo.mes.productionLines.constants.ProductionLineFields;
-import com.qcadoo.model.api.DataDefinition;
-import com.qcadoo.model.api.Entity;
-
 @Service
 public class ProductionLineHooks {
 
-    public boolean onDelete(final DataDefinition dataDefinition, final Entity productionLine) {
-        if (productionLine.getBelongsToField(ProductionLineFields.DIVISION) == null) {
-            return true;
-        }
-        productionLine.addGlobalError("productionLines.productionLine.onDelete.error");
-        return false;
-    }
 }
