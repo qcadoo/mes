bb6af13a2d40d3038dd2362fdcc8f848ef8ae7f7
Katarzyna Małyszek <katarzyna.malyszek@qcadoo.com>
2015-02-17 15:30:02 +0100

    SC# QCADOOCLS-3493 added MBR to orders

--- mes-application/sql/1.4/upgrade_mes-dev.sql
+++ mes-application/sql/1.4/upgrade_mes-dev.sql
@@ -29,3 +29,98 @@ ALTER TABLE orders_orderstatechange ALTER COLUMN dateschanged SET DEFAULT false;
 
 
 -- end
+
+-- Added MBR models to orders
+-- last touched 16.02.2015 by kama
+
+CREATE TABLE orders_recipe
+(
+  id bigint NOT NULL,
+  "number" character varying(255),
+  product_id bigint,
+  name character varying(1024),
+  version character varying(255),
+  isdefault boolean,
+  strenght character varying(255),
+  batchquantity integer,
+  batchsize character varying(255),
+  description character varying(2048),
+  state character varying(255) DEFAULT '01draft'::character varying,
+  createdate timestamp without time zone,
+  updatedate timestamp without time zone,
+  createuser character varying(255),
+  updateuser character varying(255),
+  CONSTRAINT orders_recipe_pkey PRIMARY KEY (id),
+  CONSTRAINT recipe_product_fkey FOREIGN KEY (product_id)
+      REFERENCES basic_product (id) DEFERRABLE
+);
+
+CREATE TABLE orders_instruction
+(
+  id bigint NOT NULL,
+  orderid integer,
+  description character varying(1024),
+  rangefrom integer,
+  rangeto integer,
+  rangeunit character varying(255),
+  targetvalue integer,
+  verificationrequired boolean,
+  recipe_id bigint,
+  createdate timestamp without time zone,
+  updatedate timestamp without time zone,
+  createuser character varying(255),
+  updateuser character varying(255),
+  CONSTRAINT orders_instruction_pkey PRIMARY KEY (id),
+  CONSTRAINT instruction_recipe_fkey FOREIGN KEY (recipe_id)
+      REFERENCES orders_recipe (id) DEFERRABLE
+);
+
+CREATE TABLE orders_material
+(
+  id bigint NOT NULL,
+  product_id bigint,
+  quantity numeric(12,5),
+  recipe_id bigint,
+  instruction_id bigint,
+  createdate timestamp without time zone,
+  updatedate timestamp without time zone,
+  createuser character varying(255),
+  updateuser character varying(255),
+  materials_id bigint,
+  CONSTRAINT orders_material_pkey PRIMARY KEY (id),
+  CONSTRAINT material_recipe_fkey FOREIGN KEY (recipe_id)
+      REFERENCES orders_recipe (id) DEFERRABLE,
+  CONSTRAINT material_instruction_fkey FOREIGN KEY (instruction_id)
+      REFERENCES orders_instruction (id) DEFERRABLE,
+  CONSTRAINT material_product_fkey FOREIGN KEY (product_id)
+      REFERENCES basic_product (id) DEFERRABLE,
+  CONSTRAINT material_materials_fkey FOREIGN KEY (materials_id)
+      REFERENCES basic_product (id) DEFERRABLE
+);
+
+CREATE TABLE orders_mbrstatechange
+(
+  id bigint NOT NULL,
+  dateandtime timestamp without time zone,
+  sourcestate character varying(255),
+  targetstate character varying(255),
+  status character varying(255),
+  phase integer,
+  worker character varying(255),
+  recipe_id bigint,
+  shift_id bigint,
+  CONSTRAINT orders_mbrstatechange_pkey PRIMARY KEY (id),
+  CONSTRAINT mbrstatechange_recipe_fkey FOREIGN KEY (recipe_id)
+      REFERENCES orders_recipe (id) DEFERRABLE,
+  CONSTRAINT mbrstatechange_shift_fkey FOREIGN KEY (shift_id)
+      REFERENCES basic_shift (id) DEFERRABLE
+);
+
+ALTER TABLE orders_order ADD COLUMN recipe_id bigint;
+ALTER TABLE orders_order
+  ADD CONSTRAINT order_recipe_fkey FOREIGN KEY (recipe_id)
+      REFERENCES orders_recipe (id) DEFERRABLE;
+
+ALTER TABLE orders_order ADD COLUMN batchnumber character varying(255);
+
+-- end
