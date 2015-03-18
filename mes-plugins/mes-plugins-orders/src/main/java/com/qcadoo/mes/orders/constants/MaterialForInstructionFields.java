b6d0115b7aee4d5866d54f48ada9ecf3ebad41cd
Katarzyna Małyszek <katarzyna.malyszek@qcadoo.com>
2015-03-18 16:06:58 +0100

    SC# QCADOOCLS-3713

---
+++ mes-plugins/mes-plugins-orders/src/main/java/com/qcadoo/mes/orders/constants/MaterialForInstructionFields.java
@@ -0,0 +1,15 @@
+package com.qcadoo.mes.orders.constants;
+
+public final class MaterialForInstructionFields {
+
+    public static final String MATERIAL = "material";
+
+    public static final String PRODUCT = "product";
+
+    public static final String QUANTITY = "quantity";
+
+    public static final String INSTRUCTION = "instruction";
+
+    private MaterialForInstructionFields() {
+    }
+}
