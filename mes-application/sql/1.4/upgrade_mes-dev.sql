51474eb35d4d66006c293612ec45b4b336a3920d
Katarzyna Małyszek <katarzyna.malyszek@qcadoo.com>
2015-04-27 12:33:02 +0200

    SC# DUR (311) added factories to basic plugin

--- mes-application/sql/1.4/upgrade_mes-dev.sql
+++ mes-application/sql/1.4/upgrade_mes-dev.sql
@@ -14,3 +14,23 @@ ALTER TABLE productioncounting_trackingoperationproductoutcomponent ADD COLUMN g
 ALTER TABLE productioncounting_trackingoperationproductoutcomponent ADD COLUMN givenquantity numeric(14,5);
 
 -- end
+
+-- Added factories (DUR)
+-- last touched 27.04.2015 by kama 
+
+CREATE TABLE basic_factory
+(
+  id bigint NOT NULL,
+  "number" character varying(255),
+  name character varying(1024),
+  city character varying(255),
+  active boolean DEFAULT true,
+  CONSTRAINT basic_factory_pkey PRIMARY KEY (id)
+);
+
+ALTER TABLE basic_division ADD COLUMN factory_id bigint;
+ALTER TABLE basic_division
+  ADD CONSTRAINT division_factory_fkey FOREIGN KEY (factory_id)
+      REFERENCES basic_factory (id) DEFERRABLE;
+
+-- end
