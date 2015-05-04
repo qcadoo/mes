be52195d4bb5ce7317e4d47c4b71832d36a43648
Katarzyna Małyszek <katarzyna.malyszek@qcadoo.com>
2015-05-04 13:43:17 +0200

    CM# added sql scripts

--- mes-application/sql/1.4/upgrade_mes-dev.sql
+++ mes-application/sql/1.4/upgrade_mes-dev.sql
@@ -34,3 +34,100 @@ ALTER TABLE basic_division
       REFERENCES basic_factory (id) DEFERRABLE;
 
 -- end
+
+-- Scripts for DUR
+-- last touched 04.05.2015 by kama
+
+CREATE TABLE jointable_company_workstation
+(
+  company_id bigint NOT NULL,
+  workstation_id bigint NOT NULL,
+  CONSTRAINT jointable_company_workstation_pkey PRIMARY KEY (workstation_id, company_id),
+  CONSTRAINT jointable_company_workstation_company_fkey FOREIGN KEY (company_id)
+      REFERENCES basic_company (id) DEFERRABLE,
+  CONSTRAINT jointable_company_workstation_workstation_fkey FOREIGN KEY (workstation_id)
+      REFERENCES basic_workstation (id) DEFERRABLE
+);
+
+ALTER TABLE basic_workstation ADD COLUMN division_id bigint;
+ALTER TABLE basic_workstation
+  ADD CONSTRAINT workstation_division_fkey FOREIGN KEY (division_id)
+      REFERENCES basic_division (id) DEFERRABLE;
+ALTER TABLE basic_workstation ADD COLUMN serialnumber character varying(255);
+ALTER TABLE basic_workstation ADD COLUMN udtnumber character varying(255);
+ALTER TABLE basic_workstation ADD COLUMN series character varying(255);
+ALTER TABLE basic_workstation ADD COLUMN producer character varying(255);
+ALTER TABLE basic_workstation ADD COLUMN productiondate date;
+ALTER TABLE basic_workstation ADD COLUMN wnknumber character varying(255);
+
+CREATE TABLE basic_workstationattachment
+(
+  id bigint NOT NULL,
+  workstation_id bigint,
+  attachment character varying(255),
+  name character varying(255),
+  size numeric(12,5),
+  ext character varying(255),
+  CONSTRAINT basic_workstationattachment_pkey PRIMARY KEY (id),
+  CONSTRAINT workstationattachment_workstation_fkey FOREIGN KEY (workstation_id)
+      REFERENCES basic_workstation (id) DEFERRABLE
+);
+
+CREATE TABLE jointable_division_productionline
+(
+  productionline_id bigint NOT NULL,
+  division_id bigint NOT NULL,
+  CONSTRAINT jointable_division_productionline_pkey PRIMARY KEY (division_id, productionline_id),
+  CONSTRAINT jointable_division_productionline_productionline_fkey FOREIGN KEY (productionline_id)
+      REFERENCES productionlines_productionline (id) DEFERRABLE,
+  CONSTRAINT jointable_division_productionline_division_fkey FOREIGN KEY (division_id)
+      REFERENCES basic_division (id) DEFERRABLE
+);
+
+CREATE TABLE basic_subassembly
+(
+  id bigint NOT NULL,
+  "number" character varying(255),
+  name character varying(1024),
+  workstationtype_id bigint,
+  workstation_id bigint,
+  serialnumber character varying(255),
+  series character varying(255),
+  producer character varying(255),
+  productiondate date,
+  lastrepairsdate date,
+  active boolean DEFAULT true,
+  CONSTRAINT basic_subassembly_pkey PRIMARY KEY (id),
+  CONSTRAINT subassembly_workstationtype_fkey FOREIGN KEY (workstationtype_id)
+      REFERENCES basic_workstationtype (id) DEFERRABLE,
+  CONSTRAINT subassembly_workstation_fkey FOREIGN KEY (workstation_id)
+      REFERENCES basic_workstation (id) DEFERRABLE
+);
+
+CREATE TABLE jointable_company_subassembly
+(
+  company_id bigint NOT NULL,
+  subassembly_id bigint NOT NULL,
+  CONSTRAINT jointable_company_subassembly_pkey PRIMARY KEY (subassembly_id, company_id),
+  CONSTRAINT jointable_company_subassembly_company_fkey FOREIGN KEY (company_id)
+      REFERENCES basic_company (id) DEFERRABLE,
+  CONSTRAINT jointable_company_subassembly_subassembly_fkey FOREIGN KEY (subassembly_id)
+      REFERENCES basic_subassembly (id) DEFERRABLE
+);
+
+CREATE TABLE basic_subassemblyattachment
+(
+  id bigint NOT NULL,
+  subassembly_id bigint,
+  attachment character varying(255),
+  name character varying(255),
+  size numeric(12,5),
+  ext character varying(255),
+  CONSTRAINT basic_subassemblyattachment_pkey PRIMARY KEY (id),
+  CONSTRAINT subassemblyattachment_subassembly_fkey FOREIGN KEY (subassembly_id)
+      REFERENCES basic_subassembly (id) DEFERRABLE
+);
+
+ALTER TABLE basic_workstationtype ADD COLUMN subassembly boolean;
+
+-- end
