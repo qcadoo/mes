c4be7daeb3a8a5ec06fd96c66912aacdba010cd1
Katarzyna Małyszek <katarzyna.malyszek@qcadoo.com>
2015-05-05 10:31:38 +0200

    SC# factory structure

--- mes-plugins/mes-plugins-production-lines/src/main/java/com/qcadoo/mes/productionLines/factoryStructure/FactoryStructureGenerationService.java
+++ mes-plugins/mes-plugins-production-lines/src/main/java/com/qcadoo/mes/productionLines/factoryStructure/FactoryStructureGenerationService.java
@@ -1,19 +1,29 @@
 package com.qcadoo.mes.productionLines.factoryStructure;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.google.common.collect.Lists;
+import com.qcadoo.mes.basic.ParameterService;
 import com.qcadoo.mes.basic.constants.DivisionFields;
 import com.qcadoo.mes.basic.constants.WorkstationFields;
+import com.qcadoo.mes.productionLines.constants.ProductionLinesConstants;
+import com.qcadoo.model.api.DataDefinition;
 import com.qcadoo.model.api.DataDefinitionService;
 import com.qcadoo.model.api.Entity;
 import com.qcadoo.model.api.EntityTree;
+import com.qcadoo.model.api.utils.EntityTreeUtilsService;
 
 @Service
 public class FactoryStructureGenerationService {
 
     @Autowired
     private DataDefinitionService dataDefinitionService;
+
+    @Autowired
+    private ParameterService parameterService;
 
     public EntityTree generateFactoryStructureForWorkstation(final Entity workstation) {
         Entity division = workstation.getBelongsToField(WorkstationFields.DIVISION);
@@ -25,6 +35,15 @@ public class FactoryStructureGenerationService {
             return null;
         }
 
-        return null;
+        List<Entity> factoryStructureList = Lists.newArrayList();
+
+        DataDefinition elementDD = dataDefinitionService.get(ProductionLinesConstants.PLUGIN_IDENTIFIER,
+                ProductionLinesConstants.MODEL_FACTORY_STRUCTURE_ELEMENT);
+
+        Entity root = elementDD.create();
+
+        EntityTree factoryStructure = EntityTreeUtilsService.getDetachedEntityTree(factoryStructureList);
+
+        return factoryStructure;
     }
 }
