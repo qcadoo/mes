e87922c5a271206dafe4fca7d007bd2648bbafd6
ksiwonia <kamil.siwonia@gmial.com>
2015-05-22 07:20:12 +0200

    SC#GOODFOOD-402, GOODFOOD-400, GOODFOOD-399, GOODFOOD-398

--- mes-plugins/mes-plugins-warehouse-minimal-state/src/main/java/com/qcadoo/mes/warehouseMinimalState/hooks/WarehouseMinimumStateAddMulti.java
+++ mes-plugins/mes-plugins-warehouse-minimal-state/src/main/java/com/qcadoo/mes/warehouseMinimalState/hooks/WarehouseMinimumStateAddMulti.java
@@ -1,14 +1,26 @@
 package com.qcadoo.mes.warehouseMinimalState.hooks;
 
 import com.qcadoo.view.api.ViewDefinitionState;
+import com.qcadoo.view.api.components.FormComponent;
 import com.qcadoo.view.api.components.LookupComponent;
+import com.qcadoo.view.api.components.WindowComponent;
+import com.qcadoo.view.api.ribbon.RibbonActionItem;
 import org.springframework.stereotype.Service;
 
-@Service
-public class WarehouseMinimumStateAddMulti {
+@Service public class WarehouseMinimumStateAddMulti {
+
+    private static final String LFORM = "form";
+
+    private static final String L_WINDOW = "window";
 
     public void onBeforeRender(final ViewDefinitionState view) {
         LookupComponent location = (LookupComponent) view.getComponentByReference("location");
         location.setRequired(true);
+        WindowComponent window = (WindowComponent) view.getComponentByReference(L_WINDOW);
+        FormComponent form = (FormComponent) view.getComponentByReference(LFORM);
+        RibbonActionItem addMultiButton = window.getRibbon().getGroupByName("action").getItemByName("createMultiMinimalStates");
+        addMultiButton.setMessage("warehouseMinimalState.warehouseMinimumStateAddMulti.createMultiMinimalStates.button.message");
+        addMultiButton.requestUpdate(true);
+        window.requestRibbonRender();
     }
 }
