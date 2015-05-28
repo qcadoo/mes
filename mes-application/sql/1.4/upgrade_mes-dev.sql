0bf815e1a6f5a237977436f034b80270f2b22758
Katarzyna Małyszek <katarzyna.malyszek@qcadoo.com>
2015-05-28 13:48:02 +0200

    CM# added scripts for costs norms

--- mes-application/sql/1.4/upgrade_mes-dev.sql
+++ mes-application/sql/1.4/upgrade_mes-dev.sql
@@ -128,3 +128,33 @@ CREATE TABLE jointable_product_warehouseminimumstatemulti
 );
 
 -- end
+
+-- Added updating cost norms for products
+-- last touched 28.05.2015 by kama
+
+CREATE TABLE materialflowresources_costnormsgenerator
+(
+  id bigint NOT NULL,
+  costssource character varying(255) DEFAULT '01mes'::character varying,
+  productstoupdate character varying(255) DEFAULT '01all'::character varying,
+  CONSTRAINT materialflowresources_costnormsgenerator_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE materialflowresources_costnormslocation
+(
+  id bigint NOT NULL,
+  costnormsgenerator_id bigint,
+  location_id bigint,
+  CONSTRAINT materialflowresources_costnormslocation_pkey PRIMARY KEY (id),
+  CONSTRAINT costnormslocation_costnormsgenerator_fkey FOREIGN KEY (costnormsgenerator_id)
+      REFERENCES materialflowresources_costnormsgenerator (id) DEFERRABLE,
+  CONSTRAINT costnormslocation_location_fkey FOREIGN KEY (location_id)
+      REFERENCES materialflow_location (id) DEFERRABLE
+);
+
+ALTER TABLE basic_product ADD COLUMN costnormsgenerator_id bigint;
+ALTER TABLE basic_product
+  ADD CONSTRAINT product_costnormsgenerator_fkey FOREIGN KEY (costnormsgenerator_id)
+      REFERENCES materialflowresources_costnormsgenerator (id) DEFERRABLE;
+
+-- end
