ae1eac5f0b5c2b4091c2acec88ad110a90d421e9
ksiwonia <kamil.siwonia@gmial.com>
2015-06-03 07:44:58 +0200

    SC#QCADOO-408

--- mes-application/src/main/resources/views.sql
+++ mes-application/src/main/resources/views.sql
@@ -33,3 +33,7 @@ CREATE OR REPLACE FUNCTION createWarehouseStockView () RETURNS boolean AS 'BEGIN
 SELECT createWarehouseStockView();
 
 DROP FUNCTION createWarehouseStockView ();
+
+CREATE OR REPLACE FUNCTION update_sequence() RETURNS VOID AS $$ DECLARE row record; BEGIN FOR row IN SELECT tablename FROM pg_tables p INNER JOIN information_schema.columns c on p.tablename = c.table_name WHERE c.table_schema = 'public' and p.schemaname = 'public'  and c.column_name = 'id' and data_type = 'bigint' LOOP  EXECUTE 'ALTER TABLE ' || quote_ident(row.tablename) || ' ALTER COLUMN id SET DEFAULT nextval(''' || quote_ident(row.tablename) || '_id_seq'');'; EXECUTE 'SELECT setval(''' || quote_ident(row.tablename) || '_id_seq'', COALESCE((SELECT MAX(id)+1 FROM ' || quote_ident(row.tablename) || '), 1), false);'; END LOOP; END; $$ LANGUAGE 'plpgsql';
+SELECT * FROM update_sequence();
+DROP FUNCTION update_sequence();
