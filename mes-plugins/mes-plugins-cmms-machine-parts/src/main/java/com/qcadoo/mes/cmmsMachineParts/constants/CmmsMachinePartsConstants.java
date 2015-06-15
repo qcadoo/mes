19fdadf4a34c325fd217745028b0ce632da093a4
Paweł Kochanowski <pawel.kochanowski@qcadoo.com>
2015-06-15 15:29:45 +0200

    SC# GF-457 added constants

---
+++ mes-plugins/mes-plugins-cmms-machine-parts/src/main/java/com/qcadoo/mes/cmmsMachineParts/constants/CmmsMachinePartsConstants.java
@@ -0,0 +1,14 @@
+package com.qcadoo.mes.cmmsMachineParts.constants;
+
+
+public class CmmsMachinePartsConstants {
+
+    private CmmsMachinePartsConstants() {
+    }
+
+    public static final String PLUGIN_IDENTIFIER = "cmmsMachineParts";
+
+    // MODEL
+
+    public static final String MODEL_FAULT_TYPE = "faultType";
+}
