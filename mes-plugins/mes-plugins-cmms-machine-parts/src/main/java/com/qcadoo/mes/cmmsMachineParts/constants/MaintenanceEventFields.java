cfea1d791352d827553b487296d13ec33273a02e
Tomasz Woliński <tomasz.wolinski@qcadoo.com>
2015-06-26 15:02:18 +0200

    SC #GOODFOOD-495

--- mes-plugins/mes-plugins-cmms-machine-parts/src/main/java/com/qcadoo/mes/cmmsMachineParts/constants/MaintenanceEventFields.java
+++ mes-plugins/mes-plugins-cmms-machine-parts/src/main/java/com/qcadoo/mes/cmmsMachineParts/constants/MaintenanceEventFields.java
@@ -9,4 +9,11 @@ public class MaintenanceEventFields {
     public static final String NUMBER = "number";
     public static final String TYPE = "type";
     public static final String FACTORY_STRUCTURE = "factoryStructure";
+
+    public static final String FACTORY = "factory";
+    public static final String DIVISION = "division";
+    public static final String PRODUCTION_LINE = "productionLine";
+    public static final String WORKSTATION = "workstation";
+    public static final String SUBASSEMBLY = "subassembly";
+
 }
