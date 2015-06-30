d537a0905f1620a44a2e7f2bb6a28ec0491c5bf8
Katarzyna Małyszek <katarzyna.malyszek@qcadoo.com>
2015-06-30 13:52:43 +0200

    SC# DUR(511) added filters to fault types

--- mes-plugins/mes-plugins-cmms-machine-parts/src/main/java/com/qcadoo/mes/cmmsMachineParts/constants/MaintenanceEventFields.java
+++ mes-plugins/mes-plugins-cmms-machine-parts/src/main/java/com/qcadoo/mes/cmmsMachineParts/constants/MaintenanceEventFields.java
@@ -1,19 +1,26 @@
 package com.qcadoo.mes.cmmsMachineParts.constants;
 
-
 public class MaintenanceEventFields {
 
     private MaintenanceEventFields() {
     }
 
+    public static final String FAULT_TYPE = "faultType";
+
     public static final String NUMBER = "number";
+
     public static final String TYPE = "type";
+
     public static final String FACTORY_STRUCTURE = "factoryStructure";
 
     public static final String FACTORY = "factory";
+
     public static final String DIVISION = "division";
+
     public static final String PRODUCTION_LINE = "productionLine";
+
     public static final String WORKSTATION = "workstation";
+
     public static final String SUBASSEMBLY = "subassembly";
 
 }
