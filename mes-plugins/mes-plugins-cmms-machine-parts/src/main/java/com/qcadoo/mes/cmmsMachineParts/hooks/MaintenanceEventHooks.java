31f11bfefe1147581bc532cb5ff17c0adfe4402f
Katarzyna Małyszek <katarzyna.malyszek@qcadoo.com>
2015-07-09 15:20:49 +0200

    DF# 575 fixed history tab

---
+++ mes-plugins/mes-plugins-cmms-machine-parts/src/main/java/com/qcadoo/mes/cmmsMachineParts/hooks/MaintenanceEventHooks.java
@@ -0,0 +1,29 @@
+package com.qcadoo.mes.cmmsMachineParts.hooks;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.qcadoo.mes.cmmsMachineParts.states.constants.MaintenanceEventState;
+import com.qcadoo.mes.cmmsMachineParts.states.constants.MaintenanceEventStateChangeDescriber;
+import com.qcadoo.mes.states.service.StateChangeEntityBuilder;
+import com.qcadoo.model.api.DataDefinition;
+import com.qcadoo.model.api.Entity;
+
+@Service
+public class MaintenanceEventHooks {
+
+    @Autowired
+    private MaintenanceEventStateChangeDescriber describer;
+
+    @Autowired
+    private StateChangeEntityBuilder stateChangeEntityBuilder;
+
+    public void onCreate(final DataDefinition eventDD, final Entity event) {
+        setInitialState(event);
+    }
+
+    private void setInitialState(final Entity event) {
+        stateChangeEntityBuilder.buildInitial(describer, event, MaintenanceEventState.NEW);
+    }
+
+}
