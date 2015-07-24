c63ed9112ec6e01b2cf78191fcbc4216fb0c9fc8
Tomasz Woliński <tomasz.wolinski@qcadoo.com>
2015-07-24 15:14:54 +0200

    SC #GOODFOOD-614 default factory and division sql scripts

--- mes-application/sql/1.4/upgrade_mes-dev.sql
+++ mes-application/sql/1.4/upgrade_mes-dev.sql
@@ -17,5 +17,22 @@ CREATE TABLE cmmsmachineparts_machinepartforevent
   CONSTRAINT machinepartforevent_machinepart_fkey FOREIGN KEY (machinepart_id)
       REFERENCES basic_product (id) DEFERRABLE
 );
+-- GOODFOOD-614
+
+CREATE TABLE cmmsmachineparts_maintenanceeventcontext
+(
+  id bigint NOT NULL,
+  factory_id bigint,
+  division_id bigint,
+  generated boolean DEFAULT false,
+  confirmed boolean DEFAULT false,
+  CONSTRAINT cmmsmachineparts_maintenanceeventcontext_pkey PRIMARY KEY (id),
+  CONSTRAINT maintenanceeventcontext_division_fkey FOREIGN KEY (division_id) REFERENCES basic_division (id) DEFERRABLE,
+  CONSTRAINT maintenanceeventcontext_factory_fkey FOREIGN KEY (factory_id) REFERENCES basic_factory (id) DEFERRABLE
+);
+
+ALTER TABLE cmmsmachineparts_maintenanceevent ADD maintenanceeventcontext_id bigint;
+ALTER TABLE cmmsmachineparts_maintenanceevent ADD CONSTRAINT maintenanceevent_maintenanceeventcontext_fkey FOREIGN KEY (maintenanceeventcontext_id)
+      REFERENCES cmmsmachineparts_maintenanceeventcontext (id) DEFERRABLE;
 
 -- end
