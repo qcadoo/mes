a270ee6ada0a25fac063577b367955d5c45e7d3a
Tomasz Woliński <tomasz.wolinski@qcadoo.com>
2015-07-27 09:10:22 +0200

    DF #GOODFOOD-390

--- mes-plugins/mes-plugins-cmms-machine-parts/src/main/java/com/qcadoo/mes/cmmsMachineParts/criteriaModifiers/ProductCriteriaModifiersCMP.java
+++ mes-plugins/mes-plugins-cmms-machine-parts/src/main/java/com/qcadoo/mes/cmmsMachineParts/criteriaModifiers/ProductCriteriaModifiersCMP.java
@@ -1,19 +1,20 @@
 package com.qcadoo.mes.cmmsMachineParts.criteriaModifiers;
 
-import static com.qcadoo.mes.basic.constants.ProductFields.ENTITY_TYPE;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.qcadoo.mes.basic.constants.ProductFamilyElementType;
 import com.qcadoo.mes.basic.criteriaModifiers.ProductCriteriaModifiers;
 import com.qcadoo.mes.cmmsMachineParts.constants.ProductFieldsCMP;
 import com.qcadoo.model.api.search.SearchCriteriaBuilder;
 import com.qcadoo.model.api.search.SearchRestrictions;
 import com.qcadoo.view.api.components.lookup.FilterValueHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import static com.qcadoo.mes.basic.constants.ProductFields.ENTITY_TYPE;
 
 @Service
 public class ProductCriteriaModifiersCMP {
+
+    public static final String MACHINE_PART_FILTER_PARAMETER = "machinePart";
 
     @Autowired
     private ProductCriteriaModifiers productCriteriaModifiers;
@@ -27,13 +28,20 @@ public class ProductCriteriaModifiersCMP {
         scb.add(SearchRestrictions.eq(ProductFieldsCMP.MACHINE_PART, true));
     }
 
-    public void showFamilyMachineParts(final SearchCriteriaBuilder scb) {
-        scb.add(SearchRestrictions.eq(ProductFieldsCMP.MACHINE_PART, true));
+    public void showFamiliesByMachinePartType(final SearchCriteriaBuilder scb, final FilterValueHolder filterValue) {
+        if(!filterValue.has(MACHINE_PART_FILTER_PARAMETER)){
+            throw new IllegalArgumentException(MACHINE_PART_FILTER_PARAMETER);
+        }
+
+        boolean machinePart = filterValue.getBoolean(MACHINE_PART_FILTER_PARAMETER);
+
         scb.add(SearchRestrictions.eq(ENTITY_TYPE, ProductFamilyElementType.PRODUCTS_FAMILY.getStringValue()));
+        if(machinePart){
+            scb.add(SearchRestrictions.eq(ProductFieldsCMP.MACHINE_PART, true));
+
+        } else {
+            scb.add(SearchRestrictions.or(SearchRestrictions.eq(ProductFieldsCMP.MACHINE_PART, false), SearchRestrictions.isNull(ProductFieldsCMP.MACHINE_PART)));
+        }
     }
 
-    public void showFamilyParticularProduct(final SearchCriteriaBuilder scb) {
-        scb.add(SearchRestrictions.or(SearchRestrictions.eq(ProductFieldsCMP.MACHINE_PART, false), SearchRestrictions.isNull(ProductFieldsCMP.MACHINE_PART)));
-        scb.add(SearchRestrictions.eq(ENTITY_TYPE, ProductFamilyElementType.PRODUCTS_FAMILY.getStringValue()));
-    }
 }
