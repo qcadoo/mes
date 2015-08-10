5cc8250a59671db22a2dc683c5b94a49be7db09a
Katarzyna Małyszek <katarzyna.malyszek@qcadoo.com>
2015-08-10 09:49:03 +0200

    SC# GOODFOOD-655 actions for planned events

---
+++ mes-plugins/mes-plugins-cmms-machine-parts/src/main/java/com/qcadoo/mes/cmmsMachineParts/constants/ActionForPlannedEventFields.java
@@ -0,0 +1,21 @@
+package com.qcadoo.mes.cmmsMachineParts.constants;
+
+public final class ActionForPlannedEventFields {
+
+    public static final String PLANNED_EVENT = "plannedEvent";
+
+    public static final String ACTION = "action";
+
+    public static final String RESPONSIBLE_WORKER = "responsibleWorker";
+
+    public static final String RESPONSIBLE_WORKER_NAME = "responsibleWorkerName";
+
+    public static final String DESCRIPTION = "description";
+
+    public static final String STATE = "state";
+
+    public static final String REASON = "reason";
+
+    private ActionForPlannedEventFields() {
+    }
+}
