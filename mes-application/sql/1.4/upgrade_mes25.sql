7ad676a840e37a668541c0d6fe6286dde41c2078
Lukasz Poniewierski <lukasz.poniewierski@qcadoo.com>
2015-08-17 16:00:51 +0200

    DF Fixed problem with sql.

--- mes-application/sql/1.4/upgrade_mes25.sql
+++ mes-application/sql/1.4/upgrade_mes25.sql
@@ -1,32 +1,32 @@
-
-
 -- QCADOOCLS-4315
+
 CREATE OR REPLACE VIEW materialflowresources_orderedquantity AS
- SELECT COALESCE(sum(orderedproduct.orderedquantity), 0::numeric) AS orderedquantity, resource.id as resource_id
+ SELECT COALESCE(sum(orderedproduct.orderedquantity), 0::numeric) AS orderedquantity, resource.id AS resource_id
    FROM materialflowresources_resource resource
 	 JOIN deliveries_orderedproduct orderedproduct ON (orderedproduct.product_id = resource.product_id )
 	 JOIN deliveries_delivery delivery ON (orderedproduct.delivery_id = delivery.id AND delivery.active = true AND delivery.location_id = resource.location_id AND (delivery.state::text = ANY (ARRAY['01draft'::character varying::text,
 		'02prepared'::character varying::text, '03duringCorrection'::character varying::text, '05approved'::character varying::text])))
-group by resource.id;
+GROUP BY resource.id;
 
 CREATE OR REPLACE VIEW materialflowresources_warehousestocklistdto_internal AS
  SELECT row_number() OVER () AS id,
     resource.location_id,
     resource.product_id,
-    sum(resource.quantity) AS quantity,
-    COALESCE(orderedquantity.orderedquantity, 0::numeric) as orderedquantity,
-    ( SELECT sum(warehouseminimalstate_warehouseminimumstate.minimumstate) AS sum
+    SUM(resource.quantity) AS quantity,
+    COALESCE(orderedquantity.orderedquantity, 0::numeric) AS orderedquantity,
+    (SELECT sum(warehouseminimalstate_warehouseminimumstate.minimumstate) AS sum
            FROM warehouseminimalstate_warehouseminimumstate
           WHERE warehouseminimalstate_warehouseminimumstate.product_id = resource.product_id AND warehouseminimalstate_warehouseminimumstate.location_id = resource.location_id) AS minimumstate
    FROM materialflowresources_resource resource
-   left join materialflowresources_orderedquantity orderedquantity ON (orderedquantity.resource_id = resource.id)
+   left JOIN materialflowresources_orderedquantity orderedquantity ON (orderedquantity.resource_id = resource.id)
   GROUP BY resource.location_id, resource.product_id, orderedquantity.orderedquantity;
 
+CREATE SEQUENCE materialflowresources_warehousestocklistdto_id_seq;
 
 CREATE OR REPLACE VIEW materialflowresources_warehousestocklistdto AS
-select internal.*, location.number as locationNumber, location.name as locationName, product.number as productNumber, product.name as productName, product.unit as productUnit
-	from materialflowresources_warehousestocklistdto_internal internal
-	join materialflow_location location ON (location.id = internal.location_id)
-	join basic_product product ON (product.id = internal.product_id);
+SELECT internal.*, location.number AS locationNumber, location.name AS locationName, product.number AS productNumber, product.name AS productName, product.unit AS productUnit
+	FROM materialflowresources_warehousestocklistdto_internal internal
+	JOIN materialflow_location location ON (location.id = internal.location_id)
+	JOIN basic_product product ON (product.id = internal.product_id);
 
 -- end
