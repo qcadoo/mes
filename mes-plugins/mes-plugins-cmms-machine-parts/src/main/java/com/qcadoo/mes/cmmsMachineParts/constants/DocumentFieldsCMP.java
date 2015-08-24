8c25ca6cddc8f8620b1627e1f2c671f0ad5b33f7
ksiwonia <kamil.siwonia@gmial.com>
2015-08-24 13:43:52 +0200

    SC#GOODFOOD-661

---
+++ mes-plugins/mes-plugins-cmms-machine-parts/src/main/java/com/qcadoo/mes/cmmsMachineParts/constants/DocumentFieldsCMP.java
@@ -0,0 +1,12 @@
+package com.qcadoo.mes.cmmsMachineParts.constants;
+
+public class DocumentFieldsCMP {
+
+    private DocumentFieldsCMP() {
+    }
+
+    public static final String MAINTENANCE_EVENT = "maintenanceEvent";
+
+    public static final String PLANNED_EVENT = "plannedEvent";
+
+}
