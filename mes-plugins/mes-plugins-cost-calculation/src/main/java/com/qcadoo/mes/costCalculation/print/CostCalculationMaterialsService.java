94099b4d2c092e3190d8699b98c097296c190813
Katarzyna Małyszek <katarzyna.malyszek@qcadoo.com>
2015-09-17 08:17:54 +0200

    SC# NBLS-18

---
+++ mes-plugins/mes-plugins-cost-calculation/src/main/java/com/qcadoo/mes/costCalculation/print/CostCalculationMaterialsService.java
@@ -0,0 +1,73 @@
+package com.qcadoo.mes.costCalculation.print;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.google.common.collect.Lists;
+import com.qcadoo.mes.basic.constants.ProductFields;
+import com.qcadoo.mes.costCalculation.constants.CostCalculationFields;
+import com.qcadoo.mes.costCalculation.print.utils.CostCalculationMaterial;
+import com.qcadoo.mes.costNormsForMaterials.ProductsCostCalculationService;
+import com.qcadoo.mes.technologies.ProductQuantitiesService;
+import com.qcadoo.model.api.Entity;
+import com.qcadoo.model.api.NumberService;
+
+@Service
+public class CostCalculationMaterialsService {
+
+    @Autowired
+    private ProductQuantitiesService productQuantitiesService;
+
+    @Autowired
+    private ProductsCostCalculationService productsCostCalculationService;
+
+    @Autowired
+    private NumberService numberService;
+
+    public List<CostCalculationMaterial> getSortedMaterialsFromProductQuantities(final Entity costCalculation,
+            final Map<Long, BigDecimal> neededProductQuantities, final Entity order) {
+        MathContext mathContext = numberService.getMathContext();
+        List<CostCalculationMaterial> list = Lists.newArrayList();
+        for (Map.Entry<Long, BigDecimal> neededProductQuantity : neededProductQuantities.entrySet()) {
+            Entity product = productQuantitiesService.getProduct(neededProductQuantity.getKey());
+
+            Entity productEntity = productsCostCalculationService.getAppropriateCostNormForProduct(product, order,
+                    costCalculation.getStringField(CostCalculationFields.SOURCE_OF_MATERIAL_COSTS));
+
+            BigDecimal productQuantity = neededProductQuantity.getValue();
+
+            BigDecimal costForGivenQuantity = productsCostCalculationService.calculateProductCostForGivenQuantity(productEntity,
+                    productQuantity, costCalculation.getStringField(CostCalculationFields.CALCULATE_MATERIAL_COSTS_MODE));
+
+            BigDecimal materialCostMargin = costCalculation.getDecimalField(CostCalculationFields.MATERIAL_COST_MARGIN);
+
+            if (materialCostMargin == null) {
+
+                list.add(new CostCalculationMaterial(product.getStringField(ProductFields.NUMBER), product
+                        .getStringField(ProductFields.UNIT), productQuantity, costForGivenQuantity));
+            } else {
+                BigDecimal toAdd = costForGivenQuantity.multiply(materialCostMargin.divide(new BigDecimal(100), mathContext),
+                        mathContext);
+                BigDecimal totalCosts = costForGivenQuantity.add(toAdd, mathContext);
+
+                list.add(new CostCalculationMaterial(product.getStringField(ProductFields.NUMBER), product
+                        .getStringField(ProductFields.UNIT), productQuantity, costForGivenQuantity, totalCosts, toAdd));
+            }
+
+        }
+        list.sort(new Comparator<CostCalculationMaterial>() {
+
+            @Override
+            public int compare(CostCalculationMaterial o1, CostCalculationMaterial o2) {
+                return o1.getCostForGivenQuantity().compareTo(o2.getCostForGivenQuantity());
+            }
+        });
+        return list;
+    }
+}
