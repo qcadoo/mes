dc1f776934528917994f14bd0c531cc5820665c2
Lukasz Poniewierski <lukasz.poniewierski@qcadoo.com>
2015-10-20 11:23:07 +0200

    DF#GOODFOOD-754 Fixed problem with line change over norms.

--- mes-plugins/mes-plugins-line-changeover-norms-for-orders/src/main/java/com/qcadoo/mes/lineChangeoverNormsForOrders/hooks/LineChangeoverNormsForOrderDetailsViewHooks.java
+++ mes-plugins/mes-plugins-line-changeover-norms-for-orders/src/main/java/com/qcadoo/mes/lineChangeoverNormsForOrders/hooks/LineChangeoverNormsForOrderDetailsViewHooks.java
@@ -23,23 +23,16 @@
  */
 package com.qcadoo.mes.lineChangeoverNormsForOrders.hooks;
 
-import static com.qcadoo.mes.lineChangeoverNorms.constants.LineChangeoverNormsFields.DURATION;
-import static com.qcadoo.mes.lineChangeoverNormsForOrders.constants.LineChangeoverNormsForOrdersConstants.ORDER_FIELDS;
-import static com.qcadoo.mes.lineChangeoverNormsForOrders.constants.LineChangeoverNormsForOrdersConstants.PREVIOUS_ORDER_FIELDS;
-import static com.qcadoo.mes.lineChangeoverNormsForOrders.constants.OrderFieldsLCNFO.LINE_CHANGEOVER_NORM;
-import static com.qcadoo.mes.lineChangeoverNormsForOrders.constants.OrderFieldsLCNFO.ORDER;
-import static com.qcadoo.mes.lineChangeoverNormsForOrders.constants.OrderFieldsLCNFO.OWN_LINE_CHANGEOVER;
-import static com.qcadoo.mes.lineChangeoverNormsForOrders.constants.OrderFieldsLCNFO.OWN_LINE_CHANGEOVER_DURATION;
-import static com.qcadoo.mes.lineChangeoverNormsForOrders.constants.OrderFieldsLCNFO.PREVIOUS_ORDER;
-import static com.qcadoo.mes.orders.constants.OrderFields.PRODUCTION_LINE;
-
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.qcadoo.mes.lineChangeoverNorms.ChangeoverNormsSearchService;
 import com.qcadoo.mes.lineChangeoverNorms.ChangeoverNormsService;
+import com.qcadoo.mes.lineChangeoverNorms.constants.LineChangeoverNormsFields;
 import com.qcadoo.mes.lineChangeoverNormsForOrders.LineChangeoverNormsForOrdersService;
+import com.qcadoo.mes.lineChangeoverNormsForOrders.constants.LineChangeoverNormsForOrdersConstants;
+import com.qcadoo.mes.lineChangeoverNormsForOrders.constants.OrderFieldsLCNFO;
 import com.qcadoo.mes.orders.OrderService;
 import com.qcadoo.mes.orders.constants.OrderFields;
 import com.qcadoo.model.api.Entity;
@@ -71,8 +64,8 @@ public class LineChangeoverNormsForOrderDetailsViewHooks {
     public final void fillOrderForms(final ViewDefinitionState view) {
         FormComponent orderForm = (FormComponent) view.getComponentByReference(L_FORM);
 
-        LookupComponent orderLookup = (LookupComponent) view.getComponentByReference(ORDER);
-        LookupComponent previousOrderLookup = (LookupComponent) view.getComponentByReference(PREVIOUS_ORDER);
+        LookupComponent orderLookup = (LookupComponent) view.getComponentByReference(OrderFieldsLCNFO.ORDER);
+        LookupComponent previousOrderLookup = (LookupComponent) view.getComponentByReference(OrderFieldsLCNFO.PREVIOUS_ORDER);
 
         if (orderForm.getEntityId() == null) {
             return;
@@ -81,14 +74,17 @@ public class LineChangeoverNormsForOrderDetailsViewHooks {
 
         orderLookup.setFieldValue(order.getId());
         orderLookup.requestComponentUpdateState();
-        lineChangeoverNormsForOrdersService.fillOrderForm(view, ORDER_FIELDS);
+
+        lineChangeoverNormsForOrdersService.fillOrderForm(view, LineChangeoverNormsForOrdersConstants.ORDER_FIELDS);
 
         if (previousOrderLookup.isEmpty()) {
             Entity previousOrder = lineChangeoverNormsForOrdersService.getPreviousOrderFromDB(order);
+
             if (previousOrder != null) {
                 previousOrderLookup.setFieldValue(previousOrder.getId());
                 previousOrderLookup.requestComponentUpdateState();
-                lineChangeoverNormsForOrdersService.fillOrderForm(view, PREVIOUS_ORDER_FIELDS);
+
+                lineChangeoverNormsForOrdersService.fillOrderForm(view, LineChangeoverNormsForOrdersConstants.PREVIOUS_ORDER_FIELDS);
             }
         }
     }
@@ -99,20 +95,23 @@ public class LineChangeoverNormsForOrderDetailsViewHooks {
     }
 
     private Entity findChangeoverNorm(final ViewDefinitionState view) {
-        LookupComponent orderLookup = (LookupComponent) view.getComponentByReference(ORDER);
-        LookupComponent previousOrderLookup = (LookupComponent) view.getComponentByReference(PREVIOUS_ORDER);
+        LookupComponent orderLookup = (LookupComponent) view.getComponentByReference(OrderFieldsLCNFO.ORDER);
+        LookupComponent previousOrderLookup = (LookupComponent) view.getComponentByReference(OrderFieldsLCNFO.PREVIOUS_ORDER);
 
         Entity order = orderLookup.getEntity();
+
         if (order == null) {
             return null;
         }
+
         Entity fromTechnology = extractTechnologyPrototypeFrom(previousOrderLookup.getEntity());
         Entity toTechnology = extractTechnologyPrototypeFrom(order);
 
         if (fromTechnology == null || toTechnology == null) {
             return null;
         }
-        Entity productionLine = order.getBelongsToField(PRODUCTION_LINE);
+
+        Entity productionLine = order.getBelongsToField(OrderFields.PRODUCTION_LINE);
 
         return changeoverNormsService.getMatchingChangeoverNorms(fromTechnology, toTechnology, productionLine);
     }
@@ -121,6 +120,7 @@ public class LineChangeoverNormsForOrderDetailsViewHooks {
         if (order == null) {
             return null;
         }
+
         return order.getBelongsToField(OrderFields.TECHNOLOGY_PROTOTYPE);
     }
 
@@ -134,18 +134,19 @@ public class LineChangeoverNormsForOrderDetailsViewHooks {
             lineChangeoverNormDurationField.setFieldValue(null);
         } else {
             lineChangeoverNormField.setFieldValue(lineChangeoverNorm.getId());
-            lineChangeoverNormDurationField.setFieldValue(lineChangeoverNorm.getField(DURATION));
+            lineChangeoverNormDurationField.setFieldValue(lineChangeoverNorm.getField(LineChangeoverNormsFields.DURATION));
         }
+
         lineChangeoverNormField.requestComponentUpdateState();
         lineChangeoverNormDurationField.requestComponentUpdateState();
     }
 
     public void updateRibbonState(final ViewDefinitionState view) {
-        FieldComponent productionLineField = (FieldComponent) view.getComponentByReference(PRODUCTION_LINE);
+        FieldComponent productionLineField = (FieldComponent) view.getComponentByReference(OrderFields.PRODUCTION_LINE);
 
-        LookupComponent previousOrderLookup = (LookupComponent) view.getComponentByReference(PREVIOUS_ORDER);
+        LookupComponent previousOrderLookup = (LookupComponent) view.getComponentByReference(OrderFieldsLCNFO.PREVIOUS_ORDER);
 
-        LookupComponent lineChangeoverNormLookup = (LookupComponent) view.getComponentByReference(LINE_CHANGEOVER_NORM);
+        LookupComponent lineChangeoverNormLookup = (LookupComponent) view.getComponentByReference(OrderFieldsLCNFO.LINE_CHANGEOVER_NORM);
 
         FieldComponent previousOrderTechnologyGroupNumberField = (FieldComponent) view
                 .getComponentByReference("previousOrderTechnologyGroupNumber");
@@ -221,7 +222,7 @@ public class LineChangeoverNormsForOrderDetailsViewHooks {
     }
 
     public void showOwnLineChangeoverDurationField(final ViewDefinitionState view) {
-        orderService.changeFieldState(view, OWN_LINE_CHANGEOVER, OWN_LINE_CHANGEOVER_DURATION);
+        orderService.changeFieldState(view, OrderFieldsLCNFO.OWN_LINE_CHANGEOVER, OrderFieldsLCNFO.OWN_LINE_CHANGEOVER_DURATION);
     }
 
 }
