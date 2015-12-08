c1c4c34aabe20c79336c62d27a53f3dff2ec2427
Katarzyna Małyszek <katarzyna.malyszek@qcadoo.com>
2015-12-08 09:09:13 +0100

    DF# GOODFOOD-1201

--- mes-application/sql/1.5/upgrade_mes-dev.sql
+++ mes-application/sql/1.5/upgrade_mes-dev.sql
@@ -4,7 +4,7 @@ CREATE SEQUENCE basic_subassemblylistdto_id_seq;
 create or replace view basic_subassemblyListDto as
 select
     s.id, s.active, s.number, s.name, workstation.number as workstationNumber, workstationType.number as workstationTypeNumber,
-    s.productionDate, event.maxDate as lastRepairsDate
+    date(s.productionDate), date(event.maxDate) as lastRepairsDate
     from basic_subassembly s
     left join basic_workstation workstation on (s.workstation_id = workstation.id)
     join basic_workstationType workstationType on (s.workstationtype_id = workstationType.id)
