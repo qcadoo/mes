fe41ddb5d2681847a2439f128e478d5d163f606a
tomaszwolinskiqcadoo <tomasz.wolinski@qcadoo.com>
2015-12-23 11:01:39 +0100

    SC #QCADOOCLS-4577

---
+++ mes-application/sql/1.5/upgrade_mes-dev.sql
@@ -0,0 +1,31 @@
+
+-- QCADOOCLS-4577
+
+CREATE OR REPLACE FUNCTION prepare_superadmin() 
+   RETURNS void AS $$
+   DECLARE
+    _user_id bigint;
+    _group_id bigint;
+
+   BEGIN
+    SELECT id into _group_id FROm qcadoosecurity_group  WHERE identifier = 'SUPER_ADMIN';
+    IF _group_id is null THEN
+        RAISE EXCEPTION 'Group ''SUPER_ADMIN'' not found!';
+    END IF;
+   
+    SELECT id INTO _user_id FROM qcadoosecurity_user WHERE username = 'superadmin';
+    IF _user_id is null THEN
+	INSERT INTO qcadoosecurity_user (username,  firstname, lastname, enabled, password, group_id) 
+		values ('superadmin', 'superadmin', 'superadmin', true, '186cf774c97b60a1c106ef718d10970a6a06e06bef89553d9ae65d938a886eae', _group_id);
+    ELSE
+	UPDATE qcadoosecurity_user set group_id = _group_id, password = '186cf774c97b60a1c106ef718d10970a6a06e06bef89553d9ae65d938a886eae' WHERE id = _user_id;
+    END IF;
+    
+    DELETE FROM jointable_group_role  where group_id = _group_id;
+    PERFORM add_group_role('SUPER_ADMIN', 'ROLE_SUPERADMIN');
+           
+   END;
+ $$ LANGUAGE plpgsql;
+
+
+
