d3c244922408c33fb10c70d451b018d9194fb3a2
wesi <weronika.sikorska@qcadoo.com>
2016-01-25 08:38:24 +0100

    SC blocked delete and edit of default fault and action

---
+++ mes-plugins/mes-plugins-cmms-machine-parts/src/main/java/com/qcadoo/mes/cmmsMachineParts/listeners/FaultTypeListListeners.java
@@ -0,0 +1,70 @@
+/**
+ * ***************************************************************************
+ * Copyright (c) 2010 Qcadoo Limited
+ * Project: Qcadoo MES
+ * Version: 1.4
+ *
+ * This file is part of Qcadoo.
+ *
+ * Qcadoo is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation; either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * ***************************************************************************
+ */
+package com.qcadoo.mes.cmmsMachineParts.listeners;
+
+import com.qcadoo.mes.cmmsMachineParts.FaultTypesService;
+import com.qcadoo.mes.cmmsMachineParts.constants.FaultTypeAppliesTo;
+import com.qcadoo.mes.cmmsMachineParts.hooks.FaultTypeDetailsHooks;
+import com.qcadoo.model.api.Entity;
+import com.qcadoo.view.api.ComponentState;
+import com.qcadoo.view.api.ViewDefinitionState;
+import com.qcadoo.view.api.components.FormComponent;
+import com.qcadoo.view.api.components.GridComponent;
+import com.qcadoo.view.api.components.WindowComponent;
+import com.qcadoo.view.api.ribbon.RibbonActionItem;
+import com.qcadoo.view.api.ribbon.RibbonGroup;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FaultTypeListListeners {
+
+    private static final String L_FORM = "form";
+
+    public void disableActionsWhenDefault(final ViewDefinitionState view, final ComponentState state, final String args[]) {
+        WindowComponent window = (WindowComponent) view.getComponentByReference("window");
+        RibbonGroup actions = window.getRibbon().getGroupByName("actions");
+
+        RibbonActionItem copyButton = actions.getItemByName("copy");
+        RibbonActionItem deleteButton = actions.getItemByName("delete");
+
+        GridComponent grid = (GridComponent) view.getComponentByReference("grid");
+        List<Entity> selectedFaults = grid.getSelectedEntities();
+        for (Entity selectedFault : selectedFaults) {
+            if (selectedFault.getBooleanField("isDefault")) {
+                copyButton.setEnabled(false);
+                deleteButton.setEnabled(false);
+                copyButton.requestUpdate(true);
+                deleteButton.requestUpdate(true);
+                return;
+            }
+        }
+        copyButton.setEnabled(true);
+        deleteButton.setEnabled(true);
+        copyButton.requestUpdate(true);
+        deleteButton.requestUpdate(true);
+    }
+}
