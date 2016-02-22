85d7a80b8b155a018fc0b5dbc921cd232fc6d904
Katarzyna Małyszek <katarzyna.malyszek@qcadoo.com>
2016-02-22 12:13:24 +0100

    SC# GOODFOOD-1139

--- mes-plugins/mes-plugins-production-lines/src/main/java/com/qcadoo/mes/productionLines/hooks/WorkstationTypeComponentHooks.java
+++ mes-plugins/mes-plugins-production-lines/src/main/java/com/qcadoo/mes/productionLines/hooks/WorkstationTypeComponentHooks.java
@@ -23,17 +23,18 @@
  */
 package com.qcadoo.mes.productionLines.hooks;
 
-import java.util.List;
+import java.util.Date;
+import java.util.Optional;
 
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.qcadoo.mes.productionLines.constants.ProductionLinesConstants;
+import com.qcadoo.mes.productionLines.WorkstationTypeComponentsService;
 import com.qcadoo.mes.productionLines.constants.WorkstationTypeComponentFields;
 import com.qcadoo.model.api.DataDefinition;
 import com.qcadoo.model.api.DataDefinitionService;
 import com.qcadoo.model.api.Entity;
-import com.qcadoo.model.api.search.SearchRestrictions;
 
 @Service
 public class WorkstationTypeComponentHooks {
@@ -41,25 +42,72 @@ public class WorkstationTypeComponentHooks {
     @Autowired
     private DataDefinitionService dataDefinitionService;
 
+    @Autowired
+    private WorkstationTypeComponentsService workstationTypeComponentsService;
+
     public void onSave(final DataDefinition dataDefinition, final Entity entity) {
-        checkIfThereIsTheSameWorkstation(dataDefinition, entity);
+        Optional<Entity> maybePreviousComponent = workstationTypeComponentsService.findPreviousWorkstationTypeComponent(entity);
+        if (maybePreviousComponent.isPresent()) {
+            DateTime dateFrom = new DateTime(entity.getDateField(WorkstationTypeComponentFields.DATE_FROM));
+            Entity previousComponent = maybePreviousComponent.get();
+            Date originalDateTo = previousComponent.getDateField(WorkstationTypeComponentFields.DATE_TO);
+            DateTime dateTo = dateFrom.minusMinutes(1);
+            if (originalDateTo == null || originalDateTo.compareTo(dateTo.toDate()) != 0) {
+                previousComponent.setField(WorkstationTypeComponentFields.DATE_TO, dateTo.toDate());
+                Entity savedPrevious = dataDefinition.save(previousComponent);
+                if (!savedPrevious.isValid()) {
+                    savedPrevious.getErrors().entrySet().stream()
+                            .forEach(entry -> entity.addGlobalError(entry.getValue().getMessage()));
+                }
+            }
+
+        }
+
+        if (shouldValidateDates(dataDefinition, entity)) {
+            boolean isValid = workstationTypeComponentsService.checkIfComponentForGivenTimeExists(entity);
+            if (!isValid) {
+                entity.addError(dataDefinition.getField(WorkstationTypeComponentFields.DATE_FROM),
+                        "productionLines.workstationTypeComponent.validation.otherComponentsExist");
+            }
+        }
+
     }
 
-    private void checkIfThereIsTheSameWorkstation(final DataDefinition dataDefinition, final Entity entity) {
-        List<Entity> entities = dataDefinitionService
-                .get(ProductionLinesConstants.PLUGIN_IDENTIFIER, ProductionLinesConstants.MODEL_WORKSTATION_TYPE_COMPONENT)
-                .find()
-                .add(SearchRestrictions.ne("id", entity.getId()))
-                .add(SearchRestrictions.and(
-                        SearchRestrictions.belongsTo(WorkstationTypeComponentFields.PRODUCTIONLINE,
-                                entity.getBelongsToField(WorkstationTypeComponentFields.PRODUCTIONLINE)),
-                        SearchRestrictions.belongsTo(WorkstationTypeComponentFields.WORKSTATIONTYPE,
-                                entity.getBelongsToField(WorkstationTypeComponentFields.WORKSTATIONTYPE)))).list().getEntities();
-
-        if (!entities.isEmpty()) {
-            entity.addError(dataDefinition.getField(WorkstationTypeComponentFields.WORKSTATIONTYPE),
-                    "productionLines.workstationTypeComponent.workstationType.workstationExist",
-                    entity.getBelongsToField(WorkstationTypeComponentFields.WORKSTATIONTYPE).getStringField("name"));
+    public boolean onDelete(final DataDefinition dataDefinition, final Entity entity) {
+        boolean cannotDelete = entity.getDateField(WorkstationTypeComponentFields.DATE_FROM) != null
+                && entity.getDateField(WorkstationTypeComponentFields.DATE_TO) != null;
+        if (cannotDelete) {
+            entity.addGlobalError("productionLines.workstationTypeComponent.onDelete.hasDates");
+            return false;
         }
+
+        Optional<Entity> maybePreviousComponent = workstationTypeComponentsService.findPreviousWorkstationTypeComponent(entity);
+        if (maybePreviousComponent.isPresent()) {
+            Entity previousComponent = maybePreviousComponent.get();
+            previousComponent.setField(WorkstationTypeComponentFields.DATE_TO, null);
+            dataDefinition.save(previousComponent);
+        }
+        return true;
+    }
+
+    public boolean validatesWith(final DataDefinition dataDefinition, final Entity entity) {
+        Date dateFrom = entity.getDateField(WorkstationTypeComponentFields.DATE_FROM);
+        Date dateTo = entity.getDateField(WorkstationTypeComponentFields.DATE_TO);
+        if (dateTo != null && dateTo.before(dateFrom)) {
+            entity.addError(dataDefinition.getField(WorkstationTypeComponentFields.DATE_FROM),
+                    "productionLines.workstationTypeComponent.validation.datesInvalid");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean shouldValidateDates(final DataDefinition dataDefinition, final Entity originalEntity) {
+        if (originalEntity.getId() == null) {
+            return true;
+        }
+        Entity dbEntity = dataDefinition.get(originalEntity.getId());
+        Date originalDateFrom = originalEntity.getDateField(WorkstationTypeComponentFields.DATE_FROM);
+        Date dbDateFrom = dbEntity.getDateField(WorkstationTypeComponentFields.DATE_FROM);
+        return originalDateFrom.compareTo(dbDateFrom) != 0;
     }
 }
