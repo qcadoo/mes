36a1e0cd1074a4d8023ee8c6b6ee5392a8ad6ff0
Tomasz Woliński <tomasz.wolinski@qcadoo.com>
2016-03-15 11:16:42 +0100

    SC #GOODFOOD-1554, #GOODFOOD-1489

---
+++ mes-application/sql/1.5/upgrade_mes-dev.sql
@@ -0,0 +1,4 @@
+# GOODFOOD-1554 i GOODFOOD-1489
+ALTER TABLE cmmsmachineparts_maintenanceevent ADD entityVersion BIGINT DEFAULT 0;
+ALTER TABLE cmmsmachineparts_plannedevent ADD entityVersion BIGINT DEFAULT 0; 
+-- end;
