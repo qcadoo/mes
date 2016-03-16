c9530bb0d5e35f3a5b6dca4c9358393eb99eb801
Tomasz Woliński <tomasz.wolinski@qcadoo.com>
2016-03-16 10:53:35 +0100

    SC# GOODFOOD-1494

---
+++ mes-plugins/mes-plugins-cmms-machine-parts/src/main/java/com/qcadoo/mes/cmmsMachineParts/listeners/SourceCostReportsListeners.java
@@ -0,0 +1,54 @@
+package com.qcadoo.mes.cmmsMachineParts.listeners;
+
+import com.qcadoo.mes.cmmsMachineParts.constants.SourceCostReportFilterFields;
+import com.qcadoo.model.api.Entity;
+import org.springframework.stereotype.Service;
+
+import com.qcadoo.view.api.ComponentState;
+import com.qcadoo.view.api.ViewDefinitionState;
+import com.qcadoo.view.api.components.FormComponent;
+import java.util.Date;
+
+@Service
+public class SourceCostReportsListeners {
+
+    public void generateReport(final ViewDefinitionState view, final ComponentState state, final String args[]) {
+        FormComponent form = (FormComponent) view.getComponentByReference("form");
+        form.performEvent(view, "save");
+
+        if (args != null && args.length > 0) {
+            Entity filter = form.getPersistedEntityWithIncludedFormValues();
+
+            Entity sourceCost = filter.getBelongsToField(SourceCostReportFilterFields.SOURCE_COST);
+            Date dateFrom = filter.getDateField(SourceCostReportFilterFields.FROM_DATE);
+            Date dateTo = filter.getDateField(SourceCostReportFilterFields.TO_DATE);
+            String reportType = args[0];
+
+            switch (reportType) {
+                case "workerCosts":
+                    generateWorkerCostsReport(sourceCost, dateFrom, dateTo);
+                    break;
+                case "timeReport":
+                    generateTimeReport(sourceCost, dateFrom, dateTo);
+                    break;
+
+                case "partsCosts":
+                    generatePartsCosts(sourceCost, dateFrom, dateTo);
+                    break;
+
+                default: {
+                    throw new IllegalArgumentException(String.format("Unknow event to generate report: '%s'", reportType));
+                }
+            }
+        }
+    }
+
+    private void generatePartsCosts(Entity sourceCost, Date dateFrom, Date dateTo) {
+    }
+
+    private void generateTimeReport(Entity sourceCost, Date dateFrom, Date dateTo) {
+    }
+
+    private void generateWorkerCostsReport(Entity sourceCost, Date dateFrom, Date dateTo) {
+    }
+}
