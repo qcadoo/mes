d874f29b28977235332b2ab77cbe317b1c45f19d
Tomasz Woliński <tomasz.wolinski@qcadoo.com>
2016-03-17 10:01:56 +0100

    SC# GOODFOOD-1494 transfer variables

--- mes-plugins/mes-plugins-cmms-machine-parts/src/main/java/com/qcadoo/mes/cmmsMachineParts/listeners/SourceCostReportsListeners.java
+++ mes-plugins/mes-plugins-cmms-machine-parts/src/main/java/com/qcadoo/mes/cmmsMachineParts/listeners/SourceCostReportsListeners.java
@@ -1,5 +1,6 @@
 package com.qcadoo.mes.cmmsMachineParts.listeners;
 
+import com.qcadoo.localization.api.utils.DateUtils;
 import java.util.Date;
 
 import org.springframework.stereotype.Service;
@@ -9,9 +10,12 @@ import com.qcadoo.model.api.Entity;
 import com.qcadoo.view.api.ComponentState;
 import com.qcadoo.view.api.ViewDefinitionState;
 import com.qcadoo.view.api.components.FormComponent;
+import java.text.SimpleDateFormat;
 
 @Service
 public class SourceCostReportsListeners {
+
+    private static final SimpleDateFormat df = new SimpleDateFormat(DateUtils.L_DATE_FORMAT);
 
     public void generateReport(final ViewDefinitionState view, final ComponentState state, final String args[]) {
         FormComponent form = (FormComponent) view.getComponentByReference("form");
@@ -20,25 +24,27 @@ public class SourceCostReportsListeners {
         if (args != null && args.length > 0) {
             Entity filter = form.getPersistedEntityWithIncludedFormValues();
 
-            Entity sourceCost = filter.getBelongsToField(SourceCostReportFilterFields.SOURCE_COST);
-            Date dateFrom = filter.getDateField(SourceCostReportFilterFields.FROM_DATE);
-            Date dateTo = filter.getDateField(SourceCostReportFilterFields.TO_DATE);
-            String reportType = args[0];
+            if (filter.isValid()) {
+                Entity sourceCost = filter.getBelongsToField(SourceCostReportFilterFields.SOURCE_COST);
+                Date dateFrom = filter.getDateField(SourceCostReportFilterFields.FROM_DATE);
+                Date dateTo = filter.getDateField(SourceCostReportFilterFields.TO_DATE);
+                String reportType = args[0];
 
-            switch (reportType) {
-                case "workerCosts":
-                    generateWorkerCostsReport(view, sourceCost, dateFrom, dateTo);
-                    break;
-                case "timeReport":
-                    generateTimeReport(sourceCost, dateFrom, dateTo);
-                    break;
+                switch (reportType) {
+                    case "workerCosts":
+                        generateWorkerCostsReport(view, sourceCost, dateFrom, dateTo);
+                        break;
+                    case "timeReport":
+                        generateTimeReport(sourceCost, dateFrom, dateTo);
+                        break;
 
-                case "partsCosts":
-                    generatePartsCosts(sourceCost, dateFrom, dateTo);
-                    break;
+                    case "partsCosts":
+                        generatePartsCosts(sourceCost, dateFrom, dateTo);
+                        break;
 
-                default: {
-                    throw new IllegalArgumentException(String.format("Unknow event to generate report: '%s'", reportType));
+                    default: {
+                        throw new IllegalArgumentException(String.format("Unknow event to generate report: '%s'", reportType));
+                    }
                 }
             }
         }
@@ -51,6 +57,7 @@ public class SourceCostReportsListeners {
     }
 
     private void generateWorkerCostsReport(ViewDefinitionState view, Entity sourceCost, Date dateFrom, Date dateTo) {
-        view.redirectTo("/cmmsMachineParts/workerCosts.xls?filterId=" + 0, true, false);
+        String url = "/cmmsMachineParts/workerCosts.xls?sourceCost=" + (sourceCost == null ? "" : sourceCost.getId()) + "&dateFrom=" + df.format(dateFrom) + "&dateTo=" + df.format(dateTo);
+        view.redirectTo(url, true, false);
     }
 }
--- mes-plugins/mes-plugins-cmms-machine-parts/src/main/java/com/qcadoo/mes/cmmsMachineParts/reports/WorkerCostsReportController.java
+++ mes-plugins/mes-plugins-cmms-machine-parts/src/main/java/com/qcadoo/mes/cmmsMachineParts/reports/WorkerCostsReportController.java
@@ -1,5 +1,6 @@
 package com.qcadoo.mes.cmmsMachineParts.reports;
 
+import com.qcadoo.localization.api.utils.DateUtils;
 import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,28 +13,44 @@ import org.springframework.web.servlet.ModelAndView;
 import com.qcadoo.mes.cmmsMachineParts.constants.CmmsMachinePartsConstants;
 import com.qcadoo.model.api.DataDefinition;
 import com.qcadoo.model.api.DataDefinitionService;
-import com.qcadoo.model.api.Entity;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-@Controller public class WorkerCostsReportController {
+@Controller
+public class WorkerCostsReportController {
 
     @Autowired
     private DataDefinitionService dataDefinitionService;
 
+    private static final SimpleDateFormat df = new SimpleDateFormat(DateUtils.L_DATE_FORMAT);
+
     @RequestMapping(value = "/cmmsMachineParts/workerCosts.xls", method = RequestMethod.GET)
-    public ModelAndView generatePlannedEventsReport(@RequestParam("filterId") final Long filterId) {
-        DataDefinition dataDefinition = dataDefinitionService.get(CmmsMachinePartsConstants.PLUGIN_IDENTIFIER,
-                CmmsMachinePartsConstants.MODEL_TIME_USAGE_REPORT_FILTER);
-        Entity filter = dataDefinition.get(filterId);
-        HashMap<String, Object> filtersMap = new HashMap<String, Object>();
-        /*
-         * Date fromDate = filter.getDateField(TimeUsageReportFilterFields.FROM_DATE); if (fromDate != null) {
-         * filtersMap.put(TimeUsageReportFilterFields.FROM_DATE, fromDate); } Date toDate =
-         * filter.getDateField(TimeUsageReportFilterFields.TO_DATE); if (toDate != null) {
-         * filtersMap.put(TimeUsageReportFilterFields.TO_DATE, toDate); } List<Entity> workers =
-         * filter.getManyToManyField(TimeUsageReportFilterFields.WORKERS); if (!workers.isEmpty()) { filtersMap
-         * .put(TimeUsageReportFilterFields.WORKERS, workers.stream().map(w -> w.getId()).collect(Collectors.toList())); }
-         */
-        return new ModelAndView("workerCostsXlsView", "filtersMap", filtersMap);
+    public ModelAndView generatePlannedEventsReport(@RequestParam("sourceCost") final Long sourceCost, @RequestParam("dateFrom") String dateFromString, @RequestParam("dateTo") String dateToString) {
+        try {
+            DataDefinition dataDefinition = dataDefinitionService.get(CmmsMachinePartsConstants.PLUGIN_IDENTIFIER,
+                    CmmsMachinePartsConstants.MODEL_TIME_USAGE_REPORT_FILTER);
+//        Entity filter = dataDefinition.get(filterId);
+            HashMap<String, Object> filtersMap = new HashMap<>();
+
+            Date dateFrom = df.parse(dateFromString);
+            Date dateTo = df.parse(dateToString);
+
+            System.out.println("-------------------------------------------");
+            System.out.println("sourceCost=" + sourceCost + ", dateFrom=" + dateFrom + ", dateTo=" + dateTo);
+
+            /*
+* Date fromDate = filter.getDateField(TimeUsageReportFilterFields.FROM_DATE); if (fromDate != null) {
+* filtersMap.put(TimeUsageReportFilterFields.FROM_DATE, fromDate); } Date toDate =
+* filter.getDateField(TimeUsageReportFilterFields.TO_DATE); if (toDate != null) {
+* filtersMap.put(TimeUsageReportFilterFields.TO_DATE, toDate); } List<Entity> workers =
+* filter.getManyToManyField(TimeUsageReportFilterFields.WORKERS); if (!workers.isEmpty()) { filtersMap
+* .put(TimeUsageReportFilterFields.WORKERS, workers.stream().map(w -> w.getId()).collect(Collectors.toList())); }
+             */
+            return new ModelAndView("workerCostsXlsView", "filtersMap", filtersMap);
+        } catch (ParseException ex) {
+            throw new RuntimeException(ex);
+        }
 
     }
 
