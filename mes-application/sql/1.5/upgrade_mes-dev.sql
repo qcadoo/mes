fd708860dad3ec9cd7d5686321ccf5346ad5fc65
Katarzyna Małyszek <katarzyna.malyszek@qcadoo.com>
2016-03-30 12:01:55 +0200

    SC# NBLS-154

--- mes-application/sql/1.5/upgrade_mes-dev.sql
+++ mes-application/sql/1.5/upgrade_mes-dev.sql
@@ -4,6 +4,4 @@
 ALTER TABLE technologies_operationproductoutcomponent ADD COLUMN set boolean;
 ALTER TABLE technologies_operationproductoutcomponent ALTER COLUMN set SET DEFAULT false;
 
-UPDATE technologies_operationproductoutcomponent SET set = false WHERE set IS NULL;
-
 -- end
