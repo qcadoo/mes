ffa6bef2d68b45cbba74e8049bbcc59e54c791e0
Lukasz Poniewierski <lukasz.poniewierski@qcadoo.com>
2016-04-04 14:45:31 +0200

    SC#NBLS-157 Added new role for production registration terminal.

--- mes-application/sql/1.5/upgrade_mes-dev.sql
+++ mes-application/sql/1.5/upgrade_mes-dev.sql
@@ -4,6 +4,20 @@ ALTER TABLE technologies_operationproductincomponent ADD COLUMN quantityformula 
 
 -- end
 
+
 -- #NBLS-160
+
 ALTER TABLE basic_parameter ADD COLUMN consumptionOfRawMaterialsBasedOnStandards boolean default false;
+
+-- end
+
+
+-- Added new role for production registration terminal
+-- last touched 01.04.2016 by lupo
+
+SELECT add_role('ROLE_PRODUCTION_REGISTRATION_TERMINAL','Dostęp do terminalu rejestracji produkcji');
+SELECT add_group_role('SUPER_ADMIN', 'ROLE_PRODUCTION_REGISTRATION_TERMINAL');
+SELECT add_group_role('ADMIN', 'ROLE_PRODUCTION_REGISTRATION_TERMINAL');
+SELECT add_group_role('USER', 'ROLE_PRODUCTION_REGISTRATION_TERMINAL');
+
 -- end
