e416aee5be627910af11373d299d738b00253245
Paweł Kochanowski <pawel.kochanowski@qcadoo.com>
2016-06-02 12:50:02 +0200

    DF# ESILCO-178 location date fields validation problem

--- mes-application/sql/1.5/upgrade_mes-dev.sql
+++ mes-application/sql/1.5/upgrade_mes-dev.sql
@@ -3,3 +3,11 @@
 ALTER TABLE materialflowresources_resource ADD COLUMN username character varying(255);
 
 -- end
+
+-- rename fields in settings
+﻿-- last touched 2.06.2016 by pako
+
+UPDATE materialflowresources_documentpositionparametersitem SET name = 'productionDate' WHERE name = 'productiondate';
+UPDATE materialflowresources_documentpositionparametersitem SET name = 'expirationDate' WHERE name = 'expirationdate';
+
+-- end
