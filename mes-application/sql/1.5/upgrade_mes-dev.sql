36981ca7c62897b1ed58bf54056edecb9fffb43a
Katarzyna Małyszek <katarzyna.malyszek@qcadoo.com>
2016-06-03 10:37:37 +0200

    CM# NBLS-246 scripts

--- mes-application/sql/1.5/upgrade_mes-dev.sql
+++ mes-application/sql/1.5/upgrade_mes-dev.sql
@@ -3,3 +3,11 @@
 ALTER TABLE materialflowresources_resource ADD COLUMN username character varying(255);
 
 -- end
+
+-- wastes
+-- last touched 03.06.2016 by kama
+
+ALTER TABLE productioncounting_trackingoperationproductoutcomponent ADD COLUMN wastesquantity numeric(14,5);
+ALTER TABLE orders_order ADD COLUMN wastesquantity numeric(12,5);
+
+-- end
