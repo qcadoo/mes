1d2071851c23b438184b2e27a85c8f812d1bc9b6
Katarzyna Małyszek <katarzyna.malyszek@qcadoo.com>
2016-06-23 11:38:16 +0200

    SC# ESILCO-160

--- mes-plugins/mes-plugins-material-flow-resources/src/main/java/com/qcadoo/mes/materialFlowResources/WarehouseMethodOfDisposalService.java
+++ mes-plugins/mes-plugins-material-flow-resources/src/main/java/com/qcadoo/mes/materialFlowResources/WarehouseMethodOfDisposalService.java
@@ -1,13 +1,14 @@
 package com.qcadoo.mes.materialFlowResources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.qcadoo.mes.materialFlowResources.constants.DocumentFields;
 import com.qcadoo.mes.materialFlowResources.constants.LocationFieldsMFR;
 import com.qcadoo.mes.materialFlowResources.constants.MaterialFlowResourcesConstants;
 import com.qcadoo.mes.materialFlowResources.constants.WarehouseAlgorithm;
 import com.qcadoo.model.api.DataDefinitionService;
 import com.qcadoo.model.api.Entity;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 @Service
 public class WarehouseMethodOfDisposalService {
@@ -15,12 +16,13 @@ public class WarehouseMethodOfDisposalService {
     @Autowired
     private DataDefinitionService dataDefinitionService;
 
-    public String getSqlConditionForResourceLookup(final Long documentId){
-        Entity document = dataDefinitionService.get(MaterialFlowResourcesConstants.PLUGIN_IDENTIFIER,
-                MaterialFlowResourcesConstants.MODEL_DOCUMENT).get(documentId);
+    public String getSqlConditionForResourceLookup(final Long documentId) {
+        Entity document = dataDefinitionService
+                .get(MaterialFlowResourcesConstants.PLUGIN_IDENTIFIER, MaterialFlowResourcesConstants.MODEL_DOCUMENT)
+                .get(documentId);
         String alg = document.getBelongsToField(DocumentFields.LOCATION_FROM).getStringField(LocationFieldsMFR.ALGORITHM);
         WarehouseAlgorithm algorithm = WarehouseAlgorithm.parseString(alg);
-        switch(algorithm) {
+        switch (algorithm) {
             case FEFO:
                 return " expirationdate = (select min(expirationdate) from materialflowresources_resource ";
             case FIFO:
@@ -33,12 +35,13 @@ public class WarehouseMethodOfDisposalService {
         return " expirationdate = (select min(expirationdate) from materialflowresources_resource ";
     }
 
-    public String getSqlConditionForResource(final Long documentId){
-        Entity document = dataDefinitionService.get(MaterialFlowResourcesConstants.PLUGIN_IDENTIFIER,
-                MaterialFlowResourcesConstants.MODEL_DOCUMENT).get(documentId);
+    public String getSqlConditionForResource(final Long documentId) {
+        Entity document = dataDefinitionService
+                .get(MaterialFlowResourcesConstants.PLUGIN_IDENTIFIER, MaterialFlowResourcesConstants.MODEL_DOCUMENT)
+                .get(documentId);
         String alg = document.getBelongsToField(DocumentFields.LOCATION_FROM).getStringField(LocationFieldsMFR.ALGORITHM);
         WarehouseAlgorithm algorithm = WarehouseAlgorithm.parseString(alg);
-        switch(algorithm) {
+        switch (algorithm) {
             case FEFO:
                 return " select min(expirationdate) from materialflowresources_resource ";
             case FIFO:
@@ -49,5 +52,24 @@ public class WarehouseMethodOfDisposalService {
                 return " select max(time) from materialflowresources_resource ";
         }
         return " select min(expirationdate) from materialflowresources_resource ";
+    }
+
+    public String getSqlOrderByForResource(final Long documentId) {
+        Entity document = dataDefinitionService
+                .get(MaterialFlowResourcesConstants.PLUGIN_IDENTIFIER, MaterialFlowResourcesConstants.MODEL_DOCUMENT)
+                .get(documentId);
+        String alg = document.getBelongsToField(DocumentFields.LOCATION_FROM).getStringField(LocationFieldsMFR.ALGORITHM);
+        WarehouseAlgorithm algorithm = WarehouseAlgorithm.parseString(alg);
+        switch (algorithm) {
+            case FEFO:
+                return " order by expirationdate asc ";
+            case FIFO:
+                return " order by time asc ";
+            case LEFO:
+                return " order by expirationdate desc ";
+            case LIFO:
+                return " order by time desc ";
+        }
+        return " order by expirationdate asc ";
     }
 }
