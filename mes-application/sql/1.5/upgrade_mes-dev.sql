b4a08ae9838d8fdff54fd90fd08165cdc2e75ea1
Katarzyna Małyszek <katarzyna.malyszek@qcadoo.com>
2016-06-28 13:47:14 +0200

    SC# ESILCO-190 scripts

--- mes-application/sql/1.5/upgrade_mes-dev.sql
+++ mes-application/sql/1.5/upgrade_mes-dev.sql
@@ -75,4 +75,28 @@ CREATE TABLE repairs_repairorderstatechange
       REFERENCES basic_shift (id) DEFERRABLE
 );
 
+--end
+
+-- reservations
+-- last touched 28.06.2016 by kama
+
+CREATE TABLE materialflowresources_reservation
+(
+  id bigint NOT NULL,
+  location_id bigint,
+  product_id bigint,
+  quantity numeric(12,5),
+  position_id bigint,
+  CONSTRAINT materialflowresources_reservation_pkey PRIMARY KEY (id),
+  CONSTRAINT reservation_product_fkey FOREIGN KEY (product_id)
+      REFERENCES basic_product (id) DEFERRABLE,
+  CONSTRAINT reservation_location_fkey FOREIGN KEY (location_id)
+      REFERENCES materialflow_location (id) DEFERRABLE,
+  CONSTRAINT reservation_position_fkey FOREIGN KEY (position_id)
+      REFERENCES materialflowresources_position (id) DEFERRABLE
+);
+
+ALTER TABLE materialflowresources_documentpositionparameters ADD COLUMN draftmakesreservation boolean;
+ALTER TABLE materialflowresources_documentpositionparameters ALTER COLUMN draftmakesreservation SET DEFAULT true;
+
 -- end
