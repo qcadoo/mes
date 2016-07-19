c0c7152c8682a8fdde70e17e5fa88ec61c738fb3
Katarzyna Małyszek <katarzyna.malyszek@qcadoo.com>
2016-07-19 14:58:35 +0200

    SC# ESILCO-240

--- mes-plugins/mes-plugins-tech-subcontr-for-deliveries/src/main/java/com/qcadoo/mes/techSubcontrForDeliveries/aop/DeliveryDetailsListenersTSFDOverrideUtil.java
+++ mes-plugins/mes-plugins-tech-subcontr-for-deliveries/src/main/java/com/qcadoo/mes/techSubcontrForDeliveries/aop/DeliveryDetailsListenersTSFDOverrideUtil.java
@@ -23,13 +23,14 @@
  */
 package com.qcadoo.mes.techSubcontrForDeliveries.aop;
 
-import static com.qcadoo.mes.deliveries.constants.DeliveredProductFields.PRODUCT;
 import static com.qcadoo.mes.techSubcontrForDeliveries.constants.DeliveredProductFieldsTSFD.OPERATION;
 
 import java.math.BigDecimal;
 
 import org.springframework.stereotype.Service;
 
+import com.qcadoo.mes.deliveries.constants.DeliveredProductFields;
+import com.qcadoo.mes.deliveries.constants.OrderedProductFields;
 import com.qcadoo.model.api.Entity;
 
 @Service
@@ -40,17 +41,24 @@ public class DeliveryDetailsListenersTSFDOverrideUtil {
     }
 
     public boolean checkIfProductsAndOperationsAreSame(final Entity orderedProduct, final Entity deliveredProduct) {
-        return (checkIfProductsAreSame(orderedProduct, deliveredProduct) && checkIfOperationsAreSame(orderedProduct,
-                deliveredProduct));
+        return (checkIfProductsAreSame(orderedProduct, deliveredProduct)
+                && checkIfOperationsAreSame(orderedProduct, deliveredProduct));
     }
 
     private boolean checkIfProductsAreSame(final Entity orderedProduct, final Entity deliveredProduct) {
-        return (orderedProduct.getBelongsToField(PRODUCT).getId().equals(deliveredProduct.getBelongsToField(PRODUCT).getId()));
+        Entity orderedAdditionalCode = orderedProduct.getBelongsToField(OrderedProductFields.ADDITIONAL_CODE);
+        Entity deliveredAdditionalCode = deliveredProduct.getBelongsToField(DeliveredProductFields.ADDITIONAL_CODE);
+        boolean additionalCodesMatching = orderedAdditionalCode == null && deliveredAdditionalCode == null;
+        if (orderedAdditionalCode != null && deliveredAdditionalCode != null) {
+            additionalCodesMatching = orderedAdditionalCode.getId().equals(deliveredAdditionalCode.getId());
+        }
+        return (orderedProduct.getBelongsToField(OrderedProductFields.PRODUCT).getId()
+                .equals(deliveredProduct.getBelongsToField(DeliveredProductFields.PRODUCT).getId())) && additionalCodesMatching;
     }
 
     private boolean checkIfOperationsAreSame(final Entity orderedProduct, final Entity deliveredProduct) {
-        return (checkIfOperationsAreNull(orderedProduct, deliveredProduct) || checkIfOperationsAreEqual(orderedProduct,
-                deliveredProduct));
+        return (checkIfOperationsAreNull(orderedProduct, deliveredProduct)
+                || checkIfOperationsAreEqual(orderedProduct, deliveredProduct));
     }
 
     private boolean checkIfOperationsAreNull(final Entity orderedProduct, final Entity deliveredProduct) {
@@ -58,8 +66,9 @@ public class DeliveryDetailsListenersTSFDOverrideUtil {
     }
 
     private boolean checkIfOperationsAreEqual(final Entity orderedProduct, final Entity deliveredProduct) {
-        return ((orderedProduct.getBelongsToField(OPERATION) != null) && (deliveredProduct.getBelongsToField(OPERATION) != null) && orderedProduct
-                .getBelongsToField(OPERATION).getId().equals(deliveredProduct.getBelongsToField(OPERATION).getId()));
+        return ((orderedProduct.getBelongsToField(OPERATION) != null) && (deliveredProduct.getBelongsToField(OPERATION) != null)
+                && orderedProduct.getBelongsToField(OPERATION).getId()
+                        .equals(deliveredProduct.getBelongsToField(OPERATION).getId()));
     }
 
     public void fillOrderedProductOperation(final Entity orderedProduct, final BigDecimal orderedQuantity,
