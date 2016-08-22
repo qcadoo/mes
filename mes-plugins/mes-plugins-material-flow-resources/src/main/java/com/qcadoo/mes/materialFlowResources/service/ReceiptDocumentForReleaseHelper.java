e2d40f922849a7dfd0bbdaa7ff3916a079413027
ksiwonia <kamil.siwonia@gmial.com>
2016-08-22 11:35:44 +0200

    SC#ESILCO-283

--- mes-plugins/mes-plugins-material-flow-resources/src/main/java/com/qcadoo/mes/materialFlowResources/service/ReceiptDocumentForReleaseHelper.java
+++ mes-plugins/mes-plugins-material-flow-resources/src/main/java/com/qcadoo/mes/materialFlowResources/service/ReceiptDocumentForReleaseHelper.java
@@ -3,14 +3,19 @@ package com.qcadoo.mes.materialFlowResources.service;
 import com.qcadoo.localization.api.TranslationService;
 import com.qcadoo.mes.basic.ParameterService;
 import com.qcadoo.mes.materialFlowResources.constants.DocumentFields;
+import com.qcadoo.mes.materialFlowResources.constants.MaterialFlowResourcesConstants;
 import com.qcadoo.mes.materialFlowResources.constants.PositionFields;
+import com.qcadoo.mes.materialFlowResources.constants.StorageLocationFields;
 import com.qcadoo.model.api.DataDefinitionService;
 import com.qcadoo.model.api.Entity;
+import com.qcadoo.model.api.search.SearchCriteriaBuilder;
+import com.qcadoo.model.api.search.SearchRestrictions;
 import com.qcadoo.security.api.UserService;
 import com.qcadoo.view.api.utils.NumberGeneratorService;
 import org.springframework.context.i18n.LocaleContextHolder;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ReceiptDocumentForReleaseHelper {
 
@@ -42,12 +47,13 @@ public class ReceiptDocumentForReleaseHelper {
         DocumentBuilder pzBuilder = new DocumentBuilder(dataDefinitionService, resourceManagementService, userService,
                 numberGeneratorService, translationService, parameterService);
         Entity documentDb = document.getDataDefinition().get(document.getId());
-        pzBuilder = pzBuilder.receipt(document.getBelongsToField(DocumentFields.LINKED_PZ_DOCUMENT_LOCATION));
+        Entity location = document.getBelongsToField(DocumentFields.LINKED_PZ_DOCUMENT_LOCATION);
+        pzBuilder = pzBuilder.receipt(location);
         if (fillDescription) {
             pzBuilder = pzBuilder.setField(DocumentFields.DESCRIPTION,
                     buildDescription(documentDb.getStringField(DocumentFields.NUMBER)));
         }
-        fillPositions(document, pzBuilder);
+        fillPositions(location, document, pzBuilder);
         Entity connectedReceiptDocument = null;
         if (parameterService.getParameter().getStringField("documentsStatus").equals("01accepted")) {
             connectedReceiptDocument = pzBuilder.setAccepted().build();
@@ -62,7 +68,7 @@ public class ReceiptDocumentForReleaseHelper {
 
     }
 
-    private void fillPositions(Entity document, DocumentBuilder pzBuilder) {
+    private void fillPositions(Entity location, Entity document, DocumentBuilder pzBuilder) {
         List<Entity> positions = document.getHasManyField(DocumentFields.POSITIONS);
         positions.forEach(pos -> {
             Entity pzPosition = pos.copy();
@@ -71,7 +77,13 @@ public class ReceiptDocumentForReleaseHelper {
             pzPosition.setField(PositionFields.RESOURCE, null);
             pzPosition.setField(PositionFields.TYPE_OF_PALLET, null);
             pzPosition.setField(PositionFields.PALLET_NUMBER, null);
-            pzPosition.setField(PositionFields.STORAGE_LOCATION, null);
+            Optional<Entity> maybyStorageLocation = findStorageLocationForProduct(pos.getBelongsToField(PositionFields.PRODUCT),
+                    location);
+            if (maybyStorageLocation.isPresent()) {
+                pzPosition.setField(PositionFields.STORAGE_LOCATION, maybyStorageLocation.get());
+            } else {
+                pzPosition.setField(PositionFields.STORAGE_LOCATION, null);
+            }
             pzBuilder.addPosition(pzPosition);
         });
     }
@@ -79,6 +91,14 @@ public class ReceiptDocumentForReleaseHelper {
     private String buildDescription(String number) {
         return translationService.translate("materialFlowResources.document.description.forTemplate",
                 LocaleContextHolder.getLocale(), number);
+    }
+
+    public Optional<Entity> findStorageLocationForProduct(final Entity product, final Entity location) {
+        SearchCriteriaBuilder scb = dataDefinitionService.get(MaterialFlowResourcesConstants.PLUGIN_IDENTIFIER,
+                MaterialFlowResourcesConstants.MODEL_STORAGE_LOCATION).find();
+        scb.add(SearchRestrictions.belongsTo(StorageLocationFields.PRODUCT, product));
+        scb.add(SearchRestrictions.belongsTo(StorageLocationFields.LOCATION, location));
+        return Optional.ofNullable(scb.setMaxResults(1).uniqueResult());
     }
 
 }
