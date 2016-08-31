0fcbb42beeb06717ad41650fe95d0d81bc5e48f0
Łukasz Poniewierski <lukasz.poniewierski@qcadoo.com>
2016-08-31 14:39:24 +0200

    DF Fixed problem with sql.

--- mes-application/sql/1.5/upgrade_mes2.sql
+++ mes-application/sql/1.5/upgrade_mes2.sql
@@ -1,9 +1,9 @@
 -- menu storage locations by kasi 08.10.2015
 
-INSERT INTO qcadooview_view(pluginidentifier, name, view, entityversion) VALUES ('materialFlowResources', 'storageLocations', 'storageLocationList', 0);
+INSERT INTO qcadooview_view(pluginidentifier, name, view, entityversion) VALUES ('materialFlowResources', 'storageLocationList', 'storageLocationList', 0);
 INSERT INTO qcadooview_item(pluginidentifier, name, active, category_id, view_id, succession, authrole, entityversion)
 VALUES ('materialFlowResources', 'storageLocations', true, (SELECT id FROM qcadooview_category WHERE name = 'materialFlow' LIMIT 1), (
-		SELECT id FROM qcadooview_view WHERE name = 'storageLocations' LIMIT 1),(
+		SELECT id FROM qcadooview_view WHERE name = 'storageLocationList' LIMIT 1),(
 		SELECT max(succession) + 1 FROM qcadooview_item WHERE category_id = (SELECT id FROM qcadooview_category WHERE name = 'materialFlow' LIMIT 1)), 'ROLE_MATERIAL_FLOW', 0);
 
 -- end
