c40d984b6f9ccecc01fae656d9a4ce59b65f704c
ksiwonia <kamil.siwonia@gmial.com>
2016-10-20 10:37:26 +0200

    SC#ANEKS-50

--- mes-plugins/mes-plugins-production-per-shift/src/main/java/com/qcadoo/mes/productionPerShift/services/AutomaticPpsTechNormAndWorkersService.java
+++ mes-plugins/mes-plugins-production-per-shift/src/main/java/com/qcadoo/mes/productionPerShift/services/AutomaticPpsTechNormAndWorkersService.java
@@ -1,14 +1,37 @@
 package com.qcadoo.mes.productionPerShift.services;
 
+import com.qcadoo.mes.basicProductionCounting.BasicProductionCountingService;
+import com.qcadoo.mes.orders.constants.OrderFields;
+import com.qcadoo.mes.productionPerShift.constants.ProductionPerShiftFields;
 import com.qcadoo.mes.productionPerShift.domain.ProgressForDaysContainer;
 import com.qcadoo.model.api.Entity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.Date;
 
 @Service
 public class AutomaticPpsTechNormAndWorkersService implements AutomaticPpsService {
 
+    @Autowired
+    private BasicProductionCountingService basicProductionCountingService;
+
     @Override
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void generateProgressForDays(ProgressForDaysContainer progressForDaysContainer, Entity productionPerShift) {
+        Entity order = productionPerShift.getBelongsToField(ProductionPerShiftFields.ORDER);
+        if (progressForDaysContainer.getOrder() != null) {
+            order = progressForDaysContainer.getOrder();
+        }
+        Date orderStartDate = order.getDateField(OrderFields.START_DATE);
+        BigDecimal plannedQuantity = order.getDecimalField(OrderFields.PLANNED_QUANTITY);
+        if (order.getBooleanField(OrderFields.FINAL_PRODUCTION_TRACKING)) {
+            plannedQuantity = basicProductionCountingService.getProducedQuantityFromBasicProductionCountings(order);
+        }
+
 
     }
 }
