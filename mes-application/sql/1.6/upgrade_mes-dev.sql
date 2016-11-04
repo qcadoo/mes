0fdac50beca4405bc30390b1685df17f9af07c08
Bartlomiej <bartlomiej.florczak@qcadoo.com>
2016-11-04 12:26:37 +0100

    DF# QCADOOCLS-5097

--- mes-application/sql/1.6/upgrade_mes-dev.sql
+++ mes-application/sql/1.6/upgrade_mes-dev.sql
@@ -4,3 +4,6 @@ ALTER TABLE basic_parameter ADD ppsProducedAmountRecalculatePlan boolean;
 ALTER TABLE basic_parameter ADD ppsAlgorithm character varying(255);
 -- end
 
+-- #QCADOOCLS-5097
+ALTER TABLE basic_company ALTER COLUMN website TYPE character varying(255);
+-- end
