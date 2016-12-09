8eb5f0e572edb0abb53086bb4b7c62b0c0cc06d5
ksiwonia <kamil.siwonia@gmial.com>
2016-12-09 15:39:11 +0100

    SC#QCADOOCLS-5144

--- mes-plugins/mes-plugins-basic/src/main/java/com/qcadoo/mes/basic/LogService.java
+++ mes-plugins/mes-plugins-basic/src/main/java/com/qcadoo/mes/basic/LogService.java
@@ -8,9 +8,12 @@ import com.qcadoo.model.api.DataDefinition;
 import com.qcadoo.model.api.DataDefinitionService;
 import com.qcadoo.model.api.Entity;
 import com.qcadoo.security.api.SecurityService;
-import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
 
 @Service
 public class LogService {
@@ -92,6 +95,7 @@ public class LogService {
         }
     }
 
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public Entity add(Builder builder) {
         validate(builder);
 
