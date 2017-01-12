94f636b2991fbe01f5dc5cf1e9037deb15d4b95b
Bartlomiej <bartlomiej.florczak@qcadoo.com>
2017-01-12 11:09:49 +0100

    SC# QCADOOMES-2526

---
+++ mes-plugins/mes-plugins-material-flow-resources/src/main/java/com/qcadoo/mes/materialFlowResources/hooks/WarehouseStockListHooks.java
@@ -0,0 +1,31 @@
+package com.qcadoo.mes.materialFlowResources.hooks;
+
+import com.google.common.collect.Maps;
+import com.qcadoo.security.api.UserService;
+import com.qcadoo.view.api.ViewDefinitionState;
+import com.qcadoo.view.api.components.CheckBoxComponent;
+import com.qcadoo.view.api.components.GridComponent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class WarehouseStockListHooks {
+
+    @Autowired
+    private UserService userService;
+
+    public void fillDefaultFilters(final ViewDefinitionState view) {
+        CheckBoxComponent initialized = (CheckBoxComponent) view.getComponentByReference("initialized");
+        if (initialized.isChecked()) {
+            return;
+        }
+        initialized.setChecked(true);
+        GridComponent grid = (GridComponent) view.getComponentByReference("grid");
+
+        Map<String, String> filters = Maps.newHashMap();
+        filters.put("quantity", ">0");
+        grid.setFilters(filters);
+    }
+}
