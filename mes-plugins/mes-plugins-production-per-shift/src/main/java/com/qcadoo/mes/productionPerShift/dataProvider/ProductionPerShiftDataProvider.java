a60c62d842ed3741b6d9e1d6be04108f9d2328df
ksiwonia <kamil.siwonia@gmial.com>
2017-02-21 07:56:59 +0100

    SC#QCADOOCLS-5246

--- mes-plugins/mes-plugins-production-per-shift/src/main/java/com/qcadoo/mes/productionPerShift/dataProvider/ProductionPerShiftDataProvider.java
+++ mes-plugins/mes-plugins-production-per-shift/src/main/java/com/qcadoo/mes/productionPerShift/dataProvider/ProductionPerShiftDataProvider.java
@@ -57,21 +57,6 @@ import static com.qcadoo.model.api.search.SearchRestrictions.isNull;
  */
 @Service public class ProductionPerShiftDataProvider {
 
-    private static final String PPS_FOR_TOC_ID_QUERY =
-            "select                      \n" + "  pps.id as ppsId                                                    \n"
-                    + "from #productionPerShift_productionPerShift pps                      \n"
-                    + "  inner join pps.order o                                             \n"
-                    + "where                                                                \n"
-                    + "  o.id in (select                                                    \n"
-                    + "      o.id                                                           \n"
-                    + "    from #technologies_technologyOperationComponent toc              \n"
-                    + "      inner join toc.technology tech                                 \n"
-                    + "      inner join tech.orders o                                       \n"
-                    + "    where                                                            \n"
-                    + "      toc.id = :tocId                                                \n"
-                    + "    order by o.id desc)                                              \n"
-                    + "order by pps.id desc                                                 \n";
-
     private static final String ID = "id";
 
     private static final String DOT = ".";
@@ -92,11 +77,7 @@ import static com.qcadoo.model.api.search.SearchRestrictions.isNull;
      */
     public static final String DAILY_PROGRESS_ALIAS = "dp_alias";
 
-    private static final String TECH_ALIAS = "tech";
-
     private static final String QUANTITY_SUM_PROJECTION = "quantitySumProjection";
-
-    private static final String ID_PROJECTION = "idProjection";
 
     /**
      * Restrict
@@ -114,10 +95,12 @@ import static com.qcadoo.model.api.search.SearchRestrictions.isNull;
      * @since 1.4
      */
     public Optional<ProductionPerShiftId> find(final TechnologyOperationId tocId) {
-        SearchQueryBuilder sqb = getProductionPerShiftDD().find(PPS_FOR_TOC_ID_QUERY).setLong("tocId", tocId.get());
+        //TODO fix in mesc issue
+      /*  SearchQueryBuilder sqb = getProductionPerShiftDD().find(PPS_FOR_TOC_ID_QUERY).setLong("tocId", tocId.get());
         sqb.setMaxResults(1);
         return Optional.ofNullable(sqb.uniqueResult()).flatMap(e -> Optional.ofNullable((Long) e.getField("ppsId")))
-                .map(ProductionPerShiftId::new);
+                .map(ProductionPerShiftId::new);*/
+        return Optional.empty();
     }
 
 
@@ -168,148 +151,10 @@ import static com.qcadoo.model.api.search.SearchRestrictions.isNull;
         BigDecimal quantitiesSum = projection.getDecimalField(QUANTITY_SUM_PROJECTION);
         return ObjectUtils.defaultIfNull(quantitiesSum, BigDecimal.ZERO);
     }
-/*
 
-    */
-/**
-     * Returns sum of quantities from matching daily shift progress entries. Notice that corrected quantities take precedence over
-     * planned ones (see documentation for #findIdsOfEffectiveProgressForDay).<br/>
-     * <br/>
-     * <p>
-     * By default this method counts quantities for each operations in technology tree. If you want to count only quantities for
-     * root operations pass ProductionPerShiftDataProvider#ONLY_ROOT_OPERATIONS_CRITERIA search criterion as a second argument.
-     *
-     * @param technologyId       id of technology
-     * @param additionalCriteria optional additional criteria.
-     * @return sum of quantities from matching daily shift progresses or BigDecimal#ZERO if there is no matching progress entries.
-     * @since 1.3.0
-     *//*
-
-    @Deprecated public BigDecimal countSumOfQuantities(final Long technologyId, final SearchCriterion additionalCriteria) {
-        Set<Long> pfdIds = findIdsOfEffectiveProgressForDay(technologyId);
-
-        if (pfdIds.isEmpty()) {
-            return BigDecimal.ZERO;
-        }
-
-        SearchProjectionList projectionList = list();
-        projectionList.add(alias(sum(DAILY_PROGRESS_ALIAS + DOT + DailyProgressFields.QUANTITY), QUANTITY_SUM_PROJECTION));
-        // To prevent NPE during conversion to generic entity
-        projectionList.add(rowCount());
-
-        SearchCriteriaBuilder scb = getDailyProgressDD().findWithAlias(DAILY_PROGRESS_ALIAS);
-        scb.setProjection(projectionList);
-        scb.createAlias(DAILY_PROGRESS_ALIAS + DOT + DailyProgressFields.PROGRESS_FOR_DAY, PROGRESS_FOR_DAY_ALIAS,
-                JoinType.INNER);
-        scb.createAlias(PROGRESS_FOR_DAY_ALIAS + DOT + ProgressForDayFields.TECHNOLOGY_OPERATION_COMPONENT,
-                TECHNOLOGY_OPERATION_COMPONENT_ALIAS, JoinType.INNER);
-        scb.add(in(PROGRESS_FOR_DAY_ALIAS + DOT + ID, pfdIds));
-        if (additionalCriteria != null) {
-            scb.add(additionalCriteria);
-        }
-        scb.addOrder(desc(QUANTITY_SUM_PROJECTION));
-
-        Entity projection = scb.setMaxResults(1).uniqueResult();
-        BigDecimal quantitiesSum = projection.getDecimalField(QUANTITY_SUM_PROJECTION);
-        return ObjectUtils.defaultIfNull(quantitiesSum, BigDecimal.ZERO);
-    }
-*/
-/*
-
-    */
-/**
-     * This method returns a set of corrected or planned (depending on given parameter, if corrected progress is chosen but
-     * doesn't exist, planned values are returned) progress ids for given technology.<br/>
-     * <br/>
-     * <p>
-     * I assume that each order has its own copy of technology tree.<br/>
-     * <br/>
-     *
-     * @return set of ids for progressForDay entities, related to given technology.
-     * @since 1.3.0
-     *//*
-
-    public Set<Long> findIdsOfEffectiveProgressForDay(final Long technologyId, boolean corrected) {
-        DataDefinition progressForDayDD = getProgressForDayDD();
-
-        SearchProjectionList projectionsList = list();
-        projectionsList.add(alias(max(PROGRESS_FOR_DAY_ALIAS + DOT + ID), ID_PROJECTION));
-        projectionsList.add(groupField(TECHNOLOGY_OPERATION_COMPONENT_ALIAS + DOT + ID));
-        projectionsList.add(groupField(PROGRESS_FOR_DAY_ALIAS + DOT + ProgressForDayFields.ACTUAL_DATE_OF_DAY));
-
-        SearchCriteriaBuilder scb = progressForDayDD.findWithAlias(PROGRESS_FOR_DAY_ALIAS);
-        scb.setProjection(projectionsList);
-        scb.createAlias(PROGRESS_FOR_DAY_ALIAS + DOT + ProgressForDayFields.TECHNOLOGY_OPERATION_COMPONENT,
-                TECHNOLOGY_OPERATION_COMPONENT_ALIAS, JoinType.INNER);
-        scb.createAlias(TECHNOLOGY_OPERATION_COMPONENT_ALIAS + DOT + TechnologyOperationComponentFields.TECHNOLOGY, TECH_ALIAS,
-                JoinType.INNER);
-        scb.add(eq(TECH_ALIAS + DOT + ID, technologyId));
-        scb.addOrder(desc(ID_PROJECTION));
-
-        SearchCriteriaBuilder correctedScb = scb;
-        correctedScb.add(eq(PROGRESS_FOR_DAY_ALIAS + DOT + ProgressForDayFields.CORRECTED, corrected));
-
-        List<Entity> idsProjection = correctedScb.list().getEntities();
-
-        if (idsProjection.isEmpty()) {
-            idsProjection = scb.list().getEntities();
-        }
-        return Sets.newHashSet(EntityUtils.<Long>getFieldsView(idsProjection, ID_PROJECTION));
-    }
-*/
-
-    /**
-     * This method returns a set of corrected or planned (if corrected progress doesn't exist for given day & operation
-     * combination) progress ids for given technology.<br/>
-     * <br/>
-     * <p>
-     * I assume that each order has its own copy of technology tree.<br/>
-     * <br/>
-     * <p>
-     * Corrected progresses take precedence over planned ones. For example:<br/>
-     * <br/>
-     * <p>
-     * If you have defined following planned progresses for some operation OP-1 at 1st, 2nd and 3rd day and you have also progress
-     * correction for the same operation at 2nd day, you'll get ids of the following records:
-     * <ul>
-     * <li>planned progress for 1st day</li>
-     * <li>corrected progress for 2nd day</li>
-     * <li>planned progress for 3rd day</li>
-     * </ul>
-     *
-     * @return set of ids for progressForDay entities, related to given technology.
-     * @since 1.3.0
-     */
- /*   @Deprecated public Set<Long> findIdsOfEffectiveProgressForDay(final Long technologyId) {
-        DataDefinition progressForDayDD = getProgressForDayDD();
-
-        SearchProjectionList projectionsList = list();
-        projectionsList.add(alias(max(PROGRESS_FOR_DAY_ALIAS + DOT + ID), ID_PROJECTION));
-        projectionsList.add(groupField(TECHNOLOGY_OPERATION_COMPONENT_ALIAS + DOT + ID));
-        projectionsList.add(groupField(PROGRESS_FOR_DAY_ALIAS + DOT + ProgressForDayFields.ACTUAL_DATE_OF_DAY));
-
-        SearchCriteriaBuilder scb = progressForDayDD.findWithAlias(PROGRESS_FOR_DAY_ALIAS);
-        scb.setProjection(projectionsList);
-        scb.createAlias(PROGRESS_FOR_DAY_ALIAS + DOT + ProgressForDayFields.TECHNOLOGY_OPERATION_COMPONENT,
-                TECHNOLOGY_OPERATION_COMPONENT_ALIAS, JoinType.INNER);
-        scb.createAlias(TECHNOLOGY_OPERATION_COMPONENT_ALIAS + DOT + TechnologyOperationComponentFields.TECHNOLOGY, TECH_ALIAS,
-                JoinType.INNER);
-        scb.add(eq(TECH_ALIAS + DOT + ID, technologyId));
-        scb.addOrder(desc(ID_PROJECTION));
-
-        List<Entity> idsProjection = scb.list().getEntities();
-
-        return Sets.newHashSet(EntityUtils.<Long>getFieldsView(idsProjection, ID_PROJECTION));
-    }
-*/
     public DataDefinition getProductionPerShiftDD() {
         return dataDefinitionService
                 .get(ProductionPerShiftConstants.PLUGIN_IDENTIFIER, ProductionPerShiftConstants.MODEL_PRODUCTION_PER_SHIFT);
-    }
-
-    private DataDefinition getProgressForDayDD() {
-        return dataDefinitionService
-                .get(ProductionPerShiftConstants.PLUGIN_IDENTIFIER, ProductionPerShiftConstants.MODEL_PROGRESS_FOR_DAY);
     }
 
     private DataDefinition getDailyProgressDD() {
