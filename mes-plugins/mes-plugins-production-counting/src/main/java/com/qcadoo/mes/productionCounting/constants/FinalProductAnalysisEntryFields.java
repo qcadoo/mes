0440afca82ecf93a7cb080aafc17e9f6adfdd235
Katarzyna Małyszek <katarzyna.malyszek@qcadoo.com>
2017-03-17 14:21:30 +0100

    SC# ANEKS-297

---
+++ mes-plugins/mes-plugins-production-counting/src/main/java/com/qcadoo/mes/productionCounting/constants/FinalProductAnalysisEntryFields.java
@@ -0,0 +1,40 @@
+package com.qcadoo.mes.productionCounting.constants;
+
+public final class FinalProductAnalysisEntryFields {
+
+    private FinalProductAnalysisEntryFields() {
+    }
+
+    public static final String FINAL_PRODUCT_ANALYSIS_GENERATOR = "finalProductAnalysisGenerator";
+
+    public static final String ASSORTMENT_NAME = "assortmentName";
+
+    public static final String PRODUCT_NUMBER = "productNumber";
+
+    public static final String PRODUCT_NAME = "productName";
+
+    public static final String PRODUCT_UNIT = "productUnit";
+
+    public static final String SIZE = "size";
+
+    public static final String QUANTITY = "quantity";
+
+    public static final String WASTES = "wastes";
+
+    public static final String DONE_QUANTITY = "doneQuantity";
+
+    public static final String SHIFT_NAME = "shiftName";
+
+    public static final String TIME_RANGE_FROM = "timeRangeFrom";
+
+    public static final String TIME_RANGE_TO = "timeRangeTo";
+
+    public static final String TECHNOLOGY_GENERATOR_NUMBER = "technologyGeneratorNumber";
+
+    public static final String ORDER_NUMBER = "orderNumber";
+
+    public static final String COMPANY_NUMBER = "companyNumber";
+
+    public static final String PRODUCION_LINE_NUMBER = "productionLineNumber";
+
+}
