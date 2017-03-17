d0aef4570bc0b9f0c972768ad64c247ee713a419
Krzysztof Jeliński <krzysztofjelinski@MacBook-Pro-krje.local>
2017-03-17 12:39:44 +0100

    SC#ANEKS-280 - Added generator name to the grid. Enabled total values calculations

--- mes-plugins/mes-plugins-production-counting/src/main/java/com/qcadoo/mes/productionCounting/listeners/ProductionAnalysisListeners.java
+++ mes-plugins/mes-plugins-production-counting/src/main/java/com/qcadoo/mes/productionCounting/listeners/ProductionAnalysisListeners.java
@@ -1,28 +1,38 @@
 package com.qcadoo.mes.productionCounting.listeners;
 
+import java.util.Collections;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Service;
 
-import com.google.common.collect.Maps;
-import com.qcadoo.mes.productionCounting.constants.PerformanceAnalysisDetailsDtoFields;
-import com.qcadoo.model.api.Entity;
 import com.qcadoo.view.api.ComponentState;
 import com.qcadoo.view.api.ViewDefinitionState;
-import com.qcadoo.view.api.components.GridComponent;
+import com.qcadoo.view.api.components.FieldComponent;
 
 @Service
 public class ProductionAnalysisListeners {
 
-    private static final String L_GRID = "grid";
-
-    private static final String L_FILTERS = "filters";
-
-    private static final String L_GRID_OPTIONS = "grid.options";
-
-    private static final String L_WINDOW_ACTIVE_MENU = "window.activeMenu";
+    @Autowired
+    private NamedParameterJdbcTemplate jdbcTemplate;
 
     public void calculateTotalQuantities(final ViewDefinitionState view, final ComponentState state, final String[] args) {
-        System.out.println("Ohh yeah!");
+        FieldComponent totalProducedQuantity = (FieldComponent) view.getComponentByReference("totalProducedQuantity");
+        FieldComponent numberOfWorkers = (FieldComponent) view.getComponentByReference("numberOfWorkers");
+
+        Map<String, Object> values = jdbcTemplate.queryForMap(buildQuery(), Collections.emptyMap());
+        totalProducedQuantity.setFieldValue(values.get("totalDoneQuantity"));
+        numberOfWorkers.setFieldValue(values.get("totalStaffNumber"));
+        totalProducedQuantity.requestComponentUpdateState();
+        numberOfWorkers.requestComponentUpdateState();
+    }
+
+    private String buildQuery() {
+        StringBuilder query = new StringBuilder();
+        query.append("SELECT SUM(donequantity)::bigint AS totalDoneQuantity, ");
+        query.append("COUNT(DISTINCT staff_id) AS totalStaffNumber ");
+        query.append("FROM productioncounting_productionanalysisdto");
+        return query.toString();
     }
 }
