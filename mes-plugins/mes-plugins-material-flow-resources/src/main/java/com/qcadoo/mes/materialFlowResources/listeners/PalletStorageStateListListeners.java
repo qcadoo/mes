3ad084c19e801ae5db0a42626db9c183aee9bcb2
Krzysztof Jeliński <yeloon@mail.com>
2017-03-29 15:22:46 +0200

    SC#ESILCO-120 - Added pallet storage state list view and new menu entry. Created model and DB scripts.

---
+++ mes-plugins/mes-plugins-material-flow-resources/src/main/java/com/qcadoo/mes/materialFlowResources/listeners/PalletStorageStateListListeners.java
@@ -0,0 +1,15 @@
+package com.qcadoo.mes.materialFlowResources.listeners;
+
+import org.springframework.stereotype.Service;
+
+import com.qcadoo.view.api.ComponentState;
+import com.qcadoo.view.api.ViewDefinitionState;
+
+@Service
+public class PalletStorageStateListListeners {
+
+    public void showDetails(final ViewDefinitionState view, final ComponentState state, final String[] args) {
+        // empty by now
+    }
+
+}
