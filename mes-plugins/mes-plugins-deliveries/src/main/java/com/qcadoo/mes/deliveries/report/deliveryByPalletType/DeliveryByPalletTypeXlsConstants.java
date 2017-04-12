f0ef0e35d3e2ec94ffca85be20bf3abbbd8d8a78
ksiwonia <kamil.siwonia@gmail.com>
2017-04-12 13:26:44 +0200

    SC#ESILCO-482

---
+++ mes-plugins/mes-plugins-deliveries/src/main/java/com/qcadoo/mes/deliveries/report/deliveryByPalletType/DeliveryByPalletTypeXlsConstants.java
@@ -0,0 +1,11 @@
+package com.qcadoo.mes.deliveries.report.deliveryByPalletType;
+
+class DeliveryByPalletTypeXlsConstants {
+
+    private DeliveryByPalletTypeXlsConstants(){
+
+    }
+
+    public static final String REPORT_TITLE = "deliveries.menu.requirements.deliveryByPalletTypeReport";
+
+}
--- mes-plugins/mes-plugins-deliveries/src/main/java/com/qcadoo/mes/deliveries/report/deliveryByPalletType/DeliveryByPalletTypeXlsView.java
+++ mes-plugins/mes-plugins-deliveries/src/main/java/com/qcadoo/mes/deliveries/report/deliveryByPalletType/DeliveryByPalletTypeXlsView.java
@@ -3,6 +3,8 @@ package com.qcadoo.mes.deliveries.report.deliveryByPalletType;
 import com.qcadoo.report.api.xls.abstractview.AbstractXLSXView;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,11 +12,15 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 @Component
-public class DeliveryByPalletTypeXlsView extends AbstractXLSXView {
+class DeliveryByPalletTypeXlsView extends AbstractXLSXView {
 
-    @Override protected void buildExcelDocument(Map<String, Object> model, XSSFWorkbook workbook, HttpServletRequest request,
+    @Autowired
+    private DeliveryByPalletTypeXlsService deliveryByPalletTypeXlsService;
+
+    @Override
+    protected void buildExcelDocument(Map<String, Object> model, XSSFWorkbook workbook, HttpServletRequest request,
             HttpServletResponse response) throws Exception {
-        XSSFSheet sheet = workbook.createSheet("RAPORCIK");
-
+        XSSFSheet sheet = workbook.createSheet(deliveryByPalletTypeXlsService.getReportTitle(LocaleContextHolder.getLocale()));
+        deliveryByPalletTypeXlsService.buildExcelContent(workbook,sheet,model, LocaleContextHolder.getLocale());
     }
 }
