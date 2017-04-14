36d73d1382d05e13a05e66c73d2653968e687944
Katarzyna Małyszek <katarzyna.malyszek@qcadoo.com>
2017-04-14 10:13:58 +0200

    SC# ESILCO-481

---
+++ mes-plugins/mes-plugins-material-flow-resources/src/main/java/com/qcadoo/mes/materialFlowResources/palletBalance/PalletBalanceReportHelper.java
@@ -0,0 +1,100 @@
+package com.qcadoo.mes.materialFlowResources.palletBalance;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.qcadoo.model.api.DataDefinitionService;
+import com.qcadoo.model.api.DictionaryService;
+
+@Service
+public class PalletBalanceReportHelper {
+
+    @Autowired
+    private DataDefinitionService dataDefinitionService;
+
+    @Autowired
+    private DictionaryService dictionaryService;
+
+    @Autowired
+    private NamedParameterJdbcTemplate jdbcTemplate;
+
+    public List<String> getTypesOfPallet() {
+        return dictionaryService.getKeys("typeOfPallet");
+    }
+
+    public Map<Date, List<PalletBalanceRowDto>> getCurrentState() {
+        StringBuilder query = new StringBuilder();
+        query.append("select r.typeofpallet as typeOfPallet, count(distinct p.number) as palletsCount, current_date AS day ");
+        query.append("  from materialflowresources_resource r ");
+        query.append("  join basic_palletnumber p on r.palletnumber_id = p.id ");
+        query.append("group by r.typeofpallet");
+
+        List<PalletBalanceRowDto> results = jdbcTemplate.query(query.toString(), new BeanPropertyRowMapper<>(
+                PalletBalanceRowDto.class));
+
+        Map<Date, List<PalletBalanceRowDto>> map = Maps.newHashMap();
+        map.put(new Date(), results);
+        return map;
+    }
+
+    public Map<Date, List<PalletBalanceRowDto>> getInbounds(final Date dateFrom) {
+        StringBuilder query = new StringBuilder();
+        query.append("select p.typeofpallet as typeOfPallet, date_trunc('day', d.time) as day, count(distinct pn.number) as palletsCount ");
+        query.append("  from materialflowresources_position p ");
+        query.append("  join basic_palletnumber pn on p.palletnumber_id = pn.id ");
+        query.append("  join materialflowresources_document d on p.document_id = d.id ");
+        query.append("      where d.type in ('01receipt','02internalInbound') ");
+        query.append("      and date_trunc('day', d.time) >= :dateFrom ");
+        query.append("group by p.typeofpallet, date_trunc('day',d.time)");
+
+        Map<String, Object> params = Maps.newHashMap();
+        params.put("dateFrom", dateFrom);
+
+        List<PalletBalanceRowDto> results = jdbcTemplate.query(query.toString(), params, new BeanPropertyRowMapper<>(
+                PalletBalanceRowDto.class));
+
+        return mapQueryResults(results);
+    }
+
+    // TODO - zmienić metodę po zmianach w numerach palet
+    public Map<Date, List<PalletBalanceRowDto>> getOutbounds(final Date dateFrom) {
+        StringBuilder query = new StringBuilder();
+        query.append("select p.typeofpallet as typeOfPallet, date_trunc('day', d.time) as day, count(distinct pn.number) as palletsCount ");
+        query.append("  from materialflowresources_position p ");
+        query.append("  join basic_palletnumber pn on p.palletnumber_id = pn.id ");
+        query.append("  join materialflowresources_document d on p.document_id = d.id ");
+        query.append("      where d.type in ('01receipt','02internalInbound') ");
+        query.append("      and date_trunc('day', d.time) >= :dateFrom ");
+        query.append("group by p.typeofpallet, date_trunc('day',d.time)");
+
+        Map<String, Object> params = Maps.newHashMap();
+        params.put("dateFrom", dateFrom);
+
+        List<PalletBalanceRowDto> results = jdbcTemplate.query(query.toString(), params, new BeanPropertyRowMapper<>(
+                PalletBalanceRowDto.class));
+
+        return mapQueryResults(results);
+    }
+
+    private Map<Date, List<PalletBalanceRowDto>> mapQueryResults(List<PalletBalanceRowDto> results) {
+        Map<Date, List<PalletBalanceRowDto>> map = Maps.newHashMap();
+        for (PalletBalanceRowDto dto : results) {
+            Date day = dto.getDay();
+            if (map.containsKey(day)) {
+                map.get(day).add(dto);
+            } else {
+                map.put(day, Lists.newArrayList(dto));
+            }
+        }
+        return map;
+    }
+
+}
---
+++ mes-plugins/mes-plugins-material-flow-resources/src/main/java/com/qcadoo/mes/materialFlowResources/palletBalance/PalletBalanceXlsService.java
@@ -0,0 +1,201 @@
+package com.qcadoo.mes.materialFlowResources.palletBalance;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Optional;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFPrintSetup;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.joda.time.DateTime;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.qcadoo.localization.api.TranslationService;
+import com.qcadoo.mes.materialFlowResources.constants.PalletBalanceFields;
+import com.qcadoo.model.api.Entity;
+import com.qcadoo.report.api.xls.XlsDocumentService;
+
+@Service
+public class PalletBalanceXlsService extends XlsDocumentService {
+
+    @Autowired
+    private TranslationService translationService;
+
+    @Autowired
+    private PalletBalanceReportHelper palletBalanceReportHelper;
+
+    private static final List<String> HEADER_KEYS = Lists.newArrayList("initialState", "inbounds", "outbounds", "moves",
+            "finalState");
+
+    @Override
+    public String getReportTitle(Locale locale) {
+        return translationService.translate("materialFlowResource.palletBalance.report.title", locale);
+    }
+
+    private HSSFCell createRegularCell(StylesContainer stylesContainer, HSSFRow row, int column, String content) {
+        HSSFCell cell = row.createCell(column);
+        cell.setCellValue(content);
+        cell.setCellStyle(StylesContainer.aligned(stylesContainer.regularStyle, HSSFCellStyle.ALIGN_LEFT));
+        return cell;
+    }
+
+    private HSSFCell createNumericCell(StylesContainer stylesContainer, HSSFRow row, int column, int value) {
+        HSSFCell cell = row.createCell(column, HSSFCell.CELL_TYPE_NUMERIC);
+        cell.setCellValue(value);
+        cell.setCellStyle(StylesContainer.aligned(stylesContainer.regularStyle, HSSFCellStyle.ALIGN_LEFT));
+        return cell;
+    }
+
+    private HSSFCell createHeaderCell(StylesContainer stylesContainer, HSSFRow row, String content, int column, short align) {
+        HSSFCell cell = row.createCell(column);
+        cell.setCellValue(content);
+        cell.setCellStyle(StylesContainer.aligned(stylesContainer.headerStyle, align));
+        return cell;
+    }
+
+    @Override
+    protected void addHeader(HSSFSheet sheet, Locale locale, Entity palletBalance) {
+
+        final FontsContainer fontsContainer = new FontsContainer(sheet.getWorkbook());
+        final StylesContainer stylesContainer = new StylesContainer(sheet.getWorkbook(), fontsContainer);
+
+        List<String> typesOfPallet = palletBalanceReportHelper.getTypesOfPallet();
+        HSSFRow headerRow = sheet.createRow(0);
+        HSSFRow typesOfPalletRow = sheet.createRow(1);
+        addDateHeader(sheet, locale, headerRow, stylesContainer);
+        addTypesOfPalletHeader(sheet, locale, headerRow, typesOfPalletRow, stylesContainer, typesOfPallet);
+    }
+
+    private void addDateHeader(HSSFSheet sheet, Locale locale, HSSFRow headerRow, StylesContainer stylesContainer) {
+        sheet.addMergedRegion(new CellRangeAddress(0, 1, 0, 0));
+        createHeaderCell(stylesContainer, headerRow,
+                translationService.translate("materialFlowResource.palletBalance.report.date", locale), 0,
+                HSSFCellStyle.ALIGN_CENTER);
+    }
+
+    private void addTypesOfPalletHeader(HSSFSheet sheet, Locale locale, HSSFRow headerRow, HSSFRow typesOfPalletRow,
+            StylesContainer stylesContainer, List<String> typesOfPallet) {
+
+        int columnIndex = 1;
+        int typesOfPalletCount = typesOfPallet.size();
+        int lastColumnIndex = typesOfPalletCount + columnIndex - 1;
+
+        for (String key : HEADER_KEYS) {
+            addHeaderCell(sheet, translationService.translate("materialFlowResource.palletBalance.report.header." + key, locale),
+                    headerRow, columnIndex, lastColumnIndex, stylesContainer);
+            for (int i = 0; i < typesOfPalletCount; i++) {
+                createHeaderCell(stylesContainer, typesOfPalletRow, typesOfPallet.get(i), columnIndex + i,
+                        HSSFCellStyle.ALIGN_LEFT);
+            }
+            columnIndex = lastColumnIndex + 1;
+            lastColumnIndex += typesOfPalletCount;
+        }
+    }
+
+    private void addHeaderCell(HSSFSheet sheet, String content, HSSFRow headerRow, int columnIndex, int lastColumnIndex,
+            StylesContainer stylesContainer) {
+        sheet.addMergedRegion(new CellRangeAddress(0, 0, columnIndex, lastColumnIndex));
+        createHeaderCell(stylesContainer, headerRow, content, columnIndex, HSSFCellStyle.ALIGN_CENTER);
+    }
+
+    @Override
+    protected void addSeries(HSSFSheet sheet, Entity palletBalance) {
+        sheet.getPrintSetup().setLandscape(true);
+        sheet.getPrintSetup().setPaperSize(HSSFPrintSetup.A3_PAPERSIZE);
+        sheet.getPrintSetup().setHResolution((short) 1);
+
+        final FontsContainer fontsContainer = new FontsContainer(sheet.getWorkbook());
+        final StylesContainer stylesContainer = new StylesContainer(sheet.getWorkbook(), fontsContainer);
+
+        Date dateFrom = palletBalance.getDateField(PalletBalanceFields.DATE_FROM);
+        Date dateTo = palletBalance.getDateField(PalletBalanceFields.DATE_TO);
+        List<String> typesOfPallet = palletBalanceReportHelper.getTypesOfPallet();
+
+        Map<Date, List<PalletBalanceRowDto>> inbounds = palletBalanceReportHelper.getInbounds(dateFrom);
+        Map<Date, List<PalletBalanceRowDto>> outbounds = palletBalanceReportHelper.getOutbounds(dateFrom);
+        Map<Date, List<PalletBalanceRowDto>> initialState = Maps.newHashMap();
+        Map<Date, List<PalletBalanceRowDto>> moves = Maps.newHashMap();
+        Map<Date, List<PalletBalanceRowDto>> finalState = Maps.newHashMap();
+
+        int columnIndex = 1;
+
+        int rowIndex = 2;
+        DateTime currentDate = new DateTime(dateFrom);
+        while (currentDate.toDate().compareTo(dateTo) <= 0) {
+            HSSFRow row = sheet.createRow(rowIndex);
+
+            createRegularCell(stylesContainer, row, 0, currentDate.toString("dd.MM.yyyy"));
+
+            columnIndex = createRowPart(initialState, row, columnIndex, typesOfPallet, currentDate.toDate(), stylesContainer);
+            columnIndex = createRowPart(inbounds, row, columnIndex, typesOfPallet, currentDate.toDate(), stylesContainer);
+            columnIndex = createRowPart(outbounds, row, columnIndex, typesOfPallet, currentDate.toDate(), stylesContainer);
+            columnIndex = createRowPart(moves, row, columnIndex, typesOfPallet, currentDate.toDate(), stylesContainer);
+            createRowPart(finalState, row, columnIndex, typesOfPallet, currentDate.toDate(), stylesContainer);
+
+            columnIndex = 1;
+            rowIndex++;
+            currentDate = currentDate.plusDays(1);
+        }
+    }
+
+    private int createRowPart(Map<Date, List<PalletBalanceRowDto>> data, HSSFRow row, int columnIndex,
+            List<String> typesOfPallet, Date currentDate, StylesContainer stylesContainer) {
+        for (String typeOfPallet : typesOfPallet) {
+            if (data.containsKey(currentDate)) {
+                Optional<PalletBalanceRowDto> stateForDay = data.get(currentDate).stream()
+                        .filter(dto -> typeOfPallet.equals(dto.getTypeOfPallet())).findAny();
+                if (stateForDay.isPresent()) {
+                    createNumericCell(stylesContainer, row, columnIndex, stateForDay.get().getPalletsCount());
+                } else {
+                    createNumericCell(stylesContainer, row, columnIndex, 0);
+                }
+            } else {
+                createNumericCell(stylesContainer, row, columnIndex, 0);
+            }
+            columnIndex++;
+        }
+        return columnIndex;
+    }
+
+    private static class StylesContainer {
+
+        private final HSSFCellStyle regularStyle;
+
+        private final HSSFCellStyle headerStyle;
+
+        StylesContainer(HSSFWorkbook workbook, FontsContainer fontsContainer) {
+            regularStyle = workbook.createCellStyle();
+            regularStyle.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER);
+
+            headerStyle = workbook.createCellStyle();
+            headerStyle.setFont(fontsContainer.headerFont);
+        }
+
+        private static HSSFCellStyle aligned(HSSFCellStyle style, short align) {
+            style.setAlignment(align);
+            return style;
+        }
+
+    }
+
+    private static class FontsContainer {
+
+        private final Font headerFont;
+
+        FontsContainer(HSSFWorkbook workbook) {
+
+            headerFont = workbook.createFont();
+            headerFont.setBoldweight(Font.BOLDWEIGHT_BOLD);
+        }
+    }
+}
