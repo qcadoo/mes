3d700c41bbc28479f1f38a81b49a79f9f8ac599d
Krzysztof Jeliński <yeloon@mail.com>
2017-04-26 09:46:45 +0200

    SC#NBLS-357 - Added requirement for wasteUsedQuantity field when wasteUsed.

---
+++ mes-plugins/mes-plugins-production-counting/src/main/java/com/qcadoo/mes/productionCounting/validators/TrackingOperationProductInComponentValidators.java
@@ -0,0 +1,24 @@
+package com.qcadoo.mes.productionCounting.validators;
+
+import java.math.BigDecimal;
+
+import org.springframework.stereotype.Component;
+
+import com.qcadoo.mes.productionCounting.constants.TrackingOperationProductInComponentFields;
+import com.qcadoo.model.api.DataDefinition;
+import com.qcadoo.model.api.Entity;
+import com.qcadoo.model.api.FieldDefinition;
+
+@Component
+public class TrackingOperationProductInComponentValidators {
+
+
+    public boolean validateWasteUsedQuantity(final DataDefinition dataDefinition, final FieldDefinition fieldDefinition, final Entity entity, final Object fieldOldValue, final Object fieldNewValue) {
+        BigDecimal wasteUsedQuantity = entity.getDecimalField(TrackingOperationProductInComponentFields.WASTE_USED_QUANTITY);
+        boolean wasteUsed = entity.getBooleanField(TrackingOperationProductInComponentFields.WASTE_USED);
+        if(wasteUsed && wasteUsedQuantity == null){
+            return false;
+        }
+        return true;
+    }
+}
