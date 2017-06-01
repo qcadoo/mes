ee18ed0dacd2cf4c2670487c8e12907852009e61
Katarzyna Małyszek <katarzyna.malyszek@qcadoo.com>
2017-06-01 11:55:58 +0200

    DF# ESILCO-569

--- mes-plugins/mes-plugins-material-flow-resources/src/main/java/com/qcadoo/mes/materialFlowResources/hooks/StorageLocationHooks.java
+++ mes-plugins/mes-plugins-material-flow-resources/src/main/java/com/qcadoo/mes/materialFlowResources/hooks/StorageLocationHooks.java
@@ -1,8 +1,11 @@
 package com.qcadoo.mes.materialFlowResources.hooks;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.google.common.collect.Lists;
 import com.qcadoo.mes.materialFlowResources.constants.MaterialFlowResourcesConstants;
 import com.qcadoo.mes.materialFlowResources.constants.StorageLocationFields;
 import com.qcadoo.mes.materialFlowResources.constants.StorageLocationHistoryFields;
@@ -17,13 +20,14 @@ public class StorageLocationHooks {
     private DataDefinitionService dataDefinitionService;
 
     public void onSave(final DataDefinition storageLocationDD, final Entity storageLocation) {
-
-        if (storageLocation.getId() == null) {
-            return;
-        }
-        Entity storageLocationFromDb = storageLocationDD.get(storageLocation.getId());
+        Entity oldProduct;
         Entity newProduct = storageLocation.getBelongsToField(StorageLocationFields.PRODUCT);
-        Entity oldProduct = storageLocationFromDb.getBelongsToField(StorageLocationFields.PRODUCT);
+        if (storageLocation.getId() == null) {
+            oldProduct = null;
+        } else {
+            Entity storageLocationFromDb = storageLocationDD.get(storageLocation.getId());
+            oldProduct = storageLocationFromDb.getBelongsToField(StorageLocationFields.PRODUCT);
+        }
         if (oldProduct == null) {
             if (newProduct != null) {
                 auditChanges(storageLocation, null, newProduct);
@@ -44,25 +48,53 @@ public class StorageLocationHooks {
         history.setField(StorageLocationHistoryFields.STORAGE_LOCATION, storageLocation);
         history.setField(StorageLocationHistoryFields.PRODUCT_FROM, oldProduct);
         history.setField(StorageLocationHistoryFields.PRODUCT_TO, newProduct);
-        historyDD.save(history);
+        Entity saved = historyDD.save(history);
+
+        List<Entity> existingHistory = storageLocation.getHasManyField(StorageLocationFields.HISTORY);
+
+        if (existingHistory == null) {
+            existingHistory = Lists.newArrayList();
+        } else {
+            existingHistory = Lists.newArrayList(existingHistory);
+        }
+        existingHistory.add(saved);
+        storageLocation.setField(StorageLocationFields.HISTORY, existingHistory);
 
         DataDefinition productHistoryDD = dataDefinitionService.get(MaterialFlowResourcesConstants.PLUGIN_IDENTIFIER,
                 MaterialFlowResourcesConstants.MODEL_PRODUCT_STORAGE_LOCATION_HISTORY);
         if (oldProduct == null || (newProduct != null && !oldProduct.getId().equals(newProduct.getId()))) {
             Entity productToHistory = productHistoryDD.create();
+
+            List<Entity> existingProductHistory = storageLocation.getHasManyField(StorageLocationFields.PRODUCT_TO_HISTORY);
+
+            if (existingProductHistory == null) {
+                existingProductHistory = Lists.newArrayList();
+            } else {
+                existingProductHistory = Lists.newArrayList(existingProductHistory);
+            }
             productToHistory.setField(StorageLocationHistoryFields.STORAGE_LOCATION_TO, storageLocation);
             productToHistory.setField(StorageLocationHistoryFields.PRODUCT, newProduct);
             productToHistory.setField(StorageLocationHistoryFields.LOCATION,
                     storageLocation.getBelongsToField(StorageLocationFields.LOCATION));
-            productHistoryDD.save(productToHistory);
+            existingProductHistory.add(productHistoryDD.save(productToHistory));
+            storageLocation.setField(StorageLocationFields.PRODUCT_TO_HISTORY, existingProductHistory);
         }
         if (newProduct == null || (oldProduct != null && !oldProduct.getId().equals(newProduct.getId()))) {
             Entity productFromHistory = productHistoryDD.create();
+            List<Entity> existingProductHistory = storageLocation.getHasManyField(StorageLocationFields.PRODUCT_FROM_HISTORY);
+
+            if (existingProductHistory == null) {
+                existingProductHistory = Lists.newArrayList();
+            } else {
+                existingProductHistory = Lists.newArrayList(existingProductHistory);
+            }
             productFromHistory.setField(StorageLocationHistoryFields.STORAGE_LOCATION_FROM, storageLocation);
             productFromHistory.setField(StorageLocationHistoryFields.PRODUCT, oldProduct);
             productFromHistory.setField(StorageLocationHistoryFields.LOCATION,
                     storageLocation.getBelongsToField(StorageLocationFields.LOCATION));
-            productHistoryDD.save(productFromHistory);
+
+            existingProductHistory.add(productHistoryDD.save(productFromHistory));
+            storageLocation.setField(StorageLocationFields.PRODUCT_FROM_HISTORY, existingProductHistory);
         }
 
     }
