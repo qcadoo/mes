a3de52f428d92f0db4c3c780622cd8f76f0d3dd1
Krzysztof Nadolski <krzysztof.nadolski@qcadoo.com>
2017-06-06 23:19:17 +0200

    BTW-183

--- mes-plugins/mes-plugins-deliveries/src/main/java/com/qcadoo/mes/deliveries/states/constants/DeliveryState.java
+++ mes-plugins/mes-plugins-deliveries/src/main/java/com/qcadoo/mes/deliveries/states/constants/DeliveryState.java
@@ -60,7 +60,7 @@ public enum DeliveryState implements StateEnum {
 
         @Override
         public boolean canChangeTo(final StateEnum targetState) {
-            return RECEIVED.equals(targetState) || RECEIVE_CONFIRM_WAITING.equals(targetState) || DECLINED.equals(targetState);
+            return RECEIVED.equals(targetState) || RECEIVE_CONFIRM_WAITING.equals(targetState) || DECLINED.equals(targetState) || DURING_CORRECTION.equals(targetState);
         }
     },
     RECEIVED(DeliveryStateStringValues.RECEIVED) {
