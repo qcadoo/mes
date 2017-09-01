eee5748afe0e7fd3cb0bfb5e6702105c2b83efd2
ksiwonia <kamil.siwonia@gmail.com>
2017-09-01 06:42:14 +0200

    ESILCO-783

--- mes-plugins/mes-plugins-material-flow-resources/src/main/java/com/qcadoo/mes/materialFlowResources/hooks/StocktakingModelHooks.java
+++ mes-plugins/mes-plugins-material-flow-resources/src/main/java/com/qcadoo/mes/materialFlowResources/hooks/StocktakingModelHooks.java
@@ -6,12 +6,21 @@ import com.qcadoo.model.api.DataDefinition;
 import com.qcadoo.model.api.Entity;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @Service
 public class StocktakingModelHooks {
 
     public void onSave(final DataDefinition stocktakingDD, final Entity stocktaking) {
         if (StorageLocationMode.ALL.getStringValue().equals(stocktaking.getStringField(StocktakingFields.STORAGE_LOCATION_MODE))) {
             stocktaking.setField(StocktakingFields.STORAGE_LOCATIONS, null);
+        }
+        if (Objects.nonNull(stocktaking.getId())) {
+            Entity stocktakingDb = stocktakingDD.get(stocktaking.getId());
+            if (!stocktaking.getBelongsToField("location").getId()
+                    .equals(stocktakingDb.getBelongsToField("location").getId())) {
+                stocktaking.setField("storageLocations", null);
+            }
         }
     }
 }
