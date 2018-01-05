186b7411779eba3022b1df1fe2e7e9322bee5c80
Krzysztof Nadolski <krzysztof.nadolski@qcadoo.com>
2018-01-05 15:04:21 +0100

    BTW-262

--- mes-plugins/mes-plugins-production-counting/src/main/java/com/qcadoo/mes/productionCounting/ProductionTrackingUpdateServiceImpl.java
+++ mes-plugins/mes-plugins-production-counting/src/main/java/com/qcadoo/mes/productionCounting/ProductionTrackingUpdateServiceImpl.java
@@ -1,23 +1,31 @@
 package com.qcadoo.mes.productionCounting;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
-import com.qcadoo.mes.basicProductionCounting.ProductionTrackingUpdateService;
-import com.qcadoo.mes.productionCounting.constants.*;
-import com.qcadoo.mes.productionCounting.hooks.helpers.OperationProductsExtractor;
-import com.qcadoo.model.api.DataDefinitionService;
-import com.qcadoo.model.api.Entity;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import static com.qcadoo.model.api.search.SearchProjections.alias;
+import static com.qcadoo.model.api.search.SearchProjections.field;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.google.common.collect.Sets;
+import com.qcadoo.mes.basicProductionCounting.ProductionTrackingUpdateService;
+import com.qcadoo.mes.productionCounting.constants.OrderFieldsPC;
+import com.qcadoo.mes.productionCounting.constants.ProductionCountingConstants;
+import com.qcadoo.mes.productionCounting.constants.ProductionTrackingFields;
+import com.qcadoo.mes.productionCounting.constants.TrackingOperationProductInComponentFields;
+import com.qcadoo.mes.productionCounting.constants.TrackingOperationProductOutComponentFields;
+import com.qcadoo.mes.productionCounting.hooks.helpers.OperationProductsExtractor;
+import com.qcadoo.model.api.DataDefinitionService;
+import com.qcadoo.model.api.Entity;
+
 @Service
 public class ProductionTrackingUpdateServiceImpl implements ProductionTrackingUpdateService {
+
+    private static final String PRODUCT_ID_FIELD_NAME = "productId";
 
     @Autowired
     private DataDefinitionService dataDefinitionService;
@@ -27,90 +35,79 @@ public class ProductionTrackingUpdateServiceImpl implements ProductionTrackingUp
 
     @Override
     public void updateProductionTracking(final Long productionTrackingId) {
-        Entity productionTracking = dataDefinitionService.get(ProductionCountingConstants.PLUGIN_IDENTIFIER,
-                ProductionCountingConstants.MODEL_PRODUCTION_TRACKING).get(productionTrackingId);
+        Entity productionTracking = dataDefinitionService
+                .get(ProductionCountingConstants.PLUGIN_IDENTIFIER, ProductionCountingConstants.MODEL_PRODUCTION_TRACKING)
+                .get(productionTrackingId);
 
         Entity order = productionTracking.getBelongsToField(ProductionTrackingFields.ORDER);
-
-        final boolean registerQuantityInProduct = order.getBooleanField(OrderFieldsPC.REGISTER_QUANTITY_IN_PRODUCT);
-        final boolean registerQuantityOutProduct = order.getBooleanField(OrderFieldsPC.REGISTER_QUANTITY_OUT_PRODUCT);
 
         OperationProductsExtractor.TrackingOperationProducts operationProducts = operationProductsExtractor
                 .getProductsByModelName(productionTracking);
 
-        if (registerQuantityInProduct) {
+        if (order.getBooleanField(OrderFieldsPC.REGISTER_QUANTITY_IN_PRODUCT)) {
             updateInProducts(productionTracking, operationProducts);
         }
 
-        if (registerQuantityOutProduct) {
+        if (order.getBooleanField(OrderFieldsPC.REGISTER_QUANTITY_OUT_PRODUCT)) {
             updateOutProducts(productionTracking, operationProducts);
         }
-        productionTracking = productionTracking.getDataDefinition().save(productionTracking);
     }
 
     private void updateOutProducts(final Entity productionTracking,
             final OperationProductsExtractor.TrackingOperationProducts operationProducts) {
-        List<Entity> outputs = Collections.emptyList();
-        List<Entity> productionTrackingOutputs = Collections.emptyList();
-        outputs = operationProducts.getOutputComponents();
-        productionTrackingOutputs = productionTracking
-                .getHasManyField(ProductionTrackingFields.TRACKING_OPERATION_PRODUCT_OUT_COMPONENTS);
+        List<Entity> outputs = operationProducts.getOutputComponents();
+        List<Entity> productionTrackingOutputs = productionTracking
+                .getHasManyField(ProductionTrackingFields.TRACKING_OPERATION_PRODUCT_OUT_COMPONENTS).find()
+                .setProjection(alias(field(TrackingOperationProductOutComponentFields.PRODUCT + ".id"), PRODUCT_ID_FIELD_NAME))
+                .list().getEntities();
 
         Map<Long, Entity> outputsMap = outputs.stream().collect(
                 Collectors.toMap(x -> x.getBelongsToField(TrackingOperationProductOutComponentFields.PRODUCT).getId(), x -> x));
-        Map<Long, Entity> productionTrackingOutputsMap = productionTrackingOutputs.stream().collect(
-                Collectors.toMap(x -> x.getBelongsToField(TrackingOperationProductOutComponentFields.PRODUCT).getId(), x -> x));
+        Map<Long, Entity> productionTrackingOutputsMap = productionTrackingOutputs.stream()
+                .collect(Collectors.toMap(x -> x.getLongField(PRODUCT_ID_FIELD_NAME), x -> x));
 
         Set<Long> newEntries = Sets.difference(outputsMap.keySet(), productionTrackingOutputsMap.keySet());
         Set<Long> removedEntries = Sets.difference(productionTrackingOutputsMap.keySet(), outputsMap.keySet());
 
-        Set<Long> toUpdateEntries = Sets.intersection(outputsMap.keySet(), productionTrackingOutputsMap.keySet());
-
-        List<Entity> updatedProductionTrackingOutputs = Lists.newArrayList();
         for (Long newEntry : newEntries) {
-            updatedProductionTrackingOutputs.add(outputsMap.get(newEntry));
+            Entity trackingOperationProductOutComponent = outputsMap.get(newEntry);
+            trackingOperationProductOutComponent.setField(TrackingOperationProductOutComponentFields.PRODUCTION_TRACKING,
+                    productionTracking);
+            trackingOperationProductOutComponent.getDataDefinition().save(trackingOperationProductOutComponent);
         }
 
-        if (!newEntries.isEmpty() || !removedEntries.isEmpty()) {
-            for (Long e : toUpdateEntries) {
-                Entity toUpdateEntry = productionTrackingOutputsMap.get(e);
-                updatedProductionTrackingOutputs.add(toUpdateEntry);
-            }
-
-            productionTracking.setField(ProductionTrackingFields.TRACKING_OPERATION_PRODUCT_OUT_COMPONENTS,
-                    updatedProductionTrackingOutputs);
+        for (Long removedEntry : removedEntries) {
+            dataDefinitionService.get(ProductionCountingConstants.PLUGIN_IDENTIFIER,
+                    ProductionCountingConstants.MODEL_TRACKING_OPERATION_PRODUCT_OUT_COMPONENT).delete(removedEntry);
         }
     }
 
     private void updateInProducts(final Entity productionTracking,
             final OperationProductsExtractor.TrackingOperationProducts operationProducts) {
-        List<Entity> inputs = Collections.emptyList();
-        List<Entity> productionTrackingInputs = Collections.emptyList();
-        inputs = operationProducts.getInputComponents();
-        productionTrackingInputs = productionTracking
-                .getHasManyField(ProductionTrackingFields.TRACKING_OPERATION_PRODUCT_IN_COMPONENTS);
+        List<Entity> inputs = operationProducts.getInputComponents();
+        List<Entity> productionTrackingInputs = productionTracking
+                .getHasManyField(ProductionTrackingFields.TRACKING_OPERATION_PRODUCT_IN_COMPONENTS).find()
+                .setProjection(alias(field(TrackingOperationProductInComponentFields.PRODUCT + ".id"), PRODUCT_ID_FIELD_NAME))
+                .list().getEntities();
 
         Map<Long, Entity> inputsMap = inputs.stream().collect(
                 Collectors.toMap(x -> x.getBelongsToField(TrackingOperationProductInComponentFields.PRODUCT).getId(), x -> x));
-        Map<Long, Entity> productionTrackingInputsMap = productionTrackingInputs.stream().collect(
-                Collectors.toMap(x -> x.getBelongsToField(TrackingOperationProductOutComponentFields.PRODUCT).getId(), x -> x));
+        Map<Long, Entity> productionTrackingInputsMap = productionTrackingInputs.stream()
+                .collect(Collectors.toMap(x -> x.getLongField(PRODUCT_ID_FIELD_NAME), x -> x));
 
         Set<Long> newEntries = Sets.difference(inputsMap.keySet(), productionTrackingInputsMap.keySet());
-        Set<Long> toUpdateEntries = Sets.intersection(inputsMap.keySet(), productionTrackingInputsMap.keySet());
         Set<Long> removedEntries = Sets.difference(productionTrackingInputsMap.keySet(), inputsMap.keySet());
 
-        List<Entity> updatedProductionTrackingInputs = Lists.newArrayList();
-
         for (Long newEntry : newEntries) {
-            updatedProductionTrackingInputs.add(inputsMap.get(newEntry));
+            Entity trackingOperationProductInComponent = inputsMap.get(newEntry);
+            trackingOperationProductInComponent.setField(TrackingOperationProductInComponentFields.PRODUCTION_TRACKING,
+                    productionTracking);
+            trackingOperationProductInComponent.getDataDefinition().save(trackingOperationProductInComponent);
         }
-        if (!newEntries.isEmpty() || !removedEntries.isEmpty()) {
-            for (Long e : toUpdateEntries) {
-                Entity toUpdateEntry = productionTrackingInputsMap.get(e);
-                updatedProductionTrackingInputs.add(toUpdateEntry);
-            }
-            productionTracking.setField(ProductionTrackingFields.TRACKING_OPERATION_PRODUCT_IN_COMPONENTS,
-                    updatedProductionTrackingInputs);
+
+        for (Long removedEntry : removedEntries) {
+            dataDefinitionService.get(ProductionCountingConstants.PLUGIN_IDENTIFIER,
+                    ProductionCountingConstants.MODEL_TRACKING_OPERATION_PRODUCT_IN_COMPONENT).delete(removedEntry);
         }
     }
 }
