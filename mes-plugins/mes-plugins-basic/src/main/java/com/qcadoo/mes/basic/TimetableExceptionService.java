16c84cb9eb4071757a55b1e57fb8eb97adebd787
Łukasz Poniewierski <lukasz.poniewierski@qcadoo.com>
2018-06-12 15:26:05 +0200

    DF#QCADOOCLS-6206 Fixed QSE in time table exception service and also sonar problems.

--- mes-plugins/mes-plugins-basic/src/main/java/com/qcadoo/mes/basic/TimetableExceptionService.java
+++ mes-plugins/mes-plugins-basic/src/main/java/com/qcadoo/mes/basic/TimetableExceptionService.java
@@ -1,5 +1,12 @@
 package com.qcadoo.mes.basic;
 
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.google.common.collect.Lists;
 import com.qcadoo.localization.api.utils.DateUtils;
 import com.qcadoo.mes.basic.constants.BasicConstants;
@@ -7,14 +14,6 @@ import com.qcadoo.model.api.DataDefinition;
 import com.qcadoo.model.api.DataDefinitionService;
 import com.qcadoo.model.api.Entity;
 import com.qcadoo.model.api.search.SearchQueryBuilder;
-
-import org.apache.commons.lang3.StringUtils;
-import org.joda.time.DateTime;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.Date;
-import java.util.List;
 
 @Service
 public class TimetableExceptionService {
@@ -30,39 +29,46 @@ public class TimetableExceptionService {
         return findFor(Lists.newArrayList(productionLine.getId()), Lists.newArrayList(shift.getId()), date, null);
     }
 
-    public List<Entity> findFor(final List<Long> productionLineIds, final List<Long> shiftIds, final Date date, final String type) {
-        StringBuilder query = new StringBuilder();
+    public List<Entity> findFor(final List<Long> productionLineIds, final List<Long> shiftIds, final Date date,
+            final String type) {
+        List<Entity> shiftTimetableExceptions = Lists.newArrayList();
 
-        query.append("SELECT timetableException FROM #basic_shiftTimetableException timetableException");
-        query.append(" JOIN timetableException.productionLines productionLine");
-        query.append(" JOIN timetableException.shifts shift");
-        query.append(" WHERE productionLine.id IN (:productionLines)");
-        query.append(" AND shift.id IN (:shifts)");
+        if (!productionLineIds.isEmpty() && !shiftIds.isEmpty()) {
+            StringBuilder query = new StringBuilder();
 
-        if (date != null) {
-            query.append(" AND to_char(timetableException.fromDate,'yyyy-MM-dd') <= :date AND to_char(timetableException.toDate,'yyyy-MM-dd') >= :date");
+            query.append("SELECT timetableException FROM #basic_shiftTimetableException timetableException");
+            query.append(" JOIN timetableException.productionLines productionLine");
+            query.append(" JOIN timetableException.shifts shift");
+            query.append(" WHERE productionLine.id IN (:productionLines)");
+            query.append(" AND shift.id IN (:shifts)");
+
+            if (date != null) {
+                query.append(
+                        " AND to_char(timetableException.fromDate,'yyyy-MM-dd') <= :date AND to_char(timetableException.toDate,'yyyy-MM-dd') >= :date");
+            }
+
+            if (StringUtils.isNotEmpty(type)) {
+                query.append(" AND type = :type");
+            }
+
+            SearchQueryBuilder searchQueryBuilder = getShiftTimetableExceptionDD().find(query.toString())
+                    .setParameterList("productionLines", productionLineIds).setParameterList("shifts", shiftIds);
+
+            if (date != null) {
+                searchQueryBuilder.setParameter("date", DateUtils.toDateString(date));
+            }
+
+            if (StringUtils.isNotEmpty(type)) {
+                searchQueryBuilder.setString("type", type);
+            }
+
+            shiftTimetableExceptions = searchQueryBuilder.list().getEntities();
         }
 
-        if (StringUtils.isNotEmpty(type)) {
-            query.append(" AND type = :type");
-        }
-
-        SearchQueryBuilder searchQueryBuilder = getShiftTimetableExceptionDD().find(query.toString())
-                .setParameterList("productionLines", productionLineIds)
-                .setParameterList("shifts", shiftIds);
-
-        if (date != null) {
-            searchQueryBuilder.setParameter("date", DateUtils.toDateString(date));
-        }
-
-        if (StringUtils.isNotEmpty(type)) {
-            searchQueryBuilder.setString("type", type);
-        }
-
-        return searchQueryBuilder.list().getEntities();
+        return shiftTimetableExceptions;
     }
 
-    private DataDefinition getShiftTimetableExceptionDD(){
+    private DataDefinition getShiftTimetableExceptionDD() {
         return dataDefinitionService.get(BasicConstants.PLUGIN_IDENTIFIER, BasicConstants.MODEL_SHIFT_TIMETABLE_EXCEPTION);
     }
 
