0c9338026861e236a3443e2638865885b6b57336
ksiwonia <kamil.siwonia@gmail.com>
2018-07-10 06:40:19 +0200

    QCADOOCLS-6246

--- mes-plugins/mes-plugins-basic/src/main/java/com/qcadoo/mes/basic/ShiftsServiceImpl.java
+++ mes-plugins/mes-plugins-basic/src/main/java/com/qcadoo/mes/basic/ShiftsServiceImpl.java
@@ -23,27 +23,6 @@
  */
 package com.qcadoo.mes.basic;
 
-import java.io.Serializable;
-import java.util.Calendar;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeConstants;
-import org.joda.time.IllegalFieldValueException;
-import org.joda.time.Interval;
-import org.joda.time.LocalTime;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
-
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.qcadoo.commons.dateTime.TimeRange;
@@ -56,6 +35,30 @@ import com.qcadoo.model.api.DataDefinition;
 import com.qcadoo.model.api.DataDefinitionService;
 import com.qcadoo.model.api.Entity;
 import com.qcadoo.model.api.search.SearchRestrictions;
+import com.qcadoo.view.api.ComponentState;
+import com.qcadoo.view.api.ViewDefinitionState;
+import com.qcadoo.view.api.components.FieldComponent;
+import com.qcadoo.view.api.components.FormComponent;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeConstants;
+import org.joda.time.IllegalFieldValueException;
+import org.joda.time.Interval;
+import org.joda.time.LocalTime;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ShiftsServiceImpl implements ShiftsService {
@@ -282,7 +285,7 @@ public class ShiftsServiceImpl implements ShiftsService {
         return shifts.stream().map(Shift::new).collect(Collectors.toList());
     }
 
-    boolean validateShiftTimetableException(final DataDefinition dataDefinition, final Entity entity) {
+    public boolean validateShiftTimetableException(final DataDefinition dataDefinition, final Entity entity) {
         Date dateFrom = (Date) entity.getField(FROM_DATE_FIELD);
         Date dateTo = (Date) entity.getField(TO_DATE_FIELD);
 
@@ -296,7 +299,7 @@ public class ShiftsServiceImpl implements ShiftsService {
         return true;
     }
 
-    boolean validateShiftHoursField(final DataDefinition dataDefinition, final Entity entity) {
+    public boolean validateShiftHoursField(final DataDefinition dataDefinition, final Entity entity) {
         boolean valid = true;
 
         for (String day : WEEK_DAYS) {
@@ -308,7 +311,7 @@ public class ShiftsServiceImpl implements ShiftsService {
         return valid;
     }
 
-    private boolean validateHourField(final String day, final DataDefinition dataDefinition, final Entity entity) {
+    public boolean validateHourField(final String day, final DataDefinition dataDefinition, final Entity entity) {
         boolean isDayActive = (Boolean) entity.getField(day + WORKING_LITERAL);
 
         String fieldValue = entity.getStringField(day + HOURS_LITERAL);
@@ -440,6 +443,35 @@ public class ShiftsServiceImpl implements ShiftsService {
         return removeHoursOutOfRange(mergeOverlappedHours(hours), dateFrom, dateTo);
     }
 
+    public void onDayCheckboxChange(final ViewDefinitionState viewDefinitionState, final ComponentState state,
+            final String[] args) {
+        updateDayFieldsState(viewDefinitionState);
+    }
+
+    public void setHourFieldsState(final ViewDefinitionState viewDefinitionState) {
+        updateDayFieldsState(viewDefinitionState);
+    }
+
+    public void updateDayFieldsState(final ViewDefinitionState viewDefinitionState) {
+        FormComponent form = (FormComponent) viewDefinitionState.getComponentByReference("form");
+        Entity shift = form.getEntity();
+
+        for (String day : WEEK_DAYS) {
+            updateDayFieldState(day, viewDefinitionState, shift);
+        }
+    }
+
+    public void updateDayFieldState(final String day, final ViewDefinitionState viewDefinitionState, final Entity shift) {
+        FieldComponent dayHours = (FieldComponent) viewDefinitionState.getComponentByReference(day + HOURS_LITERAL);
+
+        if (!shift.getBooleanField(day + WORKING_LITERAL)) {
+            dayHours.setEnabled(false);
+            dayHours.setRequired(false);
+        } else {
+            dayHours.setEnabled(true);
+            dayHours.setRequired(true);
+        }
+    }
     private List<ShiftHour> removeHoursOutOfRange(final List<ShiftHour> hours, final Date dateFrom, final Date dateTo) {
         List<ShiftHour> list = Lists.newArrayList();
 
