e899c502e33fd899c77638090555a810bdc6be58
Krzysztof Nadolski <krzysztof.nadolski@qcadoo.com>
2019-04-24 15:13:13 +0200

    QCADOOCLS-6505

---
+++ mes-plugins/mes-plugins-orders/src/main/java/com/qcadoo/mes/orders/constants/ScheduleFields.java
@@ -0,0 +1,11 @@
+package com.qcadoo.mes.orders.constants;
+
+public final class ScheduleFields {
+
+    private ScheduleFields() {
+    }
+
+    public static final String NUMBER = "number";
+
+    public static final String NAME = "name";
+}
