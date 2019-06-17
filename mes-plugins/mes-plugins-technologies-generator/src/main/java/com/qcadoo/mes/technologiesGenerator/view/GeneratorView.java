468d4dddec55cf672b85b5ce16d8d08235f5df0b
ksiwonia <kamil.siwonia@gmail.com>
2019-06-17 13:39:59 +0200

    QCADOOCLS-6617

--- mes-plugins/mes-plugins-technologies-generator/src/main/java/com/qcadoo/mes/technologiesGenerator/view/GeneratorView.java
+++ mes-plugins/mes-plugins-technologies-generator/src/main/java/com/qcadoo/mes/technologiesGenerator/view/GeneratorView.java
@@ -166,4 +166,11 @@ public class GeneratorView {
         }
         window.requestRibbonRender();
     }
+
+    public void setRefreshAndApplyCustomizedRibbonButtonEnabled(boolean enabled) {
+        RibbonActionItem generateRibbonItem = getRibbonItemByName("refreshAndApplyCustomized");
+        generateRibbonItem.setEnabled(enabled);
+        generateRibbonItem.requestUpdate(true);
+        window.requestRibbonRender();
+    }
 }
