15f7311ee6e1eebbf283734f78142ed86c32b83b
ksiwonia <kamil.siwonia@gmail.com>
2019-07-05 09:14:25 +0200

    sonar fix

--- mes-plugins/mes-plugins-production-counting/src/main/java/com/qcadoo/mes/productionCounting/hooks/UseReplacementHooks.java
+++ mes-plugins/mes-plugins-production-counting/src/main/java/com/qcadoo/mes/productionCounting/hooks/UseReplacementHooks.java
@@ -11,6 +11,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class UseReplacementHooks {
 
+    public static final String L_WINDOW_MAIN_TAB_FORM_BASIC_PRODUCT = "window.mainTab.form.basicProduct";
+
+    public static final String BASIC_PRODUCT = "basicProduct";
+
+    public static final String PRODUCT = "product";
+
     public void onBeforeRender(final ViewDefinitionState view) {
         fillFields(view);
     }
@@ -18,13 +24,13 @@ public class UseReplacementHooks {
     private void fillFields(ViewDefinitionState view) {
         if(view.isViewAfterRedirect()) {
             JSONObject context = view.getJsonContext();
-            LookupComponent basicProductComponent = (LookupComponent) view.getComponentByReference("basicProduct");
-            LookupComponent productComponent = (LookupComponent) view.getComponentByReference("product");
-            if(context.has("window.mainTab.form.basicProduct")) {
+            LookupComponent basicProductComponent = (LookupComponent) view.getComponentByReference(BASIC_PRODUCT);
+            LookupComponent productComponent = (LookupComponent) view.getComponentByReference(PRODUCT);
+            if(context.has(L_WINDOW_MAIN_TAB_FORM_BASIC_PRODUCT)) {
                 try {
-                    basicProductComponent.setFieldValue(context.getLong("window.mainTab.form.basicProduct"));
+                    basicProductComponent.setFieldValue(context.getLong(L_WINDOW_MAIN_TAB_FORM_BASIC_PRODUCT));
                     FilterValueHolder filterValueHolder = productComponent.getFilterValue();
-                    filterValueHolder.put("BASIC_PRODUCT_ID", context.getLong("window.mainTab.form.basicProduct"));
+                    filterValueHolder.put("BASIC_PRODUCT_ID", context.getLong(L_WINDOW_MAIN_TAB_FORM_BASIC_PRODUCT));
                     productComponent.setFilterValue(filterValueHolder);
                 } catch (JSONException ex) {
                     throw new RuntimeException(ex);
