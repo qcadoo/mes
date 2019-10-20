258cd76dfa7527e32aa16a3c5ad4d081a61a186e
Łukasz Poniewierski <lukasz.poniewierski@qcadoo.com>
2019-10-20 07:37:37 +0200

    QCADOOCLS-6858 Some fixes.

--- mes-plugins/mes-plugins-material-flow-resources/src/main/java/com/qcadoo/mes/materialFlowResources/constants/LocationFieldsMFR.java
+++ mes-plugins/mes-plugins-material-flow-resources/src/main/java/com/qcadoo/mes/materialFlowResources/constants/LocationFieldsMFR.java
@@ -3,19 +3,19 @@
  * Copyright (c) 2010 Qcadoo Limited
  * Project: Qcadoo MES
  * Version: 1.4
- *
+ * <p>
  * This file is part of Qcadoo.
- *
+ * <p>
  * Qcadoo is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
  * by the Free Software Foundation; either version 3 of the License,
  * or (at your option) any later version.
- *
+ * <p>
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Affero General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU Affero General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
@@ -25,27 +25,32 @@ package com.qcadoo.mes.materialFlowResources.constants;
 
 public final class LocationFieldsMFR {
 
-    private LocationFieldsMFR() {
+	private LocationFieldsMFR() {
 
-    }
+	}
 
-    public static final String COMPONENTS_LOCATION_DIVISIONS = "componentsLocationDivisions";
+	public static final String ALGORITHM = "algorithm";
 
-    public static final String COMPONENTS_OUTPUT_LOCATION_DIVISIONS = "componentsOutputLocationDivisions";
+	public static final String REQUIRE_PRICE = "requirePrice";
 
-    public static final String PRODUCTS_INPUT_LOCATION_DIVISIONS = "productsInputLocationDivisions";
+	public static final String REQUIRE_BATCH = "requireBatch";
 
-    public static final String ALGORITHM = "algorithm";
+	public static final String REQUIRE_PRODUCTION_DATE = "requireProductionDate";
 
-    public static final String REQUIRE_PRICE = "requirePrice";
+	public static final String REQUIRE_EXPIRATION_DATE = "requireExpirationDate";
 
-    public static final String REQUIRE_BATCH = "requireBatch";
+	public static final String DRAFT_MAKES_RESERVATION = "draftMakesReservation";
 
-    public static final String REQUIRE_PRODUCTION_DATE = "requireProductionDate";
+	public static final String RESOURCE_CORRECTIONS = "resourceCorrections";
 
-    public static final String REQUIRE_EXPIRATION_DATE = "requireExpirationDate";
+	public static final String STOCK_TAKINGS = "stocktakings";
 
-    public static final String RESOURCES = "resources";
+	public static final String RESOURCE_STOCKS = "resourceStocks";
 
-    public static final String DRAFT_MAKES_RESERVATION = "draftMakesReservation";
+	public static final String LINKED_DOCUMENTS = "linkedDocuments";
+
+	public static final String RESOURCES = "resources";
+
+	public static final String COST_NORMS_LOCATIONS = "costNormsLocations";
+
 }
