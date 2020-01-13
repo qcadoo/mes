ae3e790a371ff770eb69004ac840a4b49460c5f3
Krzysztof Nadolski <krzysztof.nadolski@qcadoo.com>
2020-01-13 22:26:27 +0100

    QCADOOCLS-7023

--- mes-plugins/mes-plugins-technologies/src/main/java/com/qcadoo/mes/technologies/imports/productToProductGroupTechnology/ProductToProductGroupTechnologyXlsxImportService.java
+++ mes-plugins/mes-plugins-technologies/src/main/java/com/qcadoo/mes/technologies/imports/productToProductGroupTechnology/ProductToProductGroupTechnologyXlsxImportService.java
@@ -14,13 +14,17 @@ public class ProductToProductGroupTechnologyXlsxImportService extends XlsxImport
 
     @Override
     public void validateEntity(final Entity entity, final DataDefinition dataDefinition) {
-        if (!ProductFamilyElementType.PRODUCTS_FAMILY.getStringValue().equals(
-                entity.getBelongsToField(ProductToProductGroupFields.PRODUCT_FAMILY).getStringField(ProductFields.ENTITY_TYPE))) {
+        if (entity.getBelongsToField(ProductToProductGroupFields.PRODUCT_FAMILY) != null
+                && !ProductFamilyElementType.PRODUCTS_FAMILY.getStringValue()
+                        .equals(entity.getBelongsToField(ProductToProductGroupFields.PRODUCT_FAMILY)
+                                .getStringField(ProductFields.ENTITY_TYPE))) {
             entity.addError(dataDefinition.getField(ProductToProductGroupFields.PRODUCT_FAMILY),
                     "technologies.productToProductGroupTechnology.validate.error.productIsntProductFamily");
         }
-        if (!entity.getBelongsToField(ProductToProductGroupFields.PRODUCT_FAMILY).equals(
-                entity.getBelongsToField(ProductToProductGroupFields.ORDER_PRODUCT).getBelongsToField(ProductFields.PARENT))) {
+        if (entity.getBelongsToField(ProductToProductGroupFields.PRODUCT_FAMILY) != null
+                && entity.getBelongsToField(ProductToProductGroupFields.ORDER_PRODUCT) != null
+                && !entity.getBelongsToField(ProductToProductGroupFields.PRODUCT_FAMILY).equals(entity
+                        .getBelongsToField(ProductToProductGroupFields.ORDER_PRODUCT).getBelongsToField(ProductFields.PARENT))) {
             entity.addError(dataDefinition.getField(ProductToProductGroupFields.ORDER_PRODUCT),
                     "technologies.productToProductGroupTechnology.validate.error.productBelongsToAnotherFamily");
         }
