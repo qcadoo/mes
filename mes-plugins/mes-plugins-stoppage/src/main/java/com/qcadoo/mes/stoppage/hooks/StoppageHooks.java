f7d601ad23a025df2a6cb398cd2d54dce7784d6c
Krzysztof Nadolski <krzysztof.nadolski@qcadoo.com>
2020-01-28 15:03:50 +0100

    QCADOOCLS-7096

--- mes-plugins/mes-plugins-stoppage/src/main/java/com/qcadoo/mes/stoppage/hooks/StoppageHooks.java
+++ mes-plugins/mes-plugins-stoppage/src/main/java/com/qcadoo/mes/stoppage/hooks/StoppageHooks.java
@@ -23,20 +23,4 @@ public class StoppageHooks {
 
         return false;
     }
-
-    public void onSave(final DataDefinition dataDefinition, final Entity entity) {
-        Date dateFrom = entity.getDateField(StoppageFields.DATE_FROM);
-        Date dateTo = entity.getDateField(StoppageFields.DATE_TO);
-        Entity entityFromDB = null;
-        if (entity.getId() != null) {
-            entityFromDB = dataDefinition.get(entity.getId());
-        }
-        if (dateFrom != null && dateTo != null
-                && (entity.getId() == null || entityFromDB != null && (entityFromDB.getDateField(StoppageFields.DATE_FROM) == null
-                        || entityFromDB.getDateField(StoppageFields.DATE_TO) == null
-                        || dateFrom.compareTo(entityFromDB.getDateField(StoppageFields.DATE_FROM)) != 0
-                        || dateTo.compareTo(entityFromDB.getDateField(StoppageFields.DATE_TO)) != 0))) {
-            entity.setField(StoppageFields.DURATION, (int) (dateTo.getTime() - dateFrom.getTime()) / 1000);
-        }
-    }
 }
--- mes-plugins/mes-plugins-stoppage/src/main/java/com/qcadoo/mes/stoppage/listeners/AllStoppagesFormListeners.java
+++ mes-plugins/mes-plugins-stoppage/src/main/java/com/qcadoo/mes/stoppage/listeners/AllStoppagesFormListeners.java
@@ -1,5 +1,10 @@
 package com.qcadoo.mes.stoppage.listeners;
 
+import com.qcadoo.localization.api.utils.DateUtils;
+import com.qcadoo.view.api.components.FieldComponent;
+import com.qcadoo.view.api.components.FormComponent;
+import org.joda.time.DateTime;
+import org.joda.time.Seconds;
 import org.springframework.stereotype.Service;
 
 import com.qcadoo.mes.stoppage.constants.StoppageFields;
@@ -8,6 +13,8 @@ import com.qcadoo.view.api.ComponentState;
 import com.qcadoo.view.api.ViewDefinitionState;
 import com.qcadoo.view.api.components.LookupComponent;
 import com.qcadoo.view.api.components.lookup.FilterValueHolder;
+
+import java.util.Date;
 
 @Service
 public class AllStoppagesFormListeners {
@@ -25,6 +32,26 @@ public class AllStoppagesFormListeners {
             holder.put(StoppageFields.ORDER, order.getId());
 
             productionTrackingLookup.setFilterValue(holder);
+        }
+    }
+
+    public void calculateDuration(final ViewDefinitionState view, final ComponentState state, final String[] args) {
+        FieldComponent dateFromFieldComponent = (FieldComponent) view.getComponentByReference(StoppageFields.DATE_FROM);
+        FieldComponent dateToFieldComponent = (FieldComponent) view.getComponentByReference(StoppageFields.DATE_TO);
+        FieldComponent durationFieldComponent = (FieldComponent) view.getComponentByReference(StoppageFields.DURATION);
+
+        Date dateFrom = DateUtils.parseDate(dateFromFieldComponent.getFieldValue());
+        Date dateTo = DateUtils.parseDate(dateToFieldComponent.getFieldValue());
+
+        if(dateFrom != null && dateTo != null) {
+            if (dateFrom.before(dateTo)) {
+                Seconds seconds = Seconds.secondsBetween(new DateTime(dateFrom), new DateTime(dateTo));
+                durationFieldComponent.setFieldValue(seconds.getSeconds());
+            }
+            durationFieldComponent.requestComponentUpdateState();
+        } else {
+            durationFieldComponent.setFieldValue(null);
+            durationFieldComponent.requestComponentUpdateState();
         }
     }
 
