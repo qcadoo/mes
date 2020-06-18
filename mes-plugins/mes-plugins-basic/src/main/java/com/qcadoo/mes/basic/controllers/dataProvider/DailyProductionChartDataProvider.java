9f715c0b35ddd4644b3c4ff473e76653116e5cae
Krzysztof Nadolski <krzysztof.nadolski@qcadoo.com>
2020-06-18 12:38:48 +0200

    QCADOOCLS-7490

--- mes-plugins/mes-plugins-basic/src/main/java/com/qcadoo/mes/basic/controllers/dataProvider/DailyProductionChartDataProvider.java
+++ mes-plugins/mes-plugins-basic/src/main/java/com/qcadoo/mes/basic/controllers/dataProvider/DailyProductionChartDataProvider.java
@@ -16,15 +16,21 @@ public class DailyProductionChartDataProvider {
 
     public List<Long> getData() {
         List<Long> data = Lists.newArrayList();
-        String query = "SELECT sum(o.plannedquantity) FROM orders_order o WHERE o.state not in ('05declined', '07abandoned') "
-                + "AND o.startdate <= current_date AND current_date <= o.finishdate ";
+        String pendingQuery = "SELECT sum(o.plannedquantity) FROM orders_order o "
+                + "WHERE o.state not in ('05declined', '07abandoned', '04completed') AND o.startdate <= current_date "
+                + "AND current_date <= o.finishdate AND coalesce(o.donequantity, 0) = 0";
+        String inProgressQuery = "SELECT sum(o.plannedquantity) FROM orders_order o "
+                + "WHERE o.state not in ('05declined', '07abandoned','04completed') AND o.startdate <= current_date "
+                + "AND current_date <= o.finishdate AND o.donequantity * 100 / o.plannedquantity > 0 "
+                + "AND o.donequantity * 100 / o.plannedquantity < 100";
+        String doneQuery = "SELECT sum(coalesce(o.donequantity, 0)) FROM orders_order o "
+                + "WHERE o.state not in ('05declined', '07abandoned') AND o.startdate <= current_date "
+                + "AND current_date <= o.finishdate AND (o.donequantity * 100 / o.plannedquantity >= 100 "
+                + "OR o.state = '04completed')";
 
-        data.add(jdbcTemplate.queryForObject(query + "AND coalesce(o.donequantity, 0) = 0", Collections.emptyMap(), Long.class));
-        data.add(jdbcTemplate.queryForObject(
-                query + "AND o.donequantity * 100 / o.plannedquantity > 0 AND o.donequantity * 100 / o.plannedquantity < 100",
-                Collections.emptyMap(), Long.class));
-        data.add(jdbcTemplate.queryForObject(query + "AND o.donequantity * 100 / o.plannedquantity >= 100",
-                Collections.emptyMap(), Long.class));
+        data.add(jdbcTemplate.queryForObject(pendingQuery, Collections.emptyMap(), Long.class));
+        data.add(jdbcTemplate.queryForObject(inProgressQuery, Collections.emptyMap(), Long.class));
+        data.add(jdbcTemplate.queryForObject(doneQuery, Collections.emptyMap(), Long.class));
         return data;
     }
 
