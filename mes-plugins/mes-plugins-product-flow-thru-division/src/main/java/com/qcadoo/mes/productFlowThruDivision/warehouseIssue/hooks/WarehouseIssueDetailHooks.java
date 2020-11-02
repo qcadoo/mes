576f9d01a318249e143cdc5b7769a2b4c788fac4
Łukasz Poniewierski <lukasz.poniewierski@qcadoo.com>
2020-11-02 06:28:09 +0100

    SC QCADOOCLS-7682 Added sequence for warehouse issue number.

--- mes-plugins/mes-plugins-product-flow-thru-division/src/main/java/com/qcadoo/mes/productFlowThruDivision/warehouseIssue/hooks/WarehouseIssueDetailHooks.java
+++ mes-plugins/mes-plugins-product-flow-thru-division/src/main/java/com/qcadoo/mes/productFlowThruDivision/warehouseIssue/hooks/WarehouseIssueDetailHooks.java
@@ -21,13 +21,20 @@
  */
 package com.qcadoo.mes.productFlowThruDivision.warehouseIssue.hooks;
 
+import java.math.BigDecimal;
+import java.util.Objects;
+import java.util.Set;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.google.common.collect.Sets;
 import com.qcadoo.mes.basic.ParameterService;
 import com.qcadoo.mes.basic.constants.UserFields;
 import com.qcadoo.mes.orders.constants.OrderFields;
 import com.qcadoo.mes.orders.constants.OrdersConstants;
 import com.qcadoo.mes.productFlowThruDivision.constants.ParameterFieldsPFTD;
-import com.qcadoo.mes.productFlowThruDivision.constants.ProductFlowThruDivisionConstants;
 import com.qcadoo.mes.productFlowThruDivision.constants.WarehouseIssueProductsSource;
 import com.qcadoo.mes.productFlowThruDivision.warehouseIssue.WarehouseIssueParameterService;
 import com.qcadoo.mes.productFlowThruDivision.warehouseIssue.constans.CollectionProducts;
@@ -40,7 +47,11 @@ import com.qcadoo.model.api.Entity;
 import com.qcadoo.security.api.UserService;
 import com.qcadoo.view.api.ComponentState;
 import com.qcadoo.view.api.ViewDefinitionState;
-import com.qcadoo.view.api.components.*;
+import com.qcadoo.view.api.components.FieldComponent;
+import com.qcadoo.view.api.components.FormComponent;
+import com.qcadoo.view.api.components.GridComponent;
+import com.qcadoo.view.api.components.LookupComponent;
+import com.qcadoo.view.api.components.WindowComponent;
 import com.qcadoo.view.api.components.lookup.FilterValueHolder;
 import com.qcadoo.view.api.ribbon.Ribbon;
 import com.qcadoo.view.api.ribbon.RibbonActionItem;
@@ -48,12 +59,6 @@ import com.qcadoo.view.api.ribbon.RibbonGroup;
 import com.qcadoo.view.api.utils.NumberGeneratorService;
 import com.qcadoo.view.constants.QcadooViewConstants;
 import com.qcadoo.view.constants.RowStyle;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.math.BigDecimal;
-import java.util.Set;
 
 @Service
 public class WarehouseIssueDetailHooks {
@@ -78,8 +83,6 @@ public class WarehouseIssueDetailHooks {
     public void onBeforeRender(final ViewDefinitionState view) {
         setViewState(view);
         setCriteriaModifierParameters(view);
-        generateIssueNumber(view);
-        // fillOrderListDtoLookup(view);
         fillWorkerWhoIssued(view);
         hideOrderFields(view);
         disableProductsToIssueGrid(view);
@@ -89,13 +92,17 @@ public class WarehouseIssueDetailHooks {
 
     private void processProductsToIssueMode(final ViewDefinitionState view) {
         FieldComponent component = (FieldComponent) view.getComponentByReference(WarehouseIssueFields.PRODUCTS_TO_ISSUE_MODE);
+
         if (!warehouseIssueParameterService.issueForOrder()) {
             component.setVisible(false);
             component.requestComponentUpdateState();
+
             return;
         }
+
         FormComponent form = (FormComponent) view.getComponentByReference(QcadooViewConstants.L_FORM);
-        if (view.isViewAfterRedirect() && form.getEntityId() == null) {
+
+        if (view.isViewAfterRedirect() && Objects.isNull(form.getEntityId())) {
             component.setFieldValue(warehouseIssueParameterService.getProductsToIssue().getStrValue());
         }
     }
@@ -112,12 +119,16 @@ public class WarehouseIssueDetailHooks {
             productsToIssue.setEnabled(false);
             productsToIssue.setMessage("productFlowThruDivision.issue.activeWhenIssueForOrder");
             productsToIssue.requestUpdate(true);
+
             window.requestRibbonRender();
         }
+
         String state = (String) view.getComponentByReference(WarehouseIssueFields.STATE).getFieldValue();
-        if (state.equals(WarehouseIssueStringValues.DISCARD ) || state.equals(WarehouseIssueStringValues.COMPLETED)) {
+
+        if (state.equals(WarehouseIssueStringValues.DISCARD) || state.equals(WarehouseIssueStringValues.COMPLETED)) {
             copyProductsToIssue.setEnabled(false);
             copyProductsToIssue.requestUpdate(true);
+
             window.requestRibbonRender();
         }
     }
@@ -128,11 +139,11 @@ public class WarehouseIssueDetailHooks {
         if (warehouseIssueParameterService.issueForOrder()) {
             grid.setEditable(false);
         } else {
-            FormComponent form = (FormComponent) view.getComponentByReference(QcadooViewConstants.L_FORM);
             String state = (String) view.getComponentByReference(WarehouseIssueFields.STATE).getFieldValue();
+
             if (StringUtils.isNotEmpty(state) && !state.equals(WarehouseIssueStringValues.DRAFT)) {
                 grid.setEditable(false);
-            } else if(StringUtils.isNotEmpty(state) && state.equals(WarehouseIssueStringValues.DRAFT)) {
+            } else if (StringUtils.isNotEmpty(state) && state.equals(WarehouseIssueStringValues.DRAFT)) {
                 grid.setEditable(true);
             }
         }
@@ -140,12 +151,14 @@ public class WarehouseIssueDetailHooks {
 
     public Set<String> fillRowStyles(final Entity issue) {
         final Set<String> rowStyles = Sets.newHashSet();
-        if (issue.getDecimalField(IssueFields.ISSUE_QUANTITY) != null) {
+
+        if (Objects.nonNull(issue.getDecimalField(IssueFields.ISSUE_QUANTITY))) {
             if (BigDecimalUtils.convertNullToZero(issue.getDecimalField(IssueFields.ISSUE_QUANTITY))
                     .compareTo(BigDecimal.ZERO) == 0) {
                 rowStyles.add(RowStyle.RED_BACKGROUND);
             }
         }
+
         return rowStyles;
     }
 
@@ -153,75 +166,73 @@ public class WarehouseIssueDetailHooks {
         FormComponent form = (FormComponent) view.getComponentByReference(QcadooViewConstants.L_FORM);
         FieldComponent collectionProductsField = (FieldComponent) view
                 .getComponentByReference(WarehouseIssueFields.COLLECTION_PRODUCTS);
-
-        LookupComponent operation = (LookupComponent) view
+        LookupComponent operationLookup = (LookupComponent) view
                 .getComponentByReference(WarehouseIssueFields.TECHNOLOGY_OPERATION_COMPONENT);
-        LookupComponent division = (LookupComponent) view.getComponentByReference(WarehouseIssueFields.DIVISION);
+        LookupComponent divisionLookup = (LookupComponent) view.getComponentByReference(WarehouseIssueFields.DIVISION);
 
         String state = (String) view.getComponentByReference(WarehouseIssueFields.STATE).getFieldValue();
+
         if (state.equals(WarehouseIssueStringValues.DRAFT)) {
             form.setFormEnabled(true);
         }
 
         if (collectionProductsField.getFieldValue().equals(CollectionProducts.ON_ORDER.getStringValue())) {
-            operation.setVisible(false);
-            operation.setRequired(false);
-            division.setVisible(false);
-            division.setRequired(false);
-            division.requestComponentUpdateState();
-            operation.requestComponentUpdateState();
+            operationLookup.setVisible(false);
+            operationLookup.setRequired(false);
+            divisionLookup.setVisible(false);
+            divisionLookup.setRequired(false);
         } else if (collectionProductsField.getFieldValue().equals(CollectionProducts.ON_DIVISION.getStringValue())) {
-            operation.setVisible(false);
-            operation.setRequired(false);
-            division.setVisible(true);
-            division.setRequired(true);
-            division.requestComponentUpdateState();
-            operation.requestComponentUpdateState();
+            operationLookup.setVisible(false);
+            operationLookup.setRequired(false);
+            divisionLookup.setVisible(true);
+            divisionLookup.setRequired(true);
         } else if (collectionProductsField.getFieldValue().equals(CollectionProducts.ON_OPERATION.getStringValue())) {
-            if (view.getComponentByReference(WarehouseIssueFields.ORDER).getFieldValue() != null) {
-                operation.setVisible(true);
-                operation.setRequired(true);
+            if (Objects.nonNull(view.getComponentByReference(WarehouseIssueFields.ORDER).getFieldValue())) {
+                operationLookup.setVisible(true);
+                operationLookup.setRequired(true);
             }
-
-            division.setVisible(false);
-            division.setRequired(false);
-            division.requestComponentUpdateState();
-            operation.requestComponentUpdateState();
+            divisionLookup.setVisible(false);
+            divisionLookup.setRequired(false);
         }
+
+        divisionLookup.requestComponentUpdateState();
+        operationLookup.requestComponentUpdateState();
 
         if (state.equals(WarehouseIssueStringValues.IN_PROGRESS) || state.equals(WarehouseIssueStringValues.DISCARD)
                 || state.equals(WarehouseIssueStringValues.COMPLETED)) {
-
             form.setFormEnabled(false);
+
             GridComponent grid = (GridComponent) view.getComponentByReference("issues");
+
             if (state.equals(WarehouseIssueStringValues.IN_PROGRESS)) {
                 grid.setEnabled(true);
             } else {
                 grid.setEnabled(false);
             }
         }
-
     }
 
     public void setCriteriaModifierParameters(final ViewDefinitionState view) {
         Long orderId = (Long) view.getComponentByReference(WarehouseIssueFields.ORDER).getFieldValue();
+
         Entity order = null;
-        if (orderId != null) {
+
+        if (Objects.nonNull(orderId)) {
             order = dataDefinitionService.get(OrdersConstants.PLUGIN_IDENTIFIER, OrdersConstants.MODEL_ORDER).get(orderId);
         }
+
         setCriteriaModifierParameters(view, order);
     }
 
     public void setCriteriaModifierParameters(final ViewDefinitionState view, final Entity order) {
         LookupComponent technologyOperationComponentLookup = (LookupComponent) view
                 .getComponentByReference(WarehouseIssueFields.TECHNOLOGY_OPERATION_COMPONENT);
-
         LookupComponent divisionLookup = (LookupComponent) view.getComponentByReference(WarehouseIssueFields.DIVISION);
 
-        if (order != null) {
+        if (Objects.nonNull(order)) {
             Entity technology = order.getBelongsToField(OrderFields.TECHNOLOGY);
 
-            if (technology != null) {
+            if (Objects.nonNull(technology)) {
                 FilterValueHolder filterValueHolder = technologyOperationComponentLookup.getFilterValue();
                 filterValueHolder.put(OrderFields.TECHNOLOGY, technology.getId());
 
@@ -232,31 +243,28 @@ public class WarehouseIssueDetailHooks {
 
                 divisionLookup.setFilterValue(divisionFilterValueHolder);
             }
-
         }
-    }
-
-    public void generateIssueNumber(final ViewDefinitionState view) {
-        numberGeneratorService.generateAndInsertNumber(view, ProductFlowThruDivisionConstants.PLUGIN_IDENTIFIER,
-                ProductFlowThruDivisionConstants.MODEL_WAREHOUSE_ISSUE, QcadooViewConstants.L_FORM, WarehouseIssueFields.NUMBER);
     }
 
     private void fillWorkerWhoIssued(final ViewDefinitionState view) {
         FieldComponent workerWhoIssued = (FieldComponent) view.getComponentByReference(WarehouseIssueFields.WORKER_WHO_ISSUED);
-        if (null == workerWhoIssued.getFieldValue()) {
+
+        if (Objects.isNull(workerWhoIssued.getFieldValue())) {
             Entity currentUserStaff = userService.getCurrentUserEntity().getBelongsToField(UserFields.STAFF);
-            if (currentUserStaff != null) {
+
+            if (Objects.nonNull(currentUserStaff)) {
                 workerWhoIssued.setFieldValue(currentUserStaff.getId());
             }
         }
     }
 
-    private void hideOrderFields(ViewDefinitionState view) {
-        String productsSource = parameterService.getParameter().getStringField(
-                ParameterFieldsPFTD.WAREHOUSE_ISSUE_PRODUCTS_SOURCE);
+    private void hideOrderFields(final ViewDefinitionState view) {
+        String productsSource = parameterService.getParameter()
+                .getStringField(ParameterFieldsPFTD.WAREHOUSE_ISSUE_PRODUCTS_SOURCE);
 
-        if (productsSource != null) {
+        if (Objects.nonNull(productsSource)) {
             WarehouseIssueProductsSource warehouseIssueProductsSource = WarehouseIssueProductsSource.parseString(productsSource);
+
             if (WarehouseIssueProductsSource.MANUAL.equals(warehouseIssueProductsSource)) {
                 hideComponents(view, WarehouseIssueFields.ORDER, WarehouseIssueFields.ORDER_START_DATE,
                         WarehouseIssueFields.ORDER_PRODUCTION_LINE_NUMBER, WarehouseIssueFields.COLLECTION_PRODUCTS);
@@ -264,10 +272,11 @@ public class WarehouseIssueDetailHooks {
         }
     }
 
-    private void hideComponents(ViewDefinitionState view, String... names) {
+    private void hideComponents(final ViewDefinitionState view, final String... names) {
         for (String name : names) {
             ComponentState component = view.getComponentByReference(name);
             component.setVisible(false);
         }
     }
+
 }
--- mes-plugins/mes-plugins-product-flow-thru-division/src/main/java/com/qcadoo/mes/productFlowThruDivision/warehouseIssue/hooks/WarehouseIssueHooks.java
+++ mes-plugins/mes-plugins-product-flow-thru-division/src/main/java/com/qcadoo/mes/productFlowThruDivision/warehouseIssue/hooks/WarehouseIssueHooks.java
@@ -23,6 +23,14 @@
  */
 package com.qcadoo.mes.productFlowThruDivision.warehouseIssue.hooks;
 
+import java.util.Date;
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import com.google.common.collect.Maps;
 import com.qcadoo.mes.orders.constants.OrderFields;
 import com.qcadoo.mes.orders.constants.OrdersConstants;
 import com.qcadoo.mes.productFlowThruDivision.constants.ProductFlowThruDivisionConstants;
@@ -35,16 +43,15 @@ import com.qcadoo.mes.states.service.StateChangeEntityBuilder;
 import com.qcadoo.model.api.DataDefinition;
 import com.qcadoo.model.api.DataDefinitionService;
 import com.qcadoo.model.api.Entity;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.Date;
 
 @Service
 public class WarehouseIssueHooks {
 
     @Autowired
     private DataDefinitionService dataDefinitionService;
+
+    @Autowired
+    private NamedParameterJdbcTemplate jdbcTemplate;
 
     @Autowired
     private StateChangeEntityBuilder stateChangeEntityBuilder;
@@ -59,38 +66,51 @@ public class WarehouseIssueHooks {
         setInitialState(warehouseIssue);
     }
 
-    public void onCopy(final DataDefinition warehouseIssueDD, final Entity warehouseIssue) {
-        setInitialState(warehouseIssue);
-    }
-
     void setInitialState(final Entity warehouseIssue) {
         stateChangeEntityBuilder.buildInitial(describer, warehouseIssue, WarehouseIssueState.DRAFT);
     }
 
     public void onSave(final DataDefinition warehouseIssueDD, final Entity warehouseIssue) {
-        if (warehouseIssue.getId() == null) {
+        if (Objects.isNull(warehouseIssue.getId())) {
             warehouseIssue.setField(WarehouseIssueFields.DATE_OF_CREATION, new Date());
+        }
+        if (Objects.isNull(warehouseIssue.getField(WarehouseIssueFields.NUMBER))) {
+            warehouseIssue.setField(WarehouseIssueFields.NUMBER, setNumberFromSequence());
         }
         if (warehouseIssueParameterService.issueForOrder()) {
             fillOrderFields(warehouseIssue);
         }
     }
 
+    private String setNumberFromSequence() {
+        return jdbcTemplate.queryForObject("SELECT generate_warehouseissue_number()", Maps.newHashMap(), String.class);
+    }
+
     private void fillOrderFields(Entity warehouseIssue) {
-        Entity order = dataDefinitionService.get(OrdersConstants.PLUGIN_IDENTIFIER, OrdersConstants.MODEL_ORDER).get(
-                warehouseIssue.getBelongsToField(WarehouseIssueFields.ORDER).getId());
-        if (warehouseIssue.getDateField(WarehouseIssueFields.ORDER_START_DATE) == null) {
+        Entity order = getOrderDD().get(warehouseIssue.getBelongsToField(WarehouseIssueFields.ORDER).getId());
+
+        if (Objects.isNull(warehouseIssue.getDateField(WarehouseIssueFields.ORDER_START_DATE))) {
             warehouseIssue.setField(WarehouseIssueFields.ORDER_START_DATE, order.getDateField(OrderFields.START_DATE));
         }
-        if (warehouseIssue.getStringField(WarehouseIssueFields.ORDER_PRODUCTION_LINE_NUMBER) == null) {
+        if (Objects.isNull(warehouseIssue.getStringField(WarehouseIssueFields.ORDER_PRODUCTION_LINE_NUMBER))) {
             Entity orderProductionLine = order.getBelongsToField(OrderFields.PRODUCTION_LINE);
+
             warehouseIssue.setField(WarehouseIssueFields.ORDER_PRODUCTION_LINE_NUMBER,
                     orderProductionLine.getStringField(ProductionLineFields.NUMBER));
         }
     }
 
-    public DataDefinition getwarehouseIssueDD() {
+    public void onCopy(final DataDefinition warehouseIssueDD, final Entity warehouseIssue) {
+        setInitialState(warehouseIssue);
+    }
+
+    private DataDefinition getOrderDD() {
+        return dataDefinitionService.get(OrdersConstants.PLUGIN_IDENTIFIER, OrdersConstants.MODEL_ORDER);
+    }
+
+    public DataDefinition getWarehouseIssueDD() {
         return dataDefinitionService.get(ProductFlowThruDivisionConstants.PLUGIN_IDENTIFIER,
                 ProductFlowThruDivisionConstants.MODEL_WAREHOUSE_ISSUE);
     }
+
 }
