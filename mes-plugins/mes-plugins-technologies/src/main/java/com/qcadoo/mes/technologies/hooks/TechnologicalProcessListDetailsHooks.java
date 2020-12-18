00489c6d27af46d1e53862dea317c72f0ff7944b
Krzysztof Nadolski <krzysztof.nadolski@qcadoo.com>
2020-12-18 16:04:01 +0100

    QCADOOCLS-7727

--- mes-plugins/mes-plugins-technologies/src/main/java/com/qcadoo/mes/technologies/hooks/TechnologicalProcessListDetailsHooks.java
+++ mes-plugins/mes-plugins-technologies/src/main/java/com/qcadoo/mes/technologies/hooks/TechnologicalProcessListDetailsHooks.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import com.qcadoo.mes.technologies.criteriaModifiers.TechnologicalProcessListDetailsCriteriaModifiers;
 import com.qcadoo.view.api.ViewDefinitionState;
 import com.qcadoo.view.api.components.FormComponent;
+import com.qcadoo.view.api.components.GridComponent;
 import com.qcadoo.view.api.components.LookupComponent;
 import com.qcadoo.view.api.components.lookup.FilterValueHolder;
 import com.qcadoo.view.constants.QcadooViewConstants;
@@ -14,14 +15,13 @@ public class TechnologicalProcessListDetailsHooks {
 
     private static final String L_TECHNOLOGICAL_PROCESS_LOOKUP = "technologicalProcessLookup";
 
+    private static final String L_TECHNOLOGIES = "technologies";
+
     public void onBeforeRender(final ViewDefinitionState view) {
+        Long technologicalProcessListId = ((FormComponent) view.getComponentByReference(QcadooViewConstants.L_FORM))
+                .getEntityId();
         LookupComponent technologicalProcessLookup = (LookupComponent) view
                 .getComponentByReference(L_TECHNOLOGICAL_PROCESS_LOOKUP);
-        setCriteriaModifierParameters(technologicalProcessLookup,
-                ((FormComponent) view.getComponentByReference(QcadooViewConstants.L_FORM)).getEntityId());
-    }
-
-    private void setCriteriaModifierParameters(LookupComponent technologicalProcessLookup, Long technologicalProcessListId) {
         FilterValueHolder filterValueHolder = technologicalProcessLookup.getFilterValue();
 
         filterValueHolder.put(TechnologicalProcessListDetailsCriteriaModifiers.L_TECHNOLOGICAL_PROCESS_LIST_ID,
@@ -29,5 +29,13 @@ public class TechnologicalProcessListDetailsHooks {
 
         technologicalProcessLookup.setFilterValue(filterValueHolder);
         technologicalProcessLookup.requestComponentUpdateState();
+
+        GridComponent technologies = (GridComponent) view.getComponentByReference(L_TECHNOLOGIES);
+        FilterValueHolder gridFilterValueHolder = technologies.getFilterValue();
+
+        gridFilterValueHolder.put(TechnologicalProcessListDetailsCriteriaModifiers.L_TECHNOLOGICAL_PROCESS_LIST_ID,
+                technologicalProcessListId);
+
+        technologies.setFilterValue(gridFilterValueHolder);
     }
 }
