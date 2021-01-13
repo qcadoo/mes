e76a237786cf9c5cc4579b55c6f59d24c94cd2db
Krzysztof Nadolski <krzysztof.nadolski@qcadoo.com>
2021-01-13 17:12:31 +0100

    QCADOOCLS-7862

---
+++ mes-plugins/mes-plugins-master-orders/src/main/java/com/qcadoo/mes/masterOrders/hooks/SalesPlanProductHooks.java
@@ -0,0 +1,19 @@
+package com.qcadoo.mes.masterOrders.hooks;
+
+import com.qcadoo.mes.masterOrders.constants.SalesPlanProductFields;
+import com.qcadoo.model.api.DataDefinition;
+import com.qcadoo.model.api.Entity;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class SalesPlanProductHooks {
+
+    public void onCopy(final DataDefinition salesPlanProductDD, final Entity salesPlanProduct) {
+        salesPlanProduct.setField(SalesPlanProductFields.ORDERED_QUANTITY, BigDecimal.ZERO);
+        salesPlanProduct.setField(SalesPlanProductFields.SURPLUS_FROM_PLAN,
+                salesPlanProduct.getDecimalField(SalesPlanProductFields.PLANNED_QUANTITY));
+    }
+
+}
