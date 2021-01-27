0a742967f8610e16a3562d32a1b1099b9daeef5d
ksiwonia <kamil.siwonia@gmail.com>
2021-01-27 13:05:22 +0100

    QCADOOCLS-7783

--- mes-plugins/mes-plugins-product-flow-thru-division/src/main/java/com/qcadoo/mes/productFlowThruDivision/OrderMaterialAvailability.java
+++ mes-plugins/mes-plugins-product-flow-thru-division/src/main/java/com/qcadoo/mes/productFlowThruDivision/OrderMaterialAvailability.java
@@ -10,19 +10,11 @@ import com.qcadoo.mes.basicProductionCounting.constants.ProductionCountingQuanti
 import com.qcadoo.mes.basicProductionCounting.constants.ProductionCountingQuantityTypeOfMaterial;
 import com.qcadoo.mes.materialFlowResources.MaterialFlowResourcesService;
 import com.qcadoo.mes.orders.constants.OrderFields;
-import com.qcadoo.mes.orders.states.constants.OrderState;
 import com.qcadoo.mes.productFlowThruDivision.constants.AvailabilityOfMaterialAvailability;
 import com.qcadoo.mes.productFlowThruDivision.constants.MaterialAvailabilityFields;
-import com.qcadoo.mes.productFlowThruDivision.constants.OperationProductInComponentFieldsPFTD;
 import com.qcadoo.mes.productFlowThruDivision.constants.OrderFieldsPFTD;
 import com.qcadoo.mes.productFlowThruDivision.constants.ProductFlowThruDivisionConstants;
 import com.qcadoo.mes.productFlowThruDivision.constants.ProductionCountingQuantityFieldsPFTD;
-import com.qcadoo.mes.technologies.ProductQuantitiesService;
-import com.qcadoo.mes.technologies.constants.OperationProductInComponentFields;
-import com.qcadoo.mes.technologies.constants.TechnologiesConstants;
-import com.qcadoo.mes.technologies.dto.OperationProductComponentEntityType;
-import com.qcadoo.mes.technologies.dto.OperationProductComponentHolder;
-import com.qcadoo.mes.technologies.dto.OperationProductComponentWithQuantityContainer;
 import com.qcadoo.model.api.DataDefinition;
 import com.qcadoo.model.api.DataDefinitionService;
 import com.qcadoo.model.api.Entity;
@@ -30,7 +22,6 @@ import com.qcadoo.model.api.EntityList;
 import com.qcadoo.model.api.NumberService;
 
 import java.math.BigDecimal;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -47,9 +38,6 @@ public class OrderMaterialAvailability {
 
     @Autowired
     private DataDefinitionService dataDefinitionService;
-
-    @Autowired
-    private ProductQuantitiesService productQuantitiesService;
 
     @Autowired
     private NumberService numberService;
@@ -91,8 +79,8 @@ public class OrderMaterialAvailability {
     }
 
     private void saveMaterialAvailability(List<Entity> materialsAvailability) {
-        DataDefinition orderMaterialAvailabilityDD = dataDefinitionService.get(ProductFlowThruDivisionConstants.PLUGIN_IDENTIFIER,
-                ProductFlowThruDivisionConstants.MODEL_MATERIAL_AVAILABILITY);
+        DataDefinition orderMaterialAvailabilityDD = dataDefinitionService.get(
+                ProductFlowThruDivisionConstants.PLUGIN_IDENTIFIER, ProductFlowThruDivisionConstants.MODEL_MATERIAL_AVAILABILITY);
 
         for (Entity materialAvailability : materialsAvailability) {
             orderMaterialAvailabilityDD.save(materialAvailability);
@@ -112,8 +100,8 @@ public class OrderMaterialAvailability {
                 if (availableComponentsInLocation.containsKey(product.getId())) {
                     BigDecimal availableQuantity = availableComponentsInLocation.get(product.getId());
 
-                    if (availableQuantity
-                            .compareTo(materialAvailability.getDecimalField(MaterialAvailabilityFields.REQUIRED_QUANTITY)) >= 0) {
+                    if (availableQuantity.compareTo(materialAvailability
+                            .getDecimalField(MaterialAvailabilityFields.REQUIRED_QUANTITY)) >= 0) {
                         materialAvailability.setField(MaterialAvailabilityFields.AVAILABILITY,
                                 AvailabilityOfMaterialAvailability.FULL.getStrValue());
                     } else if (availableQuantity.compareTo(BigDecimal.ZERO) == 0) {
@@ -145,8 +133,8 @@ public class OrderMaterialAvailability {
             Entity location = materialAvailability.getBelongsToField(MaterialAvailabilityFields.LOCATION);
 
             if (groupedMaterialAvailabilities.containsKey(location)) {
-                groupedMaterialAvailabilities.get(location)
-                        .add(materialAvailability.getBelongsToField(MaterialAvailabilityFields.PRODUCT));
+                groupedMaterialAvailabilities.get(location).add(
+                        materialAvailability.getBelongsToField(MaterialAvailabilityFields.PRODUCT));
             } else {
                 groupedMaterialAvailabilities.put(location,
                         Sets.newHashSet(materialAvailability.getBelongsToField(MaterialAvailabilityFields.PRODUCT)));
@@ -157,8 +145,8 @@ public class OrderMaterialAvailability {
 
         for (Map.Entry<Entity, Set<Entity>> entry : groupedMaterialAvailabilities.entrySet()) {
             if (entry.getKey() != null) {
-                Map<Long, BigDecimal> availableQuantities = materialFlowResourcesService
-                        .getQuantitiesForProductsAndLocation(Lists.newArrayList(entry.getValue()), entry.getKey());
+                Map<Long, BigDecimal> availableQuantities = materialFlowResourcesService.getQuantitiesForProductsAndLocation(
+                        Lists.newArrayList(entry.getValue()), entry.getKey());
 
                 availability.put(entry.getKey().getId(), availableQuantities);
             }
@@ -183,93 +171,34 @@ public class OrderMaterialAvailability {
     private List<Entity> createMaterialAvailability(final Entity order, final Entity technology) {
         List<Entity> materialsAvailability;
 
-        if (OrderState.PENDING.getStringValue().equals(order.getStringField(OrderFields.STATE))) {
-            materialsAvailability = createMaterialAvailabilityFromTechnology(order);
+        materialsAvailability = createMaterialAvailabilityFromProductionCountingQuantities(order);
 
-            order.setField(OrderFieldsPFTD.MATERIAL_AVAILABILITY, materialsAvailability);
-        } else {
-            materialsAvailability = createMaterialAvailabilityFromProductionCountingQuantities(order);
-
-            order.setField(OrderFieldsPFTD.MATERIAL_AVAILABILITY, materialsAvailability);
-        }
+        order.setField(OrderFieldsPFTD.MATERIAL_AVAILABILITY, materialsAvailability);
 
         return materialsAvailability;
     }
 
-    private List<Entity> createMaterialAvailabilityFromTechnology(final Entity order) {
-        List<Entity> newOrderMaterialAvailability = Lists.newArrayList();
-
-        DataDefinition orderMaterialAvailabilityDD = dataDefinitionService.get(ProductFlowThruDivisionConstants.PLUGIN_IDENTIFIER,
-                ProductFlowThruDivisionConstants.MODEL_MATERIAL_AVAILABILITY);
-
-        Set<OperationProductComponentHolder> nonComponents = Sets.newHashSet();
-        Map<Long, BigDecimal> operationRuns = Maps.newHashMap();
-
-        final OperationProductComponentWithQuantityContainer productComponentQuantities = productQuantitiesService
-                .getProductComponentWithQuantities(Arrays.asList(order), operationRuns, nonComponents);
-
-        Map<Entity, Map<Entity, BigDecimal>> groupedMaterials = Maps.newHashMap();
-
-        for (Map.Entry<OperationProductComponentHolder, BigDecimal> productComponentQuantity : productComponentQuantities.asMap()
-                .entrySet()) {
-            OperationProductComponentHolder componentHolder = productComponentQuantity.getKey();
-
-            if (!nonComponents.contains(componentHolder)
-                    && OperationProductComponentEntityType.OPERATION_PRODUCT_IN_COMPONENT.equals(componentHolder.getEntityType())
-                    && (componentHolder.getOperationProductComponentId() != null)) {
-                Entity opic = dataDefinitionService
-                        .get(TechnologiesConstants.PLUGIN_IDENTIFIER, TechnologiesConstants.MODEL_OPERATION_PRODUCT_IN_COMPONENT)
-                        .get(componentHolder.getOperationProductComponentId());
-
-                Entity location = opic.getBelongsToField(OperationProductInComponentFieldsPFTD.COMPONENTS_LOCATION);
-                Entity product = opic.getBelongsToField(OperationProductInComponentFields.PRODUCT);
-
-                if (groupedMaterials.containsKey(product)) {
-                    Map<Entity, BigDecimal> productEntries = groupedMaterials.get(product);
-
-                    if (productEntries.containsKey(location)) {
-                        productEntries.put(location, productEntries.get(location).add(productComponentQuantity.getValue()));
-                    } else {
-                        productEntries.put(location, productComponentQuantity.getValue());
-                    }
-                } else {
-                    Map<Entity, BigDecimal> productEntries = Maps.newHashMap();
-
-                    productEntries.put(location, productComponentQuantity.getValue());
-                    groupedMaterials.put(product, productEntries);
-                }
-            }
-        }
-
-        for (Map.Entry<Entity, Map<Entity, BigDecimal>> productEntry : groupedMaterials.entrySet()) {
-            Map<Entity, BigDecimal> locationEntries = productEntry.getValue();
-
-            for (Map.Entry<Entity, BigDecimal> locationEntry : locationEntries.entrySet()) {
-                newOrderMaterialAvailability.add(createMaterialAvailabilityEntity(orderMaterialAvailabilityDD,
-                        productEntry.getKey(), order, locationEntry.getValue(), locationEntry.getKey()));
-            }
-        }
-
-        return newOrderMaterialAvailability;
-    }
 
     private List<Entity> createMaterialAvailabilityFromProductionCountingQuantities(final Entity order) {
         List<Entity> newOrderMaterialAvailability = Lists.newArrayList();
 
         List<Entity> usedMaterials = basicProductionCountingService.getUsedMaterialsFromProductionCountingQuantities(order);
 
-        DataDefinition orderMaterialAvailabilityDD = dataDefinitionService.get(ProductFlowThruDivisionConstants.PLUGIN_IDENTIFIER,
-                ProductFlowThruDivisionConstants.MODEL_MATERIAL_AVAILABILITY);
+        DataDefinition orderMaterialAvailabilityDD = dataDefinitionService.get(
+                ProductFlowThruDivisionConstants.PLUGIN_IDENTIFIER, ProductFlowThruDivisionConstants.MODEL_MATERIAL_AVAILABILITY);
 
-        usedMaterials = usedMaterials.stream()
-                .filter(material -> material.getStringField(ProductionCountingQuantityFields.ROLE)
-                        .equals(ProductionCountingQuantityRole.USED.getStringValue())
-                        && material.getStringField(ProductionCountingQuantityFields.TYPE_OF_MATERIAL)
-                                .equals(ProductionCountingQuantityTypeOfMaterial.COMPONENT.getStringValue()))
+        usedMaterials = usedMaterials
+                .stream()
+                .filter(material -> material.getStringField(ProductionCountingQuantityFields.ROLE).equals(
+                        ProductionCountingQuantityRole.USED.getStringValue())
+                        && material.getStringField(ProductionCountingQuantityFields.TYPE_OF_MATERIAL).equals(
+                                ProductionCountingQuantityTypeOfMaterial.COMPONENT.getStringValue()))
                 .collect(Collectors.toList());
-        Map<Long, Map<Long, List<Entity>>> groupedMaterials = usedMaterials.stream().collect(Collectors.groupingBy(
-                u -> ((Entity) u).getBelongsToField(ProductionCountingQuantityFields.PRODUCT).getId(), Collectors.groupingBy(
-                        u -> ((Entity) u).getBelongsToField(ProductionCountingQuantityFieldsPFTD.COMPONENTS_LOCATION).getId())));
+        Map<Long, Map<Long, List<Entity>>> groupedMaterials = usedMaterials.stream().collect(
+                Collectors.groupingBy(
+                        u -> ((Entity) u).getBelongsToField(ProductionCountingQuantityFields.PRODUCT).getId(),
+                        Collectors.groupingBy(u -> ((Entity) u).getBelongsToField(
+                                ProductionCountingQuantityFieldsPFTD.COMPONENTS_LOCATION).getId())));
 
         for (Map.Entry<Long, Map<Long, List<Entity>>> productEntry : groupedMaterials.entrySet()) {
             Map<Long, List<Entity>> materialsForProduct = productEntry.getValue();
@@ -284,8 +213,8 @@ public class OrderMaterialAvailability {
                     Entity location = baseUsedMaterial
                             .getBelongsToField(ProductionCountingQuantityFieldsPFTD.COMPONENTS_LOCATION);
 
-                    newOrderMaterialAvailability.add(createMaterialAvailabilityEntity(orderMaterialAvailabilityDD, product, order,
-                            totalQuantity, location));
+                    newOrderMaterialAvailability.add(createMaterialAvailabilityEntity(orderMaterialAvailabilityDD, product,
+                            order, totalQuantity, location));
                 }
             }
         }
@@ -297,8 +226,9 @@ public class OrderMaterialAvailability {
             final Entity order, final BigDecimal value, final Entity location) {
         Entity materialAvailability = orderMaterialAvailabilityDD.create();
 
-        List<Entity> replacements = product.getDataDefinition().get(product.getId()).getHasManyField(ProductFields.SUBSTITUTE_COMPONENTS);
-        if(!replacements.isEmpty()){
+        List<Entity> replacements = product.getDataDefinition().get(product.getId())
+                .getHasManyField(ProductFields.SUBSTITUTE_COMPONENTS);
+        if (!replacements.isEmpty()) {
             materialAvailability.setField(MaterialAvailabilityFields.REPLACEMENT, true);
         }
         materialAvailability.setField(MaterialAvailabilityFields.ORDER, order);
