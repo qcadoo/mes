cb2b09b791a69efe11a03dd9d1bd47e55dc097b3
Krzysztof Nadolski <krzysztof.nadolski@qcadoo.com>
2021-03-16 18:34:28 +0100

    QCADOOCLS-7939

--- mes-plugins/mes-plugins-orders/src/main/java/com/qcadoo/mes/orders/hooks/DictionaryItemHooksO.java
+++ mes-plugins/mes-plugins-orders/src/main/java/com/qcadoo/mes/orders/hooks/DictionaryItemHooksO.java
@@ -23,47 +23,35 @@
  */
 package com.qcadoo.mes.orders.hooks;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.qcadoo.mes.basic.util.ColorService;
 import com.qcadoo.model.api.DataDefinition;
 import com.qcadoo.model.api.Entity;
-import com.qcadoo.model.api.search.SearchRestrictions;
-import com.qcadoo.model.constants.DictionaryFields;
 import com.qcadoo.model.constants.DictionaryItemFields;
-import com.qcadoo.plugins.dictionaries.DictionariesService;
 
 @Service
 public class DictionaryItemHooksO {
 
-    private static final String L_COLOR = "color";
-
-    private static final String L_HEX_COLOR_PATTERN = "^#(?:[0-9a-fA-F]{3}){1,2}$";
-
     @Autowired
-    private DictionariesService dictionariesService;
+    private ColorService colorService;
 
     public boolean validatesWith(final DataDefinition dictionaryItemDD, final Entity dictionaryItem) {
-        boolean isValid = true;
-
-        isValid = isValid && checkIfDescriptionIsHexColor(dictionaryItemDD, dictionaryItem);
-
-        return isValid;
+        return checkIfDescriptionIsHexColor(dictionaryItemDD, dictionaryItem);
     }
 
     private boolean checkIfDescriptionIsHexColor(final DataDefinition dictionaryItemDD, final Entity dictionaryItem) {
         Entity dictionary = dictionaryItem.getBelongsToField(DictionaryItemFields.DICTIONARY);
 
-        if (checkIfDictionaryIsColorDictionary(dictionary)) {
+        if (colorService.checkIfDictionaryIsColorDictionary(dictionary)) {
             String description = dictionaryItem.getStringField(DictionaryItemFields.DESCRIPTION);
 
             if (StringUtils.isNotEmpty(description)) {
-                if (description.contains("#") && !checkIfIsHexColor(description)) {
-                    dictionaryItem.addError(dictionaryItemDD.getField(DictionaryItemFields.DESCRIPTION), "qcadooModel.dictionaryItem.description.error.hexColorIsIncorrect");
+                if (description.contains("#") && !colorService.checkIfIsHexColor(description)) {
+                    dictionaryItem.addError(dictionaryItemDD.getField(DictionaryItemFields.DESCRIPTION),
+                            "qcadooModel.dictionaryItem.description.error.hexColorIsIncorrect");
 
                     return false;
                 }
@@ -71,22 +59,6 @@ public class DictionaryItemHooksO {
         }
 
         return true;
-    }
-
-    private boolean checkIfDictionaryIsColorDictionary(final Entity dictionary) {
-        return ((dictionary != null) && dictionary.getId().equals(getColorDictionary().getId()));
-    }
-
-    private boolean checkIfIsHexColor(final String description) {
-         Pattern pattern = Pattern.compile(L_HEX_COLOR_PATTERN);
-         Matcher matcher = pattern.matcher(description);
-
-        return matcher.matches();
-    }
-
-    private Entity getColorDictionary() {
-        return dictionariesService.getDictionaryDD().find().add(SearchRestrictions.eq(DictionaryFields.NAME, L_COLOR))
-                .setMaxResults(1).uniqueResult();
     }
 
 }
