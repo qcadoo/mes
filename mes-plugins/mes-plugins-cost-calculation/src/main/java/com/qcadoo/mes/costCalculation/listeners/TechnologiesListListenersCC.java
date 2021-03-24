864226ba9f6db6bdc13c703703555c5d40cae756
Krzysztof Nadolski <krzysztof.nadolski@qcadoo.com>
2021-03-24 14:17:42 +0100

    QCADOOCLS-8031

--- mes-plugins/mes-plugins-cost-calculation/src/main/java/com/qcadoo/mes/costCalculation/listeners/TechnologiesListListenersCC.java
+++ mes-plugins/mes-plugins-cost-calculation/src/main/java/com/qcadoo/mes/costCalculation/listeners/TechnologiesListListenersCC.java
@@ -1,6 +1,15 @@
 package com.qcadoo.mes.costCalculation.listeners;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.google.common.collect.Maps;
+import com.qcadoo.mes.basic.ParameterService;
 import com.qcadoo.mes.costCalculation.constants.CostCalculationConstants;
 import com.qcadoo.mes.costCalculation.constants.CostCalculationFields;
 import com.qcadoo.mes.costCalculation.constants.MaterialCostsUsed;
@@ -14,13 +23,6 @@ import com.qcadoo.view.api.ViewDefinitionState;
 import com.qcadoo.view.api.components.GridComponent;
 import com.qcadoo.view.api.utils.NumberGeneratorService;
 import com.qcadoo.view.constants.QcadooViewConstants;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Service
 public class TechnologiesListListenersCC {
@@ -30,6 +32,9 @@ public class TechnologiesListListenersCC {
 
     @Autowired
     private NumberGeneratorService numberGeneratorService;
+
+    @Autowired
+    private ParameterService parameterService;
 
     public final void createCostCalculation(final ViewDefinitionState view, final ComponentState componentState,
             final String[] args) {
@@ -46,9 +51,37 @@ public class TechnologiesListListenersCC {
             Entity costCalculation = costCalculationDD.create();
             costCalculation.setField(CostCalculationFields.TECHNOLOGIES, technologies);
             costCalculation.setField(CostCalculationFields.QUANTITY, BigDecimal.ONE);
-            costCalculation.setField(CostCalculationFields.MATERIAL_COSTS_USED, MaterialCostsUsed.NOMINAL.getStringValue());
+            Entity parameter = parameterService.getParameter();
+            costCalculation.setField(CostCalculationFields.MATERIAL_COSTS_USED,
+                    parameter.getStringField(CostCalculationFields.MATERIAL_COSTS_USED) != null
+                            ? parameter.getStringField(CostCalculationFields.MATERIAL_COSTS_USED)
+                            : MaterialCostsUsed.NOMINAL.getStringValue());
+            costCalculation.setField(CostCalculationFields.USE_NOMINAL_COST_PRICE_NOT_SPECIFIED,
+                    parameter.getBooleanField(CostCalculationFields.USE_NOMINAL_COST_PRICE_NOT_SPECIFIED));
             costCalculation.setField(CostCalculationFields.SOURCE_OF_OPERATION_COSTS,
-                    SourceOfOperationCosts.TECHNOLOGY_OPERATION.getStringValue());
+                    parameter.getStringField(CostCalculationFields.SOURCE_OF_OPERATION_COSTS) != null
+                            ? parameter.getStringField(CostCalculationFields.SOURCE_OF_OPERATION_COSTS)
+                            : SourceOfOperationCosts.TECHNOLOGY_OPERATION.getStringValue());
+            costCalculation.setField(CostCalculationFields.STANDARD_LABOR_COST,
+                    parameter.getBelongsToField(CostCalculationFields.STANDARD_LABOR_COST));
+            costCalculation.setField(CostCalculationFields.AVERAGE_MACHINE_HOURLY_COST,
+                    parameter.getDecimalField(CostCalculationFields.AVERAGE_MACHINE_HOURLY_COST));
+            costCalculation.setField(CostCalculationFields.AVERAGE_LABOR_HOURLY_COST,
+                    parameter.getDecimalField(CostCalculationFields.AVERAGE_LABOR_HOURLY_COST));
+            costCalculation.setField(CostCalculationFields.INCLUDE_TPZ,
+                    parameter.getBooleanField(CostCalculationFields.INCLUDE_TPZ));
+            costCalculation.setField(CostCalculationFields.INCLUDE_ADDITIONAL_TIME,
+                    parameter.getBooleanField(CostCalculationFields.INCLUDE_ADDITIONAL_TIME));
+
+            costCalculation.setField(CostCalculationFields.MATERIAL_COST_MARGIN,
+                    parameter.getDecimalField(CostCalculationFields.MATERIAL_COST_MARGIN));
+            costCalculation.setField(CostCalculationFields.PRODUCTION_COST_MARGIN,
+                    parameter.getDecimalField(CostCalculationFields.PRODUCTION_COST_MARGIN));
+            costCalculation.setField(CostCalculationFields.ADDITIONAL_OVERHEAD,
+                    parameter.getDecimalField(CostCalculationFields.ADDITIONAL_OVERHEAD));
+            costCalculation.setField(CostCalculationFields.REGISTRATION_PRICE_OVERHEAD,
+                    parameter.getDecimalField(CostCalculationFields.REGISTRATION_PRICE_OVERHEAD));
+            costCalculation.setField(CostCalculationFields.PROFIT, parameter.getDecimalField(CostCalculationFields.PROFIT));
             costCalculation.setField(CostCalculationFields.NUMBER, numberGeneratorService
                     .generateNumber(CostCalculationConstants.PLUGIN_IDENTIFIER, CostCalculationConstants.MODEL_COST_CALCULATION));
             costCalculation = costCalculationDD.save(costCalculation);
