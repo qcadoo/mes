d04ea90b2f9acb1a773acbfcbce98d7c525c489e
Krzysztof Nadolski <krzysztof.nadolski@qcadoo.com>
2021-04-15 20:09:27 +0200

    QCADOOCLS-8073

--- mes-plugins/mes-plugins-orders/src/main/java/com/qcadoo/mes/orders/states/aop/listener/OrderPackOrderStateAspect.java
+++ mes-plugins/mes-plugins-orders/src/main/java/com/qcadoo/mes/orders/states/aop/listener/OrderPackOrderStateAspect.java
@@ -1,7 +1,7 @@
 package com.qcadoo.mes.orders.states.aop.listener;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
@@ -9,11 +9,9 @@ import org.springframework.beans.factory.annotation.Configurable;
 import com.qcadoo.mes.orders.OrderPackService;
 import com.qcadoo.mes.orders.constants.OrdersConstants;
 import com.qcadoo.mes.orders.states.aop.OrderStateChangeAspect;
-import com.qcadoo.mes.orders.states.constants.OrderStateChangePhase;
 import com.qcadoo.mes.orders.states.constants.OrderStateStringValues;
 import com.qcadoo.mes.states.StateChangeContext;
 import com.qcadoo.mes.states.annotation.RunForStateTransition;
-import com.qcadoo.mes.states.annotation.RunInPhase;
 import com.qcadoo.mes.states.aop.AbstractStateListenerAspect;
 import com.qcadoo.plugin.api.RunIfEnabled;
 
@@ -29,10 +27,9 @@ public class OrderPackOrderStateAspect extends AbstractStateListenerAspect {
     protected void targetServicePointcut() {
     }
 
-    @RunInPhase(OrderStateChangePhase.DEFAULT)
     @RunForStateTransition(targetState = OrderStateStringValues.IN_PROGRESS)
-    @Before(PHASE_EXECUTION_POINTCUT)
-    public void generateOrderPacks(final StateChangeContext stateChangeContext, final int phase) {
+    @After(CHANGE_STATE_EXECUTION_POINTCUT)
+    public void generateOrderPacks(final StateChangeContext stateChangeContext) {
         orderPackService.generateOrderPacks(stateChangeContext.getOwner());
     }
 }
