ccbfb32acdcce0fe06c00051aa20e2c5f9cd3b85
ksiwonia <kamil.siwonia@gmail.com>
2021-05-06 05:52:55 +0200

    QCADOOCLS-8126

--- mes-plugins/mes-plugins-deliveries/src/main/java/com/qcadoo/mes/deliveries/hooks/CompanyProductsDetailsHooks.java
+++ mes-plugins/mes-plugins-deliveries/src/main/java/com/qcadoo/mes/deliveries/hooks/CompanyProductsDetailsHooks.java
@@ -1,9 +1,6 @@
 package com.qcadoo.mes.deliveries.hooks;
 
-import java.util.Objects;
-
-import org.springframework.stereotype.Service;
-
+import com.qcadoo.localization.api.TranslationService;
 import com.qcadoo.mes.basic.constants.ProductFields;
 import com.qcadoo.mes.deliveries.constants.CompanyProductFields;
 import com.qcadoo.mes.technologies.constants.ProductComponentFields;
@@ -14,15 +11,27 @@ import com.qcadoo.view.api.components.FormComponent;
 import com.qcadoo.view.api.components.LookupComponent;
 import com.qcadoo.view.constants.QcadooViewConstants;
 
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 @Service
 public class CompanyProductsDetailsHooks {
 
     private static final String L_UNIT = "unit";
 
+    private static final String L_DAYS = "days";
+
+    @Autowired
+    private TranslationService translationService;
+
     public void onBeforeRenderForProduct(final ViewDefinitionState view) {
         FormComponent productCompanyForm = (FormComponent) view.getComponentByReference(QcadooViewConstants.L_FORM);
         FieldComponent unitField = (FieldComponent) view.getComponentByReference(L_UNIT);
-
+        FieldComponent daysField = (FieldComponent) view.getComponentByReference(L_DAYS);
+        daysField.setFieldValue(translationService.translate(
+                "deliveries.deliveryDetails.window.mainTab.deliveriesDetails.deliveryDateBufferDays.label", view.getLocale()));
         Entity productCompany = productCompanyForm.getEntity();
         Entity product = productCompany.getBelongsToField(ProductComponentFields.PRODUCT);
 
@@ -35,7 +44,9 @@ public class CompanyProductsDetailsHooks {
     public void onBeforeRenderForCompany(final ViewDefinitionState view) {
         LookupComponent productLookupComponent = (LookupComponent) view.getComponentByReference(CompanyProductFields.PRODUCT);
         FieldComponent unitField = (FieldComponent) view.getComponentByReference(L_UNIT);
-
+        FieldComponent daysField = (FieldComponent) view.getComponentByReference(L_DAYS);
+        daysField.setFieldValue(translationService.translate(
+                "deliveries.deliveryDetails.window.mainTab.deliveriesDetails.deliveryDateBufferDays.label", view.getLocale()));
         if (productLookupComponent.isEmpty()) {
             unitField.setFieldValue("");
         } else {
