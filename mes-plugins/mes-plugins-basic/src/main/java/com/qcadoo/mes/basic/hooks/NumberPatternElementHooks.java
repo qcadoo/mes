508692222b47ce8b6d16545a6aa8766634e691c8
ksiwonia <kamil.siwonia@gmail.com>
2021-07-13 06:10:32 +0200

    QCADOOCLS-8261

--- mes-plugins/mes-plugins-basic/src/main/java/com/qcadoo/mes/basic/hooks/NumberPatternElementHooks.java
+++ mes-plugins/mes-plugins-basic/src/main/java/com/qcadoo/mes/basic/hooks/NumberPatternElementHooks.java
@@ -7,11 +7,46 @@ import com.qcadoo.model.api.DataDefinition;
 import com.qcadoo.model.api.Entity;
 import com.qcadoo.model.api.search.SearchCriteriaBuilder;
 import com.qcadoo.model.api.search.SearchRestrictions;
+
+import java.util.List;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NumberPatternElementHooks {
+
+    public void onSave(final DataDefinition dataDefinition, final Entity numberPatternElement) {
+        String sequenceCycle = numberPatternElement.getStringField(NumberPatternElementFields.SEQUENCE_CYCLE);
+        if (StringUtils.isNoneEmpty(sequenceCycle)) {
+            Entity numberPattern = numberPatternElement.getBelongsToField(NumberPatternElementFields.NUMBER_PATTERN);
+            List<Entity> elements = numberPattern.getHasManyField(NumberPatternFields.NUMBER_PATTERN_ELEMENTS);
+            if (sequenceCycle.equals("01monthly")) {
+                boolean hasMMElement = false;
+                for (Entity element : elements) {
+                    if (element.getStringField(NumberPatternElementFields.ELEMENT).equals("02mm")) {
+                        hasMMElement = true;
+                    }
+                }
+                if (!hasMMElement) {
+                    numberPatternElement.addGlobalMessage("basic.numberPatternElement.sequenceCycle.monthlyInfo");
+                }
+            }
+
+            if (sequenceCycle.equals("02annual")) {
+                boolean hasRRElement = false;
+                for (Entity element : elements) {
+                    if (element.getStringField(NumberPatternElementFields.ELEMENT).equals("03rr")
+                            || element.getStringField(NumberPatternElementFields.ELEMENT).equals("04rrrr")) {
+                        hasRRElement = true;
+                    }
+                }
+                if (!hasRRElement) {
+                    numberPatternElement.addGlobalMessage("basic.numberPatternElement.sequenceCycle.annualInfo");
+                }
+            }
+        }
+    }
 
     public boolean validatesWith(final DataDefinition dataDefinition, final Entity entity) {
         if (NumberPatternElement.XX.getStringValue().equals(entity.getStringField(NumberPatternElementFields.ELEMENT))
@@ -23,14 +58,17 @@ public class NumberPatternElementHooks {
         }
         if (NumberPatternElement.N999.getStringValue().equals(entity.getStringField(NumberPatternElementFields.ELEMENT))
                 || NumberPatternElement.N9999.getStringValue().equals(entity.getStringField(NumberPatternElementFields.ELEMENT))
-                || NumberPatternElement.N99999.getStringValue()
-                        .equals(entity.getStringField(NumberPatternElementFields.ELEMENT))) {
+                || NumberPatternElement.N99999.getStringValue().equals(entity.getStringField(NumberPatternElementFields.ELEMENT))) {
             Entity numberPattern = entity.getBelongsToField(NumberPatternElementFields.NUMBER_PATTERN);
-            SearchCriteriaBuilder scb = numberPattern.getHasManyField(NumberPatternFields.NUMBER_PATTERN_ELEMENTS).find()
-                    .add(SearchRestrictions.or(SearchRestrictions.or(
-                            SearchRestrictions.eq(NumberPatternElementFields.ELEMENT, NumberPatternElement.N999.getStringValue()),
-                            SearchRestrictions.eq(NumberPatternElementFields.ELEMENT,
-                                    NumberPatternElement.N9999.getStringValue())),
+            SearchCriteriaBuilder scb = numberPattern
+                    .getHasManyField(NumberPatternFields.NUMBER_PATTERN_ELEMENTS)
+                    .find()
+                    .add(SearchRestrictions.or(
+                            SearchRestrictions.or(
+                                    SearchRestrictions.eq(NumberPatternElementFields.ELEMENT,
+                                            NumberPatternElement.N999.getStringValue()),
+                                    SearchRestrictions.eq(NumberPatternElementFields.ELEMENT,
+                                            NumberPatternElement.N9999.getStringValue())),
                             SearchRestrictions.eq(NumberPatternElementFields.ELEMENT,
                                     NumberPatternElement.N99999.getStringValue())));
             if (entity.getId() != null) {
@@ -43,7 +81,9 @@ public class NumberPatternElementHooks {
             }
         } else if (!NumberPatternElement.XX.getStringValue().equals(entity.getStringField(NumberPatternElementFields.ELEMENT))) {
             Entity numberPattern = entity.getBelongsToField(NumberPatternElementFields.NUMBER_PATTERN);
-            SearchCriteriaBuilder scb = numberPattern.getHasManyField(NumberPatternFields.NUMBER_PATTERN_ELEMENTS).find()
+            SearchCriteriaBuilder scb = numberPattern
+                    .getHasManyField(NumberPatternFields.NUMBER_PATTERN_ELEMENTS)
+                    .find()
                     .add(SearchRestrictions.eq(NumberPatternElementFields.ELEMENT,
                             entity.getStringField(NumberPatternElementFields.ELEMENT)));
             if (entity.getId() != null) {
