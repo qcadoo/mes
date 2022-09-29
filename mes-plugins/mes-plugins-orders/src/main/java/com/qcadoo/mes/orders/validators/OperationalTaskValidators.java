2a14e75c2838905d0e6588e453cd4905f2de80a8
Łukasz Poniewierski <lukasz.poniewierski@qcadoo.com>
2022-09-29 08:21:23 +0200

    QCADOOCLS-9284

--- mes-plugins/mes-plugins-orders/src/main/java/com/qcadoo/mes/orders/validators/OperationalTaskValidators.java
+++ mes-plugins/mes-plugins-orders/src/main/java/com/qcadoo/mes/orders/validators/OperationalTaskValidators.java
@@ -23,24 +23,12 @@
  */
 package com.qcadoo.mes.orders.validators;
 
-import static com.qcadoo.model.api.search.SearchOrders.asc;
-import static com.qcadoo.model.api.search.SearchProjections.alias;
-import static com.qcadoo.model.api.search.SearchProjections.rowCount;
-
-import java.util.Date;
-import java.util.List;
-import java.util.Objects;
-
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.google.common.collect.Lists;
+import com.qcadoo.mes.basic.constants.WorkstationFields;
 import com.qcadoo.mes.orders.OperationalTasksService;
 import com.qcadoo.mes.orders.constants.OperationalTaskFields;
 import com.qcadoo.mes.orders.constants.OperationalTaskType;
 import com.qcadoo.mes.orders.constants.OrderFields;
-import com.qcadoo.mes.orders.constants.OrdersConstants;
 import com.qcadoo.mes.orders.states.constants.OperationalTaskStateStringValues;
 import com.qcadoo.mes.technologies.constants.TechnologyOperationComponentFields;
 import com.qcadoo.mes.timeNormsForOperations.constants.TechnologyOperationComponentFieldsTNFO;
@@ -49,13 +37,25 @@ import com.qcadoo.model.api.DataDefinitionService;
 import com.qcadoo.model.api.Entity;
 import com.qcadoo.model.api.search.SearchCriteriaBuilder;
 import com.qcadoo.model.api.search.SearchRestrictions;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static com.qcadoo.model.api.search.SearchOrders.asc;
+import static com.qcadoo.model.api.search.SearchProjections.alias;
+import static com.qcadoo.model.api.search.SearchProjections.rowCount;
 
 @Service
 public class OperationalTaskValidators {
 
-    private static final String NAME_IS_BLANK_MESSAGE = "orders.operationalTask.error.nameIsBlank";
+    private static final String L_NAME_IS_BLANK_MESSAGE = "orders.operationalTask.error.nameIsBlank";
 
-    private static final String WRONG_DATES_ORDER_MESSAGE = "orders.operationalTask.error.finishDateIsEarlier";
+    private static final String L_WRONG_DATES_ORDER_MESSAGE = "orders.operationalTask.error.finishDateIsEarlier";
 
     private static final String L_COUNT = "count";
 
@@ -67,79 +67,22 @@ public class OperationalTaskValidators {
 
     public boolean onValidate(final DataDefinition operationalTaskDD, final Entity operationalTask) {
         boolean isValid = hasName(operationalTaskDD, operationalTask);
-        isValid = datesAreInCorrectOrder(operationalTaskDD, operationalTask) && isValid;
-        isValid = datesAreCorrect(operationalTaskDD, operationalTask) && isValid;
-        isValid = checkIfOrderHasTechnology(operationalTaskDD, operationalTask) && isValid;
-        isValid = checkIfFieldSet(operationalTaskDD, operationalTask) && isValid;
-        isValid = checkIfAlreadyExists(operationalTaskDD, operationalTask) && isValid;
-        isValid = checkWorkstationIsCorrect(operationalTaskDD, operationalTask) && isValid;
-        isValid = checkStaff(operationalTaskDD, operationalTask) && isValid;
+        isValid = isValid && datesAreInCorrectOrder(operationalTaskDD, operationalTask);
+        isValid = isValid && datesAreCorrect(operationalTaskDD, operationalTask);
+        isValid = isValid && checkIfOrderHasTechnology(operationalTaskDD, operationalTask);
+        isValid = isValid && checkIfFieldSet(operationalTaskDD, operationalTask);
+        isValid = isValid && checkIfAlreadyExists(operationalTaskDD, operationalTask);
+        isValid = isValid && checkWorkstationIsCorrect(operationalTaskDD, operationalTask);
+        isValid = isValid && checkStaff(operationalTaskDD, operationalTask);
 
         return isValid;
-    }
-
-    private boolean checkStaff(DataDefinition operationalTaskDD, Entity operationalTask) {
-        Entity technologyOperationComponent = operationalTask
-                .getBelongsToField(OperationalTaskFields.TECHNOLOGY_OPERATION_COMPONENT);
-        Integer actualStaff = operationalTask.getIntegerField(OperationalTaskFields.ACTUAL_STAFF);
-        int minStaff;
-        if (!Objects.isNull(technologyOperationComponent)) {
-            minStaff = technologyOperationComponent.getIntegerField(TechnologyOperationComponentFieldsTNFO.MIN_STAFF);
-        } else {
-            minStaff = 1;
-        }
-        if (actualStaff != null && minStaff > actualStaff) {
-            operationalTask.addError(operationalTaskDD.getField(OperationalTaskFields.ACTUAL_STAFF),
-                    "orders.operationalTask.error.minStaffBiggerThanActualStaff");
-            return false;
-        }
-        if (actualStaff != null && actualStaff % minStaff != 0) {
-            operationalTask.addError(operationalTaskDD.getField(OperationalTaskFields.ACTUAL_STAFF),
-                    "orders.operationalTask.error.actualStaffMustBeMultipleMinStaff", String.valueOf(minStaff));
-            return false;
-        }
-        return true;
-    }
-
-    private boolean checkIfAlreadyExists(DataDefinition operationalTaskDD, Entity operationalTask) {
-        String type = operationalTask.getStringField(OperationalTaskFields.TYPE);
-
-        if (OperationalTaskType.EXECUTION_OPERATION_IN_ORDER.getStringValue().equalsIgnoreCase(type)) {
-
-            Entity order = operationalTask.getBelongsToField(OperationalTaskFields.ORDER);
-            Entity toc = operationalTask.getBelongsToField(OperationalTaskFields.TECHNOLOGY_OPERATION_COMPONENT);
-
-            SearchCriteriaBuilder scb = dataDefinitionService
-                    .get(OrdersConstants.PLUGIN_IDENTIFIER, OrdersConstants.MODEL_OPERATIONAL_TASK).find()
-                    .add(SearchRestrictions.belongsTo(OperationalTaskFields.ORDER, order))
-                    .add(SearchRestrictions.belongsTo(OperationalTaskFields.TECHNOLOGY_OPERATION_COMPONENT, toc))
-                    .add(SearchRestrictions.in(OperationalTaskFields.STATE,
-                            Lists.newArrayList(OperationalTaskStateStringValues.PENDING, OperationalTaskStateStringValues.STARTED,
-                                    OperationalTaskStateStringValues.FINISHED)));
-            if (Objects.nonNull(operationalTask.getId())) {
-                scb.add(SearchRestrictions.idNe(operationalTask.getId()));
-            }
-
-            scb.setProjection(alias(rowCount(), L_COUNT));
-            scb.addOrder(asc(L_COUNT));
-
-            Entity countProjection = scb.setMaxResults(1).uniqueResult();
-
-            boolean isValid = ((countProjection == null) || ((Long) countProjection.getField(L_COUNT) == 0));
-            if (!isValid) {
-                operationalTask.addGlobalError("orders.operationalTask.error.notUnique");
-            }
-            return isValid;
-        }
-
-        return true;
     }
 
     private boolean hasName(final DataDefinition operationalTaskDD, final Entity operationalTask) {
         String type = operationalTask.getStringField(OperationalTaskFields.TYPE);
 
         if (OperationalTaskType.OTHER_CASE.getStringValue().equalsIgnoreCase(type) && hasBlankName(operationalTask)) {
-            operationalTask.addError(operationalTaskDD.getField(OperationalTaskFields.NAME), NAME_IS_BLANK_MESSAGE);
+            operationalTask.addError(operationalTaskDD.getField(OperationalTaskFields.NAME), L_NAME_IS_BLANK_MESSAGE);
 
             return false;
         }
@@ -156,8 +99,8 @@ public class OperationalTaskValidators {
         Date finishDate = operationalTask.getDateField(OperationalTaskFields.FINISH_DATE);
 
         if (finishDate.before(startDate)) {
-            operationalTask.addError(operationalTaskDD.getField(OperationalTaskFields.START_DATE), WRONG_DATES_ORDER_MESSAGE);
-            operationalTask.addError(operationalTaskDD.getField(OperationalTaskFields.FINISH_DATE), WRONG_DATES_ORDER_MESSAGE);
+            operationalTask.addError(operationalTaskDD.getField(OperationalTaskFields.START_DATE), L_WRONG_DATES_ORDER_MESSAGE);
+            operationalTask.addError(operationalTaskDD.getField(OperationalTaskFields.FINISH_DATE), L_WRONG_DATES_ORDER_MESSAGE);
 
             return false;
         }
@@ -165,102 +108,203 @@ public class OperationalTaskValidators {
         return true;
     }
 
-    private boolean datesAreCorrect(DataDefinition operationalTaskDD, Entity operationalTask) {
+    private boolean datesAreCorrect(final DataDefinition operationalTaskDD, final Entity operationalTask) {
         Entity technologyOperationComponent = operationalTask
                 .getBelongsToField(OperationalTaskFields.TECHNOLOGY_OPERATION_COMPONENT);
-        if (technologyOperationComponent != null
-                && operationalTask.getBelongsToField(OperationalTaskFields.WORKSTATION) != null) {
+
+        if (Objects.nonNull(technologyOperationComponent)
+                && Objects.nonNull(operationalTask.getBelongsToField(OperationalTaskFields.WORKSTATION))) {
             Date startDate = operationalTask.getDateField(OperationalTaskFields.START_DATE);
             Date finishDate = operationalTask.getDateField(OperationalTaskFields.FINISH_DATE);
             Entity order = operationalTask.getBelongsToField(OperationalTaskFields.ORDER);
-            if (technologyOperationComponent.getBelongsToField(TechnologyOperationComponentFields.PARENT) != null) {
+
+            if (Objects.nonNull(technologyOperationComponent.getBelongsToField(TechnologyOperationComponentFields.PARENT))) {
                 Entity parent = operationalTasksService.getParent(technologyOperationComponent, order);
-                if (parent != null && parent.getBelongsToField(OperationalTaskFields.WORKSTATION) != null) {
+
+                if (Objects.nonNull(parent) && Objects.nonNull(parent.getBelongsToField(OperationalTaskFields.WORKSTATION))) {
                     if (parent.getDateField(OperationalTaskFields.START_DATE).before(startDate)) {
                         operationalTask.addError(operationalTaskDD.getField(OperationalTaskFields.START_DATE),
                                 "orders.operationalTask.error.inappropriateStartDateNext");
+
                         return false;
                     }
+
                     if (parent.getDateField(OperationalTaskFields.FINISH_DATE).before(finishDate)) {
                         operationalTask.addError(operationalTaskDD.getField(OperationalTaskFields.FINISH_DATE),
                                 "orders.operationalTask.error.inappropriateFinishDateNext");
+
                         return false;
                     }
                 }
             }
+
             List<Entity> children = operationalTasksService.getChildren(technologyOperationComponent, order);
+
             for (Entity child : children) {
-                if (child.getBelongsToField(OperationalTaskFields.WORKSTATION) != null) {
+                if (Objects.nonNull(child.getBelongsToField(OperationalTaskFields.WORKSTATION))) {
                     if (child.getDateField(OperationalTaskFields.START_DATE).after(startDate)) {
                         operationalTask.addError(operationalTaskDD.getField(OperationalTaskFields.START_DATE),
                                 "orders.operationalTask.error.inappropriateStartDatePrevious");
+
                         return false;
                     }
+
                     if (child.getDateField(OperationalTaskFields.FINISH_DATE).after(finishDate)) {
                         operationalTask.addError(operationalTaskDD.getField(OperationalTaskFields.FINISH_DATE),
                                 "orders.operationalTask.error.inappropriateFinishDatePrevious");
+
                         return false;
                     }
                 }
             }
         }
+
         return true;
     }
 
     private boolean checkIfOrderHasTechnology(final DataDefinition operationalTaskDD, final Entity operationalTask) {
         Entity order = operationalTask.getBelongsToField(OperationalTaskFields.ORDER);
 
-        if (order == null) {
+        if (Objects.isNull(order)) {
             return true;
         }
 
         Entity technology = order.getBelongsToField(OrderFields.TECHNOLOGY);
 
-        if (technology == null) {
+        if (Objects.isNull(technology)) {
             operationalTask.addError(operationalTaskDD.getField(OperationalTaskFields.ORDER),
                     "orders.operationalTask.order.error.technologyIsNull");
+
             return false;
         }
 
         return true;
     }
 
-    private boolean checkIfFieldSet(DataDefinition operationalTaskDD, Entity operationalTask) {
+    private boolean checkIfFieldSet(final DataDefinition operationalTaskDD, final Entity operationalTask) {
         String type = operationalTask.getStringField(OperationalTaskFields.TYPE);
 
         if (operationalTasksService.isOperationalTaskTypeExecutionOperationInOrder(type)) {
-            boolean valid = true;
+            boolean isValid = true;
+
             Entity order = operationalTask.getBelongsToField(OperationalTaskFields.ORDER);
+
             if (Objects.isNull(order)) {
                 operationalTask.addError(operationalTaskDD.getField(OperationalTaskFields.ORDER),
                         "qcadooView.validate.field.error.missing");
-                valid = false;
+
+                isValid = false;
             }
 
-            Entity toc = operationalTask.getBelongsToField(OperationalTaskFields.TECHNOLOGY_OPERATION_COMPONENT);
-            if (Objects.isNull(toc)) {
+            Entity technologyOperationComponent = operationalTask.getBelongsToField(OperationalTaskFields.TECHNOLOGY_OPERATION_COMPONENT);
+
+            if (Objects.isNull(technologyOperationComponent)) {
                 operationalTask.addError(operationalTaskDD.getField(OperationalTaskFields.TECHNOLOGY_OPERATION_COMPONENT),
                         "qcadooView.validate.field.error.missing");
-                valid = false;
+
+                isValid = false;
             }
-            return valid;
+
+            return isValid;
         }
+
         return true;
     }
 
-    private boolean checkWorkstationIsCorrect(DataDefinition operationalTaskDD, Entity operationalTask) {
+    private boolean checkIfAlreadyExists(final DataDefinition operationalTaskDD, final Entity operationalTask) {
+        String type = operationalTask.getStringField(OperationalTaskFields.TYPE);
+
+        if (OperationalTaskType.EXECUTION_OPERATION_IN_ORDER.getStringValue().equalsIgnoreCase(type)) {
+            Entity order = operationalTask.getBelongsToField(OperationalTaskFields.ORDER);
+            Entity technologyOperationComponent = operationalTask.getBelongsToField(OperationalTaskFields.TECHNOLOGY_OPERATION_COMPONENT);
+
+            SearchCriteriaBuilder searchCriteriaBuilder = operationalTaskDD.find()
+                    .add(SearchRestrictions.belongsTo(OperationalTaskFields.ORDER, order))
+                    .add(SearchRestrictions.belongsTo(OperationalTaskFields.TECHNOLOGY_OPERATION_COMPONENT, technologyOperationComponent))
+                    .add(SearchRestrictions.in(OperationalTaskFields.STATE,
+                            Lists.newArrayList(OperationalTaskStateStringValues.PENDING, OperationalTaskStateStringValues.STARTED,
+                                    OperationalTaskStateStringValues.FINISHED)));
+
+            if (Objects.nonNull(operationalTask.getId())) {
+                searchCriteriaBuilder.add(SearchRestrictions.idNe(operationalTask.getId()));
+            }
+
+            searchCriteriaBuilder.setProjection(alias(rowCount(), L_COUNT));
+            searchCriteriaBuilder.addOrder(asc(L_COUNT));
+
+            Entity countProjection = searchCriteriaBuilder.setMaxResults(1).uniqueResult();
+
+            boolean isValid = (Objects.isNull(countProjection) || ((Long) countProjection.getField(L_COUNT) == 0));
+
+            if (!isValid) {
+                operationalTask.addGlobalError("orders.operationalTask.error.notUnique");
+            }
+
+            return isValid;
+        }
+
+        return true;
+    }
+
+    private boolean checkWorkstationIsCorrect(final DataDefinition operationalTaskDD, final Entity operationalTask) {
         Entity technologyOperationComponent = operationalTask
                 .getBelongsToField(OperationalTaskFields.TECHNOLOGY_OPERATION_COMPONENT);
         Entity workstation = operationalTask.getBelongsToField(OperationalTaskFields.WORKSTATION);
-        if (technologyOperationComponent != null && workstation != null) {
+
+        if (Objects.nonNull(technologyOperationComponent) && Objects.nonNull(workstation)) {
             List<Entity> workstations = technologyOperationComponent
                     .getManyToManyField(TechnologyOperationComponentFields.WORKSTATIONS);
-            if (!workstations.isEmpty() && workstations.stream().noneMatch(w -> w.getId().equals(workstation.getId()))) {
+
+            if (!workstations.isEmpty() && checkWorkstation(workstations, workstation.getId())) {
+                String workstationNumbers = getWorkstationsNumbers(workstations);
+
                 operationalTask.addError(operationalTaskDD.getField(OperationalTaskFields.WORKSTATION),
-                        "orders.error.inappropriateWorkstationForOperation");
+                        "orders.error.inappropriateWorkstationForOperation", workstationNumbers);
+
                 return false;
             }
         }
+
         return true;
     }
+
+    private boolean checkWorkstation(final List<Entity> workstations, final Long workstationId) {
+        return workstations.stream().noneMatch(workstation -> workstation.getId().equals(workstationId));
+    }
+
+    private String getWorkstationsNumbers(final List<Entity> workstations) {
+        return workstations.stream().map(workstation -> workstation.getStringField(WorkstationFields.NUMBER))
+                .collect(Collectors.joining(", "));
+    }
+
+    private boolean checkStaff(final DataDefinition operationalTaskDD, final Entity operationalTask) {
+        Entity technologyOperationComponent = operationalTask
+                .getBelongsToField(OperationalTaskFields.TECHNOLOGY_OPERATION_COMPONENT);
+        Integer actualStaff = operationalTask.getIntegerField(OperationalTaskFields.ACTUAL_STAFF);
+
+        int minStaff;
+
+        if (!Objects.isNull(technologyOperationComponent)) {
+            minStaff = technologyOperationComponent.getIntegerField(TechnologyOperationComponentFieldsTNFO.MIN_STAFF);
+        } else {
+            minStaff = 1;
+        }
+
+        if (Objects.nonNull(actualStaff) && minStaff > actualStaff) {
+            operationalTask.addError(operationalTaskDD.getField(OperationalTaskFields.ACTUAL_STAFF),
+                    "orders.operationalTask.error.minStaffBiggerThanActualStaff");
+
+            return false;
+        }
+
+        if (Objects.nonNull(actualStaff) && actualStaff % minStaff != 0) {
+            operationalTask.addError(operationalTaskDD.getField(OperationalTaskFields.ACTUAL_STAFF),
+                    "orders.operationalTask.error.actualStaffMustBeMultipleMinStaff", String.valueOf(minStaff));
+
+            return false;
+        }
+
+        return true;
+    }
+
 }
