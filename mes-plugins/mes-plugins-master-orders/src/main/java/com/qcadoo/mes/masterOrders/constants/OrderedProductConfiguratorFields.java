fb701d1cb53ddca534209454a61f5892523fec3f
Łukasz Poniewierski <lukasz.poniewierski@qcadoo.com>
2022-10-17 14:21:33 +0200

    QCADOOCLS-8653

---
+++ mes-plugins/mes-plugins-master-orders/src/main/java/com/qcadoo/mes/masterOrders/constants/OrderedProductConfiguratorFields.java
@@ -0,0 +1,17 @@
+package com.qcadoo.mes.masterOrders.constants;
+
+public final class OrderedProductConfiguratorFields {
+
+    private OrderedProductConfiguratorFields() {
+
+    }
+
+    public static final String NUMBER = "number";
+
+    public static final String NAME = "name";
+
+    public static final String ORDERED_PRODUCT_CONFIGURATOR_ATTRIBUTES = "orderedProductConfiguratorAttributes";
+
+    public static final String PRODUCTS = "products";
+
+}
