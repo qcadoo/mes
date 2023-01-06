41e37f185859a3df8228622bfa1a922ec80f4b7b
ksiwonia <kamil.siwonia@gmail.com>
2023-01-06 07:55:54 +0100

    QCADOOCLS-9063

--- mes-plugins/mes-plugins-production-counting/src/main/java/com/qcadoo/mes/productionCounting/hooks/TrackingOperationProductInComponentHooks.java
+++ mes-plugins/mes-plugins-production-counting/src/main/java/com/qcadoo/mes/productionCounting/hooks/TrackingOperationProductInComponentHooks.java
@@ -1,19 +1,25 @@
 package com.qcadoo.mes.productionCounting.hooks;
 
+import com.qcadoo.mes.basic.ParameterService;
 import com.qcadoo.mes.basic.constants.BasicConstants;
 import com.qcadoo.mes.basicProductionCounting.constants.BasicProductionCountingConstants;
 import com.qcadoo.mes.basicProductionCounting.constants.ProductionCountingQuantityFields;
 import com.qcadoo.mes.basicProductionCounting.constants.ProductionCountingQuantityRole;
 import com.qcadoo.mes.basicProductionCounting.constants.ProductionCountingQuantityTypeOfMaterial;
+import com.qcadoo.mes.operationTimeCalculations.constants.PlanOrderTimeCalculationFields;
+import com.qcadoo.mes.productionCounting.constants.ProductionCountingConstants;
 import com.qcadoo.mes.productionCounting.constants.ProductionTrackingFields;
 import com.qcadoo.mes.productionCounting.constants.TrackingOperationProductInComponentFields;
+import com.qcadoo.mes.productionCounting.constants.UsedBatchFields;
 import com.qcadoo.model.api.DataDefinition;
 import com.qcadoo.model.api.DataDefinitionService;
 import com.qcadoo.model.api.Entity;
+import com.qcadoo.model.api.EntityList;
 import com.qcadoo.model.api.search.SearchCriteriaBuilder;
 import com.qcadoo.model.api.search.SearchRestrictions;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,16 +29,62 @@ import org.springframework.stereotype.Service;
 public class TrackingOperationProductInComponentHooks {
 
     private static final String PRODUCT = "product";
+    public static final String L_AUTOMATICALLY_USE_THE_BATCH_PLANNED_IN_ORDER = "automaticallyUseTheBatchPlannedInOrder";
 
     @Autowired
     private DataDefinitionService dataDefinitionService;
 
+    @Autowired
+    private ParameterService parameterService;
+
     public void onSave(final DataDefinition trackingOperationProductOutComponentDD,
-            final Entity trackingOperationProductInComponent) {
+                       final Entity trackingOperationProductInComponent) {
 
         if (Objects.isNull(trackingOperationProductInComponent.getId())) {
             createProductionCountingIfReplacement(trackingOperationProductInComponent);
         }
+
+        if (Objects.nonNull(trackingOperationProductInComponent.getId()) && parameterService.getParameter().getBooleanField(L_AUTOMATICALLY_USE_THE_BATCH_PLANNED_IN_ORDER)) {
+            Entity trackingOperationProductInComponentDb = trackingOperationProductInComponent.getDataDefinition().get(trackingOperationProductInComponent.getId());
+
+            if (Objects.nonNull(trackingOperationProductInComponent.getDecimalField(TrackingOperationProductInComponentFields.USED_QUANTITY))
+                    && Objects.isNull(trackingOperationProductInComponentDb.getDecimalField(TrackingOperationProductInComponentFields.USED_QUANTITY))
+                    && trackingOperationProductInComponent.getHasManyField(TrackingOperationProductInComponentFields.USED_BATCHES).isEmpty()) {
+
+                Entity productionTracking = trackingOperationProductInComponent.getBelongsToField(TrackingOperationProductInComponentFields.PRODUCTION_TRACKING);
+                Entity order = productionTracking.getBelongsToField(ProductionTrackingFields.ORDER);
+                Entity toc = productionTracking.getBelongsToField(ProductionTrackingFields.TECHNOLOGY_OPERATION_COMPONENT);
+
+                SearchCriteriaBuilder scb = getProductionCountingQuantityDD()
+                        .find()
+                        .add(SearchRestrictions.belongsTo(ProductionCountingQuantityFields.ORDER, order))
+                        .add(SearchRestrictions.belongsTo(ProductionCountingQuantityFields.PRODUCT, trackingOperationProductInComponent.getBelongsToField(TrackingOperationProductInComponentFields.PRODUCT)))
+                        .add(SearchRestrictions.eq(ProductionCountingQuantityFields.ROLE,
+                                ProductionCountingQuantityRole.USED.getStringValue()))
+                        .add(SearchRestrictions.eq(ProductionCountingQuantityFields.TYPE_OF_MATERIAL, "01component"));
+
+                if (Objects.nonNull(toc)) {
+                    scb.add(SearchRestrictions.belongsTo(ProductionCountingQuantityFields.TECHNOLOGY_OPERATION_COMPONENT, toc));
+                }
+
+                Entity pcq = scb.setMaxResults(1).uniqueResult();
+
+                if(Objects.nonNull(pcq.getHasManyField(ProductionCountingQuantityFields.BATCHES))) {
+                    List<Entity> batches = pcq.getHasManyField(ProductionCountingQuantityFields.BATCHES);
+
+                    if (batches.size() == 1) {
+                        Entity usedBatch = dataDefinitionService.get(ProductionCountingConstants.PLUGIN_IDENTIFIER, ProductionCountingConstants.MODEL_USED_BATCH).create();
+                        usedBatch.setField(UsedBatchFields.QUANTITY, trackingOperationProductInComponent.getDecimalField(TrackingOperationProductInComponentFields.USED_QUANTITY));
+                        usedBatch.setField(UsedBatchFields.BATCH, batches.get(0).getId());
+                        usedBatch.setField(UsedBatchFields.TRACKING_OPERATION_PRODUCT_IN_COMPONENT,
+                                trackingOperationProductInComponent.getId());
+                        usedBatch = usedBatch.getDataDefinition().fastSave(usedBatch);
+                    }
+                }
+            }
+        }
+
+
     }
 
     private void createProductionCountingIfReplacement(final Entity trackingOperationProductInComponent) {
@@ -108,4 +160,11 @@ public class TrackingOperationProductInComponentHooks {
         }
         productionCountingQuantity = productionCountingQuantity.getDataDefinition().save(productionCountingQuantity);
     }
+
+
+    private DataDefinition getProductionCountingQuantityDD() {
+        return dataDefinitionService.get(BasicProductionCountingConstants.PLUGIN_IDENTIFIER,
+                BasicProductionCountingConstants.MODEL_PRODUCTION_COUNTING_QUANTITY);
+    }
+
 }
