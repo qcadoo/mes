6d03bf022e277a054a52f29ef27313c7be1140d0
Krzysztof Nadolski <krzysztof.nadolski@qcadoo.com>
2023-01-26 16:26:56 +0100

    QCADOOCLS-8581

--- mes-plugins/mes-plugins-cost-calculation/src/main/java/com/qcadoo/mes/costCalculation/print/OperationsCostCalculationService.java
+++ mes-plugins/mes-plugins-cost-calculation/src/main/java/com/qcadoo/mes/costCalculation/print/OperationsCostCalculationService.java
@@ -3,19 +3,19 @@
  * Copyright (c) 2010 Qcadoo Limited
  * Project: Qcadoo MES
  * Version: 1.4
- *
+ * <p>
  * This file is part of Qcadoo.
- *
+ * <p>
  * Qcadoo is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
  * by the Free Software Foundation; either version 3 of the License,
  * or (at your option) any later version.
- *
+ * <p>
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Affero General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU Affero General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
@@ -24,6 +24,7 @@
 package com.qcadoo.mes.costCalculation.print;
 
 import com.qcadoo.model.api.Entity;
+
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -32,11 +33,13 @@ import java.math.BigDecimal;
 public interface OperationsCostCalculationService {
 
     /**
-     * 
+     *
      * @param costCalculation
      *            cost calculation
      * @param technology
      *            technology
      */
     BigDecimal calculateOperationsCost(final Entity costCalculation, final Entity technology);
+
+    BigDecimal getCurrentRate(Entity pieceRate);
 }
