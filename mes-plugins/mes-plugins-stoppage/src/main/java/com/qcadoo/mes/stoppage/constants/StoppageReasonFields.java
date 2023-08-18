8e018289f8a4de1825875d63e23f8fa0a0cfc545
Kamil Siwonia <kamil.siwonia@gmail.com>
2023-08-18 11:54:14 +0200

    QCADOOCLS-7077

---
+++ mes-plugins/mes-plugins-stoppage/src/main/java/com/qcadoo/mes/stoppage/constants/StoppageReasonFields.java
@@ -0,0 +1,14 @@
+package com.qcadoo.mes.stoppage.constants;
+
+public final class StoppageReasonFields {
+
+    private StoppageReasonFields() {
+    }
+
+    public static final String NAME = "name";
+
+    public static final String DESCRIPTION = "description";
+
+    public static final String CREATE_FAILURE = "createFailure";
+
+}
