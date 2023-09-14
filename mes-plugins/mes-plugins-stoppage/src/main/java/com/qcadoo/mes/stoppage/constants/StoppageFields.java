5d99d984c4097e13e27e119dc7cee1a0bf361a10
Łukasz Poniewierski <lukasz.poniewierski@qcadoo.com>
2023-09-14 19:50:20 +0200

    QCADOOCLS-7122

--- mes-plugins/mes-plugins-stoppage/src/main/java/com/qcadoo/mes/stoppage/constants/StoppageFields.java
+++ mes-plugins/mes-plugins-stoppage/src/main/java/com/qcadoo/mes/stoppage/constants/StoppageFields.java
@@ -5,17 +5,18 @@ public final class StoppageFields {
     private StoppageFields() {
     }
 
-    public static final String DATE_FROM = "dateFrom";
-
-    public static final String DATE_TO = "dateTo";
-
     public static final String ORDER = "order";
 
     public static final String PRODUCTION_TRACKING = "productionTracking";
 
-    public static final String DURATION = "duration";
-
-    public static final String STOPPAGE_REASON = "reason";
+    public static final String REASON = "reason";
 
     public static final String DESCRIPTION = "description";
+
+    public static final String DATE_FROM = "dateFrom";
+
+    public static final String DATE_TO = "dateTo";
+
+    public static final String DURATION = "duration";
+
 }
