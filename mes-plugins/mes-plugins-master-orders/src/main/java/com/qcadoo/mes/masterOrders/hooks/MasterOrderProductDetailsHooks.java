4c1fb9818ce633c4ac7b893ff98ccca82e753c95
Łukasz Poniewierski <lukasz.poniewierski@qcadoo.com>
2024-02-23 01:51:55 +0100

    QCADOOCLS-10284

--- mes-plugins/mes-plugins-master-orders/src/main/java/com/qcadoo/mes/masterOrders/hooks/MasterOrderProductDetailsHooks.java
+++ mes-plugins/mes-plugins-master-orders/src/main/java/com/qcadoo/mes/masterOrders/hooks/MasterOrderProductDetailsHooks.java
@@ -23,31 +23,26 @@
  */
 package com.qcadoo.mes.masterOrders.hooks;
 
-import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.Objects;
-
-import com.qcadoo.mes.masterOrders.constants.*;
-import com.qcadoo.view.api.components.GridComponent;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.qcadoo.mes.basic.constants.ProductFields;
+import com.qcadoo.mes.masterOrders.constants.*;
 import com.qcadoo.mes.orders.TechnologyServiceO;
 import com.qcadoo.mes.orders.criteriaModifiers.TechnologyCriteriaModifiersO;
-import com.qcadoo.model.api.BigDecimalUtils;
-import com.qcadoo.model.api.DataDefinitionService;
-import com.qcadoo.model.api.Entity;
-import com.qcadoo.model.api.ExpressionService;
-import com.qcadoo.model.api.NumberService;
+import com.qcadoo.model.api.*;
 import com.qcadoo.plugin.api.PluginUtils;
 import com.qcadoo.view.api.ComponentState.MessageType;
 import com.qcadoo.view.api.ViewDefinitionState;
 import com.qcadoo.view.api.components.FieldComponent;
 import com.qcadoo.view.api.components.FormComponent;
+import com.qcadoo.view.api.components.GridComponent;
 import com.qcadoo.view.api.components.LookupComponent;
 import com.qcadoo.view.api.components.lookup.FilterValueHolder;
 import com.qcadoo.view.constants.QcadooViewConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Objects;
 
 @Service
 public class MasterOrderProductDetailsHooks {
@@ -72,88 +67,45 @@ public class MasterOrderProductDetailsHooks {
         disableAttrValuesGrid(view);
     }
 
-    private void disableAttrValuesGrid(final ViewDefinitionState view) {
-        FormComponent form = (FormComponent) view.getComponentByReference(QcadooViewConstants.L_FORM);
-        if(Objects.nonNull(form.getEntityId())) {
-            Entity productComponent = form.getEntity();
-            Entity mo = productComponent.getBelongsToField(MasterOrderProductFields.MASTER_ORDER);
-            String state = mo.getStringField(MasterOrderFields.STATE);
-            if(MasterOrderState.DECLINED.getStringValue().equals(state) || MasterOrderState.COMPLETED.getStringValue().equals(state)) {
-                GridComponent attrValuesGrid = (GridComponent) view.getComponentByReference("masterOrderProductAttrValues");
-                attrValuesGrid.setEditable(false);
-            }
-        }
-
-    }
-
     public void fillUnitField(final ViewDefinitionState view) {
         LookupComponent productField = (LookupComponent) view.getComponentByReference(MasterOrderProductFields.PRODUCT);
+
         Entity product = productField.getEntity();
         String unit = null;
 
-        if (product != null) {
+        if (Objects.nonNull(product)) {
             unit = product.getStringField(ProductFields.UNIT);
-
         }
+
         for (String reference : Arrays.asList("cumulatedOrderQuantityUnit", "masterOrderQuantityUnit",
                 "producedOrderQuantityUnit", "leftToReleaseUnit", "quantityRemainingToOrderUnit",
                 "quantityTakenFromWarehouseUnit")) {
-            FieldComponent field = (FieldComponent) view.getComponentByReference(reference);
-            field.setFieldValue(unit);
-            field.requestComponentUpdateState();
-        }
-
-    }
-
-    private void fillQuantities(final ViewDefinitionState view) {
-        FormComponent form = (FormComponent) view.getComponentByReference(QcadooViewConstants.L_FORM);
-        Entity productComponent = form.getEntity();
-        if (productComponent.getId() != null) {
-            Entity masterOrderProductDto = dataDefinitionService.get(MasterOrdersConstants.PLUGIN_IDENTIFIER,
-                    MasterOrdersConstants.MODEL_MASTER_ORDER_POSITION_DTO).get(productComponent.getId());
-            FieldComponent cumulatedOrderQuantity = (FieldComponent) view
-                    .getComponentByReference(MasterOrderProductFields.CUMULATED_ORDER_QUANTITY);
-            cumulatedOrderQuantity.setFieldValue(numberService.format(masterOrderProductDto
-                    .getDecimalField(MasterOrderPositionDtoFields.CUMULATED_MASTER_ORDER_QUANTITY)));
-            cumulatedOrderQuantity.requestComponentUpdateState();
-
-            FieldComponent leftToRelease = (FieldComponent) view.getComponentByReference(MasterOrderProductFields.LEFT_TO_RELASE);
-            leftToRelease.setFieldValue(numberService.format(masterOrderProductDto
-                    .getDecimalField(MasterOrderPositionDtoFields.LEFT_TO_RELEASE)));
-            leftToRelease.requestComponentUpdateState();
-
-            FieldComponent producedOrderQuantity = (FieldComponent) view
-                    .getComponentByReference(MasterOrderProductFields.PRODUCED_ORDER_QUANTITY);
-            producedOrderQuantity.setFieldValue(numberService.format(masterOrderProductDto
-                    .getDecimalField(MasterOrderPositionDtoFields.PRODUCED_ORDER_QUANTITY)));
-            producedOrderQuantity.requestComponentUpdateState();
-
-            FieldComponent quantityRemainingToOrder = (FieldComponent) view
-                    .getComponentByReference(MasterOrderProductFields.QUANTITY_REMAINING_TO_ORDER);
-            quantityRemainingToOrder.setFieldValue(numberService.format(masterOrderProductDto
-                    .getDecimalField(MasterOrderPositionDtoFields.QUANTITY_REMAINING_TO_ORDER)));
-            quantityRemainingToOrder.requestComponentUpdateState();
+            FieldComponent fieldComponent = (FieldComponent) view.getComponentByReference(reference);
+            fieldComponent.setFieldValue(unit);
+            fieldComponent.requestComponentUpdateState();
         }
     }
 
     private void fillDefaultTechnology(final ViewDefinitionState view) {
         if (PluginUtils.isEnabled("goodFood")) {
-            FieldComponent technology = (FieldComponent) view.getComponentByReference("technology");
-            technology.setRequired(true);
-            technology.requestComponentUpdateState();
+            FieldComponent technologyField = (FieldComponent) view.getComponentByReference(MasterOrderProductFields.TECHNOLOGY);
+
+            technologyField.setRequired(true);
+            technologyField.requestComponentUpdateState();
         }
-        LookupComponent productField = (LookupComponent) view.getComponentByReference("product");
-        FieldComponent defaultTechnologyField = (FieldComponent) view.getComponentByReference("defaultTechnology");
-        LookupComponent technologyLookup = (LookupComponent) view.getComponentByReference("technology");
+
+        LookupComponent productField = (LookupComponent) view.getComponentByReference(MasterOrderProductFields.PRODUCT);
+        FieldComponent defaultTechnologyField = (FieldComponent) view.getComponentByReference(MasterOrderProductFields.DEFAULT_TECHNOLOGY);
+        LookupComponent technologyLookup = (LookupComponent) view.getComponentByReference(MasterOrderProductFields.TECHNOLOGY);
 
         Entity product = productField.getEntity();
 
         if (Objects.nonNull(product)) {
-            FilterValueHolder holder = technologyLookup.getFilterValue();
+            FilterValueHolder filterValueHolder = technologyLookup.getFilterValue();
 
-            holder.put(TechnologyCriteriaModifiersO.PRODUCT_PARAMETER, product.getId());
+            filterValueHolder.put(TechnologyCriteriaModifiersO.PRODUCT_PARAMETER, product.getId());
 
-            technologyLookup.setFilterValue(holder);
+            technologyLookup.setFilterValue(filterValueHolder);
 
             Entity defaultTechnology = technologyServiceO.getDefaultTechnology(product);
 
@@ -162,7 +114,8 @@ public class MasterOrderProductDetailsHooks {
                         view.getLocale());
 
                 defaultTechnologyField.setFieldValue(defaultTechnologyValue);
-                if (technologyLookup.getFieldValue() == null && (technologyLookup.getCurrentCode() == null
+
+                if (Objects.isNull(technologyLookup.getFieldValue()) && (Objects.isNull(technologyLookup.getCurrentCode())
                         || technologyLookup.getCurrentCode().isEmpty())) {
                     technologyLookup.setFieldValue(defaultTechnology.getId());
                 }
@@ -173,22 +126,79 @@ public class MasterOrderProductDetailsHooks {
     private void showErrorWhenCumulatedQuantity(final ViewDefinitionState view) {
         if (view.isViewAfterRedirect()) {
             FormComponent masterOrderProductForm = (FormComponent) view.getComponentByReference(QcadooViewConstants.L_FORM);
+            
             Entity masterOrderProduct = masterOrderProductForm.getPersistedEntityWithIncludedFormValues();
 
-            if ((masterOrderProduct == null) || !masterOrderProduct.isValid()) {
+            if (Objects.isNull(masterOrderProduct) || !masterOrderProduct.isValid()) {
                 return;
             }
 
             if (Objects.nonNull(masterOrderProduct.getId())) {
-                Entity masterOrderProductDto = dataDefinitionService.get(MasterOrdersConstants.PLUGIN_IDENTIFIER,
-                        MasterOrdersConstants.MODEL_MASTER_ORDER_POSITION_DTO).get(masterOrderProduct.getId());
-                if (BigDecimal.ZERO.compareTo(BigDecimalUtils.convertNullToZero(masterOrderProductDto
+                Entity masterOrderPositionDto = getMasterOrderPositionDtoDD().get(masterOrderProduct.getId());
+
+                if (BigDecimal.ZERO.compareTo(BigDecimalUtils.convertNullToZero(masterOrderPositionDto
                         .getDecimalField(MasterOrderPositionDtoFields.QUANTITY_REMAINING_TO_ORDER))) < 0) {
                     masterOrderProductForm.addMessage("masterOrders.masterOrder.masterOrderCumulatedQuantityField.wrongQuantity",
                             MessageType.INFO, false);
                 }
             }
         }
+    }
+
+    private void fillQuantities(final ViewDefinitionState view) {
+        FormComponent masterOrderProductForm = (FormComponent) view.getComponentByReference(QcadooViewConstants.L_FORM);
+
+        Long masterOrderProductFormId = masterOrderProductForm.getEntityId();
+
+        if (Objects.nonNull(masterOrderProductFormId)) {
+            Entity masterOrderPositionDto = getMasterOrderPositionDtoDD().get(masterOrderProductFormId);
+
+            FieldComponent cumulatedOrderQuantity = (FieldComponent) view
+                    .getComponentByReference(MasterOrderProductFields.CUMULATED_ORDER_QUANTITY);
+            cumulatedOrderQuantity.setFieldValue(numberService.format(masterOrderPositionDto
+                    .getDecimalField(MasterOrderPositionDtoFields.CUMULATED_MASTER_ORDER_QUANTITY)));
+            cumulatedOrderQuantity.requestComponentUpdateState();
+
+            FieldComponent leftToRelease = (FieldComponent) view.getComponentByReference(MasterOrderProductFields.LEFT_TO_RELASE);
+            leftToRelease.setFieldValue(numberService.format(masterOrderPositionDto
+                    .getDecimalField(MasterOrderPositionDtoFields.LEFT_TO_RELEASE)));
+            leftToRelease.requestComponentUpdateState();
+
+            FieldComponent producedOrderQuantity = (FieldComponent) view
+                    .getComponentByReference(MasterOrderProductFields.PRODUCED_ORDER_QUANTITY);
+            producedOrderQuantity.setFieldValue(numberService.format(masterOrderPositionDto
+                    .getDecimalField(MasterOrderPositionDtoFields.PRODUCED_ORDER_QUANTITY)));
+            producedOrderQuantity.requestComponentUpdateState();
+
+            FieldComponent quantityRemainingToOrder = (FieldComponent) view
+                    .getComponentByReference(MasterOrderProductFields.QUANTITY_REMAINING_TO_ORDER);
+            quantityRemainingToOrder.setFieldValue(numberService.format(masterOrderPositionDto
+                    .getDecimalField(MasterOrderPositionDtoFields.QUANTITY_REMAINING_TO_ORDER)));
+            quantityRemainingToOrder.requestComponentUpdateState();
+        }
+    }
+
+    private void disableAttrValuesGrid(final ViewDefinitionState view) {
+        FormComponent masterOrderProductForm = (FormComponent) view.getComponentByReference(QcadooViewConstants.L_FORM);
+
+        Long masterOrderProductFormId = masterOrderProductForm.getEntityId();
+
+        if (Objects.nonNull(masterOrderProductFormId)) {
+            Entity masterOrderProduct = masterOrderProductForm.getPersistedEntityWithIncludedFormValues();
+
+            Entity masterOrder = masterOrderProduct.getBelongsToField(MasterOrderProductFields.MASTER_ORDER);
+            String state = masterOrder.getStringField(MasterOrderFields.STATE);
+
+            if (MasterOrderState.DECLINED.getStringValue().equals(state) || MasterOrderState.COMPLETED.getStringValue().equals(state)) {
+                GridComponent attrValuesGrid = (GridComponent) view.getComponentByReference(MasterOrderProductFields.MASTER_ORDER_PRODUCT_ATTR_VALUES);
+                attrValuesGrid.setEditable(false);
+            }
+        }
+    }
+
+    private DataDefinition getMasterOrderPositionDtoDD() {
+        return dataDefinitionService.get(MasterOrdersConstants.PLUGIN_IDENTIFIER,
+                MasterOrdersConstants.MODEL_MASTER_ORDER_POSITION_DTO);
     }
 
 }
