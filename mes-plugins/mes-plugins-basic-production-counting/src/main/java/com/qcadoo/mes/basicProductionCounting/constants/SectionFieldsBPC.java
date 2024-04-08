09510b8379d6004965e4e61a5fd1bf265dc0ca42
Krzysztof Nadolski <krzysztof.nadolski@qcadoo.com>
2024-04-08 17:32:40 +0200

    QCADOOCLS-10264

---
+++ mes-plugins/mes-plugins-basic-production-counting/src/main/java/com/qcadoo/mes/basicProductionCounting/constants/SectionFieldsBPC.java
@@ -0,0 +1,13 @@
+package com.qcadoo.mes.basicProductionCounting.constants;
+
+public final class SectionFieldsBPC {
+
+    private SectionFieldsBPC() {
+
+    }
+
+    public static final String PRODUCTION_COUNTING_QUANTITY = "productionCountingQuantity";
+
+    public static final String UNIT = "unit";
+
+}
