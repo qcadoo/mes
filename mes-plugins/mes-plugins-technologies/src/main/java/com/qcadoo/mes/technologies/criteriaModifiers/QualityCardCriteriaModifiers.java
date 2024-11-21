47f9f1ee424869ef0e542bd51743923776cf7407
Krzysztof Nadolski <krzysztof.nadolski@qcadoo.com>
2024-11-21 13:59:02 +0100

    QCADOOCLS-10670

--- mes-plugins/mes-plugins-technologies/src/main/java/com/qcadoo/mes/technologies/criteriaModifiers/QualityCardCriteriaModifiers.java
+++ mes-plugins/mes-plugins-technologies/src/main/java/com/qcadoo/mes/technologies/criteriaModifiers/QualityCardCriteriaModifiers.java
@@ -63,7 +63,7 @@ public class QualityCardCriteriaModifiers {
 
         StringBuilder query = new StringBuilder();
         query.append("SELECT qd.id ");
-        query.append("FROM technologies_qualitycard qd ");
+        query.append("FROM basic_qualitycard qd ");
         query.append("WHERE CAST(array(SELECT p.product_id FROM jointable_product_qualitycard p where p.qualitycard_id = qd.id) as bigint[]) @> CAST(ARRAY[ :products ] as bigint[]) ");
 
         return jdbcTemplate.queryForList(query.toString(), parameters, Long.class);
