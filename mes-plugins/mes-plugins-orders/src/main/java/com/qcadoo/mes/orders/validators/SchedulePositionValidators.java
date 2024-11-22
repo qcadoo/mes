0e715712b6ac1f3c8a63f40dac60227641079fe0
Krzysztof Nadolski <krzysztof.nadolski@qcadoo.com>
2024-11-22 12:59:31 +0100

    QCADOOCLS-10668

--- mes-plugins/mes-plugins-orders/src/main/java/com/qcadoo/mes/orders/validators/SchedulePositionValidators.java
+++ mes-plugins/mes-plugins-orders/src/main/java/com/qcadoo/mes/orders/validators/SchedulePositionValidators.java
@@ -88,7 +88,7 @@ public class SchedulePositionValidators {
                         .collect(Collectors.toList());
             }
         }
-        return workstations;
+        return workstations.stream().filter(e -> e.getBooleanField(WorkstationFields.ACTIVE)).collect(Collectors.toList());
     }
 
     private boolean validateDates(DataDefinition dataDefinition, Entity schedulePosition) {
