e77249b68d4c8738f2afb21bff82811bff222053
Krzysztof Nadolski <krzysztof.nadolski@qcadoo.com>
2026-01-29 19:25:02 +0100

    QCADOOCLS-10601

--- mes-plugins/mes-plugins-production-counting/src/main/java/com/qcadoo/mes/productionCounting/listeners/ProductionBalanceDetailsListeners.java
+++ mes-plugins/mes-plugins-production-counting/src/main/java/com/qcadoo/mes/productionCounting/listeners/ProductionBalanceDetailsListeners.java
@@ -3,19 +3,19 @@
  * Copyright (c) 2010 Qcadoo Limited
  * Project: Qcadoo MES
  * Version: 1.4
- *
+ * <p>
  * This file is part of Qcadoo.
- *
+ * <p>
  * Qcadoo is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
  * by the Free Software Foundation; either version 3 of the License,
  * or (at your option) any later version.
- *
+ * <p>
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Affero General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU Affero General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
@@ -23,25 +23,14 @@
  */
 package com.qcadoo.mes.productionCounting.listeners;
 
-import java.io.IOException;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
-
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.google.common.collect.Lists;
-import com.qcadoo.mes.orders.constants.OrdersConstants;
+import com.qcadoo.mes.basic.constants.WorkstationFields;
 import com.qcadoo.mes.productionCounting.ProductionCountingService;
 import com.qcadoo.mes.productionCounting.constants.ProductionBalanceFields;
 import com.qcadoo.mes.productionCounting.constants.ProductionCountingConstants;
 import com.qcadoo.mes.productionCounting.xls.ProductionBalanceXlsService;
+import com.qcadoo.mes.productionLines.constants.WorkstationFieldsPL;
 import com.qcadoo.model.api.Entity;
 import com.qcadoo.model.api.file.FileService;
-import com.qcadoo.model.api.search.SearchRestrictions;
 import com.qcadoo.report.api.ReportService;
 import com.qcadoo.security.api.SecurityService;
 import com.qcadoo.view.api.ComponentState;
@@ -50,6 +39,16 @@ import com.qcadoo.view.api.ViewDefinitionState;
 import com.qcadoo.view.api.components.FormComponent;
 import com.qcadoo.view.api.components.GridComponent;
 import com.qcadoo.view.constants.QcadooViewConstants;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductionBalanceDetailsListeners {
@@ -128,27 +127,15 @@ public class ProductionBalanceDetailsListeners {
     }
 
     public void printProductionBalance(final ViewDefinitionState view, final ComponentState state, final String[] args) {
-        reportService.printGeneratedReport(view, state, new String[] { args[0], ProductionCountingConstants.PLUGIN_IDENTIFIER,
-                ProductionCountingConstants.MODEL_PRODUCTION_BALANCE });
+        reportService.printGeneratedReport(view, state, new String[]{args[0], ProductionCountingConstants.PLUGIN_IDENTIFIER,
+                ProductionCountingConstants.MODEL_PRODUCTION_BALANCE});
     }
 
-    public final void addAllOrdersWithCriteria(final ViewDefinitionState view, final ComponentState state, final String[] args) {
-        GridComponent ordersGrid = (GridComponent) view.getComponentByReference(ProductionBalanceFields.ORDERS);
+    public void onRemoveSelectedEntity(final ViewDefinitionState view, final ComponentState state, final String[] args) {
+        GridComponent grid = (GridComponent) view.getComponentByReference(ProductionBalanceFields.ORDERS);
         FormComponent form = (FormComponent) view.getComponentByReference(QcadooViewConstants.L_FORM);
-        Entity balance = form.getPersistedEntityWithIncludedFormValues();
-        List<Entity> orders = Lists.newArrayList(balance.getHasManyField(ProductionBalanceFields.ORDERS));
-        for (Entity entity : ordersGrid.getSelectedEntities()) {
-            Entity root = entity.getBelongsToField("root");
-            if (root == null) {
-                root = entity;
-            }
-            orders.add(root);
-            List<Entity> children = entity.getDataDefinition().find().add(SearchRestrictions.belongsTo("root",
-                    OrdersConstants.PLUGIN_IDENTIFIER, OrdersConstants.MODEL_ORDER, root.getId())).list().getEntities();
-            orders.addAll(children);
-        }
-        balance.setField(ProductionBalanceFields.ORDERS, orders);
-        balance = balance.getDataDefinition().save(balance);
-        form.setEntity(balance);
+        Entity productionBalance = form.getPersistedEntityWithIncludedFormValues();
+        productionBalance.setField(ProductionBalanceFields.ORDERS, productionBalance.getManyToManyField(ProductionBalanceFields.ORDERS).stream().filter(e -> !grid.getSelectedEntitiesIds().contains(e.getId())).collect(Collectors.toList()));
+        productionBalance.getDataDefinition().save(productionBalance);
     }
 }
